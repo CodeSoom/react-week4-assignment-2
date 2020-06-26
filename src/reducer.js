d9ae8a897628b238f522a11728a3a66f675f9316
morykwon <kwonmory@gmail.com>
2020-06-27 00:56:46 +0900

    리듀서 작성하기

---
+++ src/reducer.js
@@ -0,0 +1,40 @@
+const initialState = {
+  input: {
+    name: '',
+    category: '',
+    address: '',
+  },
+  restaurants: []
+};
+
+export default function reducer(state = initialState, action) {
+
+  if (action.type === 'addRestaurant') {
+    const {name, category, address} = action.payload.input;
+
+    if (!name || !category || !address) {
+      return state;
+    }
+
+    return {
+      ...state,
+      restaurants: [...state.restaurants, {
+        name,
+        category,
+        address,
+      }],
+    }
+  }
+
+  if (action.type === 'updateInput') {
+    return {
+      ...state,
+      input: {
+        ...state.input,
+        [action.payload.key]: action.payload.value
+      }
+    }
+  }
+
+  return state;
+}
