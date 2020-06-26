d6792661ed162a1b764f23fdd58e565cdb752104
SeaFalcon <seafalcon323@gmail.com>
2020-06-27 07:50:33 +0900

    App 리팩토링

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,13 @@
 import React from 'react';
 import ListContainer from './Component/ListContainer';
+import InputContainer from './Component/InputContainer';
 
 export default function App() {
   return (
     <div>
-      {/* App Component */}
       <h1>Restaurants</h1>
-      {/* List Component */}
       <ListContainer />
-      {/* Input Component */}
-      <p>
-        <input type="text" placeholder="이름" />
-        <input type="text" placeholder="분류" />
-        <input type="text" placeholder="주소" />
-        <button type="button">등록</button>
-      </p>
+      <InputContainer />
     </div>
   );
 }
