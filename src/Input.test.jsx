bb86754b60c8ed9e1a1478b769c635205e133d2b
KiChang Shin <kc7851@gmail.com>
2020-06-26 23:33:40 +0900

    Input test 작성

---
+++ src/Input.test.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import Input from './Input';
+
+describe('Input', () => {
+  context('without input values', () => {
+    it('값이 없는 3개의 input이 보인다.', () => {
+      const { container } = render(
+        <Input />,
+      );
+
+      const nameInput = container.querySelector('input[name = name]');
+      const categoryInput = container.querySelector('input[name = category]');
+      const addressInput = container.querySelector('input[name = address]');
+
+      expect(nameInput).not.toBeNull();
+      expect(categoryInput).not.toBeNull();
+      expect(addressInput).not.toBeNull();
+    });
+  });
+});
