b237773c60861b5f5def99a8a7c5fce5fae4e124
KiChang Shin <kc7851@gmail.com>
2020-06-26 23:52:50 +0900

    Input test 구현

--- src/Input.jsx
+++ src/Input.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-export default function Input() {
+export default function Input({ informations: { name, category, address } }) {
   return (
     <>
-      <input type="text" name="name" />
-      <input type="text" name="category" />
-      <input type="text" name="address" />
+      <input type="text" name="name" value={name} />
+      <input type="text" name="category" value={category} />
+      <input type="text" name="address" value={address} />
       <button type="button">등록</button>
     </>
   );
