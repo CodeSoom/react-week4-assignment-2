8b9cf5b649b92c34e164175d1d75953b7b94e377
morykwon <kwonmory@gmail.com>
2020-06-27 02:26:49 +0900

    InputContainer 테스트하기

---
+++ src/InputContainer.test.jsx
@@ -0,0 +1,32 @@
+import { render, fireEvent } from '@testing-library/react';
+
+import { useDispatch, useSelector } from 'react-redux';
+
+import InputContainer from './InputContainer';
+
+jest.mock('react-redux');
+describe('InputContainer', () => {
+
+  it ('클릭', () => {
+    const dispatch = jest.fn();
+
+    useDispatch.mockImplementation(() => dispatch);
+
+    useSelector.mockImplementation((selector) => selector({
+      input: {
+        name: '하하',
+        category: '호호',
+        address: '히히',
+      }
+    }));
+
+    const {getByText} = render(<InputContainer />);
+
+    expect(dispatch).not.toBeCalled();
+
+    fireEvent.click(getByText(/등록/));
+
+    expect(dispatch).toBeCalled();
+  });
+
+});
