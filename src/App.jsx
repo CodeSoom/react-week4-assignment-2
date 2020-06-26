6c8a552c0969ebd8911f6846e58ddeebaba20bda
SeaFalcon <seafalcon323@gmail.com>
2020-06-27 02:21:48 +0900

    레스토랑 정보확인 페이지 레이아웃 마크업

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,26 @@ import React from 'react';
 export default function App() {
   return (
     <div>
-      Hello
+      {/* App Component */}
+      <h1>Restaurants</h1>
+      {/* List Component */}
+      <ul>
+        {/* Item Component */}
+        <li>
+          마녀주방
+          |
+          한식
+          |
+          서울시 강남구
+        </li>
+      </ul>
+      {/* Input Component */}
+      <p>
+        <input type="text" placeholder="이름" />
+        <input type="text" placeholder="분류" />
+        <input type="text" placeholder="주소" />
+        <button type="button">등록</button>
+      </p>
     </div>
   );
 }
