1390885d73d9a24920bd6fb05409e1ffe5d60087
KiChang Shin <kc7851@gmail.com>
2020-06-26 22:52:09 +0900

    App test 작성

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -13,5 +13,19 @@ describe('App', () => {
 
       expect(container).toHaveTextContent('Restaurants');
     });
+
+    it('3개의 input이 있다.', () => {
+      const { container } = render(
+        <App />,
+      );
+
+      const nameInput = container.querySelector('input[name = name]');
+      const categoryInput = container.querySelector('input[name = category]');
+      const addressInput = container.querySelector('input[name = address]');
+
+      expect(nameInput).not.toBeNull();
+      expect(categoryInput).not.toBeNull();
+      expect(addressInput).not.toBeNull();
+    });
   });
 });
