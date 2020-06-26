82ce45d7e8124063ac7002aec5a2347f37a511ef
KiChang Shin <kc7851@gmail.com>
2020-06-26 23:39:41 +0900

    Input test 구현

---
+++ src/Input.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Input() {
+  return (
+    <>
+      <input type="text" name="name" />
+      <input type="text" name="category" />
+      <input type="text" name="address" />
+      <button type="button">등록</button>
+    </>
+  );
+}
--- src/Page.jsx
+++ src/Page.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 
+import Input from './Input';
+
 export default function Page() {
   return (
     <div>
       <h1>Restaurants</h1>
-      <input type="text" name="name" />
-      <input type="text" name="category" />
-      <input type="text" name="address" />
-      <button type="button">등록</button>
+      <Input />
     </div>
   );
 }
