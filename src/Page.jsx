cec11fb092358a12c8391723c369696df8ec6624
KiChang Shin <kc7851@gmail.com>
2020-06-26 23:14:01 +0900

    Page test 구현

---
+++ src/Page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Restaurants</h1>
+      <input type="text" name="name" />
+      <input type="text" name="category" />
+      <input type="text" name="address" />
+      <button type="button">등록</button>
+    </div>
+  );
+}
