ec4bf987a6b42e64e837a289aabb1b705f8c4fb4
SeaFalcon <seafalcon323@gmail.com>
2020-06-27 05:11:09 +0900

    reducer 테스트_2 추가

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -19,4 +19,42 @@ describe('reducer', () => {
       expect(state.name).toBe('콩나물해장국');
     });
   });
+
+  describe('updateCategory', () => {
+    it('분류가 변경된다.', () => {
+      const previousState = {
+        category: '',
+      };
+
+      const action = {
+        type: 'updateCategory',
+        payload: {
+          category: '한식',
+        },
+      };
+
+      const state = reducer(previousState, action);
+
+      expect(state.category).toBe('한식');
+    });
+  });
+
+  describe('updateAddress', () => {
+    it('주소가 변경된다.', () => {
+      const previousState = {
+        address: '',
+      };
+
+      const action = {
+        type: 'updateAddress',
+        payload: {
+          address: '강릉',
+        },
+      };
+
+      const state = reducer(previousState, action);
+
+      expect(state.address).toBe('강릉');
+    });
+  });
 });
