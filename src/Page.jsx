74397067eeea8cfe6a2561e282552f967c153bcc
KiChang Shin <kc7851@gmail.com>
2020-06-26 23:53:20 +0900

    Page component 리팩토링
    Input에 informations props 전달

--- src/Page.jsx
+++ src/Page.jsx
@@ -3,10 +3,18 @@ import React from 'react';
 import Input from './Input';
 
 export default function Page() {
+  const informations = {
+    name: '',
+    category: '',
+    address: '',
+  };
+
   return (
     <div>
       <h1>Restaurants</h1>
-      <Input />
+      <Input
+        informations={informations}
+      />
     </div>
   );
 }
