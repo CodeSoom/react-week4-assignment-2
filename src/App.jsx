6475164e8aa66170c229d51e8fe3a4de3ef44bda
daadaadaah <daadaadaah@gmail.com>
2020-06-26 22:54:20 +0900

    [Green] Implement a App - context : with restaurants - it : shows restaurants

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ export default function App() {
   return (
     <div>
       <h2>Restaurants</h2>
+      <p>레스토랑1|분류1|주소1</p>
+      <p>레스토랑2|분류2|주소2</p>
+      <p>레스토랑3|분류3|주소3</p>
+
       <input placeholder="이름" />
       <input placeholder="분류" />
       <input placeholder="주소" />
