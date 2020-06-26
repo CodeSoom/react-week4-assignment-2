7a81be14657361fa1823f769d95f5631fabbb65b
KiChang Shin <kc7851@gmail.com>
2020-06-27 01:47:50 +0900

    InputContainer test 구현

--- src/InputContainer.jsx
+++ src/InputContainer.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import Input from './Input';
 
+import {
+  updateInformations,
+} from './actions';
+
 export default function InputContainer() {
+  const dispatch = useDispatch();
+
   const informations = useSelector((state) => state.informations);
 
-  function handleChangeInformations() {
-    return null;
+  function handleChangeInformations(event) {
+    const target = event.target.name;
+    const { value } = event.target;
+    dispatch(updateInformations(target, value));
   }
 
   function handleClick() {
