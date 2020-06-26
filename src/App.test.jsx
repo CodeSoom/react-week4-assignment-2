435389fd10bfaf1b41c02ae325cca557f2282261
daadaadaah <daadaadaah@gmail.com>
2020-06-26 19:39:25 +0900

    Write App test scenario

---
+++ src/App.test.jsx
@@ -0,0 +1,20 @@
+describe('<App />', () => {
+  context('when the user does nothing', () => {
+    it('shows a app title', () => {});
+
+    it('shows 3 inputs', () => {});
+
+    it('shows a button for adding a restaurant ', () => {});
+  });
+
+  context('when the user input a new restaurant', () => {
+    it('shows a new restaurant in inputs', () => {});
+  });
+
+  context('when the user add a new restaurant', () => {
+    it('input is empty', () => {});
+
+    it('shows a new restaurant into restaurants', () => {});
+  });
+
+});
