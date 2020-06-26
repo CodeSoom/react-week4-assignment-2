52de70efd3b8eb6eaeeda6641fa4055509fc19fa
daadaadaah <daadaadaah@gmail.com>
2020-06-26 22:25:34 +0900

    [Red] Add a App test - context : when the user does nothing - it : shows 3 inputs

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -16,20 +16,23 @@ describe('<App />', () => {
       expect(container).toHaveTextContent('Restaurants');
     });
 
-    it('shows 3 inputs', () => {});
+    it('shows 3 inputs', () => {
+      const { getByPlaceholderText } = renderApp();
+
+      expect(getByPlaceholderText(/이름/i)).toBeInTheDocument();
+      expect(getByPlaceholderText(/분류/i)).toBeInTheDocument();
+      expect(getByPlaceholderText(/주소/i)).toBeInTheDocument();
+    });
 
     it('shows a button for adding a restaurant ', () => {
       const { getByRole } = renderApp();
 
       expect(getByRole('button')).toHaveTextContent(/등록/i);
-
     });
   });
 
   context('when the user input a new restaurant', () => {
-    it('shows a new restaurant in inputs', () => {
-      
-    });
+    it('shows a new restaurant in inputs', () => {});
   });
 
   context('when the user add a new restaurant', () => {
