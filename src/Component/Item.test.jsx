270df34aad1bce91505273ea7a617b36dcd8f6ee
SeaFalcon <seafalcon323@gmail.com>
2020-06-27 02:33:33 +0900

    Item 테스트 추가

---
+++ src/Component/Item.test.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import Item from './Item';
+
+describe('Item', () => {
+  it('초기화면이 나타난다.', () => {
+    const { container } = render((
+      <Item />
+    ));
+
+    expect(container).toHaveTextContent(/마녀주방/);
+  });
+});
