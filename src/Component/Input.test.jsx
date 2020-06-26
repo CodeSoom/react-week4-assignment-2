ac8eaa49f2c608a4dffc864276397f8b2089196c
SeaFalcon <seafalcon323@gmail.com>
2020-06-27 03:34:57 +0900

    Input 테스트_2 추가

--- src/Component/Input.test.jsx
+++ src/Component/Input.test.jsx
@@ -5,14 +5,36 @@ import { render } from '@testing-library/react';
 import Input from './Input';
 
 describe('Input', () => {
-  it('초기화면이 나타난다.', () => {
-    const { getByText, getByTestId } = render((
-      <Input />
-    ));
+  context('이름, 분류, 주소 정보가 없을 경우', () => {
+    it('초기화면이 나타난다.', () => {
+      const { getByText, getByTestId } = render((
+        <Input />
+      ));
 
-    expect(getByTestId('name')).not.toBeNull();
-    expect(getByTestId('category')).not.toBeNull();
-    expect(getByTestId('address')).not.toBeNull();
-    expect(getByText('등록')).not.toBeNull();
+      expect(getByTestId(/name/)).not.toBeNull();
+      expect(getByTestId(/category/)).not.toBeNull();
+      expect(getByTestId(/address/)).not.toBeNull();
+      expect(getByText(/등록/)).not.toBeNull();
+    });
+  });
+
+  context('이름, 분류, 주소 정보가 있을 경우', () => {
+    it('화면에 이름, 분류, 주소 정보가 표시된다..', () => {
+      const information = {
+        name: '마녀주방',
+        category: '한식',
+        address: '강남',
+      };
+
+      const { getByDisplayValue } = render((
+        <Input
+          information={information}
+        />
+      ));
+
+      expect(getByDisplayValue(/마녀주방/)).not.toBeNull();
+      expect(getByDisplayValue(/한식/)).not.toBeNull();
+      expect(getByDisplayValue(/강남/)).not.toBeNull();
+    });
   });
 });
