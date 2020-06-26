0d00742bf8da6f4d6175dfe4b48ca6c84d522734
KiChang Shin <kc7851@gmail.com>
2020-06-26 23:46:34 +0900

    Input test 작성

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -24,4 +24,28 @@ describe('Input', () => {
       expect(addressInput.value).toBe('');
     });
   });
+
+  context('with input values', () => {
+    it('input에 value가 보인다.', () => {
+      const informations = {
+        name: '한식당',
+        category: '한식',
+        address: '강남구',
+      };
+
+      const { container } = render(
+        <Input
+          informations={informations}
+        />,
+      );
+
+      const nameInput = container.querySelector('input[name = name]');
+      const categoryInput = container.querySelector('input[name = category]');
+      const addressInput = container.querySelector('input[name = address]');
+
+      expect(nameInput.value).toBe(informations.name);
+      expect(categoryInput.value).toBe(informations.category);
+      expect(addressInput.value).toBe(informations.address);
+    });
+  });
 });
