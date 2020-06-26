4d7cf18dd31d1cea8890eb288c8ead97c82d6b99
daadaadaah <daadaadaah@gmail.com>
2020-06-26 22:29:56 +0900

    [Green] Implement a App - context : when the user does nothing - it : shows 3 inputs

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ export default function App() {
   return (
     <div>
       <h2>Restaurants</h2>
+      <input placeholder="이름" />
+      <input placeholder="분류" />
+      <input placeholder="주소" />
       <button type="button">등록</button>
     </div>
   );
