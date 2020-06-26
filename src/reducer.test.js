21c5be9b2e33639f46964f4f81b8bd75fcffc4f5
KiChang Shin <kc7851@gmail.com>
2020-06-27 00:41:57 +0900

    recuder updateInformations test 추가 작성

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -3,16 +3,16 @@ import reducer from './reducer';
 describe('reducer', () => {
   describe('updateInformations', () => {
     context('with new informations', () => {
-      it('new state를 반환한다.', () => {
-        const state = {
-          informations: {
-            name: '',
-            category: '',
-            address: '',
-          },
-        };
+      const previousState = {
+        informations: {
+          name: '',
+          category: '',
+          address: '',
+        },
+      };
 
-        const newState = reducer(state, {
+      it('new state를 반환한다.', () => {
+        const newState = reducer(previousState, {
           type: 'updateInformations',
           payload: {
             target: 'name',
@@ -21,6 +21,30 @@ describe('reducer', () => {
         });
 
         expect(newState.informations.name).toEqual('한식당');
+      });
+
+      it('new state를 반환한다.', () => {
+        const newState = reducer(previousState, {
+          type: 'updateInformations',
+          payload: {
+            target: 'category',
+            value: '한식',
+          },
+        });
+
+        expect(newState.informations.category).toEqual('한식');
+      });
+
+      it('new state를 반환한다.', () => {
+        const newState = reducer(previousState, {
+          type: 'updateInformations',
+          payload: {
+            target: 'address',
+            value: '강남구',
+          },
+        });
+
+        expect(newState.informations.address).toEqual('강남구');
       });
     });
   });
