d2163a48d6f5b544ee4889d3e38edf54c0a7f040
SeaFalcon <seafalcon323@gmail.com>
2020-06-27 05:08:25 +0900

    reducer 테스트 구현 및 reducer initialState 추가

--- src/reducer.js
+++ src/reducer.js
@@ -1,3 +1,17 @@
-export default function reducer(state, action) {
+const initialState = {
+  newId: 100,
+  name: '',
+  category: '',
+  address: '',
+  informations: [],
+};
+
+export default function reducer(state = initialState, action) {
+  if (action.type === 'updateName') {
+    return {
+      ...state,
+      name: action.payload.name,
+    };
+  }
   return state;
 }
--- src/reducer.test.js
+++ src/reducer.test.js
@@ -2,19 +2,21 @@ import reducer from './reducer';
 
 describe('reducer', () => {
   describe('updateName', () => {
-    const previousState = {
-      name: '',
-    };
+    it('이름이 변경된다.', () => {
+      const previousState = {
+        name: '',
+      };
 
-    const action = {
-      type: 'updateName',
-      payload: {
-        name: '콩나물해장국',
-      },
-    };
+      const action = {
+        type: 'updateName',
+        payload: {
+          name: '콩나물해장국',
+        },
+      };
 
-    const state = reducer(previousState, action);
+      const state = reducer(previousState, action);
 
-    expect(state.name).toBe('콩나물해장국');
+      expect(state.name).toBe('콩나물해장국');
+    });
   });
 });
