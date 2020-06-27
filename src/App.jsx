6e93a7688404237a22470781a431a1522bdcb2e9
naraekn <34229459+naraekn@users.noreply.github.com>
2020-06-27 09:07:58 +0900

    Implement App
    Add title and form

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 export default function App() {
   return (
     <div>
-      Hello
+      <h1>Restaurants</h1>
+      <form>
+        <input type="text" placeholder="이름" />
+        <input type="text" placeholder="분류" />
+        <input type="text" placeholder="주소" />
+        <input type="submit" value="등록" />
+      </form>
     </div>
   );
 }
