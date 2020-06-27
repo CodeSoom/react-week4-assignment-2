b895245f86586c15a9f8df73bc21877d1e71d9ad
sogoagain <imyong0@gmail.com>
2020-06-27 15:50:29 +0900

    Refactor <Form /> 컴포넌트의 input 핸들러를 개선한다

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,42 +1,29 @@
 import React from 'react';
 
 export default function Form({ value, onChangeInput, onSubmit }) {
+  const handleChangeInput = (e) => onChangeInput({ name: e.target.name, value: e.target.value });
+
   return (
     <form onSubmit={onSubmit}>
       <input
         name="name"
         type="text"
         value={value.name}
-        onChange={(e) => onChangeInput(
-          {
-            name: e.target.name,
-            value: e.target.value,
-          },
-        )}
+        onChange={handleChangeInput}
         placeholder="이름"
       />
       <input
         name="category"
         type="text"
         value={value.category}
-        onChange={(e) => onChangeInput(
-          {
-            name: e.target.name,
-            value: e.target.value,
-          },
-        )}
+        onChange={handleChangeInput}
         placeholder="분류"
       />
       <input
         name="address"
         type="text"
         value={value.address}
-        onChange={(e) => onChangeInput(
-          {
-            name: e.target.name,
-            value: e.target.value,
-          },
-        )}
+        onChange={handleChangeInput}
         placeholder="주소"
       />
       <button
