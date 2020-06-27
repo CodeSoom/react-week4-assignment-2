1458ed23cad130af36b7c911b7a2c2d774a35f83
naraekn <34229459+naraekn@users.noreply.github.com>
2020-06-28 03:21:24 +0900

    Update RestarantFormContainer
    Use redux to manage state in RestaurantFormContainer

--- src/RestaurantFormContainer.jsx
+++ src/RestaurantFormContainer.jsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import RestaurantForm from './RestaurantForm';
+import {
+  changeRestaurant, saveNewRestaurant,
+} from './actions';
 
 export default function RestaurantFormContainer() {
+  const { newRestaurant } = useSelector((state) => ({
+    newRestaurant: state.newRestaurant,
+  }));
+
+  const dispatch = useDispatch();
+
+  function handleChange(event) {
+    dispatch(changeRestaurant(
+      event.target.name,
+      event.target.value,
+    ));
+  }
+
+  function handleClickSubmit(event) {
+    event.preventDefault();
+    dispatch(saveNewRestaurant());
+  }
+
   return (
-    <div>RestaurantForm</div>
+    <RestaurantForm
+      newRestaurant={newRestaurant}
+      handleChange={handleChange}
+      handleClickSubmit={handleClickSubmit}
+    />
   );
 }
