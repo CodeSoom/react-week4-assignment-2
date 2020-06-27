9a5b99d59683c557f107a3d2399a0fe8b54c207b
naraekn <34229459+naraekn@users.noreply.github.com>
2020-06-27 09:12:06 +0900

    Update test for App
    Test if onChange and onSubmit work well
    Test if restaurants list is rendered

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import App from './App';
 
@@ -9,9 +9,16 @@ test('App', () => {
     <App />
   ));
 
+  const name = '깐부치킨';
+  const category = '치킨';
+  const address = '용인수지';
+
   expect(getByText(/Restaurants/)).not.toBeNull();
-  expect(getByPlaceholderText(/이름/)).not.toBeNull();
-  expect(getByPlaceholderText(/분류/)).not.toBeNull();
-  expect(getByPlaceholderText(/주소/)).not.toBeNull();
-  expect(getByText(/등록/)).not.toBeNull();
+
+  fireEvent.change(getByPlaceholderText(/이름/), { target: { value: name } });
+  fireEvent.change(getByPlaceholderText(/분류/), { target: { value: category } });
+  fireEvent.change(getByPlaceholderText(/주소/), { target: { value: address } });
+  fireEvent.click(getByText(/등록/));
+
+  expect(getByText(`${name} | ${category} | ${address}`)).not.toBeNull();
 });
