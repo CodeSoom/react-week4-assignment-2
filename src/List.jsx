d39a84e6bc3014150e5227d2ccc7fe699b595204
sogoagain <imyong0@gmail.com>
2020-06-27 15:12:56 +0900

    Implement <List /> 컴포넌트를 구현한다

--- src/List.jsx
+++ src/List.jsx
@@ -1,3 +1,15 @@
-export default function List() {
-  return null;
+import React from 'react';
+
+export default function List({ restaurants }) {
+  const restaurantIntoText = ({ name, category, address }) => `${name} | ${category} | ${address}`;
+
+  return (
+    <ul>
+      {restaurants.map((restaurant) => (
+        <li key={restaurant.id}>
+          {restaurantIntoText(restaurant)}
+        </li>
+      ))}
+    </ul>
+  );
 }
--- src/List.test.jsx
+++ src/List.test.jsx
@@ -32,16 +32,19 @@ describe('<List />', () => {
     it('print the restaurant items', () => {
       // given
       const restaurants = [{
+        id: 1,
         name: '시카고피자',
         category: '양식',
         address: '이태원동',
       },
       {
+        id: 2,
         name: '마녀주방',
         category: '한식',
         address: '서울시 강남구',
       },
       {
+        id: 3,
         name: '키와미',
         category: '일식',
         address: '분당구 정자동',
