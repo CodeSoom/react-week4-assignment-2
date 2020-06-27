0cdc960f9bb10e60bc2680d64e583cfae243ac47
morykwon <kwonmory@gmail.com>
2020-06-27 13:44:49 +0900

    Input 리팩토링

--- src/Input.jsx
+++ src/Input.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
 
+import { useSelector } from 'react-redux';
+
 export default function Input({ onChange, onClick }) {
+  const { name, category, address } = useSelector((selector) => ({
+    name: selector.input.name,
+    category: selector.input.category,
+    address: selector.input.address
+  }));
 
   return (
     <div>
       <form onSubmit={onClick}>
         <input type="text" name="name" placeholder="이름" onChange={
-          (e) => onChange({ key: 'name', value: e.target.value })
-        }/>
+          (e) => onChange(e)
+        } value={name}/>
         <input type="text" name="category" placeholder="분류" onChange={
-          (e) => onChange({ key: 'category', value: e.target.value })
-        }/>
+          (e) => onChange(e)
+        } value={category}/>
         <input type="text" name="address" placeholder="주소" onChange={
-          (e) => onChange({ key: 'address', value: e.target.value })
-        }/>
+          (e) => onChange(e)
+        } value={address}/>
         <button type="submit">등록</button>
       </form>
     </div>
