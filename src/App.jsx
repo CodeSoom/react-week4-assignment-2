d41dc972e4e95e33a8b635b78076d12c08938520
morykwon <kwonmory@gmail.com>
2020-06-27 13:54:56 +0900

    App 구현하기

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { setRestaurants } from './actions';
+import ListContainer from './ListContainer';
+import InputContainer from './InputContainer';
 
 export default function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setRestaurants());
+  }, []);
+
   return (
     <div>
-      Hello
+      <h1>Restaurants</h1>
+      <ListContainer/>
+      <InputContainer/>
     </div>
   );
 }
