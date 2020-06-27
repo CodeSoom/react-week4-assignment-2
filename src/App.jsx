1b6f3ec43b7b3515b51815684d28f3b6bb63c8eb
sogoagain <imyong0@gmail.com>
2020-06-27 16:02:19 +0900

    Refactor <App /> 컴포넌트에서 상태를 관리하는 로직을 외부로 추출한다

--- src/App.jsx
+++ src/App.jsx
@@ -3,47 +3,53 @@ import React, { useState } from 'react';
 import List from './List';
 import Form from './Form';
 
-export default function App() {
-  const [state, setState] = useState({
-    id: 1,
-    input: {
-      name: '',
-      category: '',
-      address: '',
+const initialState = {
+  id: 1,
+  input: {
+    name: '',
+    category: '',
+    address: '',
+  },
+  restaurants: [],
+};
+
+const registerRestaurant = (state) => ({
+  ...state,
+  id: state.id + 1,
+  input: {
+    name: '',
+    category: '',
+    address: '',
+  },
+  restaurants: [
+    ...state.restaurants,
+    {
+      id: state.id,
+      name: state.input.name,
+      category: state.input.category,
+      address: state.input.address,
     },
-    restaurants: [],
-  });
+  ],
+});
+
+const updateInput = (state, { name, value }) => ({
+  ...state,
+  input: {
+    ...state.input,
+    [name]: value,
+  },
+});
+
+export default function App() {
+  const [state, setState] = useState(initialState);
 
   const handleSubmit = (event) => {
-    setState({
-      ...state,
-      id: state.id + 1,
-      input: {
-        name: '',
-        category: '',
-        address: '',
-      },
-      restaurants: [
-        ...state.restaurants,
-        {
-          id: state.id,
-          name: state.input.name,
-          category: state.input.category,
-          address: state.input.address,
-        },
-      ],
-    });
+    setState(registerRestaurant(state));
     event.preventDefault();
   };
 
   const handleChangeInput = ({ name, value }) => {
-    setState({
-      ...state,
-      input: {
-        ...state.input,
-        [name]: value,
-      },
-    });
+    setState(updateInput(state, { name, value }));
   };
 
   return (
