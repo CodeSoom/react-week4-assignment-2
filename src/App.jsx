c58352cbcfa3179d5fa3eff927764155bd640838
jiunkoo <jiunkoo0106@gmail.com>
2020-06-28 05:48:48 +0900

    App - handleChangeRestaurant 테스트 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Input from './Input';
+import List from './List';
+
+const initialState = {
+  restaurant: {
+    name: '',
+    category: '',
+    address: '',
+  },
+  reservations: [],
+};
 
 export default function App() {
+  const [state, setState] = useState(initialState);
+
+  const { restaurant, reservations } = state;
+
+  function handleChangeRestaurantName(event) {
+    const { restaurant } = state;
+    setState({
+      ...state,
+      restaurant: {
+        name: event.target.value,
+        category: restaurant.category,
+        address: restaurant.address,
+      }
+    })
+  }
+
+  function handleChangeRestaurantCategory(event) {
+    const { restaurant } = state;
+    setState({
+      ...state,
+      restaurant: {
+        name: restaurant.name,
+        category: event.target.value,
+        address: restaurant.address,
+      }
+    })
+  }
+
+  function handleChangeRestaurantAddress(event) {
+    const { restaurant } = state;
+    setState({
+      ...state,
+      restaurant: {
+        name: restaurant.name,
+        category: restaurant.category,
+        address: event.target.value,
+      }
+    })
+  }
+
   return (
     <div>
-      Hello
+      <h1>Restaurants</h1>
+      <List
+        reservations={reservations}
+      />
+      <Input
+        value={restaurant}
+        onChangeName={handleChangeRestaurantName}
+        onChangeCategory={handleChangeRestaurantCategory}
+        onChangeAddress={handleChangeRestaurantAddress}
+      />
     </div>
   );
 }
