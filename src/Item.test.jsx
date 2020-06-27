342172fa640567cc704959eb8b2b0b3196f7f29d
jiunkoo <jiunkoo0106@gmail.com>
2020-06-28 03:53:32 +0900

    Item - Item test 추가

--- src/Item.test.jsx
+++ src/Item.test.jsx
@@ -1,4 +1,21 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
 import Item from './Item';
 
 test('Item', () => {
+  const restaurant = {
+    name: '지코바',
+    category: '한식',
+    address: '서울시 강남구',
+  };
+
+  const { container } = render(
+    <Item
+      restaurant={restaurant}
+    />
+  );
+
+  expect(container).toHaveTextContent('지코바|한식|서울시 강남구');
 });
