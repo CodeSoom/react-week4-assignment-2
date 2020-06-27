23837c0276026253d5806ce3c4cc68a808d61c11
sogoagain <imyong0@gmail.com>
2020-06-27 13:43:19 +0900

    Implement <App />이 처음 렌더링 될 때 갖는 요소들을 구현한다

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,27 @@ import React from 'react';
 
 export default function App() {
   return (
-    <div>
-      Hello
-    </div>
+    <>
+      <h1>Restaurants</h1>
+      <form>
+        <input
+          type="text"
+          placeholder="이름"
+        />
+        <input
+          type="text"
+          placeholder="분류"
+        />
+        <input
+          type="text"
+          placeholder="주소"
+        />
+        <button
+          type="submit"
+        >
+          등록
+        </button>
+      </form>
+    </>
   );
 }
