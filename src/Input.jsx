a5ad73f9a8ff5f55e72e00baaf6c6408ba88d02b
jiunkoo <jiunkoo0106@gmail.com>
2020-06-28 05:12:57 +0900

    Input - Input 테스트 구현

--- src/Input.jsx
+++ src/Input.jsx
@@ -1,2 +1,47 @@
-export default function Input(){
+import React from 'react';
+
+export default function Input({
+  value,
+  onChangeName,
+  onChangeCategory,
+  onChangeAddress,
+  onClick,
+}){
+  return (
+    <p>
+      <label htmlFor="input-restaurant-name">
+        이름
+      </label>
+      <input
+        id="input-restaurant-name"
+        type="text"
+        placeholder="이름"
+        value={value.name}
+        onChange={onChangeName}
+      />
+      <label htmlFor="input-restaurant-category">
+        분류
+      </label>
+      <input
+        id="input-restaurant-category"
+        type="text"
+        placeholder="분류"
+        value={value.category}
+        onChange={onChangeCategory}
+      />
+      <label htmlFor="input-restaurant-address">
+        주소
+      </label>
+      <input
+        id="input-restaurant-address"
+        type="text"
+        placeholder="주소"
+        value={value.address}
+        onChange={onChangeAddress}
+      />
+      <button type="button" onClick={onClick}>
+        등록
+      </button>
+    </p>
+  );
 }
