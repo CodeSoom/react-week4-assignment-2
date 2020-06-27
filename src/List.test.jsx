dbca694bcc8365042ff0fc9e861d1bf15916a5e1
naraekn <34229459+naraekn@users.noreply.github.com>
2020-06-27 16:01:19 +0900

    Add test for List without restaurants
    Add no restaurants case
    Refactor List test code using describe-context-it

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -3,24 +3,32 @@ import { render } from '@testing-library/react';
 
 import List from './List';
 
-test('List', () => {
-  // TODO: with restaurants
-  // TODO: without restaurants
-  const restaurants = {
-    newId: 2,
-    name: '고봉민김밥',
-    category: '분식',
-    address: '강남',
-    restaurants: [{
-      id: 1, name: '맘스터치', category: '패스트푸드', address: '용인수지',
-    }],
-  };
+describe('List', () => {
+  function renderList(restaurants) {
+    return render((
+      <List
+        restaurants={restaurants}
+      />
+    ));
+  }
 
-  const { getByText } = render((
-    <List
-      restaurants={restaurants}
-    />
-  ));
+  context('with restaurants', () => {
+    it('renders list', () => {
+      const restaurants = [{
+        id: 1, name: '맘스터치', category: '패스트푸드', address: '용인수지',
+      }];
+      const { getByText } = renderList(restaurants);
 
-  expect(getByText('맘스터치 | 패스트푸드 | 용인수지')).not.toBeNull();
+      expect(getByText('맘스터치 | 패스트푸드 | 용인수지')).not.toBeNull();
+    });
+  });
+
+  context('without restaurants', () => {
+    it('renders ', () => {
+      const restaurants = [];
+      const { container } = renderList(restaurants);
+
+      expect(container.innerText).toBeUndefined();
+    });
+  });
 });
