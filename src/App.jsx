14e4252d299237feffdaaa6ecd5376dd6e2f902e
naraekn <34229459+naraekn@users.noreply.github.com>
2020-06-27 09:22:31 +0900

    Implement App with restaurant list

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,14 @@ export default function App() {
   return (
     <div>
       <h1>Restaurants</h1>
+      <ul>
+        <li>깐부치킨 | 치킨 | 용인수지</li>
+      </ul>
       <form>
         <input type="text" placeholder="이름" />
         <input type="text" placeholder="분류" />
         <input type="text" placeholder="주소" />
-        <input type="submit" value="등록" />
+        <input type="button" value="등록" />
       </form>
     </div>
   );
