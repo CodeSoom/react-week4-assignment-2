43da90d6d7d55b7244683d2625715cbfa258cc10
jiunkoo <jiunkoo0106@gmail.com>
2020-06-28 07:47:26 +0900

    reducer - updateRestaurant 테스트 추가

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,4 +1,46 @@
 import reducer from './reducer';
 
-test('reducer', () => {
+import {
+  updateRestaurantName,
+  updateRestaurantCategory,
+  updateRestaurantAddress,
+} from './actions';
+
+describe('reducer', () => {
+  const initialState = {
+    id: 1,
+    restaurant: {
+      name: '',
+      category: '',
+      address: '',
+    },
+    reservations: [],
+  };
+
+  test('updateRestaurantName', () => {
+    const state = reducer(
+      initialState,
+      updateRestaurantName('롯데리아'),
+    );
+
+    expect(state.restaurant.name).toBe('롯데리아');
+  });
+
+  test('updateRestaurantCategory', () => {
+    const state = reducer(
+      initialState,
+      updateRestaurantCategory('패스트푸드'),
+    );
+
+    expect(state.restaurant.category).toBe('패스트푸드');
+  });
+
+  test('updateRestaurantAddress', () => {
+    const state = reducer(
+      initialState,
+      updateRestaurantAddress('서울시 성북구'),
+    );
+
+    expect(state.restaurant.address).toBe('서울시 성북구');
+  });
 });
