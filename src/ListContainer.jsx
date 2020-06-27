35b0975f7ffad6d3fc693f2e1c6ee6d38809ec15
Minwook Park <mwpark0975@gmail.com>
2020-06-27 16:44:49 +0900

    ListContainer - implementation

--- src/ListContainer.jsx
+++ src/ListContainer.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
+import { useSelector } from 'react-redux';
+
 import List from './List';
 
 export default function ListContainer() {
+  const { restaurants } = useSelector((state) => ({
+    restaurants: state.restaurants,
+  }));
 
+  return (
+    <List
+      restaurants={restaurants}
+    />
+  );
 }
