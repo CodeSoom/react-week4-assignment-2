7fcf015a7fa58ca3411614693e9f7b5f5933296b
naraekn <34229459+naraekn@users.noreply.github.com>
2020-06-28 03:24:59 +0900

    Refactor App to use RestaurantFormContainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,14 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
 
-import { changeRestaurant, saveNewRestaurant } from './actions';
 import RestaurantsContainer from './RestaurantsContainer';
+import RestaurantFormContainer from './RestaurantFormContainer';
 
 export default function App() {
-  const { newRestaurant } = useSelector((state) => ({
-    newRestaurant: state.newRestaurant,
-  }));
-
-  const dispatch = useDispatch();
-
-  function handleChange(event) {
-    dispatch(changeRestaurant(
-      event.target.name,
-      event.target.value,
-    ));
-  }
-
-  function handleClickSubmit(event) {
-    event.preventDefault();
-    dispatch(saveNewRestaurant());
-  }
-
   return (
     <div>
       <h1>Restaurants</h1>
       <RestaurantsContainer />
-      <form onSubmit={handleClickSubmit}>
-        <input
-          type="text"
-          name="name"
-          value={newRestaurant.name}
-          placeholder="이름"
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          name="category"
-          value={newRestaurant.category}
-          placeholder="분류"
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          name="address"
-          value={newRestaurant.address}
-          placeholder="주소"
-          onChange={handleChange}
-        />
-        <input type="submit" value="등록" />
-      </form>
+      <RestaurantFormContainer />
     </div>
   );
 }
