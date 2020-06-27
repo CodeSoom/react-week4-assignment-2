f36b71e5d8d8c4923ce46d4ca8da44e059eadf6c
naraekn <34229459+naraekn@users.noreply.github.com>
2020-06-27 10:11:40 +0900

    Update test for reducer in case of handleSubmit

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -4,7 +4,7 @@ import {
   changeName, changeCategory, changeAddress, clickSubmit,
 } from './actions';
 
-test('reducer', () => {
+describe('reducer', () => {
   const testState = {
     newId: 2,
     name: '고봉민김밥',
@@ -14,6 +14,7 @@ test('reducer', () => {
       id: 1, name: '맘스터치', category: '패스트푸드', address: '용인수지',
     }],
   };
+
   // TODO: set initial state
   // TODO: no action type
   // TODO: changeName test
@@ -21,10 +22,26 @@ test('reducer', () => {
   // TODO: changeAddress test
   // TODO: clickSubmit - context1: all inputs are filled, context2: any input is missing
 
-  const state = reducer(testState, clickSubmit());
+  describe('clickSubmit', () => {
+    context('when all inputs are filled', () => {
+      it('appends a new restaurant into restaurants', () => {
+        const state = reducer(testState, clickSubmit());
 
-  expect(state.restaurants[1].id).not.toBeUndefined();
-  expect(state.restaurants[1].name).toBe('고봉민김밥');
-  expect(state.restaurants[1].category).toBe('분식');
-  expect(state.restaurants[1].address).toBe('강남');
+        expect(state.restaurants[1].id).not.toBeUndefined();
+        expect(state.restaurants[1].name).toBe('고봉민김밥');
+        expect(state.restaurants[1].category).toBe('분식');
+        expect(state.restaurants[1].address).toBe('강남');
+      });
+    });
+    context('when any input is missing', () => {
+      it('appends a new restaurant with empty input', () => {
+        // TODO: Decide how to handle empty cases later
+        const state = reducer({
+          ...testState,
+          name: '',
+        }, clickSubmit());
+        expect(state.name).toBe('');
+      });
+    });
+  });
 });
