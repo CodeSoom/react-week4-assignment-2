27e59c345d901d78aa9a945287cca2884106c844
sogoagain <imyong0@gmail.com>
2020-06-27 15:09:46 +0900

    Test <List /> 식당 정보를 출력하는 List 컴포넌트에 대해 테스트한다

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import { render, screen, fireEvent } from '@testing-library/react';
 
 import App from './App';
---
+++ src/List.test.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+
+import { render, screen } from '@testing-library/react';
+
+import List from './List';
+
+function renderList(restaurants) {
+  render(<List
+    restaurants={restaurants}
+  />);
+
+  return {
+    restaurantListItems: screen.queryAllByRole('listitem'),
+  };
+}
+
+describe('<List />', () => {
+  context('without restaurants', () => {
+    it('do not print restaurant item', () => {
+      // given
+      const restaurants = [];
+
+      // when
+      const { restaurantListItems } = renderList(restaurants);
+
+      // then
+      expect(restaurantListItems).toHaveLength(0);
+    });
+  });
+
+  context('with restaurants', () => {
+    it('print the restaurant items', () => {
+      // given
+      const restaurants = [{
+        name: '시카고피자',
+        category: '양식',
+        address: '이태원동',
+      },
+      {
+        name: '마녀주방',
+        category: '한식',
+        address: '서울시 강남구',
+      },
+      {
+        name: '키와미',
+        category: '일식',
+        address: '분당구 정자동',
+      }];
+
+      // when
+      const { restaurantListItems } = renderList(restaurants);
+
+      // then
+      expect(restaurantListItems).toHaveLength(restaurants.length);
+      restaurantListItems.forEach((item, index) => {
+        expect(item.innerHTML).toBe(`${restaurants[index].name} | ${restaurants[index].category} | ${restaurants[index].address}`);
+      });
+    });
+  });
+});
