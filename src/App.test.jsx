8d9d25297aea19c4b0b7714605a289ab9f592fb2
sogoagain <imyong0@gmail.com>
2020-06-27 13:47:30 +0900

    Refactor <App />의 초기 상태를 테스트 하는 코드를 개선한다

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -3,22 +3,29 @@ import { render, screen } from '@testing-library/react';
 
 import App from './App';
 
+function renderApp() {
+  render(<App />);
+  return {
+    heading: screen.getByText(/Restaurants/),
+    nameInput: screen.getByPlaceholderText(/이름/),
+    categoryInput: screen.getByPlaceholderText(/분류/),
+    addressInput: screen.getByPlaceholderText(/주소/),
+    registerButton: screen.getByRole('button', { name: /등록/ }),
+  };
+}
+
 describe('<App />', () => {
   context('when initialized', () => {
     it('renders title', () => {
       // when
-      render(<App />);
-      const heading = screen.getByText(/Restaurants/);
+      const { heading } = renderApp();
       // then
       expect(heading).toBeInTheDocument();
     });
 
     it('renders input boxes', () => {
       // when
-      render(<App />);
-      const nameInput = screen.getByPlaceholderText(/이름/);
-      const categoryInput = screen.getByPlaceholderText(/분류/);
-      const addressInput = screen.getByPlaceholderText(/주소/);
+      const { nameInput, categoryInput, addressInput } = renderApp();
       // then
       expect(nameInput).toBeInTheDocument();
       expect(categoryInput).toBeInTheDocument();
@@ -27,8 +34,7 @@ describe('<App />', () => {
 
     it('renders register button', () => {
       // when
-      render(<App />);
-      const registerButton = screen.getByRole('button', { name: /등록/ });
+      const { registerButton } = renderApp();
       // then
       expect(registerButton).toBeInTheDocument();
     });
