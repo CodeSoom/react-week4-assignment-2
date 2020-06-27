124fe8d46b5a2251bb38b6b3caf8e0fbd267840e
sogoagain <imyong0@gmail.com>
2020-06-27 21:17:14 +0900

    Refactor <App />에서 <FormContainer />를 사용한다

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,14 @@
 import React from 'react';
 
-import { useDispatch, useSelector } from 'react-redux';
-
-import Form from './Form';
-
-import { registerRestaurant, updateInput } from './actions';
 import ListContainer from './ListContainer';
+import FormContainer from './FormContainer';
 
 export default function App() {
-  const dispatch = useDispatch();
-  const { input } = useSelector((state) => ({
-    input: state.input,
-  }));
-
-  const handleSubmit = (event) => {
-    dispatch(registerRestaurant());
-    event.preventDefault();
-  };
-
-  const handleChangeInput = ({ name, value }) => {
-    dispatch(updateInput({ name, value }));
-  };
-
   return (
     <>
       <h1>Restaurants</h1>
       <ListContainer />
-      <Form
-        value={input}
-        onChangeInput={handleChangeInput}
-        onSubmit={handleSubmit}
-      />
+      <FormContainer />
     </>
   );
 }
