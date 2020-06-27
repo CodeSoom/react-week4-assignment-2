cbcf7db6e97d331cfd6c0b38796e1d8bc9af1347
jiunkoo <jiunkoo0106@gmail.com>
2020-06-28 05:12:48 +0900

    Input - Input 테스트 추가

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -1,4 +1,53 @@
+import React from 'react';
+
+import { render, fireEvent } from '@testing-library/react';
+
 import Input from './Input';
 
 test('Input', () => {
+  const restaurant = {
+    name: '초기 이름',
+    category: '초기 분류',
+    address: '초기 주소',
+  };
+  const handleChangeName = jest.fn();
+  const handleChangeCategory = jest.fn();
+  const handleChangeAddress = jest.fn();
+  const handleClick = jest.fn();
+
+  const { getByDisplayValue, getByLabelText, getByText } = render(
+    <Input
+      value={restaurant}
+      onChangeName={handleChangeName}
+      onChangeCategory={handleChangeCategory}
+      onChangeAddress={handleChangeAddress}
+      onClick={handleClick}
+    />,
+  );
+
+  expect(getByDisplayValue('초기 이름')).not.toBeNull();
+  expect(getByDisplayValue('초기 분류')).not.toBeNull();
+  expect(getByDisplayValue('초기 주소')).not.toBeNull();
+
+  fireEvent.change(getByLabelText('이름'),
+    { target: { value: '황궁쟁반짜장' } },
+  );
+
+  expect(handleChangeName).toBeCalled();
+
+  fireEvent.change(getByLabelText('분류'),
+  { target: { value: '중식' } },
+  );
+
+  expect(handleChangeCategory).toBeCalled();
+
+  fireEvent.change(getByLabelText('주소'),
+    { target: { value: '서울시 서초구' } },
+  );
+
+  expect(handleChangeAddress).toBeCalled();
+
+  fireEvent.click(getByText('등록'));
+
+  expect(handleClick).toBeCalled();
 });
