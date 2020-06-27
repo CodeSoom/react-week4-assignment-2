3b62da2d9541668650257ffa66ef7f1605f9a1c8
jiunkoo <jiunkoo0106@gmail.com>
2020-06-28 04:42:08 +0900

    List -List test 구현

--- src/List.jsx
+++ src/List.jsx
@@ -1,2 +1,16 @@
-export default function List() {
+import React from 'react';
+
+import Item from './Item';
+
+export default function List({ reservations }) {
+  return (
+    <ul>
+      {reservations.map((reservation) => (
+        <Item
+          key={reservation.id}
+          restaurant={reservation.restaurant}
+        />
+      ))}
+    </ul>
+  );
 }
