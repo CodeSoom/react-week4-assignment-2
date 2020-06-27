e5b0f55d96667223961aad88bf5605f6df452312
naraekn <34229459+naraekn@users.noreply.github.com>
2020-06-27 23:52:46 +0900

    Update test for reducer with new actions

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,15 +1,17 @@
 import reducer from './reducer';
 
 import {
-  changeName, changeCategory, changeAddress, clickSubmit,
+  changeRestaurant, saveNewRestaurant,
 } from './actions';
 
 describe('reducer', () => {
   const testState = {
     newId: 2,
-    name: '고봉민김밥',
-    category: '분식',
-    address: '강남',
+    newRestaurant: {
+      name: '고봉민김밥',
+      category: '분식',
+      address: '강남',
+    },
     restaurants: [{
       id: 1, name: '맘스터치', category: '패스트푸드', address: '용인수지',
     }],
@@ -22,9 +24,9 @@ describe('reducer', () => {
       const state = reducer(undefined, triggerMockAction());
 
       expect(state.newId).not.toBeNull();
-      expect(state.name).toHaveLength(0);
-      expect(state.category).toHaveLength(0);
-      expect(state.address).toHaveLength(0);
+      expect(state.newRestaurant.name).toHaveLength(0);
+      expect(state.newRestaurant.category).toHaveLength(0);
+      expect(state.newRestaurant.address).toHaveLength(0);
       expect(state.restaurants).toHaveLength(0);
     });
   });
@@ -37,50 +39,40 @@ describe('reducer', () => {
     });
   });
 
-  describe('changeName', () => {
-    it('changes name', () => {
-      const state = reducer(testState, changeName('고봉민김밥2'));
+  describe('changeRestaurant', () => {
+    context('when add name', () => {
+      it('changes name', () => {
+        const state = reducer(testState, changeRestaurant('name', '고봉민김밥2'));
 
-      expect(state.name).toBe('고봉민김밥2');
-    });
-  });
-
-  describe('changeCategory', () => {
-    it('changes category', () => {
-      const state = reducer(testState, changeCategory('분식2'));
-
-      expect(state.category).toBe('분식2');
-    });
-  });
-
-  describe('changeAddress', () => {
-    it('changes address', () => {
-      const state = reducer(testState, changeAddress('강남2'));
-
-      expect(state.address).toBe('강남2');
-    });
-  });
-
-  describe('clickSubmit', () => {
-    context('when all inputs are filled', () => {
-      it('appends a new restaurant into restaurants', () => {
-        const state = reducer(testState, clickSubmit());
-
-        expect(state.restaurants[1].id).not.toBeUndefined();
-        expect(state.restaurants[1].name).toBe('고봉민김밥');
-        expect(state.restaurants[1].category).toBe('분식');
-        expect(state.restaurants[1].address).toBe('강남');
+        expect(state.newRestaurant.name).toBe('고봉민김밥2');
       });
     });
-    context('when any input is missing', () => {
-      it('appends a new restaurant with empty input', () => {
-        // TODO: Decide how to handle empty cases later
-        const state = reducer({
-          ...testState,
-          name: '',
-        }, clickSubmit());
-        expect(state.name).toBe('');
+
+    context('when add category', () => {
+      it('changes category', () => {
+        const state = reducer(testState, changeRestaurant('category', '분식2'));
+
+        expect(state.newRestaurant.category).toBe('분식2');
       });
+    });
+
+    context('when add address', () => {
+      it('changes address', () => {
+        const state = reducer(testState, changeRestaurant('address', '강남2'));
+
+        expect(state.newRestaurant.address).toBe('강남2');
+      });
+    });
+  });
+
+  describe('saveNewRestaurant', () => {
+    it('appends a new restaurant into restaurants', () => {
+      const state = reducer(testState, saveNewRestaurant());
+
+      expect(state.restaurants[1].id).not.toBeUndefined();
+      expect(state.restaurants[1].name).toBe('고봉민김밥');
+      expect(state.restaurants[1].category).toBe('분식');
+      expect(state.restaurants[1].address).toBe('강남');
     });
   });
 });
