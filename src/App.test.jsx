4f5c5467db6167a7bf82f49ce14af1fc192925be
naraekn <34229459+naraekn@users.noreply.github.com>
2020-06-27 09:07:00 +0900

    Add test for App
    Test if title and form are rendered

---
+++ src/App.test.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import App from './App';
+
+test('App', () => {
+  const { getByText, getByPlaceholderText } = render((
+    <App />
+  ));
+
+  expect(getByText(/Restaurants/)).not.toBeNull();
+  expect(getByPlaceholderText(/이름/)).not.toBeNull();
+  expect(getByPlaceholderText(/분류/)).not.toBeNull();
+  expect(getByPlaceholderText(/주소/)).not.toBeNull();
+  expect(getByText(/등록/)).not.toBeNull();
+});
