a07e48b3a09c01797d473fd17b7de9922789b9da
jiunkoo <jiunkoo0106@gmail.com>
2020-06-28 03:53:43 +0900

    Item - Item test 구현

--- src/Item.jsx
+++ src/Item.jsx
@@ -1,4 +1,9 @@
 import React from 'react';
 
-export default function Item(){
+export default function Item({ restaurant: { name, category, address } }){
+  return (
+    <li>
+      {name}|{category}|{address}
+    </li>
+  );
 }
