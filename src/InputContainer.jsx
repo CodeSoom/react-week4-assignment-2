548dab2ef06c05f4abfa95b8368b646c2dac3771
jiunkoo <jiunkoo0106@gmail.com>
2020-06-28 13:51:48 +0900

    InputContainer - handleChangeRestaurant 테스트 구현

--- src/InputContainer.jsx
+++ src/InputContainer.jsx
@@ -1,2 +1,39 @@
+import React from 'react';
+
+import { useSelector, useDispatch } from 'react-redux';
+
+import Input from './Input';
+
+import {
+  updateRestaurantName,
+  updateRestaurantCategory,
+  updateRestaurantAddress,
+} from './actions';
+
 export default function InputContainer() {
+  const { restaurant } = useSelector((state) => ({
+    restaurant: state.restaurant,
+  }));
+  const dispatch = useDispatch();
+
+  function handleChangeRestaurantName(event) {
+    dispatch(updateRestaurantName(event.target.value));
+  }
+
+  function handleChangeRestaurantCategory(event) {
+    dispatch(updateRestaurantCategory(event.target.value));
+  }
+
+  function handleChangeRestaurantAddress(event) {
+    dispatch(updateRestaurantAddress(event.target.value));
+  }
+
+  return (
+    <Input
+      value={restaurant}
+      onChangeName={handleChangeRestaurantName}
+      onChangeCategory={handleChangeRestaurantCategory}
+      onChangeAddress={handleChangeRestaurantAddress}
+    />
+  );
 }
