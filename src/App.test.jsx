dbf05b6fdafaba6028da00ea8903d70e38cddbf6
Kwakcena <rhkrgudwh@gmail.com>
2020-06-28 18:41:31 +0900

    [RED] App component 리덕스 사용 테스트
    이제 리덕스를 사용하게 되었으므로 레스토랑 목록을 테스트에서 바꿔 볼 수
    있다.

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,22 +2,27 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import { useSelector } from 'react-redux';
+
 import App from './App';
+
+jest.mock('react-redux');
 
 describe('<App />', () => {
   context('레스토랑 목록이 있으면', () => {
     it('화면에 보여준다.', () => {
       const restaurants = [
         {
-          id: 1, title: '마녀주방', kind: '한식', address: '서울시 강남구',
+          id: 1, title: '시카고피자', kind: '양식', address: '이태원동',
         },
         {
-          id: 2, title: '홍콩반점', kind: '중식', address: '인천 주안',
-        },
-        {
-          id: 3, title: '스시천', kind: '일식', address: '인천 남동구',
+          id: 2, title: '키와미', kind: '일식', address: '분당구',
         },
       ];
+
+      useSelector.mockImplementation((selector) => selector({
+        restaurants,
+      }));
 
       const { getByText } = render(<App />);
       restaurants.forEach(({ title, kind, address }) => {
