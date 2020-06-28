eba663ad2680317bbe819b6d93bff027a1a5ae9b
Kwakcena <rhkrgudwh@gmail.com>
2020-06-28 16:52:14 +0900

    [RED] App 테스트. 레스토랑 목록이 있으면 보여주기
    레스토랑 목록이 있으면 화면에 보여주도록 한다.

---
+++ src/App.test.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import App from './App';
+
+describe('<App />', () => {
+  context('레스토랑 목록이 있으면', () => {
+    it('화면에 보여준다.', () => {
+      const restaurants = [
+        {
+          id: 1, title: '마녀주방', kind: '한식', address: '서울시 강남구',
+        },
+        {
+          id: 2, title: '홍콩반점', kind: '중식', address: '인천 주안',
+        },
+        {
+          id: 3, title: '스시천', kind: '일식', address: '인천 남동구',
+        },
+      ];
+
+      const { getByText } = render(<App />);
+      restaurants.forEach(({ title, kind, address }) => {
+        expect(getByText(`${title} | ${kind} | ${address}`));
+      });
+    });
+  });
+});
