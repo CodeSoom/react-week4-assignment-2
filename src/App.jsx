b9ba5973271fbcfd77fc9576b4fc761d65af68dd
jiunkoo <jiunkoo0106@gmail.com>
2020-06-28 14:07:05 +0900

    App - 기존 코드 제거, 통합 테스트 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,106 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import Input from './Input';
-import List from './List';
-
-const initialState = {
-  id: 1,
-  restaurant: {
-    name: '',
-    category: '',
-    address: '',
-  },
-  reservations: [],
-};
-
-function updateRestaurantName(state, value) {
-  const { restaurant } = state;
-
-  return {
-    ...state,
-    restaurant: {
-      name: value,
-      category: restaurant.category,
-      address: restaurant.address,
-    },
-  };
-}
-
-function updateRestaurantCategory(state, value) {
-  const { restaurant } = state;
-
-  return {
-    ...state,
-    restaurant: {
-      name: restaurant.name,
-      category: value,
-      address: restaurant.address,
-    },
-  };
-}
-
-function updateRestaurantAddress(state, value) {
-  const { restaurant } = state;
-
-  return {
-    ...state,
-    restaurant: {
-      name: restaurant.name,
-      category: restaurant.category,
-      address: value,
-    },
-  };
-}
-
-function registerReservation(state) {
-  const { id, restaurant, reservations } = state;
-
-  return {
-    ...state,
-    id: id + 1,
-    restaurant: {
-      name: '',
-      category: '',
-      address: '',
-    },
-    reservations: [...reservations, { id, restaurant }],
-  };
-}
+import InputContainer from './InputContainer';
+import ListContainer from './ListContainer';
 
 export default function App() {
-  const [state, setState] = useState(initialState);
-
-  const { restaurant, reservations } = state;
-
-  function handleChangeRestaurantName(event) {
-    setState(updateRestaurantName(state, event.target.value));
-  }
-
-  function handleChangeRestaurantCategory(event) {
-    setState(updateRestaurantCategory(state, event.target.value));
-  }
-
-  function handleChangeRestaurantAddress(event) {
-    setState(updateRestaurantAddress(state, event.target.value));
-  }
-
-  function handleClickRegisterReservation() {
-    setState(registerReservation(state));
-  }
-
   return (
     <div>
       <h1>Restaurants</h1>
-      <List
-        reservations={reservations}
-      />
-      <Input
-        value={restaurant}
-        onChangeName={handleChangeRestaurantName}
-        onChangeCategory={handleChangeRestaurantCategory}
-        onChangeAddress={handleChangeRestaurantAddress}
-        onClick={handleClickRegisterReservation}
-      />
+      <ListContainer />
+      <InputContainer />
     </div>
   );
 }
