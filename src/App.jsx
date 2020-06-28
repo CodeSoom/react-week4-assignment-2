ffee81c86608f5d0da538f494d6d0d4c701bb733
Kwakcena <rhkrgudwh@gmail.com>
2020-06-28 17:01:43 +0900

    [GREEN] 레스토랑 목록을 보여주기 위한 테스트 통과
    하드코딩으로 통과

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,12 @@ import React from 'react';
 export default function App() {
   return (
     <div>
-      Hello
+      <h1>Restaurant</h1>
+      <ul>
+        <li>마녀주방 | 한식 | 서울시 강남구</li>
+        <li>홍콩반점 | 중식 | 인천 주안</li>
+        <li>스시천 | 일식 | 인천 남동구</li>
+      </ul>
     </div>
   );
 }
