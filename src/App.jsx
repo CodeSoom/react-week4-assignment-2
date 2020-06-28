acf435897fa687119a3454f0a1a9fda204473bb9
Kwakcena <rhkrgudwh@gmail.com>
2020-06-28 18:43:12 +0900

    [GREEN] ListContainer를 사용하여 App 테스트 통과
    전체 레스토랑 목록을 보여주는 통합 테스트를 통과시키도록 ListContainer
    컴포넌트를 사용한다.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 
+import ListContainer from './ListContainer';
+
 export default function App() {
   return (
     <div>
       <h1>Restaurant</h1>
-      <ul>
-        <li>마녀주방 | 한식 | 서울시 강남구</li>
-        <li>홍콩반점 | 중식 | 인천 주안</li>
-        <li>스시천 | 일식 | 인천 남동구</li>
-      </ul>
+      <ListContainer />
     </div>
   );
 }
