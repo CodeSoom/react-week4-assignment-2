5e08b5c5d320ae0ab9b758cf18581addb1dabe3f
Joylish <yeonzzu2@gmail.com>
2020-06-28 19:34:46 +0900

    create RestaurantListContainer to manage data by redux

--- src/RestaurantListContainer.jsx
+++ src/RestaurantListContainer.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
+import { useSelector } from 'react-redux';
+
 import RestaurantList from './RestaurantList';
 
 // RestaurantList 데이터를 관리하기 위해서 필요
 export default function RestaurantListContainer() {
-  const restaurantList = {
-    id: 1,
-    name: '호식당',
-    category: '일식',
-    address: '경기도 수정구 태평동',
-  };
+  const { restaurantList } = useSelector((state) => {
+    state.restaurantList;
+  });
+
   return (
     <RestaurantList restaurantList={restaurantList} />
   );
