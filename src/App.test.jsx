da70fbe0f25dc4a783c0d2eda8a05b413ac6a225
Joylish <yeonzzu2@gmail.com>
2020-06-28 18:39:56 +0900

    create inital App test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -4,10 +4,10 @@ import {render} from '@testing-library/react'
 
 import App from './App'
 
-// 화면에 무엇이 있을 것이냐
 test('App', ()=>{
-    const {getByText} = render({
-        
-    })
-})
+    const {getByText} = render((
+        <App />
+    ));
 
+    expect(getByText(/호식당/)).not.toBeNull();
+})
