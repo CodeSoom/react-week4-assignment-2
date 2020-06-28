8785f251550656ebae748f1ceb706406f1efec04
jiunkoo <jiunkoo0106@gmail.com>
2020-06-28 14:05:45 +0900

    App - 기존 테스트 제거, 통합 테스트 추가

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,70 +2,161 @@ import React from 'react';
 
 import { render, fireEvent } from '@testing-library/react';
 
+import { useSelector, useDispatch } from 'react-redux';
+
 import App from './App';
 
+import {
+  updateRestaurantName,
+  updateRestaurantCategory,
+  updateRestaurantAddress,
+  registerReservation,
+} from './actions';
+
 describe('App', () => {
-  test('handleChangeRestaurant', () => {
-    const restaurant = {
-      name: '서브웨이',
-      category: '양식',
-      address: '서울시 도봉구',
-    };
+  test('handleChangeRestaurantName', () => {
+    const dispatch = jest.fn();
+
+    useSelector.mockImplementation((selector) => selector({
+      restaurant: {
+        name: '',
+        category: '',
+        address: '',
+      },
+      reservations: [],
+    }));
+    useDispatch.mockImplementation(() => dispatch);
+
     const { getByPlaceholderText } = render(
       <App />,
     );
 
     fireEvent.change(
       getByPlaceholderText('이름'),
-      { target: { value: restaurant.name } },
-    );
-    fireEvent.change(
-      getByPlaceholderText('분류'),
-      { target: { value: restaurant.category } },
-    );
-    fireEvent.change(
-      getByPlaceholderText('주소'),
-      { target: { value: restaurant.address } },
+      { target: { value: 'Handle Change Restaurant Name' } },
     );
 
-    expect(getByPlaceholderText('이름')).toHaveAttribute('value', restaurant.name);
-    expect(getByPlaceholderText('분류')).toHaveAttribute('value', restaurant.category);
-    expect(getByPlaceholderText('주소')).toHaveAttribute('value', restaurant.address);
+    expect(dispatch).toHaveBeenCalledTimes(1);
+    expect(dispatch).toBeCalledWith(
+      updateRestaurantName('Handle Change Restaurant Name'),
+    );
   });
 
-  test('handleClickRegisterReservation', () => {
-    const restaurant = {
-      name: '서브웨이',
-      category: '양식',
-      address: '서울시 도봉구',
-    };
-    const { container, getByPlaceholderText, getByText } = render(
+  test('handleChangeRestaurantCategory', () => {
+    const dispatch = jest.fn();
+
+    useSelector.mockImplementation((selector) => selector({
+      restaurant: {
+        name: '',
+        category: '',
+        address: '',
+      },
+      reservations: [],
+    }));
+    useDispatch.mockImplementation(() => dispatch);
+
+    const { getByPlaceholderText } = render(
       <App />,
     );
 
     fireEvent.change(
-      getByPlaceholderText('이름'),
-      { target: { value: restaurant.name } },
-    );
-    fireEvent.change(
       getByPlaceholderText('분류'),
-      { target: { value: restaurant.category } },
-    );
-    fireEvent.change(
-      getByPlaceholderText('주소'),
-      { target: { value: restaurant.address } },
+      { target: { value: 'Handle Change Restaurant Category' } },
     );
 
-    expect(getByPlaceholderText('이름')).toHaveAttribute('value', restaurant.name);
-    expect(getByPlaceholderText('분류')).toHaveAttribute('value', restaurant.category);
-    expect(getByPlaceholderText('주소')).toHaveAttribute('value', restaurant.address);
+    expect(dispatch).toHaveBeenCalledTimes(1);
+    expect(dispatch).toBeCalledWith(
+      updateRestaurantCategory('Handle Change Restaurant Category'),
+    );
+  });
+
+  test('handleChangeRestaurantAddress', () => {
+    const dispatch = jest.fn();
+
+    useSelector.mockImplementation((selector) => selector({
+      restaurant: {
+        name: '',
+        category: '',
+        address: '',
+      },
+      reservations: [],
+    }));
+    useDispatch.mockImplementation(() => dispatch);
+
+    const { getByPlaceholderText } = render(
+      <App />,
+    );
+
+    fireEvent.change(
+      getByPlaceholderText('주소'),
+      { target: { value: 'Handle Change Restaurant Address' } },
+    );
+
+    expect(dispatch).toHaveBeenCalledTimes(1);
+    expect(dispatch).toBeCalledWith(
+      updateRestaurantAddress('Handle Change Restaurant Address'),
+    );
+  });
+
+  test('registerReservation', () => {
+    const dispatch = jest.fn();
+
+    useSelector.mockImplementation((selector) => selector({
+      id: 1,
+      restaurant: {
+        name: '',
+        category: '',
+        address: '',
+      },
+      reservations: [],
+    }));
+    useDispatch.mockImplementation(() => dispatch);
+
+    const { getByText } = render(
+      <App />,
+    );
 
     fireEvent.click(getByText('등록'));
 
-    expect(getByPlaceholderText('이름')).toHaveAttribute('value', '');
-    expect(getByPlaceholderText('분류')).toHaveAttribute('value', '');
-    expect(getByPlaceholderText('주소')).toHaveAttribute('value', '');
+    expect(dispatch).toHaveBeenCalledTimes(1);
+    expect(dispatch).toBeCalledWith(registerReservation());
+  });
 
-    expect(container).toHaveTextContent('서브웨이|양식|서울시 도봉구');
+  test('reservations', () => {
+    const restaurant = {
+      name: '',
+      category: '',
+      address: '',
+    };
+    const reservations = [
+      {
+        id: 1,
+        restaurant: {
+          name: '지코바',
+          category: '한식',
+          address: '서울시 강남구',
+        },
+      },
+      {
+        id: 2,
+        restaurant: {
+          name: '아웃백',
+          category: '양식',
+          address: '서울시 종로구',
+        },
+      },
+    ];
+
+    useSelector.mockImplementation((selector) => selector({
+      restaurant,
+      reservations,
+    }));
+
+    const { container } = render(
+      <App />,
+    );
+
+    expect(container).toHaveTextContent('지코바|한식|서울시 강남구');
+    expect(container).toHaveTextContent('아웃백|양식|서울시 종로구');
   });
 });
