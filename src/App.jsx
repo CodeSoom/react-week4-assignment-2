4a107065681bf593cf98a9a5644c867469127008
Joylish <yeonzzu2@gmail.com>
2020-06-28 18:40:20 +0900

    create inital App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
 
 export default function App() {
-
+  return (
+    <div>
+      <h1>Restaurants</h1>
+      <ul>
+        <li>
+        호식당
+        </li>
+      </ul>    
+    </div>
+  )
 }
