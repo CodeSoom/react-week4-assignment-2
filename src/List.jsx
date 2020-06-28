a436f9ac9674d97c5be0c335f45326fdf75da353
KiChang Shin <kc7851@gmail.com>
2020-06-28 12:47:20 +0900

    List test 구현

---
+++ src/List.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function List() {
+  return (
+    <div>
+      <ul>
+        <li>한식당 | 한식 | 강남구</li>
+        <li>중식당 | 중식 | 송파구</li>
+      </ul>
+    </div>
+  );
+}
--- src/Page.jsx
+++ src/Page.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import List from './List';
 import InputContainer from './InputContainer';
 
 export default function Page() {
   return (
     <div>
       <h1>Restaurants</h1>
+      <List />
       <InputContainer />
     </div>
   );
