6226f8727486d701edb1440a91b4b1fb52575127
jiunkoo <jiunkoo0106@gmail.com>
2020-06-28 15:06:17 +0900

    App - useEffect 활용해서 초기값 지정

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import { useDispatch } from 'react-redux';
 
 import InputContainer from './InputContainer';
 import ListContainer from './ListContainer';
 
+import {
+  setReservations,
+} from './actions';
+
+import reservations from '../data/reservations';
+
 export default function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setReservations(reservations))
+  }, []);
+
   return (
     <div>
       <h1>Restaurants</h1>
