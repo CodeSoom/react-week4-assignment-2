7c697d40d0daba4cbc5e1f38478e71e664a988ca
Kwakcena <rhkrgudwh@gmail.com>
2020-06-28 19:05:04 +0900

    [RED] reducer 테스트 - 이름 변경 테스트 추가
    Input 컴포넌트에서 이름 변경 기능을 추가하기 위해 테스트를 추가한다.

--- src/reducer.test.jsx
+++ src/reducer.test.jsx
@@ -11,4 +11,21 @@ describe('reducer', () => {
       expect(newState).toEqual(previousState);
     });
   });
+
+  context('이름을 입력하면', () => {
+    it('이름이 바뀐 상태를 받는다.', () => {
+      const previousState = {
+        title: '',
+        restaurants: [],
+      };
+      const newState = reducer(previousState, {
+        type: 'updateTitle',
+        payload: {
+          title: '마녀주방',
+        },
+      });
+
+      expect(newState.title).toBe('마녀주방');
+    });
+  });
 });
