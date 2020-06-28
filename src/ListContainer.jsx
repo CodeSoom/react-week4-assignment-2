ef5c344d08b3e4823118e1fe9b4baaf61075fe68
jiunkoo <jiunkoo0106@gmail.com>
2020-06-28 13:23:39 +0900

    ListContainer - 테스트 구현

---
+++ src/ListContainer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { useSelector } from 'react-redux';
+
+import List from './List';
+
+export default function ListContainer() {
+  const { reservations } = useSelector((state) => ({
+    reservations: state.reservations,
+  }));
+
+  return (
+    <List
+      reservations={reservations}
+    />
+  );
+}
