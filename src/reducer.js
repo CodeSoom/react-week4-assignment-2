d3e2d2e67cc03c349e3191c41aab2fca1e22a3f0
YONGJAE LEE <yongjae.lee@nanocellect.com>
2020-06-27 17:12:10 -0700

    implement: reducer - append a new restaruant into restaurants

--- src/reducer.js
+++ src/reducer.js
@@ -1,11 +1,33 @@
+import { bindActionCreators } from "redux";
+
 const initialState = {
   newId: 100,
-  name: '',
-  category: '',
-  address: '',
+  restaurantName: '',
+  restaurantCategory: '',
+  restaurantAddress: '',
   restaurants: [],
 };
 
 export default function reducer(state = initialState, action) {
+  if (action.type === 'addRestaurant') {
+    const {
+      newId, restaurantName, restaurantCategory, restaurantAddress, restaurants,
+    } = state;
+
+    if (!restaurantName || !restaurantCategory || !restaurantAddress || !restaurants) {
+      return state;
+    }
+
+    return {
+      ...state,
+      newId: newId + 1,
+      restaurantName: '',
+      restaurantCategory: '',
+      restaurantAddress: '',
+      restaurants: [...restaurants, {
+        id: newId, name: restaurantName, category: restaurantCategory, address: restaurantAddress,
+      }],
+    };
+  }
   return state;
 }
