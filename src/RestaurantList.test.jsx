ca760b504cd9f1d658457740606997cf8e34a20c
daadaadaah <daadaadaah@gmail.com>
2020-06-28 23:01:54 +0900

    Update a RestaurantList test

--- src/RestaurantList.test.jsx
+++ src/RestaurantList.test.jsx
@@ -1,58 +1,38 @@
 import React from 'react';
 
-import { useSelector, useDispatch } from 'react-redux';
-
 import { render } from '@testing-library/react';
 
 import RestaurantList from './RestaurantList';
 
 import restaurants from '../__fixture__/data';
 
-jest.mock('react-redux');
-
-function renderRestaurantList() {
-  return render(<RestaurantList />);
-}
-
 describe('<RestaurantList />', () => {
   describe('Initial Rendering', () => {
     context('with restaurants', () => {
       it('shows restaurants', () => {
-        useSelector.mockImplementation((selector) => selector({
-          restaurants,
-          restaurant: {},
-        }));
+        const { queryByText } = render(<RestaurantList
+          restaurants={restaurants}
+        />);
 
-        const dispatch = jest.fn();
-        useDispatch.mockImplementation(() => dispatch);
+        expect(queryByText(/레스토랑1/i)).toBeInTheDocument();
+        expect(queryByText(/분류1/i)).toBeInTheDocument();
+        expect(queryByText(/주소1/i)).toBeInTheDocument();
 
-        const { getByText } = renderRestaurantList();
+        expect(queryByText(/레스토랑2/i)).toBeInTheDocument();
+        expect(queryByText(/분류2/i)).toBeInTheDocument();
+        expect(queryByText(/주소2/i)).toBeInTheDocument();
 
-        expect(getByText(/레스토랑1/i)).toBeInTheDocument();
-        expect(getByText(/분류1/i)).toBeInTheDocument();
-        expect(getByText(/주소1/i)).toBeInTheDocument();
-
-        expect(getByText(/레스토랑2/i)).toBeInTheDocument();
-        expect(getByText(/분류2/i)).toBeInTheDocument();
-        expect(getByText(/주소2/i)).toBeInTheDocument();
-
-        expect(getByText(/레스토랑3/i)).toBeInTheDocument();
-        expect(getByText(/분류3/i)).toBeInTheDocument();
-        expect(getByText(/주소3/i)).toBeInTheDocument();
+        expect(queryByText(/레스토랑3/i)).toBeInTheDocument();
+        expect(queryByText(/분류3/i)).toBeInTheDocument();
+        expect(queryByText(/주소3/i)).toBeInTheDocument();
       });
     });
 
     context('without restaurants', () => {
       it('shows nothing', () => {
-        const dispatch = jest.fn();
-        useDispatch.mockImplementation(() => dispatch);
-
-        useSelector.mockImplementation((selector) => selector({
-          restaurants: [],
-          restaurant: {},
-        }));
-
-        const { container } = renderRestaurantList();
+        const { container } = render(<RestaurantList
+          restaurants={[]}
+        />);
 
         expect(container).not.toHaveTextContent(/레스토랑1/i);
         expect(container).not.toHaveTextContent(/분류1/i);
