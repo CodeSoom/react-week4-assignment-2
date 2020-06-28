95cd4b4973a7c0289761a8e23cc794f5f7539e42
naraekn <34229459+naraekn@users.noreply.github.com>
2020-06-28 19:57:18 +0900

    Add useEffect to set initial restaurants

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import { useDispatch } from 'react-redux';
 
 import RestaurantsContainer from './RestaurantsContainer';
 import RestaurantFormContainer from './RestaurantFormContainer';
 
+import { setInitialRestaurants } from './actions';
+
 export default function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setInitialRestaurants([]));
+  }, []);
+
   return (
     <div>
       <h1>Restaurants</h1>
