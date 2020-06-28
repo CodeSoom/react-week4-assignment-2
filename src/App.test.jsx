20fc6147222d7e71592b84b0638cce3be88dc8ec
Law Woo <law@cupist.com>
2020-06-28 12:12:07 +0900

    Update App.test.jsx

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -9,30 +9,34 @@ import App from './App';
 jest.mock('react-redux');
 
 describe('App', () => {
-  const testRestaurants = [
-    {
-      id: 1, name: '버튼업', category: '경양식', address: '마포구',
-    },
-    {
-      id: 2, name: '스시로로', category: '일식', address: '동작구',
-    },
-  ];
+  context('with restaurant infomations', () => {
+    it('renders restaurant list', () => {
+      const testRestaurants = [
+        {
+          id: 1, name: '버튼업', category: '경양식', address: '마포구',
+        },
+        {
+          id: 2, name: '스시로로', category: '일식', address: '동작구',
+        },
+      ];
 
-  useSelector.mockImplementation((selector) => selector({
-    restaurant: {
-      name: '',
-      category: '',
-      address: '',
-    },
-    restaurants: testRestaurants,
-  }));
+      useSelector.mockImplementation((selector) => selector({
+        restaurant: {
+          name: '',
+          category: '',
+          address: '',
+        },
+        restaurants: testRestaurants,
+      }));
 
-  const { getByText } = render((
-    <App />
-  ));
+      const { getByText } = render((
+        <App />
+      ));
 
-  expect(getByText(/등록/)).toBeInTheDocument();
-  expect(
-    getByText(`${testRestaurants[0].name} | ${testRestaurants[0].category} | ${testRestaurants[0].address}`),
-  ).toBeInTheDocument();
+      expect(getByText(/등록/)).toBeInTheDocument();
+      expect(
+        getByText(`${testRestaurants[0].name} | ${testRestaurants[0].category} | ${testRestaurants[0].address}`),
+      ).toBeInTheDocument();
+    });
+  });
 });
