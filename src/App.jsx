6745b54ec11df3c9586a7c27f168089960adc456
Kwakcena <rhkrgudwh@gmail.com>
2020-06-28 20:12:21 +0900

    [Refactor] InputContaner 사용해보기
    현재는 title만 추가되도록 되어 있지만 잘 된다.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import ListContainer from './ListContainer';
+import InputContainer from './InputContainer';
 
 export default function App() {
   return (
     <div>
       <h1>Restaurant</h1>
       <ListContainer />
+      <InputContainer />
     </div>
   );
 }
