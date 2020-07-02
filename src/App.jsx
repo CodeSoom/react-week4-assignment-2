8d0d7760625f3997922024dc265f1b53e3132fff
Paul An <anpaul0615@gmail.com>
2020-07-02 22:58:04 +0900

    App 컴포넌트 리팩토링

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,14 @@
 import React from 'react';
 
-import { useSelector, useDispatch } from 'react-redux';
-import {
-  updateRestaurantInfo, addRestaurant,
-} from './stores/action/action-creators';
-
 import RestaurantsListContainer from './containers/RestaurantsListContainer';
-import InputBox from './components/InputBox';
-import AddButton from './components/AddButton';
+import RestaurantsInputContainer from './containers/RestaurantsInputContainer';
 
 export default function App() {
-  const { restaurant } = useSelector((state) => ({
-    restaurant: state.restaurant,
-  }));
-
-  const dispatch = useDispatch();
-
-  function handleUpdateRestaurantInfo(ev) {
-    dispatch(updateRestaurantInfo(ev.target.name, ev.target.value));
-  }
-
-  function handleAddRestaurant() {
-    dispatch(addRestaurant());
-  }
-
   return (
     <div>
       <h1>Restaurants</h1>
       <RestaurantsListContainer />
-      <InputBox inputProperty="name" inputValue={restaurant.name} handleChangeInputValue={handleUpdateRestaurantInfo} />
-      <InputBox inputProperty="category" inputValue={restaurant.category} handleChangeInputValue={handleUpdateRestaurantInfo} />
-      <InputBox inputProperty="address" inputValue={restaurant.address} handleChangeInputValue={handleUpdateRestaurantInfo} />
-      <AddButton handleClickButton={handleAddRestaurant} />
+      <RestaurantsInputContainer />
     </div>
   );
 }
