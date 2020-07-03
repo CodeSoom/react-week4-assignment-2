906b3389f18a1d47cd0a04d3f156e9295d2f5563
Joylish <yeonzzu2@gmail.com>
2020-07-03 21:33:33 +0900

    call restaurantList data in fixtures and add mock for react-redux in App.test.jsx

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,9 +2,19 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import { useSelector } from 'react-redux';
+
 import App from './App';
 
+import restaurantList from '../fixtures/restaurantList';
+
+jest.mock('react-redux');
+
 test('App', () => {
+  useSelector.mockImplementation((selector) => selector({
+    restaurantList,
+  }));
+
   const { getByText } = render((
     <App />
   ));
