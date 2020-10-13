6a6027553551577b74b1004561bd6eb1b38b43cf
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-13 19:53:55 +0900

    [red] Create test for ListContainer.

---
+++ src/ListContainer.test.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import { useSelector } from 'react-redux';
+
+import ListContainer from './ListContainer';
+
+jest.mock('react-redux');
+
+test('ListContainer', () => {
+  const restaurants = [
+    { name: '마녀주방', category: '한식', address: '서울시 강남구' },
+    { name: '시카고피자', category: '양식', address: '이태원동' },
+  ];
+
+  useSelector.mockImplementation(() => ({
+    restaurants,
+  }));
+
+  const { getByText } = render(
+    <ListContainer />,
+  );
+
+  restaurants.forEach((item) => {
+    expect(getByText(`${item.name} | ${item.category} | ${item.address}`)).not.toBeNull();
+  });
+});
