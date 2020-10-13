3052d3d7075645f32142ccaeb6013721e468f23c
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-13 18:54:09 +0900

    [green] Add passing test into List test.
    Add a test of the situation when there's no restaurant.

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -4,16 +4,32 @@ import { render } from '@testing-library/react';
 
 import List from './List';
 
-test('List', () => {
-  const restaurants = [
-    { name: '마녀주방', category: '한식', address: '서울시 강남구' },
-    { name: '시카고피자', category: '양식', address: '이태원동' },
-  ];
+describe('List', () => {
+  context('with restaurants', () => {
+    it('renders restaurants', () => {
+      const restaurants = [
+        { name: '마녀주방', category: '한식', address: '서울시 강남구' },
+        { name: '시카고피자', category: '양식', address: '이태원동' },
+      ];
 
-  const { getByText } = render(
-    <List restaurants={restaurants} />,
-  );
+      const { getByText } = render(
+        <List restaurants={restaurants} />,
+      );
 
-  expect(getByText('마녀주방 | 한식 | 서울시 강남구')).not.toBeNull();
-  expect(getByText('시카고피자 | 양식 | 이태원동')).not.toBeNull();
+      expect(getByText('마녀주방 | 한식 | 서울시 강남구')).not.toBeNull();
+      expect(getByText('시카고피자 | 양식 | 이태원동')).not.toBeNull();
+    });
+  });
+
+  context('without restaurants', () => {
+    it('renders nothing', () => {
+      const restaurants = [];
+
+      const { getByRole } = render(
+        <List restaurants={restaurants} />,
+      );
+
+      expect(getByRole('list')).toBeEmptyDOMElement();
+    });
+  });
 });
