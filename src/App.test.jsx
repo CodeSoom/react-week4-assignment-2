96369681703fb7017bf4b64bd02c7de77a49f257
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-13 21:37:21 +0900

    [refactor] Apply ListContainer to App.
    Apply ListContainer component to App component and add mocks to App
    component to test App compontent.

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,9 +2,22 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import { useSelector } from 'react-redux';
+
 import App from './App';
 
+jest.mock('react-redux');
+
 test('App', () => {
+  const restaurants = [
+    { name: '마녀주방', category: '한식', address: '서울시 강남구' },
+    { name: '시카고피자', category: '양식', address: '이태원동' },
+  ];
+
+  useSelector.mockImplementation(() => ({
+    restaurants,
+  }));
+
   const { getByText, getByPlaceholderText } = render(
     <App />,
   );
