6f70b4f59d4c2729fae495ffb72559e6ff14b345
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-13 18:13:23 +0900

    [red] Create test for List.jsx

---
+++ src/List.test.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import List from './List';
+
+test('List', () => {
+  const restaurants = [
+    { name: '마녀주방', category: '한식', address: '서울시 강남구' },
+    { name: '시카고피자', category: '양식', address: '이태원동' },
+  ];
+
+  const { getByText } = render(
+    <List restaurants={restaurants} />,
+  );
+
+  expect(getByText('마녀주방 | 한식 | 서울시 강남구')).not.toBeNull();
+  expect(getByText('시카고피자 | 양식 | 이태원동')).not.toBeNull();
+});
