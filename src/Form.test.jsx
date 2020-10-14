fd8a22f32ea7ae0a96b9392ae012c09348f36959
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-14 21:10:52 +0900

    [red] Add test for "완료" button in Form.

--- src/Form.test.jsx
+++ src/Form.test.jsx
@@ -4,21 +4,32 @@ import { render } from '@testing-library/react';
 
 import Form from './Form';
 
-test('Form', () => {
-  const inputs = [
-    { placehoder: '이름', value: '마녀주방' },
-    { placehoder: '분류', value: '한식' },
-    { placehoder: '주소', value: '서울시 강남구' },
-  ];
-  const { getAllByRole, getByPlaceholderText } = render((
-    <Form />
-  ));
+describe('Form', () => {
+  it('renders 3 inputs with different placeholder ', () => {
+    const inputs = [
+      { placehoder: '이름', value: '마녀주방' },
+      { placehoder: '분류', value: '한식' },
+      { placehoder: '주소', value: '서울시 강남구' },
+    ];
 
-  expect(getAllByRole('textbox')).toHaveLength(3);
+    const { getAllByRole, getByPlaceholderText } = render((
+      <Form />
+    ));
 
-  inputs.forEach((input) => {
-    expect(getByPlaceholderText(input.placehoder)).not.toBeNull();
+    expect(getAllByRole('textbox')).toHaveLength(3);
 
-    expect(getByPlaceholderText(input.placehoder)).toHaveDisplayValue(input.value);
+    inputs.forEach((input) => {
+      expect(getByPlaceholderText(input.placehoder)).not.toBeNull();
+
+      expect(getByPlaceholderText(input.placehoder)).toHaveDisplayValue(input.value);
+    });
+  });
+
+  it('renders "등록" button', () => {
+    const { container } = render((
+      <Form />
+    ));
+
+    expect(container).toHaveTextContent('등록');
   });
 });
