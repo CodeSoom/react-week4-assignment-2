12384ec0e69636343d9bce85ccbca7f074d22102
JamieShin0201 <jamieshin1450@gmail.com>
2020-10-14 14:57:21 +0900

    Implement App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,16 @@ import React from 'react';
 export default function App() {
   return (
     <div>
-      Hello
+      <h1>Restaurants</h1>
+      <ul>
+        <li>마녀주방 | 한식 | 서울시 강남구</li>
+      </ul>
+      <input type="text" placeholder="이름" />
+      <input type="text" placeholder="분류" />
+      <input type="text" placeholder="주소" />
+      <button type="button">
+        등록
+      </button>
     </div>
   );
 }
