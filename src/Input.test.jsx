b93553668b6b0e6b6df5b389abb77c04761eb313
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-14 17:24:56 +0900

    [red] Add placeholder test for Input.

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -5,7 +5,11 @@ import { render } from '@testing-library/react';
 import Input from './Input';
 
 test('Input', () => {
-  const { getByRole } = render((<Input />));
+  const placeholderText = '이름';
+
+  const { getByRole, getByPlaceholderText } = render((<Input placeholderText={placeholderText} />));
 
   expect(getByRole('textbox')).not.toBeNull();
+
+  expect(getByPlaceholderText(placeholderText)).not.toBeNull();
 });
