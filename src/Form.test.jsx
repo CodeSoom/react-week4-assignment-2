71379bf9acf0f8814850299613340028244ac86f
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-14 18:37:17 +0900

    [red] Add a test for placeholder verification in Form.

--- src/Form.test.jsx
+++ src/Form.test.jsx
@@ -5,9 +5,18 @@ import { render } from '@testing-library/react';
 import Form from './Form';
 
 test('Form', () => {
-  const { getAllByRole } = render((
+  const inputs = [
+    { placehoder: '이름' },
+    { placehoder: '분류' },
+    { placehoder: '주소' },
+  ];
+  const { getAllByRole, getByPlaceholderText } = render((
     <Form />
   ));
 
   expect(getAllByRole('textbox')).toHaveLength(3);
+
+  inputs.forEach((input) => {
+    expect(getByPlaceholderText(input.placehoder)).not.toBeNull();
+  });
 });
