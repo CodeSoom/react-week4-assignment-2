ce178ab6a6ad685ccd46d369338a85578e506e24
JamieShin0201 <jamieshin1450@gmail.com>
2020-10-14 17:27:11 +0900

    Fix App.test.jsx

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,9 +2,26 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import { useSelector } from 'react-redux';
+
 import App from './App';
 
 test('App', () => {
+  useSelector.mockImplementation((selector) => selector({
+    restaurants: [
+      {
+        name: '마녀주방',
+        classification: '한식',
+        location: '서울시 강남구',
+      },
+      {
+        name: '시카고피자',
+        classification: '양식',
+        location: '이태원동',
+      },
+    ],
+  }));
+
   const { container, getByText } = render((
     <App />
   ));
