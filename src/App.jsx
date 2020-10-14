0f1e473f0658164e7713307d7eb469ea2686c401
JamieShin0201 <jamieshin1450@gmail.com>
2020-10-14 17:27:46 +0900

    Refactor App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
+import ListContainer from './ListContainer';
+
 export default function App() {
   return (
     <div>
       <h1>Restaurants</h1>
-      <ul>
-        <li>마녀주방 | 한식 | 서울시 강남구</li>
-      </ul>
+      <ListContainer />
       <input type="text" placeholder="이름" />
       <input type="text" placeholder="분류" />
       <input type="text" placeholder="주소" />
