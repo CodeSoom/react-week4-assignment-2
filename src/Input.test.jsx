7fbcb6bcea017573340fcb67731751766fa6c379
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-14 17:46:54 +0900

    [red] Add value test for Input.

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -6,10 +6,16 @@ import Input from './Input';
 
 test('Input', () => {
   const placeholderText = '이름';
+  const value = '마녀주방';
 
-  const { getByRole, getByPlaceholderText } = render((<Input placeholderText={placeholderText} />));
+  const { getByRole, getByPlaceholderText } = render((
+    <Input
+      placeholderText={placeholderText}
+      value={value}
+    />
+  ));
 
   expect(getByRole('textbox')).not.toBeNull();
 
-  expect(getByPlaceholderText(placeholderText)).not.toBeNull();
+  expect(getByPlaceholderText(placeholderText)).toHaveDisplayValue(value);
 });
