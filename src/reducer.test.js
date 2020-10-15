71184d0dad76ef5da754a92bda5c95d1a779ed74
gringrape <53764714+gringrape@users.noreply.github.com>
2020-10-15 18:56:44 +0900

    Add reducer test, addRestaurant case

---
+++ src/reducer.test.js
@@ -0,0 +1,23 @@
+import reducer from './reducer';
+
+describe('reducer', () => {
+  describe('addRestaurant', () => {
+    it('returns new state with new restaurant added to restaurants', () => {
+      const previousState = {
+        newId: 103,
+        restaurant: {
+          name: '키와미',
+          category: '일식',
+          address: '분당구 정자동',
+        },
+        restaurants: [],
+      };
+
+      const action = { type: 'addRestaurant' };
+
+      const newState = reducer(previousState, action);
+
+      expect(newState.restaurants).toContainEqual(previousState.restaurant);
+    });
+  });
+});
