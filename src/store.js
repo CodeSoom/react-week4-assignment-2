b3f734d94fc5dc108b8d18ef2db0ad75b22339e4
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-15 19:00:32 +0900

    [refactor] Apply reducer to store.

--- src/store.js
+++ src/store.js
@@ -1,13 +1,6 @@
 import { createStore } from 'redux';
 
-const initialState = { restaurants: [], inputs: [] };
-const initialType = { type: 'initialType' };
-
-function reducer(state = initialState, action = initialType) {
-  if (action.type === 'initialType') return state;
-
-  return state;
-}
+import reducer from './reducer';
 
 const store = createStore(reducer);
 
