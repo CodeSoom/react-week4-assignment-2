f6f84da4856f759c51d8d3b6d948b13c6b03e392
Woong <37302935+TaeWoongKim2@users.noreply.github.com>
2020-10-15 22:52:04 +0900

    ListContainer.jsx 생성
    현재는 레스토랑 목록만 보여주지만, 추후 삭제에 대한 상태관리 기능이 추가될 경우를 대비하여 생성.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import ListContainer from './ListContainer';
+
 export default function App() {
   return (
     <div>
       <h1>Restaurants</h1>
+      <ListContainer />
       <input type="text" placeholder="이름" />
       <input type="text" placeholder="분류" />
       <input type="text" placeholder="주소" />
