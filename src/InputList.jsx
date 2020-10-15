ac9b3f050768fd730b1cfaa72b4fb935327c867c
gringrape <53764714+gringrape@users.noreply.github.com>
2020-10-15 17:44:50 +0900

    Implement InputList

--- src/InputList.jsx
+++ src/InputList.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+const isBlank = (string) => string.trim().length === 0;
+
 export default function InputList({ restaurant, onChange, onClick }) {
   const placeholders = {
     name: '이름',
@@ -7,12 +9,14 @@ export default function InputList({ restaurant, onChange, onClick }) {
     address: '주소',
   };
 
+  const isAddable = Object.entries((restaurant)).every(([, value]) => !isBlank(value));
+
   return (
     <div>
       {Object.entries(restaurant).map(([key, value]) => (
         <input value={value} onChange={onChange(key)} placeholder={placeholders[key]} />
       ))}
-      <button type="button" onClick={onClick}>등록</button>
+      <button type="button" onClick={onClick} disabled={!isAddable}>등록</button>
     </div>
   );
 }
