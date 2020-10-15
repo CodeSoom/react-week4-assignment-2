bbda2e3d016e9eecfdee2b2c453c47ce12eee708
Dong A Kim <dongakim90@gmail.com>
2020-10-15 18:46:26 +0900

    Input 테스트 리팩토링

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -5,14 +5,14 @@ import { render } from '@testing-library/react';
 import Input from './Input';
 
 describe('<Input />', () => {
-  describe('renders', () => {
-    const renderInput = ({ placeholder }) => render((
-      <Input placeholder={placeholder} />
-    ));
+  const renderInput = () => render((
+    <Input placeholder="이름" />
+  ));
 
+  describe('renders', () => {
     it('placeholder', () => {
       // When
-      const { getByPlaceholderText } = renderInput({ placeholder: '이름' });
+      const { getByPlaceholderText } = renderInput();
 
       // Then
       expect(getByPlaceholderText('이름')).toBeInTheDocument();
