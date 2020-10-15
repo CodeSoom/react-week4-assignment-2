26c45b0c88ade8ae86ff0d48c262feae4441c893
saseungmin <dbd02169@naver.com>
2020-10-16 00:23:21 +0900

    App 컴포넌트 input handleChange 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Page from './Page';
 
 export default function App() {
+  const [state, setState] = useState({
+    restaurant: {
+      name: '',
+      type: '',
+      address: '',
+    },
+  });
+
+  function handleChangeTitle(event) {
+    const { value, name } = event.target;
+    setState({
+      ...state,
+      restaurant: {
+        ...state.restaurant,
+        [name]: value,
+      },
+    });
+  }
+
   return (
-    <div>
-      Restaurants
-    </div>
+    <>
+      <Page onChangeRestaurant={handleChangeTitle} />
+    </>
   );
 }
