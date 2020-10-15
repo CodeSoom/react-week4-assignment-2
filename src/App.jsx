982a1356897521ed751f38df225e8e509e6ddd7b
JamieShin0201 <jamieshin1450@gmail.com>
2020-10-15 17:27:59 +0900

    Refactor App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
 
 import ListContainer from './ListContainer';
+import FormContainer from './FormContainer';
 
 export default function App() {
   return (
     <div>
       <h1>Restaurants</h1>
       <ListContainer />
-      <input type="text" placeholder="이름" />
-      <input type="text" placeholder="분류" />
-      <input type="text" placeholder="주소" />
-      <button type="button">
-        등록
-      </button>
+      <FormContainer />
     </div>
   );
 }
