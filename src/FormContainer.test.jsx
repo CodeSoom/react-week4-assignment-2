944423b098e2507b3412d06bd4688df47b94c3e9
JamieShin0201 <jamieshin1450@gmail.com>
2020-10-15 16:34:55 +0900

    Add test FormContainer.test.jsx

--- src/FormContainer.test.jsx
+++ src/FormContainer.test.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import FormContainer from './FormContainer';
 
 jest.mock('react-redux');
 
 describe('FormContainer', () => {
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
+
   useSelector.mockImplementation((selector) => selector({
     restaurant: {
       name: '마녀주방',
@@ -28,13 +32,33 @@ describe('FormContainer', () => {
     const { getByDisplayValue } = renderFormContainer();
 
     expect(getByDisplayValue(/마녀주방/)).not.toBeNull();
-    expect(getByDisplayValue(/마녀주방/)).not.toBeNull();
-    expect(getByDisplayValue(/마녀주방/)).not.toBeNull();
+    expect(getByDisplayValue(/한식/)).not.toBeNull();
+    expect(getByDisplayValue(/서울시 강남구/)).not.toBeNull();
   });
 
   it('renders “등록” button to add task', () => {
     const { getByText } = renderFormContainer();
 
     expect(getByText(/등록/)).not.toBeNull();
+  });
+
+  describe('handleChangeRestaurant', () => {
+    it('dispatchs handleChangeRestaurant action', () => {
+      const { getByPlaceholderText } = renderFormContainer();
+
+      fireEvent.change(getByPlaceholderText('이름'), {
+        target: { value: '시카고피자' },
+      });
+
+      expect(dispatch).toBeCalledWith(
+        {
+          type: 'handleChangeRestaurant',
+          payload: {
+            placeholder: '이름',
+            value: '시카고피자',
+          },
+        },
+      );
+    });
   });
 });
