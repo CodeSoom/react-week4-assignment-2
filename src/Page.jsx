684a4f18d027e2e53bc7f36d29adc2970dd51435
gringrape <53764714+gringrape@users.noreply.github.com>
2020-10-15 14:42:44 +0900

    Implement Page

--- src/Page.jsx
+++ src/Page.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function Page({ restaurants, restaurant }) {
+export default function Page({ restaurants, restaurant, onClickAdd }) {
   return (
     <div>
       <h1>Restaurants</h1>
@@ -16,6 +16,7 @@ export default function Page({ restaurants, restaurant }) {
       <input type="text" value={restaurant.name} />
       <input type="text" value={restaurant.category} />
       <input type="text" value={restaurant.address} />
+      <button type="button" onClick={onClickAdd}>등록</button>
     </div>
   );
 }
