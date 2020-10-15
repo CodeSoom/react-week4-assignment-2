e77a2162209d4ca34271476d7289899b53d607ef
saseungmin <dbd02169@naver.com>
2020-10-15 23:18:52 +0900

    Input을 구현한다.

--- src/Input.jsx
+++ src/Input.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Input = () => (
-  <input type="text" placeholder="이름" />
+  <>
+    <input type="text" placeholder="이름" />
+    <input type="text" placeholder="분류" />
+    <input type="text" placeholder="주소" />
+  </>
 );
 
 export default Input;
