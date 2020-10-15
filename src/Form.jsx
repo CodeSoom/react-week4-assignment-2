255d567fe42af6e34947c887b0f2bf877090bbc0
JamieShin0201 <jamieshin1450@gmail.com>
2020-10-15 15:33:24 +0900

    Refactor Form.jsx

--- src/Form.jsx
+++ src/Form.jsx
@@ -2,22 +2,29 @@ import React from 'react';
 
 import Input from './Input';
 
-export default function Form({ onChange, onClick }) {
+export default function Form(
+  {
+    placeholders,
+    restaurant: { name, classification, location },
+    onChange,
+    onClick,
+  },
+) {
   return (
     <p>
       <Input
-        placeholder="이름"
-        value="마녀주방"
+        placeholder={placeholders[0]}
+        value={name}
         onChange={onChange}
       />
       <Input
-        placeholder="분류"
-        value="한식"
+        placeholder={placeholders[1]}
+        value={classification}
         onChange={onChange}
       />
       <Input
-        placeholder="주소"
-        value="서울시 강남구"
+        placeholder={placeholders[2]}
+        value={location}
         onChange={onChange}
       />
       <button type="button" onClick={onClick}>
