f0579a40a63d2a6b9fdf6d2078797ea0c5e90f28
saseungmin <dbd02169@naver.com>
2020-10-15 22:59:40 +0900

    Button에 대한 테스트 코드를 작성한다.
    - 버튼 클릭시 호출되는지 확인한다.

---
+++ src/Button.test.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { render, fireEvent } from '@testing-library/react';
+import Button from './Button';
+
+describe('Button', () => {
+  const handleClick = jest.fn();
+  const buttonRender = () => render((
+    <Button onClick={handleClick} />
+  ));
+
+  it('버튼 클릭시 호출되는지 확인한다.', () => {
+    const { getByText } = buttonRender();
+    const button = getByText(/등록/);
+
+    expect(button).not.toBeNull();
+
+    expect(handleClick).not.toBeCalled();
+
+    fireEvent.click(button);
+
+    expect(handleClick).toHaveBeenCalledTimes(1);
+  });
+});
