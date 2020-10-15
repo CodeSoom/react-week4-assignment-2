4168e7c21d48910791d232de3c1d95a3eec5a432
Woong <37302935+TaeWoongKim2@users.noreply.github.com>
2020-10-15 23:09:54 +0900

    FormContainer 구상
    FormContainer 및 Form, Input에 대한 *.jsx 컴포넌트 파일 생성.

---
+++ src/Form.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Input from './Input';
+
+export default function Form() {
+  return (
+    <div>
+      <Input />
+      <Input />
+      <Input />
+      <button type="button">등록</button>
+    </div>
+  );
+}
--- src/ListContainer.jsx
+++ src/ListContainer.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 import List from './List';
 
 export default function ListContainer() {
+  const restaurants = [];
   // 상태 변화에 대해 추가할 예정.
+
   return (
-    <div>
-      <List />
-    </div>
+    <List restaurants={restaurants} />
   );
 }
