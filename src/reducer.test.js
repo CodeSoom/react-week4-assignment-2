2e2fae51bf502f0876462392ada8c2b3c389d9a5
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-15 21:43:45 +0900

    [red] Add change updateInputText test for reducer.

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,19 +1,37 @@
 import reducer from './reducer';
 
 describe('reducer', () => {
+  const initalRestaurants = [];
+  const initalInputs = [
+    { placeholder: '이름', value: '' },
+    { placeholder: '분류', value: '' },
+    { placeholder: '주소', value: '' },
+  ];
+
   describe('undefined action', () => {
     it('changes nothing', () => {
-      const initalRestaurants = [];
-      const initalInputs =  [
-        { placeholder: '이름', value: '' },
-        { placeholder: '분류', value: '' },
-        { placeholder: '주소', value: '' },
-      ];
-
-      const {restaurants, inputs} = reducer();
+      const { restaurants, inputs } = reducer();
 
       expect(restaurants).toStrictEqual(initalRestaurants);
       expect(inputs).toStrictEqual(initalInputs);
+    });
+  });
+
+  describe('updateInputText', () => {
+    it('changes input texts', () => {
+      const newInputText = '마녀주방';
+      const payload = { ...initalInputs[0], value: newInputText };
+      const action = {
+        type: 'updateInputText',
+        payload,
+      };
+
+      const { inputs } = reducer({
+        restaurants: initalRestaurants,
+        inputs: initalInputs,
+      }, action);
+
+      expect(inputs[0].value).toBe('마녀주방');
     });
   });
 });
