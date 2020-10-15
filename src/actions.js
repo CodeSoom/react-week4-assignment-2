8c1e4c87aa28ee8f38609f3ff85ebf13db727bd2
Dong A Kim <dongakim90@gmail.com>
2020-10-15 20:19:08 +0900

    action 구현

--- src/actions.js
+++ src/actions.js
@@ -1,5 +1,10 @@
 // action types
 export const ADD_RESTAURANT = 'ADD_RESTAURANT';
+export const CHANGE_RESTAURANT_INPUT = {
+  NAME: 'CHANGE_RESTAURANT_INPUT_NAME',
+  TYPE: 'CHANGE_RESTAURANT_INPUT_TYPE',
+  PLACE: 'CHANGE_RESTAURANT_INPUT_PLACE',
+};
 
 // action creators
 function addRestaurant(value) {
@@ -9,6 +14,14 @@ function addRestaurant(value) {
   };
 }
 
+function changeRestaurantInput(type, value) {
+  return {
+    type: CHANGE_RESTAURANT_INPUT[type.toUpperCase()],
+    payload: value,
+  };
+}
+
 export default {
   addRestaurant,
+  changeRestaurantInput,
 };
