2e5ddb51ff3896ffdda3f5a1c83025d0457f0908
Dong A Kim <dongakim90@gmail.com>
2020-10-15 19:25:33 +0900

    reducer 구현

---
+++ src/reducer.js
@@ -0,0 +1,17 @@
+const initialState = {
+  restaurants: [],
+};
+
+export default function reducer(state = initialState, action) {
+  if (action.type === 'ADD_RESTAURANT') {
+    return {
+      ...state,
+      restaurants: [
+        ...state.restaurants,
+        action.payload,
+      ],
+    };
+  }
+
+  return state;
+}
