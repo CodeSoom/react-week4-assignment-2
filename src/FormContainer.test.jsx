ca37be7c7324465e18fa2068a529f20d47cba7c0
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-15 18:26:03 +0900

    [refactor] refactoring FormContainer test.
    Organize tests to show the purpose of each test.

--- src/FormContainer.test.jsx
+++ src/FormContainer.test.jsx
@@ -15,34 +15,48 @@ beforeEach(() => {
   useDispatch.mockImplementation(() => dispatch);
 });
 
-test('FormContainer', () => {
+describe('FormContainer', () => {
   const inputs = [
     { placeholder: '이름', value: '시카고피자' },
     { placeholder: '분류', value: '양식' },
     { placeholder: '주소', value: '이태원동' },
   ];
 
-  useSelector.mockImplementation(() => ({
-    inputs,
-  }));
+  function renderFormContainer() {
+    return render((
+      <FormContainer />
+    ));
+  }
 
-  const { container, getAllByRole, getByDisplayValue } = render((
-    <FormContainer />
-  ));
+  describe('change each input text', () => {
+    useSelector.mockImplementation(() => ({
+      inputs,
+    }));
 
-  inputs.forEach(({ value }) => {
-    expect(getByDisplayValue(value)).not.toBeNull();
+    it('dispatches updateInputText action', () => {
+      const { getAllByRole, getByDisplayValue } = renderFormContainer();
+
+      inputs.forEach(({ value }) => {
+        expect(getByDisplayValue(value)).not.toBeNull();
+      });
+
+      const textBoxes = getAllByRole('textbox');
+
+      expect(textBoxes).not.toBeNull();
+
+      textBoxes.forEach((textBox) => {
+        fireEvent.change(textBox, { target: { value: '마녀주방' } });
+      });
+
+      expect(dispatch).toBeCalledTimes(3);
+    });
   });
 
-  expect(container).toHaveTextContent('등록');
+  describe('click add restaurant button', () => {
+    it('dispatches addRestaurant action', () => {
+      const { container } = renderFormContainer();
 
-  const textBoxes = getAllByRole('textbox');
-
-  expect(textBoxes).not.toBeNull();
-
-  textBoxes.forEach((textBox) => {
-    fireEvent.change(textBox, { target: { value: '마녀주방' } });
+      expect(container).toHaveTextContent('등록');
+    });
   });
-
-  expect(dispatch).toBeCalledTimes(3);
 });
