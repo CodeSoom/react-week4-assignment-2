90583d254e072b3ddde65d8e564a4e3e12f07cab
gringrape <53764714+gringrape@users.noreply.github.com>
2020-10-15 20:56:19 +0900

    Add test to FormContainer

--- src/FormContainer.test.jsx
+++ src/FormContainer.test.jsx
@@ -1,30 +1,30 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 import { render, fireEvent } from '@testing-library/react';
 
 import FormContainer from './FormContainer';
 
 test('FormContainer', () => {
-  const handleClickAdd = jest.fn();
-  const handleChange = jest.fn();
+  const dispatch = jest.fn();
 
-  const restaurant = {
-    name: '키와미',
-    category: '일식',
-    address: '분당구 정자동',
-  };
+  useSelector.mockImplementation((selector) => selector({
+    restaurant: {
+      name: '키와미',
+      category: '일식',
+      address: '분당구 정자동',
+    },
+  }));
+
+  useDispatch.mockImplementation(() => dispatch);
 
   const { getByDisplayValue, getByText } = render((
-    <FormContainer
-      restaurant={restaurant}
-      onClickAdd={handleClickAdd}
-      onChange={handleChange}
-    />
+    <FormContainer />
   ));
 
-  expect(getByDisplayValue(restaurant.name)).toBeInTheDocument();
+  expect(getByDisplayValue(/키와미/)).toBeInTheDocument();
 
   fireEvent.click(getByText(/등록/));
 
-  expect(handleClickAdd).toBeCalled();
+  expect(dispatch).toBeCalled();
 });
