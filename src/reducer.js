5b2bfb4f156702dc8ebf3a9889ee88378c59be71
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-15 21:57:22 +0900

    [refactor] Create reducers.

--- src/reducer.js
+++ src/reducer.js
@@ -9,15 +9,12 @@ const initialState = {
 
 const initialAction = { type: 'initialType' };
 
+const reducers = {
+  updateInputText: (state, payload) => ({ ...state, inputs: [{ placeholder: '이름', value: '마녀주방' }] }),
+};
+
 export default function reducer(state = initialState, action = initialAction) {
   if (action.type === 'initialType') return state;
 
-  return ({
-    ...state,
-    inputs: [
-      { placeholder: '이름', value: '마녀주방' },
-      { placeholder: '분류', value: '' },
-      { placeholder: '주소', value: '' },
-    ],
-  });
+  return reducers[action.type](state, action.payload);
 }
