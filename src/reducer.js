6f3d3979bcd9ad037f9d97a24e522492958139e2
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-15 19:00:02 +0900

    [refactor] refactoring reducer.

--- src/reducer.js
+++ src/reducer.js
@@ -1,3 +1,8 @@
-export default function reducer(state, action) {
-  return { restaurants: [] };
+const initialState = { restaurants: [] };
+const initialAction = { type: 'initialType' };
+
+export default function reducer(state = initialState, action = initialAction) {
+  if (action.type === 'initialType') return state;
+
+  return state;
 }
