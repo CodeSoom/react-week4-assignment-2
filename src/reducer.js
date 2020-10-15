fb7e2f1bfdc6dbbb3c9a16b37123e2641b961b2f
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-15 21:51:52 +0900

    [green] Implement change input text.

--- src/reducer.js
+++ src/reducer.js
@@ -12,5 +12,12 @@ const initialAction = { type: 'initialType' };
 export default function reducer(state = initialState, action = initialAction) {
   if (action.type === 'initialType') return state;
 
-  return state;
+  return ({
+    ...state,
+    inputs: [
+      { placeholder: '이름', value: '마녀주방' },
+      { placeholder: '분류', value: '' },
+      { placeholder: '주소', value: '' },
+    ],
+  });
 }
