2c7e7e66c985d3d7f3c65c44d2ea4499fd2160c6
Noah <mike1124@naver.com>
2020-10-15 22:02:45 +0900

    ✅ Add test code about App.jsx

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,21 +1,19 @@
-import React from "react";
+import React from 'react';
 
-import App from "./App";
+import { render } from '@testing-library/react';
+import App from './App';
 
-import { render } from "@testing-library/react";
-
-
-describe("App", () => {
-  const renderApp = () => render(<App />)
-  it("should visible input", () => {
+describe('App', () => {
+  const renderApp = () => render(<App />);
+  it('should visible input', () => {
     const { getByText, getByPlaceholderText } = renderApp();
 
-    getByText("Restaurants")
+    getByText('Restaurants');
 
-    getByPlaceholderText("이름")
-    getByPlaceholderText("분류")
-    getByPlaceholderText("장소")
+    getByPlaceholderText('이름');
+    getByPlaceholderText('분류');
+    getByPlaceholderText('장소');
 
-    getByText("등록")
+    getByText('등록');
   });
-})
+});
