60e9087bd1c0304882ec06c8efe5c84b2aec78c9
gringrape <53764714+gringrape@users.noreply.github.com>
2020-10-15 20:37:27 +0900

    Refactor App to use redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,25 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { addRestaurant, updateRestaurant } from './actions';
 
 import Page from './Page';
 
 export default function App() {
-  const [state, setState] = useState({
-    newId: 103,
-    restaurant: {
-      name: '키와미',
-      category: '일식',
-      address: '분당구 정자동',
-    },
-    restaurants: [
-      {
-        id: 101,
-        name: '마녀주방',
-        category: '한식',
-        address: '서울시 강남구',
-      },
-      {
-        id: 102,
-        name: '시카고피자',
-        category: '양식',
-        address: '이태원동',
-      },
-    ],
-  });
+  const { restaurants, restaurant } = useSelector((state) => ({
+    newId: state.newId,
+    restaurant: state.restaurant,
+    restaurants: state.restaurants,
+  }));
 
-  const { newId, restaurants, restaurant } = state;
+  const dispatch = useDispatch();
 
   const handleClickAdd = () => {
-    setState({
-      newId: newId + 1,
-      restaurant: {
-        name: '',
-        category: '',
-        address: '',
-      },
-      restaurants: [...restaurants, { ...restaurant, id: newId }],
-    });
+    dispatch(addRestaurant());
   };
 
   const handleChange = (field) => (event) => {
-    setState({
-      ...state,
-      restaurant: {
-        ...restaurant,
-        [field]: event.target.value,
-      },
-    });
+    dispatch(updateRestaurant(field, event.target.value));
   };
 
   return (
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -7,6 +7,12 @@ import App from './App';
 
 test('App', () => {
   useSelector.mockImplementation((selector) => selector({
+    newId: 100,
+    restaurant: {
+      name: '',
+      category: '',
+      address: '',
+    },
     restaurants: [
       { name: '마녀주방', category: '한식', address: '서울시 강남구' },
       { name: '시카고피자', category: '양식', address: '이태원동' },
