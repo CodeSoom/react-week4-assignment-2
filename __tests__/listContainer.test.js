84f1982598d83c09c4cbb016e363205f9518ebdc
ParkDongJo <pdj10211@gmail.com>
2020-10-16 01:57:12 +0900

    [week4-2] listcontainer 테스트 코드 작성

--- __tests__/listContainer.test.js
+++ __tests__/listContainer.test.js
@@ -2,4 +2,24 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
-import List from './../src/components/ListContainer';
+import { useSelector } from 'react-redux';
+
+import ListContainer from '../src/containers/ListContainer';
+
+jest.mock('react-redux');
+
+test('ListContainer', () => {
+  useSelector.mockImplementation((selector) => selector({
+    restaurants: [
+      { name: '마녀주방', category: '한식', address: '서울시 성북구' },
+      { name: '시카고피자', category: '양식', address: '서울시 아태원' },
+    ],
+  }));
+
+  const { getByText } = render((
+    <ListContainer />
+  ));
+
+  expect(getByText(/마녀주방/)).not.toBeNull();
+  expect(getByText(/시카고피자/)).not.toBeNull();
+});
