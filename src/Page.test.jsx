55f29ba81e227b96cf58c1ec19897fe63f9a0eb1
gringrape <53764714+gringrape@users.noreply.github.com>
2020-10-15 14:40:27 +0900

    Add test to Page

--- src/Page.test.jsx
+++ src/Page.test.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import Page from './Page';
 
 test('Page', () => {
+  const handleClickAdd = jest.fn();
+
   const restaurants = [
     { name: '마녀주방', category: '한식', address: '서울시 강남구' },
     { name: '시카고피자', category: '양식', address: '이태원동' },
@@ -16,14 +18,19 @@ test('Page', () => {
     address: '분당구 정자동',
   };
 
-  const { container, getByDisplayValue } = render((
+  const { container, getByDisplayValue, getByText } = render((
     <Page
       restaurants={restaurants}
       restaurant={restaurant}
+      onClickAdd={handleClickAdd}
     />
   ));
 
   expect(container).toHaveTextContent(/Restaurants/);
   expect(container).toHaveTextContent(restaurants[0].name);
   expect(getByDisplayValue(restaurant.name)).toBeInTheDocument();
+
+  fireEvent.click(getByText(/등록/));
+
+  expect(handleClickAdd).toBeCalled();
 });
