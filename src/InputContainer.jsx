0dc6c18754402fc4b2751a88ebf7bd7450029de1
Dong A Kim <dongakim90@gmail.com>
2020-10-15 19:28:34 +0900

    InputContainer 구현

--- src/InputContainer.jsx
+++ src/InputContainer.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 
+import { useDispatch } from 'react-redux';
+
 import Input from './Input';
 
 export default function InputContainer() {
+  const dispatch = useDispatch();
+
+  const handleClick = () => {
+    dispatch({ type: 'ADD_RESTAURANT', payload: '이름 | 분류 | 주소' });
+  };
+
   return (
     <div>
       <Input
@@ -14,7 +22,12 @@ export default function InputContainer() {
       <Input
         placeholder="주소"
       />
-      <button type="button">등록</button>
+      <button
+        type="button"
+        onClick={handleClick}
+      >
+        등록
+      </button>
     </div>
   );
 }
