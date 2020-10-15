34f88c758562f11c2260eec797f3ff9d23d57994
saseungmin <dbd02169@naver.com>
2020-10-15 23:10:04 +0900

    Input에 대한 테스트 코드를 작성한다.
    - "input"이 존재하는지 확인한다.

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -7,4 +7,10 @@ describe('Input', () => {
   const inputRender = () => render((
     <Input />
   ));
+
+  it('"input"이 존재하는지 확인한다.', () => {
+    const { getByPlaceholderText } = inputRender();
+
+    expect(getByPlaceholderText(/이름/)).not.toBeNull();
+  });
 });
