0fa229d1dcbe7933f835f6354f1e21649b5cd55e
gringrape <53764714+gringrape@users.noreply.github.com>
2020-10-15 20:15:22 +0900

    Update App test to use redux

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 
 import { render } from '@testing-library/react';
+import { useSelector } from 'react-redux';
 
 import App from './App';
 
 test('App', () => {
-  const initialState = {
+  useSelector.mockImplementation((selector) => selector({
     restaurants: [
       { name: '마녀주방', category: '한식', address: '서울시 강남구' },
       { name: '시카고피자', category: '양식', address: '이태원동' },
     ],
-  };
+  }));
 
   const { container } = render(<App />);
 
-  expect(container).toHaveTextContent(initialState.restaurants[0].name);
-  expect(container).toHaveTextContent(initialState.restaurants[1].name);
+  expect(container).toHaveTextContent(/마녀주방/);
+  expect(container).toHaveTextContent(/시카고피자/);
 });
