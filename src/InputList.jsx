c13306f2b6abed489440e7be98535b58b155b36c
gringrape <53764714+gringrape@users.noreply.github.com>
2020-10-15 18:00:55 +0900

    Fix InputList to have key in array

--- src/InputList.jsx
+++ src/InputList.jsx
@@ -14,7 +14,12 @@ export default function InputList({ restaurant, onChange, onClick }) {
   return (
     <div>
       {Object.entries(restaurant).map(([key, value]) => (
-        <input value={value} onChange={onChange(key)} placeholder={placeholders[key]} />
+        <input
+          key={key}
+          value={value}
+          onChange={onChange(key)}
+          placeholder={placeholders[key]}
+        />
       ))}
       <button type="button" onClick={onClick} disabled={!isAddable}>등록</button>
     </div>
