f689016bfd35f7b20a44b3db400ad59e7022c092
gringrape <53764714+gringrape@users.noreply.github.com>
2020-10-15 14:19:40 +0900

    Delete new line after block opening

--- src/Page.test.jsx
+++ src/Page.test.jsx
@@ -5,7 +5,6 @@ import { render } from '@testing-library/react';
 import Page from './Page';
 
 test('Page', () => {
-
   const restaurants = [
     { name: '마녀주방', category: '한식', address: '서울시 강남구' },
     { name: '시카고피자', category: '양식', address: '이태원동' },
