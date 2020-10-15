978dc7998c3b36a5e53eee1428a90b655133666e
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-15 15:10:11 +0900

    [green] Add test for checking  "완료" button in FormContainer.

--- src/FormContainer.test.jsx
+++ src/FormContainer.test.jsx
@@ -5,9 +5,10 @@ import { render } from '@testing-library/react';
 import FormContainer from './FormContainer';
 
 test('FormContainer', () => {
-  const { getAllByRole } = render((
+  const { container, getAllByRole } = render((
     <FormContainer />
   ));
 
   expect(getAllByRole('textbox')).not.toBeNull();
+  expect(container).toHaveTextContent('등록');
 });
