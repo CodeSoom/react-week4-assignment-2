673035fb78763f861bd3154481aa982605d947e7
gringrape <53764714+gringrape@users.noreply.github.com>
2020-10-15 15:03:14 +0900

    Implement List

--- src/List.jsx
+++ src/List.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
 export default function Page({ restaurants }) {
+  if (restaurants.length === 0) {
+    return '식당이 없어요!';
+  }
+
   return (
     <ul>
       {restaurants.map(({ name, category, address }) => (
