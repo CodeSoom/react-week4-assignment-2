f1b3ac16694616db08c8980d06df0e34d1410ad6
saseungmin <dbd02169@naver.com>
2020-10-15 23:45:46 +0900

    Input 테스트를 구현한다.
    - 값이 변경되는지 확인한다.

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -1,19 +1,41 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import Input from './Input';
 
 describe('Input', () => {
-  const inputRender = () => render((
-    <Input />
+  const handleChange = jest.fn();
+
+  const inputRender = (value) => render((
+    <Input
+      text={value}
+      onChange={handleChange}
+    />
   ));
-  const placeholderName = ['이름', '분류', '주소'];
+
+  const inputType = [
+    { placeholderName: '이름', inputName: 'name' },
+    { placeholderName: '분류', inputName: 'type' },
+    { placeholderName: '주소', inputName: 'address' },
+  ];
 
   it('"input"이 존재하는지 확인한다.', () => {
     const { getByPlaceholderText } = inputRender();
 
-    placeholderName.forEach((placeholder) => {
-      expect(getByPlaceholderText(placeholder)).not.toBeNull();
+    inputType.forEach(({ placeholderName }) => {
+      expect(getByPlaceholderText(placeholderName)).not.toBeNull();
+    });
+  });
+
+  it('값이 변경되는지 확인한다.', () => {
+    const { getByPlaceholderText } = inputRender();
+
+    inputType.forEach(({ placeholderName, inputName }) => {
+      fireEvent.change(getByPlaceholderText(placeholderName), {
+        target: { value: '시카고 피자', name: inputName },
+      });
+
+      expect(getByPlaceholderText(placeholderName)).toHaveDisplayValue('시카고 피자');
     });
   });
 });
