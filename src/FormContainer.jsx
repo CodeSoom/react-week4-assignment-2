4061c37b0a40a1cb0aec2bec72db7bd35e6e6195
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-15 18:12:58 +0900

    [green] Implement dispatch in FormContainer.

--- src/FormContainer.jsx
+++ src/FormContainer.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import Form from './Form';
 
 export default function FormContainer() {
+  const dispatch = useDispatch();
+
   const { inputs } = useSelector((state) => ({
     inputs: state.inputs,
   }));
 
+  function handleChangeInput() {
+    dispatch();
+  }
+
   return (
-    <Form inputs={inputs} />
+    <Form inputs={inputs} onChangeInput={handleChangeInput} />
   );
 }
