4f9acf680716761e35d7eb8610f9430063be1ff7
Dong A Kim <dongakim90@gmail.com>
2020-10-15 18:48:36 +0900

    Input 구현

--- src/Input.jsx
+++ src/Input.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 export default function Input({
   placeholder,
+  onChange,
 }) {
   return (
     <input
       type="text"
       placeholder={placeholder}
+      onChange={onChange}
     />
   );
 }
