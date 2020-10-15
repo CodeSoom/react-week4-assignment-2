bc14c0f8af1a477ca69eb17e1201794de8698eb1
gringrape <53764714+gringrape@users.noreply.github.com>
2020-10-15 14:47:10 +0900

    Add List test

---
+++ src/List.test.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import List from './List';
+
+test('List', () => {
+  const restaurants = [
+    { name: '마녀주방', category: '한식', address: '서울시 강남구' },
+    { name: '시카고피자', category: '양식', address: '이태원동' },
+  ];
+
+  const { container } = render((
+    <List restaurants={restaurants} />
+  ));
+
+  restaurants.forEach((restaurant) => {
+    Object.entries(restaurant).forEach(([, value]) => {
+      expect(container).toHaveTextContent(value);
+    });
+  });
+});
