c910c2ddc4f2e049e1214d648a1e6654c84b8de3
gringrape <53764714+gringrape@users.noreply.github.com>
2020-10-15 16:43:42 +0900

    Add Input test

---
+++ src/Input.test.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import { fireEvent, render } from '@testing-library/react';
+
+import Input from './Input';
+
+test('Input', () => {
+  const handleChange = jest.fn();
+
+  const value = '마녀식당';
+
+  const { getByDisplayValue } = render((
+    <Input value={value} onChange={handleChange} />
+  ));
+
+  expect(getByDisplayValue(value).toBeInTheDocument());
+
+  expect(handleChange).not.toBeCalled();
+
+  fireEvent.change(getByDisplayValue(value), {
+    target: {
+      value: 'something',
+    },
+  });
+
+  expect(handleChange).toBeCalledTimes(1);
+});
