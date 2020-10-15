580c70741d37f9f7ce11ea0196ee155bb8c3c444
gringrape <53764714+gringrape@users.noreply.github.com>
2020-10-15 14:36:29 +0900

    Implement App

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,20 @@ import Page from './Page';
 
 export default function App() {
   const [state, setState] = useState({
+    restaurant: {
+      name: '키와미',
+      category: '일식',
+      address: '분당구 정자동',
+    },
     restaurants: [
       { name: '마녀주방', category: '한식', address: '서울시 강남구' },
       { name: '시카고피자', category: '양식', address: '이태원동' },
     ],
   });
 
-  const { restaurants } = state;
+  const { restaurants, restaurant } = state;
 
   return (
-    <Page restaurants={restaurants} />
+    <Page restaurants={restaurants} restaurant={restaurant} />
   );
 }
