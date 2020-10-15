94833c269e0bcc9e3a5610e0500ceb5bc8aaf114
JamieShin0201 <jamieshin1450@gmail.com>
2020-10-15 15:58:59 +0900

    Add test reducer.test.js

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,9 +1,30 @@
 import reducer from './reducer';
 
-test('reducer', () => {
-  const state = reducer();
+describe('reducer', () => {
+  describe('updateRestaurant', () => {
+    it('changes restaurant', () => {
+      const state = reducer({
+        restaurant: {
+          name: '',
+          classification: '',
+          location: '',
+        },
+      }, updateRestaurant('이름', '시카고피자'));
 
-  expect(state.restaurants).toHaveLength(0);
-  expect(state.restaurant.name).toBe('');
-  expect(state.placeholders).toHaveLength(3);
+      expect(state.restaurant.name).toBe('시카고피자');
+    });
+  });
+
+  context('with not existed action type', () => {
+    const action = () => ({
+      type: 'notExistedActionType',
+    });
+    it('return state', () => {
+      const state = reducer(undefined, action());
+
+      expect(state.restaurants).toHaveLength(0);
+      expect(state.restaurant.name).toBe('');
+      expect(state.placeholders).toHaveLength(3);
+    });
+  });
 });
