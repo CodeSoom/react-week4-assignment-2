ef24ff627a8c6472935e3e1a5cde60d0647388be
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-15 16:34:56 +0900

    [green] Implement test for FormContainer.

--- src/Form.jsx
+++ src/Form.jsx
@@ -2,13 +2,7 @@ import React from 'react';
 
 import Input from './Input';
 
-export default function Form({ onClickAddRestautant, onChangeInput }) {
-  const inputs = [
-    { placeholder: '이름', value: '마녀주방' },
-    { placeholder: '분류', value: '한식' },
-    { placeholder: '주소', value: '서울시 강남구' },
-  ];
-
+export default function Form({ inputs, onClickAddRestautant, onChangeInput }) {
   return (
     <div>
       {inputs.map(({ placeholder, value }) => (
--- src/FormContainer.jsx
+++ src/FormContainer.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 import Form from './Form';
 
 export default function FormContainer() {
+  const inputs = [
+    { placeholder: '이름', value: '시카고피자' },
+    { placeholder: '분류', value: '양식' },
+    { placeholder: '주소', value: '이태원동' },
+  ];
+
   return (
-    <Form />
+    <Form inputs={inputs} />
   );
 }
