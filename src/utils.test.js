549bda61452d6f931e5be41cc2bc8388ac0d9572
gringrape <53764714+gringrape@users.noreply.github.com>
2020-10-15 22:19:15 +0900

    Add utils test

---
+++ src/utils.test.js
@@ -0,0 +1,17 @@
+import { trimValue } from './utils';
+
+describe('utils', () => {
+  describe('trimValues', () => {
+    it('returns new object with trimmed value', () => {
+      const prevObj = {
+        value1: 'wel  ',
+        value2: '    www    ',
+      };
+
+      expect(trimValues(prevObj)).toEqual({
+        value1: 'wel',
+        value2: 'www',
+      });
+    });
+  });
+});
