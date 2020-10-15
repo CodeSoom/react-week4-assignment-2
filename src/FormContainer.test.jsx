a1f2bfd0f85bbc4c7ba586de996b37aa2005f6c8
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-15 16:31:54 +0900

    [red] Add test for getting inputs from store at FormContainer.

--- src/FormContainer.test.jsx
+++ src/FormContainer.test.jsx
@@ -2,13 +2,31 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import { useSelector } from 'react-redux';
+
 import FormContainer from './FormContainer';
 
+jest.mock('react-redux');
+
 test('FormContainer', () => {
-  const { container, getAllByRole } = render((
+  const inputs = [
+    { placeholder: '이름', value: '시카고피자' },
+    { placeholder: '분류', value: '양식' },
+    { placeholder: '주소', value: '이태원동' },
+  ];
+
+  useSelector.mockImplementation(() => ({
+    inputs,
+  }));
+
+  const { container, getAllByRole, getByDisplayValue } = render((
     <FormContainer />
   ));
 
   expect(getAllByRole('textbox')).not.toBeNull();
   expect(container).toHaveTextContent('등록');
+
+  inputs.forEach(({ value }) => {
+    expect(getByDisplayValue(value)).not.toBeNull();
+  });
 });
