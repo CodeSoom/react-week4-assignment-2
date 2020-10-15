8b22ae1b058fcb76f8056158bc7ab52841529e6b
Dong A Kim <dongakim90@gmail.com>
2020-10-16 08:20:56 +0900

    InputContainer 구현
    redux store 연결

--- src/InputContainer.jsx
+++ src/InputContainer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import actions from './actions';
 
@@ -8,31 +8,39 @@ import Input from './Input';
 
 export default function InputContainer() {
   const dispatch = useDispatch();
+  const {
+    name,
+    type,
+    place,
+  } = useSelector((state) => state);
 
   const handleClick = () => {
     dispatch(actions.addRestaurant('이름 | 분류 | 주소'));
   };
 
   const handleChange = (event) => {
-    const { name, value } = event.target;
+    const { name: inputName, value } = event.target;
 
-    dispatch(actions.changeRestaurantInput(name, value));
+    dispatch(actions.changeRestaurantInput(inputName, value));
   };
 
   return (
     <div>
       <Input
         name="name"
+        value={name}
         placeholder="이름"
         onChange={handleChange}
       />
       <Input
         name="type"
+        value={type}
         placeholder="분류"
         onChange={handleChange}
       />
       <Input
         name="place"
+        value={place}
         placeholder="주소"
         onChange={handleChange}
       />
