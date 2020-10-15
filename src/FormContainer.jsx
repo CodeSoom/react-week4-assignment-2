3815526b60835c19f9db02c3c25216f2467d8521
JamieShin0201 <jamieshin1450@gmail.com>
2020-10-15 15:51:48 +0900

    Refactor FormContainer.jsx

--- src/FormContainer.jsx
+++ src/FormContainer.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 
+import { useSelector } from 'react-redux';
+
 import Form from './Form';
 
 export default function FormContainer() {
-  const restaurant = {
-    name: '마녀주방',
-    classification: '한식',
-    location: '서울시 강남구',
-  };
-
-  const placeholders = ['이름', '분류', '주소'];
+  const { restaurant, placeholders } = useSelector((state) => ({
+    restaurant: state.restaurant,
+    placeholders: state.placeholders,
+  }));
 
   return (
     <div>
