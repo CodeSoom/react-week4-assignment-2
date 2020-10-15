ef29b75a0acd9750cdf15c4dfe2a92a724ecd5d9
saseungmin <dbd02169@naver.com>
2020-10-15 23:46:11 +0900

    Input 컴포넌트를 구현한다.

--- src/Input.jsx
+++ src/Input.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Input = () => (
   <>
-    <input type="text" placeholder="이름" />
-    <input type="text" placeholder="분류" />
-    <input type="text" placeholder="주소" />
+    <input type="text" placeholder="이름" name="name" />
+    <input type="text" placeholder="분류" name="type" />
+    <input type="text" placeholder="주소" name="address" />
   </>
 );
 
