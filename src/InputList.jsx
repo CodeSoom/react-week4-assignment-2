8f2c676947146f6f9cf3fa07705d8d7f8d68474e
gringrape <53764714+gringrape@users.noreply.github.com>
2020-10-15 17:15:39 +0900

    Refactor InputList to have placeholder on each

--- src/InputList.jsx
+++ src/InputList.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 
 export default function InputList({ restaurant, onChange, onClick }) {
+  const placeholders = {
+    name: '이름',
+    category: '분류',
+    address: '주소',
+  };
+
   return (
     <div>
       {Object.entries(restaurant).map(([key, value]) => (
-        <input value={value} onChange={onChange(key)} />
+        <input value={value} onChange={onChange(key)} placeholder={placeholders[key]} />
       ))}
       <button type="button" onClick={onClick}>등록</button>
     </div>
