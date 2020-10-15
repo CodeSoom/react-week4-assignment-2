d3a3c6a53d30f52e5e1340bf80abe6fd5f2a532e
gringrape <53764714+gringrape@users.noreply.github.com>
2020-10-15 14:10:26 +0900

    Implement App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function App() {
+  const [state, setState] = useState({
+    restaurants: [
+      { name: '마녀주방', category: '한식', address: '서울시 강남구' },
+      { name: '시카고피자', category: '양식', address: '이태원동' },
+    ],
+  });
+
+  const { restaurants } = state;
+
   return (
-    <div>
-      Hello
-    </div>
+    restaurants.map(({ name, category, address }) => (
+      <p>
+        {name}
+        |
+        {category}
+        |
+        {address}
+      </p>
+    ))
   );
 }
