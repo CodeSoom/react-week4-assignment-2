f38a51f0d826ddf896d6b5a316a8e0ff0b15e557
oceanide <oceanide@gmail.com>
2020-10-16 00:09:21 +0900

    Update Input.jsx to add props

--- src/Input.jsx
+++ src/Input.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
 
-export default function Input({ name, group, address }) {
+export default function Input({
+  name, group, address,
+  onChangeName, onChangeGroup, onChangeAddress,
+  onClick,
+}) {
   return (
     <div>
-      <input type="text" value={name} placeholder="이름" />
-      <input type="text" value={group} placeholder="분류" />
-      <input type="text" value={address} placeholder="주소" />
+      <input
+        type="text"
+        value={name}
+        placeholder="이름"
+        onChange={onChangeName}
+      />
+      <input
+        type="text"
+        value={group}
+        placeholder="분류"
+        onChange={onChangeGroup}
+      />
+      <input
+        type="text"
+        value={address}
+        placeholder="주소"
+        onChange={onChangeAddress}
+      />
+      <button type="button" onClick={onClick}>등록</button>
     </div>
   );
 }
