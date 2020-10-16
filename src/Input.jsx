71b405c0757fb1f9dcdea35515c8fade99a708ff
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-16 11:16:57 +0900

    [green] Implement name property in Input.

--- src/Input.jsx
+++ src/Input.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
-export default function Input({ placeholderText, value, onChange }) {
+export default function Input({
+  placeholderText, value, onChange,
+}) {
   return (
     <input
       type="text"
+      name="name"
       placeholder={placeholderText}
       value={value}
       onChange={(e) => onChange(placeholderText, e.target.value)}
