3787c998aa1b73d290541be85acb8293c6356ebc
ParkDongJo <pdj10211@gmail.com>
2020-10-17 00:08:46 +0900

    [week4-2] restaurant reducer 테스트 코드작성
    * name, category, address 입력값 테스트코드
    * restaurant 추가 시 테스트코드

---
+++ __tests__/reducers/restaurant.test.js
@@ -0,0 +1,111 @@
+import reducer from '../../src/reducers';
+
+import {
+  updateRestaurantName,
+  updateRestaurantCategory,
+  updateRestaurantAddr,
+  addRestaurant,
+} from '../../src/actions';
+
+describe('restaurant reducer', () => {
+  describe('updateRestaurantName', () => {
+    const reduceUpdateRestaurantName = () => reducer({
+      name: '',
+    }, updateRestaurantName('마녀주방'));
+
+    it('change restaurnt name', () => {
+      const state = reduceUpdateRestaurantName();
+
+      expect(state.name).toBe('마녀주방');
+    });
+  });
+
+  describe('updateRestaurantCategory', () => {
+    const reduceUpdateRestaurantCategory = () => reducer({
+      category: '',
+    }, updateRestaurantCategory('한식'));
+    it('change restaurnt category', () => {
+      const state = reduceUpdateRestaurantCategory();
+
+      expect(state.category).toBe('한식');
+    });
+  });
+
+  describe('updateRestaurantAddr', () => {
+    const reduceUpdateRestaurantAddr = () => reducer({
+      address: '',
+    }, updateRestaurantAddr('서울시 성동구'));
+    it('change restaurnt category', () => {
+      const state = reduceUpdateRestaurantAddr();
+
+      expect(state.address).toBe('서울시 성동구');
+    });
+  });
+
+  describe('addRestaurant', () => {
+    const reduceAddRestaurant = ({ name, category, address }) => reducer({
+      name,
+      category,
+      address,
+      restaurants: [],
+    }, addRestaurant());
+
+    context('add restaurnt category', () => {
+      it('appends a new state into tasks', () => {
+        const state = reduceAddRestaurant({
+          name: '마녀사냥',
+          category: '한식',
+          address: '서울시 성동구',
+        });
+
+        expect(state.restaurants).toHaveLength(1);
+        expect(state.restaurants[0].id).not.toBeUndefined();
+        expect(state.restaurants[0].name).toBe('마녀사냥');
+        expect(state.restaurants[0].category).toBe('한식');
+        expect(state.restaurants[0].address).toBe('서울시 성동구');
+      });
+
+      it('clear name', () => {
+        const state = reduceAddRestaurant({
+          name: '마녀사냥',
+          category: '한식',
+          address: '서울시 성동구',
+        });
+
+        expect(state.name).toBe('');
+      });
+
+      it('clear category', () => {
+        const state = reduceAddRestaurant({
+          name: '마녀사냥',
+          category: '한식',
+          address: '서울시 성동구',
+        });
+
+        expect(state.category).toBe('');
+      });
+
+      it('clear address', () => {
+        const state = reduceAddRestaurant({
+          name: '마녀사냥',
+          category: '한식',
+          address: '서울시 성동구',
+        });
+
+        expect(state.address).toBe('');
+      });
+    });
+
+    context('without name & category & address', () => {
+      it("doesn't work", () => {
+        const state = reduceAddRestaurant({
+          name: '',
+          category: '',
+          address: '',
+        });
+
+        expect(state.restaurants).toHaveLength(0);
+      });
+    });
+  });
+});
