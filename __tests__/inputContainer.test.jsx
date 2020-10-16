1476e4293db690a2214f68ea4cbf2dd50f7e38a0
ParkDongJo <pdj10211@gmail.com>
2020-10-17 02:06:11 +0900

    [week4-2] InputContainer 테스트 코드 작성

---
+++ __tests__/inputContainer.test.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import { render, fireEvent } from '@testing-library/react';
+
+import { useSelector, useDispatch } from 'react-redux';
+
+import InputContainer from '../src/containers/InputContainer';
+
+jest.mock('react-redux');
+
+test('InputContainer', () => {
+  const dispatch = jest.fn();
+  useDispatch.mockImplementation(() => dispatch);
+
+  useSelector.mockImplementation((selector) => selector({
+    name: '마녀주방',
+    category: '한식',
+    address: '서울시 성동구',
+  }));
+
+  const { getByText, getByDisplayValue, getByPlaceholderText } = render((
+    <InputContainer />
+  ));
+
+  expect(getByText(/등록/)).not.toBeNull();
+
+  expect(getByDisplayValue(/마녀주방/)).not.toBeNull();
+  expect(getByDisplayValue(/한식/)).not.toBeNull();
+  expect(getByDisplayValue(/서울시 성동구/)).not.toBeNull();
+
+  fireEvent.change(getByPlaceholderText('이름'), {
+    target: { value: '시카고피자' },
+  });
+  expect(dispatch).toBeCalledWith(
+    { type: 'updateRestaurantName', payload: { name: '시카고피자' } },
+  );
+
+  fireEvent.click(getByText(/등록/));
+  expect(dispatch).toBeCalledWith({ type: 'addRestaurant' });
+});
