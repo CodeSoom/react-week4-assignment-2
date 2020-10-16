d1398446bc29cda227885b7daee0e9650f3b6cc4
JamieShin0201 <jamieshin1450@gmail.com>
2020-10-16 14:05:08 +0900

    Add test in reducer.test.js

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -4,16 +4,36 @@ import { addRestaurant, updateRestaurant } from './actions';
 
 describe('reducer', () => {
   describe('updateRestaurant', () => {
-    it('changes restaurant', () => {
-      const state = reducer({
-        restaurant: {
-          name: '',
-          classification: '',
-          location: '',
-        },
-      }, updateRestaurant('이름', '시카고피자'));
+    context('with vaild key', () => {
+      const key = '이름';
 
-      expect(state.restaurant.name).toBe('시카고피자');
+      it('changes restaurant', () => {
+        const state = reducer({
+          restaurant: {
+            name: '',
+            classification: '',
+            location: '',
+          },
+        }, updateRestaurant(key, '시카고피자'));
+
+        expect(state.restaurant.name).toBe('시카고피자');
+      });
+    });
+
+    context('with invaild key', () => {
+      const key = '';
+
+      it('changes restaurant', () => {
+        const state = reducer({
+          restaurant: {
+            name: '',
+            classification: '',
+            location: '',
+          },
+        }, updateRestaurant(key, '시카고피자'));
+
+        expect(state.restaurant.name).toBe('');
+      });
     });
   });
 
@@ -30,6 +50,7 @@ describe('reducer', () => {
         classification: '한식',
         location: '서울시 강남구',
       };
+
       it('appends a new restaurant into restaurants', () => {
         const state = reduceAddRestaurant(restaurant);
 
@@ -62,6 +83,7 @@ describe('reducer', () => {
       const action = () => ({
         type: 'notExistedActionType',
       });
+
       it('return state', () => {
         const state = reducer(undefined, action());
 
