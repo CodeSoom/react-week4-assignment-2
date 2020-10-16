a594b756fdb7c34b921f77410c9be45d62849bdd
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-16 18:02:33 +0900

    [green] Create fixture.

--- __fixtuers__/fixture.js
+++ __fixtuers__/fixture.js
@@ -1,10 +1,13 @@
-export const restaurants = [
-  { name: '마녀주방', category: '한식', address: '서울시 강남구' },
-  { name: '시카고피자', category: '양식', address: '이태원동' },
-];
+const fixture = {
+  restaurants: [
+    { name: '마녀주방', category: '한식', address: '서울시 강남구' },
+    { name: '시카고피자', category: '양식', address: '이태원동' },
+  ],
+  inputInfos: [
+    { name: 'name', placeholder: '이름', value: '시카고피자' },
+    { name: 'category', placeholder: '분류', value: '양식' },
+    { name: 'address', placeholder: '주소', value: '이태원동' },
+  ],
+};
 
-export const inputInfos = [
-  { name: 'name', placeholder: '이름', value: '시카고피자' },
-  { name: 'category', placeholder: '분류', value: '양식' },
-  { name: 'address', placeholder: '주소', value: '이태원동' },
-];
+export default fixture;
