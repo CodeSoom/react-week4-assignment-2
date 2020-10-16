a65424c78f86822eb0a3a9133da3bd52ec061154
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-16 18:11:01 +0900

    [refactor] Apply fixture to ListContainer test.

--- src/ListContainer.test.jsx
+++ src/ListContainer.test.jsx
@@ -4,15 +4,14 @@ import { render } from '@testing-library/react';
 
 import { useSelector } from 'react-redux';
 
+import fixture from '../__fixtuers__/fixture';
+
 import ListContainer from './ListContainer';
 
 jest.mock('react-redux');
 
 test('ListContainer', () => {
-  const restaurants = [
-    { name: '마녀주방', category: '한식', address: '서울시 강남구' },
-    { name: '시카고피자', category: '양식', address: '이태원동' },
-  ];
+  const { restaurants } = fixture;
 
   useSelector.mockImplementation((selector) => selector({
     restaurants,
