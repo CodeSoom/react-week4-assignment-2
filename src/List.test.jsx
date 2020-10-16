1bd960ecb9bc264567b5ca75c9c5efb0f7bd849b
wjy5446 <wjy5446@gmail.com>
2020-10-17 01:06:00 +0900

    fix) RestaurantInfo에 id요소 추가

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { render } from '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
 
 import List from './List';
 
@@ -8,16 +8,19 @@ describe('List', () => {
   context('restaurantInfos가 3개 주어졌을 때', () => {
     const restaurantInfos = [
       {
+        id: 1,
         name: '마녀주방',
         type: '한식',
         location: '서울시 강남구',
       },
       {
+        id: 2,
         name: '시카고피자',
         type: '양식',
         location: '이태원동',
       },
       {
+        id: 3,
         name: '키와미',
         type: '일식',
         location: '분당구 정자동',
