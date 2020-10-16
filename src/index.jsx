d8241d478a175470927bd3d9bdb659d23179b304
gringrape <53764714+gringrape@users.noreply.github.com>
2020-10-16 09:42:04 +0900

    Extract store out of index

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
-import reducer from './reducer';
+import store from './store';
 
 import App from './App';
-
-const store = createStore(reducer);
 
 ReactDOM.render(
   <Provider store={store}>
