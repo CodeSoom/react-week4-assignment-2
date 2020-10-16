643921e6dc7557b42f2f99a776008bccd736a616
oceanide <oceanide@gmail.com>
2020-10-16 14:52:00 +0900

    Test(InputContainer.test.jsx): add handleChangeName

--- src/InputContainer.test.jsx
+++ src/InputContainer.test.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import InputContainer from './InputContainer';
 
 jest.mock('react-redux');
 
 describe('InputContainer', () => {
+  const dispatch = jest.fn();
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  useDispatch.mockImplementation(() => dispatch);
+
   useSelector.mockImplementation((selector) => selector({
     restaurant: {
       name: '한식이 좋아!',
@@ -24,6 +32,27 @@ describe('InputContainer', () => {
       expect(getByDisplayValue('한식이 좋아!')).not.toBeNull();
       expect(getByDisplayValue('한식')).not.toBeNull();
       expect(getByDisplayValue('서울시 강남구')).not.toBeNull();
+    });
+  });
+
+  context('with restaurant name is changed', () => {
+    it('dispatches handleChangeName', () => {
+      const { getByPlaceholderText, getByDisplayValue } = render(
+        <InputContainer />,
+      );
+
+      expect(getByDisplayValue('한식이 좋아!')).not.toBeNull();
+
+      fireEvent.change(getByPlaceholderText('이름'), {
+        target: { value: '중식이 좋아!' },
+      });
+
+      expect(dispatch).toBeCalledWith({
+        type: 'updateRestaurantName',
+        payload: {
+          name: '중식이 좋아!',
+        },
+      });
     });
   });
 });
