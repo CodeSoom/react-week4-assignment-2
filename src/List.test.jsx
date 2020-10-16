097606d036c4aa7bd30db873340022d9ada2591e
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-16 18:09:46 +0900

    [refactor] Apply fixture to List test.

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import fixture from '../__fixtuers__/fixture';
+
 import List from './List';
 
 describe('List', () => {
@@ -13,10 +15,7 @@ describe('List', () => {
 
   context('with restaurants', () => {
     it('renders restaurants', () => {
-      const restaurants = [
-        { name: '마녀주방', category: '한식', address: '서울시 강남구' },
-        { name: '시카고피자', category: '양식', address: '이태원동' },
-      ];
+      const { restaurants } = fixture;
 
       const { container } = renderList(restaurants);
 
