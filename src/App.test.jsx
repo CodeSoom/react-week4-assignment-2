6549db6c41f1b86e3d80e68d0f301adcc2d69040
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-16 16:38:06 +0900

    [refactor] Change inputs to inputInfos in App test.

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -14,15 +14,15 @@ test('App', () => {
     { name: '시카고피자', category: '양식', address: '이태원동' },
   ];
 
-  const inputs = [
-    { placeholder: '이름', value: '시카고피자' },
-    { placeholder: '분류', value: '양식' },
-    { placeholder: '주소', value: '이태원동' },
+  const inputInfos = [
+    { name: 'name', placeholder: '이름', value: '시카고피자' },
+    { name: 'category', placeholder: '분류', value: '양식' },
+    { name: 'address', placeholder: '주소', value: '이태원동' },
   ];
 
   useSelector.mockImplementation(() => ({
     restaurants,
-    inputs,
+    inputInfos,
   }));
 
   const { getByText, getByPlaceholderText } = render(
@@ -35,7 +35,7 @@ test('App', () => {
     expect(getByText(`${name} | ${category} | ${address}`)).not.toBeNull();
   });
 
-  inputs.forEach(({ placeholder, value }) => {
+  inputInfos.forEach(({ placeholder, value }) => {
     expect(getByPlaceholderText(placeholder)).toHaveDisplayValue(value);
   });
 
