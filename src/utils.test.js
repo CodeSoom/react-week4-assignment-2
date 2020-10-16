315aaf21fb812bf9661d4a0213d8305b6c19350c
gringrape <53764714+gringrape@users.noreply.github.com>
2020-10-16 11:14:04 +0900

    Add test to utils
    test about function of mapping values of object

--- src/utils.test.js
+++ src/utils.test.js
@@ -1,16 +1,29 @@
 import utils from './utils';
 
 describe('utils', () => {
+  const prevObj = {
+    value1: 'wel  ',
+    value2: '    www    ',
+  };
+
   describe('trimValues', () => {
     it('returns new object with trimmed value', () => {
-      const prevObj = {
-        value1: 'wel  ',
-        value2: '    www    ',
-      };
-
       expect(utils.trimValues(prevObj)).toEqual({
         value1: 'wel',
         value2: 'www',
+      });
+    });
+  });
+
+  describe('mapValues', () => {
+    context('with empty string as trim function', () => {
+      const mapFn = () => '';
+
+      it('returns new object with values of empty string', () => {
+        expect(utils.mapValues(prevObj, mapFn)).toEqual({
+          value1: '',
+          value2: '',
+        });
       });
     });
   });
