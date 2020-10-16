879dd611ffa0ed64ea292aaef4b095eecc5c9760
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-16 11:27:05 +0900

    [green] Implement name property in Form.

--- src/Form.jsx
+++ src/Form.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 
 import Input from './Input';
 
-export default function Form({ inputs, onClickAddRestautant, onChangeInput }) {
+export default function Form({
+  inputs, onClickAddRestautant, onChangeInput,
+}) {
   return (
     <div>
-      {inputs.map(({ placeholder, value }) => (
+      {inputs.map(({ name, placeholder, value }) => (
         <Input
           key={placeholder}
+          name={name}
           placeholderText={placeholder}
           value={value}
           onChange={onChangeInput}
