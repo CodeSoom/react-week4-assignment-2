3dfd5725415cb955723fc08f9130c6818fd3d841
JamieShin0201 <jamieshin1450@gmail.com>
2020-10-16 13:44:14 +0900

    Fix test in reducer.test.js

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -67,7 +67,6 @@ describe('reducer', () => {
 
         expect(state.restaurants).toHaveLength(0);
         expect(state.restaurant.name).toBe('');
-        expect(state.placeholders).toHaveLength(3);
       });
     });
   });
