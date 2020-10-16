48e0228b74f4a0745b87acc1c4ec4d6a4bafa0ff
oceanide <oceanide@gmail.com>
2020-10-17 01:05:19 +0900

    Refactor(ListContainer.test.jsx): refactor testcase

--- src/ListContainer.test.jsx
+++ src/ListContainer.test.jsx
@@ -13,15 +13,15 @@ describe('ListContainer', () => {
     restaurants: [
       {
         id: 1,
-        name: '한식이 좋아!',
+        name: '마녀주방',
         category: '한식',
         address: '서울시 강남구',
       },
       {
         id: 2,
-        name: '중식이 좋아!',
-        category: '중식',
-        address: '경기도 분당구',
+        name: '시카고피자',
+        category: '양식',
+        address: '이태원동',
       },
     ],
   }));
@@ -30,13 +30,13 @@ describe('ListContainer', () => {
     it('renders each restaurant', () => {
       const { container } = render(<ListContainer />);
 
-      expect(container).toHaveTextContent('한식이 좋아!');
+      expect(container).toHaveTextContent('마녀주방');
       expect(container).toHaveTextContent('한식');
       expect(container).toHaveTextContent('서울시 강남구');
 
-      expect(container).toHaveTextContent('중식이 좋아!');
-      expect(container).toHaveTextContent('중식');
-      expect(container).toHaveTextContent('경기도 분당구');
+      expect(container).toHaveTextContent('시카고피자');
+      expect(container).toHaveTextContent('양식');
+      expect(container).toHaveTextContent('이태원동');
     });
   });
 });
