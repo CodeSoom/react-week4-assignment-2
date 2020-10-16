d006a8afe79c376836eacea31d5af09c0939ea5b
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-16 18:26:35 +0900

    [refactor] Apply fixture to reducer test.

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,19 +1,21 @@
 import reducer from './reducer';
 
+import fixture from '../__fixtuers__/fixture';
+
 import {
   updateInputText,
   addRestaurant,
 } from './actions';
 
 describe('reducer', () => {
-  const initalRestaurants = [];
-  const initalInputInfos = [
-    { name: 'name', placeholder: '이름', value: '' },
-    { name: 'category', placeholder: '분류', value: '' },
-    { name: 'address', placeholder: '주소', value: '' },
-  ];
-
   describe('undefined action', () => {
+    const initalRestaurants = [];
+    const initalInputInfos = [
+      { name: 'name', placeholder: '이름', value: '' },
+      { name: 'category', placeholder: '분류', value: '' },
+      { name: 'address', placeholder: '주소', value: '' },
+    ];
+
     it('changes nothing', () => {
       const { restaurants, inputInfos } = reducer();
 
@@ -24,32 +26,29 @@ describe('reducer', () => {
 
   describe('updateInputText', () => {
     it('changes input texts', () => {
+      const { restaurants, inputInfos } = fixture;
+      const { placeholder } = inputInfos[0];
       const newInputText = '마녀주방';
-      const { placeholder } = initalInputInfos[0];
 
-      const { inputInfos } = reducer({
-        restaurants: initalRestaurants,
-        inputInfos: initalInputInfos,
+      const state = reducer({
+        restaurants,
+        inputInfos,
       }, updateInputText(placeholder, newInputText));
 
-      expect(inputInfos[0].value).toBe('마녀주방');
+      expect(state.inputInfos[0].value).toBe('마녀주방');
     });
   });
 
   describe('addRestaurant', () => {
     it('add restaurant to restaurants', () => {
-      const previousInputInfos = [
-        { name: 'name', placeholder: '이름', value: '시카고피자' },
-        { name: 'category', placeholder: '분류', value: '양식' },
-        { name: 'address', placeholder: '주소', value: '이태원동' },
-      ];
+      const { restaurants, inputInfos } = fixture;
 
-      const { restaurants } = reducer({
-        restaurants: initalRestaurants,
-        inputInfos: previousInputInfos,
+      const state = reducer({
+        restaurants,
+        inputInfos,
       }, addRestaurant());
 
-      expect(restaurants).toHaveLength(initalRestaurants.length + 1);
+      expect(state.restaurants).toHaveLength(restaurants.length + 1);
     });
   });
 });
