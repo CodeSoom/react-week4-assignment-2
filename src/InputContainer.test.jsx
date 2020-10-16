a2262bc96f7fa5d3c2e5775bec1e02e3d572859e
oceanide <oceanide@gmail.com>
2020-10-17 00:33:50 +0900

    Test(InputContainer.test.jsx); refactor testcase

--- src/InputContainer.test.jsx
+++ src/InputContainer.test.jsx
@@ -19,80 +19,67 @@ describe('InputContainer', () => {
 
   useSelector.mockImplementation((selector) => selector({
     restaurant: {
-      name: '한식이 좋아!',
+      name: '마녀주방',
       category: '한식',
       address: '서울시 강남구',
     },
   }));
 
-  context('with restaurant information', () => {
-    it('renders name, category, address', () => {
-      const { getByDisplayValue } = render(<InputContainer />);
-
-      expect(getByDisplayValue('한식이 좋아!')).not.toBeNull();
-      expect(getByDisplayValue('한식')).not.toBeNull();
-      expect(getByDisplayValue('서울시 강남구')).not.toBeNull();
-    });
-  });
-
-  context('with restaurant name is changed', () => {
-    it('dispatches handleChangeName', () => {
-      const { getByPlaceholderText, getByDisplayValue } = render(
+  context('changing restaurant name', () => {
+    it('called handleChange with restaurant name', () => {
+      const { getByPlaceholderText } = render(
         <InputContainer />,
       );
-
-      expect(getByDisplayValue('한식이 좋아!')).not.toBeNull();
 
       fireEvent.change(getByPlaceholderText('이름'), {
-        target: { value: '중식이 좋아!' },
+        target: { value: '시카고피자' },
       });
 
       expect(dispatch).toBeCalledWith({
-        type: 'updateRestaurantName',
+        type: 'updateRestaurant',
         payload: {
-          name: '중식이 좋아!',
+          name: 'name',
+          value: '시카고피자',
         },
       });
     });
   });
 
-  context('with restaurant category is changed', () => {
-    it('dispatches handleChangeCategory', () => {
-      const { getByPlaceholderText, getByDisplayValue } = render(
+  context('changing restaurant category', () => {
+    it('called handleChange with restaurant category', () => {
+      const { getByPlaceholderText } = render(
         <InputContainer />,
       );
-
-      expect(getByDisplayValue('한식')).not.toBeNull();
 
       fireEvent.change(getByPlaceholderText('분류'), {
-        target: { value: '중식' },
+        target: { value: '양식' },
       });
 
       expect(dispatch).toBeCalledWith({
-        type: 'updateRestaurantCategory',
+        type: 'updateRestaurant',
         payload: {
-          category: '중식',
+          name: 'category',
+          value: '양식',
         },
       });
     });
   });
 
-  context('when restaurant address is changed', () => {
-    it('dispatches handleChangeAddress', () => {
-      const { getByPlaceholderText, getByDisplayValue } = render(
+  context('changing restaurant address', () => {
+    it('called handleChange with restaurant address', () => {
+      const { getByPlaceholderText } = render(
         <InputContainer />,
       );
 
-      expect(getByDisplayValue('서울시 강남구')).not.toBeNull();
-
       fireEvent.change(getByPlaceholderText('주소'), {
-        target: { value: '경기도 분당구' },
+        target: { value: '이태원동' },
       });
 
       expect(dispatch).toBeCalledWith({
-        type: 'updateRestaurantAddress',
+        type: 'updateRestaurant',
         payload: {
-          address: '경기도 분당구',
+          name: 'address',
+          value: '이태원동',
         },
       });
     });
