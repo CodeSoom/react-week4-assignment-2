7baf5b446e4d025a5dab9707685d42d19cbe6719
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-16 18:04:34 +0900

    [refactor] Apply fixture to App test.

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -4,21 +4,14 @@ import { render } from '@testing-library/react';
 
 import { useSelector } from 'react-redux';
 
+import fixture from '../__fixtuers__/fixture';
+
 import App from './App';
 
 jest.mock('react-redux');
 
 test('App', () => {
-  const restaurants = [
-    { name: '마녀주방', category: '한식', address: '서울시 강남구' },
-    { name: '시카고피자', category: '양식', address: '이태원동' },
-  ];
-
-  const inputInfos = [
-    { name: 'name', placeholder: '이름', value: '시카고피자' },
-    { name: 'category', placeholder: '분류', value: '양식' },
-    { name: 'address', placeholder: '주소', value: '이태원동' },
-  ];
+  const { restaurants, inputInfos } = fixture;
 
   useSelector.mockImplementation(() => ({
     restaurants,
