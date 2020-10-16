00e1eb794a5ad5a17da88d35a703812a6440f938
oceanide <oceanide@gmail.com>
2020-10-16 22:29:28 +0900

    Refactor(reducer.js): add reducer function

--- src/reducer.js
+++ src/reducer.js
@@ -1,1 +1,61 @@
-const reducers = {};
+const initialState = {
+  id: 100,
+  restaurant: {},
+  restaurants: [],
+};
+
+const reducers = {
+  updateRestaurantName(state, action) {
+    const { name } = action.payload;
+
+    return {
+      ...state,
+      restaurant: {
+        name,
+      },
+    };
+  },
+
+  updateRestaurantCategory(state, action) {
+    const { category } = action.payload;
+
+    return {
+      ...state,
+      restaurant: {
+        category,
+      },
+    };
+  },
+
+  updateRestaurantAddress(state, action) {
+    const { address } = action.payload;
+
+    return {
+      ...state,
+      restaurant: {
+        address,
+      },
+    };
+  },
+
+  addRestaurant(state) {
+    const { id, restaurant, restaurants } = state;
+
+    return {
+      ...state,
+      id: id + 1,
+      restaurant: {},
+      restaurants: [...restaurants, restaurant],
+    };
+  },
+};
+
+export default function reducer(state = initialState, action = {}) {
+  const reduce = reducers[action.type];
+
+  if (!reduce) {
+    return state;
+  }
+
+  return reduce(state, action);
+}
