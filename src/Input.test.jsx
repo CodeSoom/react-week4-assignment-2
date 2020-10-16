6727f3b9a0a43acd89830cdff4e0dc4d7ae8db20
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-16 11:15:49 +0900

    [red] Add name property test for Input.

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -7,17 +7,19 @@ import Input from './Input';
 test('Input', () => {
   const placeholderText = '이름';
   const newText = '마녀주방';
+  const name = 'name';
 
   const handleChangeInput = jest.fn();
 
   const { getByRole, getByPlaceholderText } = render((
     <Input
+      name={name}
       placeholderText={placeholderText}
       onChange={handleChangeInput}
     />
   ));
 
-  expect(getByRole('textbox')).not.toBeNull();
+  expect(getByRole('textbox')).toHaveAttribute('name', 'name');
 
   const input = getByPlaceholderText(placeholderText);
 
