e47c3931db0ebb288b43407b7b2aec48d18c7805
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-16 10:37:32 +0900

    [refactor] refactoring test for App.

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -31,11 +31,13 @@ test('App', () => {
 
   expect(getByText('Restaurants')).not.toBeNull();
 
-  expect(getByText('마녀주방 | 한식 | 서울시 강남구')).not.toBeNull();
-  expect(getByText('시카고피자 | 양식 | 이태원동')).not.toBeNull();
+  restaurants.forEach(({ name, category, address }) => {
+    expect(getByText(`${name} | ${category} | ${address}`)).not.toBeNull();
+  });
 
-  expect(getByPlaceholderText('이름')).not.toBeNull();
-  expect(getByPlaceholderText('분류')).not.toBeNull();
-  expect(getByPlaceholderText('주소')).not.toBeNull();
+  inputs.forEach(({ placeholder, value }) => {
+    expect(getByPlaceholderText(placeholder)).toHaveDisplayValue(value);
+  });
+
   expect(getByText('등록')).not.toBeNull();
 });
