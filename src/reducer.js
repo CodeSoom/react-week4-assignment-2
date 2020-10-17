94c67f04c9c6480c91b2fc9ea4b32438efb22694
saseungmin <dbd02169@naver.com>
2020-10-17 21:03:43 +0900

    reducer를 'addRestaurant' 구현한다

--- src/reducer.js
+++ src/reducer.js
@@ -1,9 +1,11 @@
 const initialState = {
+  id: 1,
   restaurant: {
     name: '',
     type: '',
     address: '',
   },
+  restaurants: [],
 };
 
 const reducers = {
@@ -14,6 +16,24 @@ const reducers = {
       [name]: value,
     },
   }),
+  addRestaurant: (state) => {
+    const { id, restaurant, restaurants } = state;
+    const { name, type, address } = restaurant;
+    if (!name || !type || !address) {
+      return state;
+    }
+
+    return {
+      ...state,
+      id: id + 1,
+      restaurant: {
+        name: '',
+        type: '',
+        address: '',
+      },
+      restaurants: [...restaurants, { id, ...restaurant }],
+    };
+  },
 };
 
 const reducer = (state = initialState, { type, payload }) => {
