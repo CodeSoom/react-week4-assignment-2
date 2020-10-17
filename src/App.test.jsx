b562be0e34f6917b8ccff4dac6bc704225653eb5
hyeonhyejin <dev.hyejin04@gmail.com>
2020-10-17 21:40:49 +0900

    Add test cases for App component

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -5,21 +5,24 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 describe('App', () => {
+  const { container, getByText, getByPlaceholderText } = render((
+    <App />
+  ));
+
   context('When first loaded', () => {
     it('show Restaurant title', () => {
-      const { container } = render((
-        <App />
-      ));
-
       expect(container).toHaveTextContent('Restaurant');
     });
 
     it('show input that add Restaurant into list', () => {
-
+      expect(getByPlaceholderText(/이름/)).not.toBeNull();
+      expect(getByPlaceholderText(/분류/)).not.toBeNull();
+      expect(getByPlaceholderText(/주소/)).not.toBeNull();
+      expect(getByText(/등록/)).not.toBeNull();
     });
 
     it('show Restaurant list', () => {
-
+      expect(getByText(/마녀주방 | 한식 | 서울시 강남구/)).not.toBeNull();
     });
   });
 });
