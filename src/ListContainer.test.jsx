8e9f9c8b98a4956b3911625cc79c684453f83b85
Dong A Kim <dongakim90@gmail.com>
2020-10-17 10:12:27 +0900

    ListContainer 테스트 수정
    useSelector mock 구현

--- src/ListContainer.test.jsx
+++ src/ListContainer.test.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 
+import { useSelector } from 'react-redux';
+
 import { render } from '@testing-library/react';
 
 import ListContainer from './ListContainer';
 
+jest.mock('react-redux');
+
 describe('<ListContainer />', () => {
+  useSelector.mockImplementation(() => ({
+    restaurants: ['낙원관 | 중식 | 서울 성동구'],
+  }));
   const renderList = () => render(<ListContainer />);
 
   describe('render', () => {
@@ -13,7 +20,7 @@ describe('<ListContainer />', () => {
       const { getByRole } = renderList();
 
       // Then
-      expect(getByRole('list')).toBeInTheDocument();
+      expect(getByRole('list')).toHaveTextContent('낙원관 | 중식 | 서울 성동구');
     });
   });
 });
