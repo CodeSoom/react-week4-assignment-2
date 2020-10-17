c925ac093e6044f4321a8d8d0b3ff35529aa7234
davidyang2149 <davidyang2149@gmail.com>
2020-10-17 16:29:48 +0900

    [Green] Update App.jsx for Implement
    4강 리덕스 - 4주 차 과제 2
    - App.jsx 업데이트.
    - 테스트 코드 작성 후 UI 구현.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,15 @@ import React from 'react';
 export default function App() {
   return (
     <div>
-      Restaurants
+      <h1>Restaurants</h1>
+      <p>
+        <input type="text" placeholder="이름" />
+        <input type="text" placeholder="분류" />
+        <input type="text" placeholder="주소" />
+        <button type="button">
+          등록
+      </button>
+      </p>
     </div>
   );
 }
