31bda70a37bcfed16c99cd848df6c20a6e993cd5
saseungmin <dbd02169@naver.com>
2020-10-17 19:25:33 +0900

    List 컴포넌트를 수정한다.

--- src/List.jsx
+++ src/List.jsx
@@ -2,12 +2,19 @@ import React from 'react';
 
 import Item from './Item';
 
-const List = ({ restaurants }) => (
-  <ul>
-    {restaurants.map((restaurant) => (
-      <Item key={restaurant.id} restaurant={restaurant} />
-    ))}
-  </ul>
-);
+const List = ({ restaurants }) => {
+  if (restaurants.length === 0) {
+    return (
+      <></>
+    );
+  }
+  return (
+    <ul>
+      {restaurants.map((restaurant) => (
+        <Item key={restaurant.id} restaurant={restaurant} />
+      ))}
+    </ul>
+  );
+};
 
 export default List;
