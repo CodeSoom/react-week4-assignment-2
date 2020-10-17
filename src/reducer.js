5b25b1a7a6f0285818cb8dbee79bcae7ef22c9d8
wjy5446 <wjy5446@gmail.com>
2020-10-17 14:17:03 +0900

    feat) reducer 작성

---
+++ src/reducer.js
@@ -0,0 +1,45 @@
+export const initialState = {
+  restaurantInfo: {
+    name: '',
+    type: '',
+    location: '',
+  },
+  listRestaurantInfo: [],
+};
+
+export default function reducer(state = initialState, action) {
+  const router = {
+    updateRestaurantName: {
+      ...state,
+      restaurantInfo: {
+        name: action.payload,
+      },
+    },
+    updateRestaurantType: {
+      ...state,
+      restaurantInfo: {
+        type: action.payload,
+      },
+    },
+    updateRestaurantLocation: {
+      ...state,
+      restaurantInfo: {
+        location: action.payload,
+      },
+    },
+    addRestaurantInfo: {
+      ...state,
+      restaurantInfo: {
+        name: '',
+        type: '',
+        location: '',
+      },
+      listRestaurantInfo: [
+        ...state.listRestaurantInfo,
+        action.payload,
+      ],
+    },
+  };
+
+  return router[action.type];
+};
