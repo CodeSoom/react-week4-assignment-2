6777036937ffb5ab7159c3e8be3643840adf746f
wjy5446 <wjy5446@gmail.com>
2020-10-17 14:21:42 +0900

    refac) reducer 렌더링시에 initialState를 사용

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,3 +1,4 @@
+import { updateRestaurantName, updateRestaurantType, updateRestaurantLocation, addRestaurantInfo } from './actions';
 import reducer, { initialState } from './reducer';
 
 describe('initialState', () => {
@@ -10,73 +11,61 @@ describe('initialState', () => {
   });
 
   it('초기 listRestaurantInfo은 빈 배열로 초기화됩니다.', () => {
-    const { listRestaurantInfo } = initialState.listRestaurantInfo;
+    const { listRestaurantInfo } = initialState;
 
     expect(listRestaurantInfo).toHaveLength(0);
   });
 });
 
 describe('reducer', () => {
-  const updateRestaurantName = jest.fn();
-  const updateRestaurantType = jest.fn();
-  const updateRestaurantLocation = jest.fn();
-  const addRestaurantInfo = jest.fn();
-
   describe('updateRestaurantName', () => {
     it('restaurantName을 반환합니다.', () => {
-      const state = reducer({
-        restaurantInfo: {
-          name: '',
-        },
-      }, updateRestaurantName('마녀주방'));
+      const { restaurantInfo: { name } } = reducer(
+        initialState,
+        updateRestaurantName('마녀주방'),
+      );
 
-      expect(state.restaurantName).toBe('마녀주방');
+      expect(name).toBe('마녀주방');
     });
   });
 
   describe('updateRestaurantType', () => {
     it('restaurantType을 반환합니다.', () => {
-      const state = reducer({
-        restaurantInfo: {
-          type: '',
-        },
-      }, updateRestaurantType('한식'));
+      const { restaurantInfo: { type } } = reducer(
+        initialState,
+        updateRestaurantType('한식'),
+      );
 
-      expect(state.restaurantType).toBe('한식');
+      expect(type).toBe('한식');
     });
   });
 
   describe('updateRestaurantLocation', () => {
     it('restaurantLocation을 반환합니다.', () => {
-      const state = reducer({
-        restaurantInfo: {
-          location: '',
-        },
-      }, updateRestaurantLocation('서울시 강남구'));
+      const { restaurantInfo: { location } } = reducer(
+        initialState,
+        updateRestaurantLocation('서울시 강남구'),
+      );
 
-      expect(state.restaurantLocation).toBe('서울시 강남구');
+      expect(location).toBe('서울시 강남구');
     });
   });
 
   describe('AddRestaurantInfo', () => {
     it('restaurantInfo을 리스트에 추가합니다.', () => {
-      const state = reducer({
-        restaurantInfo: {
-          name: '',
-          type: '',
-          location: '',
-        },
-        listRestaurantInfo: [],
-      }, addRestaurantInfo({ name: '마녀주방', type: '한식', location: '서울시 강남구' }));
+      const { listRestaurantInfo } = reducer(
+        initialState,
+        addRestaurantInfo({ name: '마녀주방', type: '한식', location: '서울시 강남구' }),
+      );
 
-      expect(state.listRestaurantInfo).toHaveLength(1);
-      expect(state.listRestaurantInfo[0].name).toBe('마녀주방');
-      expect(state.listRestaurantInfo[0].type).toBe('한식');
-      expect(state.listRestaurantInfo[0].location).toBe('서울시 강남구');
+      expect(listRestaurantInfo).toHaveLength(1);
+      expect(listRestaurantInfo[0].name).toBe('마녀주방');
+      expect(listRestaurantInfo[0].type).toBe('한식');
+      expect(listRestaurantInfo[0].location).toBe('서울시 강남구');
     });
 
     it('restaurantInfo가 초기화 됩니다.', () => {
-      const state = reducer({
+      const { restaurantInfo: { name, type, location } } = reducer({
         restaurantInfo: {
           name: '마녀주방',
           type: '한식',
@@ -85,9 +74,9 @@ describe('reducer', () => {
         listRestaurantInfo: [],
       }, addRestaurantInfo({ name: '마녀주방', type: '한식', location: '서울시 강남구' }));
 
-      expect(state.restaurantInfo.name).toBe('');
-      expect(state.restaurantInfo.type).toBe('');
-      expect(state.restaurantInfo.location).toBe('');
+      expect(name).toBe('');
+      expect(type).toBe('');
+      expect(location).toBe('');
     });
   });
 });
