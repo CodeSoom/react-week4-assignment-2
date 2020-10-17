d5c2c1d53b7cd3a215e3feb941dd4686550bc04d
wjy5446 <wjy5446@gmail.com>
2020-10-17 22:10:51 +0900

    feat) FormContainer 컴포넌트 추가

--- src/FormContainer.jsx
+++ src/FormContainer.jsx
@@ -1,9 +1,45 @@
 import React from 'react';
 
+import { useSelector, useDispatch } from 'react-redux';
+
+import {
+  updateRestaurantName,
+  updateRestaurantType,
+  updateRestaurantLocation,
+  addRestaurantInfo,
+} from './actions';
+
+import Form from './Form';
+
 export default function FormContainer() {
+
+  const dispatch = useDispatch();
+
+  const { restaurantInfo } = useSelector((state) => state);
+
+  function handleClickAdd() {
+    dispatch(addRestaurantInfo(restaurantInfo));
+  }
+
+  function handleChangeRestaurantName(event) {
+    dispatch(updateRestaurantName(event.target.value));
+  }
+
+  function handleChangeRestaurantType(event) {
+    dispatch(updateRestaurantType(event.target.value));
+  }
+
+  function handleChangeRestaurantLocation(event) {
+    dispatch(updateRestaurantLocation(event.target.value));
+  }
+
   return (
-    <div>
-      Hello
-    </div>
+    <Form
+      restaurantInfo={restaurantInfo}
+      handleClickAdd={handleClickAdd}
+      handleChangeRestaurantName={handleChangeRestaurantName}
+      handleChangeRestaurantType={handleChangeRestaurantType}
+      handleChangeRestaurantLocation={handleChangeRestaurantLocation}
+    />
   );
 }
