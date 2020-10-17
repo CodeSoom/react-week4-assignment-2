91f912acdb39165ed0678cdf0c969f7a74a6b847
davidyang2149 <davidyang2149@gmail.com>
2020-10-17 23:04:50 +0900

    [Refactor] Update App.test.jsx - Selector add
    4강 리덕스 - 4주 차 과제 2
    - App.test.jsx 수정(Update).
    - selector 함수 추가.
    - 테스트 params 추가.

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import { useSelector } from 'react-redux';
+
 import App from './App';
 
 describe('App', () => {
@@ -20,12 +22,38 @@ describe('App', () => {
 
   context('when render ListContainer', () => {
     it('show restaurant items ', () => {
+      const restaurants = [
+        {
+          id: 101,
+          name: '마녀주방',
+          category: '한식',
+          address: '서울시 강남구',
+        },
+        {
+          id: 102,
+          name: '시카고피자',
+          category: '양식',
+          address: '이태원동',
+        },
+        {
+          id: 103,
+          name: '카와미',
+          category: '일식',
+          address: '분당구 정자동',
+        },
+      ];
+
+      useSelector.mockImplementation((selector) => selector({
+        restaurants,
+      }));
+
       const { getByText } = render((
         <App />
       ));
 
       expect(getByText('마녀주방 | 한식 | 서울시 강남구')).not.toBeNull();
-      // expect(getByText('시카고피자 | 양식 | 이태원동')).not.toBeNull();
+      expect(getByText('시카고피자 | 양식 | 이태원동')).not.toBeNull();
+      expect(getByText('카와미 | 일식 | 분당구 정자동')).not.toBeNull();
     });
   });
 });
