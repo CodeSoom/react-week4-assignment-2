1e718768b348ec8aca825bd520dc28dbaa54aeaa
davidyang2149 <davidyang2149@gmail.com>
2020-10-17 22:44:43 +0900

    [Refactor] Update ListContainer.test.jsx - Selector add
    4강 리덕스 - 4주 차 과제 2
    - ListContainer.test.jsx 수정(Update).
    - Selector 추가.
    - 테스트를 위한 params 추가.

--- src/ListContainer.test.jsx
+++ src/ListContainer.test.jsx
@@ -2,15 +2,43 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import { useSelector } from 'react-redux';
+
 import ListContainer from './ListContainer';
 
 describe('ListContainer Component', () => {
-  it('Show restaurant items', () => {
+  it('Show List(restaurants)', () => {
+    const restaurants = [
+      {
+        id: 101,
+        name: '마녀주방',
+        category: '한식',
+        address: '서울시 강남구',
+      },
+      {
+        id: 102,
+        name: '시카고피자',
+        category: '양식',
+        address: '이태원동',
+      },
+      {
+        id: 103,
+        name: '카와미',
+        category: '일식',
+        address: '분당구 정자동',
+      },
+    ];
+
+    useSelector.mockImplementation((selector) => selector({
+      restaurants,
+    }));
+
     const { getByText } = render((
       <ListContainer />
     ));
 
     expect(getByText('마녀주방 | 한식 | 서울시 강남구')).not.toBeNull();
-    // expect(getByText('시카고피자 | 양식 | 이태원동')).not.toBeNull();
+    expect(getByText('시카고피자 | 양식 | 이태원동')).not.toBeNull();
+    expect(getByText('카와미 | 일식 | 분당구 정자동')).not.toBeNull();
   });
 });
