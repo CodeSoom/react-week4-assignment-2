784a3fe9d9e25bd1079b8cbe1ab613563c7a7056
wjy5446 <wjy5446@gmail.com>
2020-10-17 15:35:53 +0900

    feat) Form 컴포넌트 추가

--- src/Form.jsx
+++ src/Form.jsx
@@ -2,8 +2,38 @@ import React from 'react';
 
 import Input from './Input';
 
-export default function Form() {
+export default function Form({
+  restaurantInfo,
+  handleClickAdd,
+  handleChangeRestaurantName,
+  handleChangeRestaurantType,
+  handleChangeRestaurantLocation,
+}) {
+  const { name, type, location } = restaurantInfo;
+
   return (
-    <Input />
+    <>
+      <Input
+        placeholder="이름"
+        value={name}
+        onChange={handleChangeRestaurantName}
+      />
+      <Input
+        placeholder="분류"
+        value={type}
+        onChange={handleChangeRestaurantType}
+      />
+      <Input
+        placeholder="주소"
+        value={location}
+        onChange={handleChangeRestaurantLocation}
+      />
+      <button
+        type="submit"
+        onClick={handleClickAdd}
+      >
+        등록
+      </button>
+    </>
   );
 }
