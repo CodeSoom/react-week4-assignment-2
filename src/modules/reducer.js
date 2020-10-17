2b1469e82152f74241b8793951adefe02fbd72a0
juunini <juuni.ni.i@gmail.com>
2020-10-17 21:52:27 +0900

    [RED] Add reducer

---
+++ src/modules/reducer.js
@@ -0,0 +1,61 @@
+const initialState = {
+  newId: 1,
+  reservationName: '',
+  reservationCategory: '',
+  reservationAddress: '',
+  reservationse: [],
+};
+
+export default function reducer(state = initialState, action) {
+  const actions = {
+    updateReservationName: () => ({
+      ...state,
+      reservationName: action.payload.reservationName,
+    }),
+
+    updateReservationCategory: () => ({
+      ...state,
+      reservationCategory: action.payload.reservationCategory,
+    }),
+
+    updateReservationAddress: () => ({
+      ...state,
+      reservationAddress: action.payload.reservationAddress,
+    }),
+
+    addReservation: () => {
+      const {
+        newId,
+        reservations,
+        reservationName,
+        reservationCategory,
+        reservationAddress,
+      } = state;
+
+      if (!reservationName) return state;
+      if (!reservationCategory) return state;
+      if (!reservationAddress) return state;
+
+      return ({
+        ...state,
+        newId: newId + 1,
+        reservationName: '',
+        reservationCategory: '',
+        reservationAddress: '',
+        reservations: [
+          ...reservations,
+          {
+            id: newId,
+            name: reservationName,
+            category: reservationCategory,
+            address: reservationAddress,
+          },
+        ],
+      });
+    },
+  };
+
+  return actions[action.type]
+    ? actions[action.type]()
+    : state;
+}
