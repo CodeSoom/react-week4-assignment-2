87773dac1b370c34f2e940c936698f44b0487409
saseungmin <dbd02169@naver.com>
2020-10-17 19:00:31 +0900

    App 컴포넌트를 구현한다.

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,49 @@ import Page from './Page';
 
 export default function App() {
   const [state, setState] = useState({
+    id: 0,
     restaurant: {
       name: '',
       type: '',
       address: '',
     },
+    restaurants: [],
   });
 
-  function handleChangeTitle(event) {
+  const { id, restaurant, restaurants } = state;
+
+  function handleChangeRestaurant(event) {
     const { value, name } = event.target;
     setState({
       ...state,
       restaurant: {
-        ...state.restaurant,
+        ...restaurant,
         [name]: value,
       },
     });
   }
 
+  function handleClickSubmit() {
+    setState({
+      ...state,
+      restaurant: {
+        name: '',
+        type: '',
+        address: '',
+      },
+      id: id + 1,
+      restaurants: [...restaurants, { id, ...restaurant }],
+    });
+  }
+
   return (
     <>
-      <Page onChangeRestaurant={handleChangeTitle} />
+      <Page
+        restaurant={restaurant}
+        restaurants={restaurants}
+        onChangeRestaurant={handleChangeRestaurant}
+        onClickSubmit={handleClickSubmit}
+      />
     </>
   );
 }
