7358f0b7e894b65d8fa2115dc601e59bb93043f0
juunini <juuni.ni.i@gmail.com>
2020-10-17 22:26:35 +0900

    [REFACTORING] App.test.jsx

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -6,5 +6,11 @@ import App from './App';
 test('App', () => {
   render(<App />);
 
-  expect(screen.getByText('Restaurants')).toBeInTheDocument();
+  const { getByText, getByPlaceholderText, getByRole } = screen;
+
+  expect(getByText('Restaurants')).toBeInTheDocument();
+  expect(getByPlaceholderText('이름')).toBeInTheDocument();
+  expect(getByPlaceholderText('분류')).toBeInTheDocument();
+  expect(getByPlaceholderText('주소')).toBeInTheDocument();
+  expect(getByRole('button', { name: '등록' })).toBeInTheDocument();
 });
