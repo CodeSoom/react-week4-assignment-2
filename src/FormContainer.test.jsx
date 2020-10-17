87be59864cf5644a9f21deec992e5c98e4012a04
wjy5446 <wjy5446@gmail.com>
2020-10-17 21:36:33 +0900

    feat) dispatch가 호출되는 지에 대한 테스트 코드 작성

--- src/FormContainer.test.jsx
+++ src/FormContainer.test.jsx
@@ -1,12 +1,77 @@
 import React from 'react';
 
-import { render, fireEvent } from 'react-redux';
+import { render, fireEvent } from '@testing-library/react';
+
+import { useDispatch } from 'react-redux';
 
 import FormContainer from './FormContainer';
 
 jest.mock('react-redux');
 
 describe('FormContainer', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
+
+  it('이름 Input의 값이 입력되면 updateRestaurantName action이 dispatch 됩니다.', () => {
+    const { getByPlaceholderText } = render(<FormContainer />);
+    const inputRestaurantName = getByPlaceholderText('이름');
+
+    fireEvent.change(inputRestaurantName, { target: { value: '마녀주방' } });
+    expect(dispatch).toBeCalledWith({
+      type: 'updateRestaurantName',
+      payload: '마녀주방',
+    });
+  });
+
+  it('분류 Input의 값이 입력되면 updateRestaurantType action이 dispatch 됩니다.', () => {
+    const { getByPlaceholderText } = render(<FormContainer />);
+    const inputRestaurantType = getByPlaceholderText('분류');
+
+    fireEvent.change(inputRestaurantType, { target: { value: '힌식' } });
+    expect(dispatch).toBeCalledWith({
+      type: 'updateRestaurantType',
+      payload: '한식',
+    });
+  });
+
+  it('주소 Input의 값이 입력되면 updateRestaurantLocation action이 dispatch 됩니다.', () => {
+    const { getByPlaceholderText } = render(<FormContainer />);
+    const inputRestaurantLocation = getByPlaceholderText('주소');
+
+    fireEvent.change(inputRestaurantLocation, { target: { value: '서울시 강남구' } });
+    expect(dispatch).toBeCalledWith({
+      type: 'updateRestaurantLocation',
+      payload: '서울시 강남구',
+    });
+  });
+
+  it('등록 버튼을 클릭하면 addRestaurantInfo action이 dispatch 됩니다.', () => {
+    const { getByPlaceholderText, getByText } = render(<FormContainer />);
+    const inputRestaurantName = getByPlaceholderText('이름');
+    const inputRestaurantType = getByPlaceholderText('분류');
+    const inputRestaurantLocation = getByPlaceholderText('주소');
+    const buttonAdd = getByText('등록');
+
+    fireEvent.change(inputRestaurantName, { target: { value: '마녀주방' } });
+    fireEvent.change(inputRestaurantType, { target: { value: '한식' } });
+    fireEvent.change(inputRestaurantLocation, { target: { value: '서울시 강남구' } });
+    fireEvent.click(buttonAdd);
+
+    expect(dispatch).toBeCalledWith({
+      type: 'addRestaurantInfo',
+      payload: {
+        name: '마녀주방',
+        type: '한식',
+        location: '서울시 강남구',
+      },
+    });
+  });
+
   it('이름 Input의 값이 입력되면 이름 Input 값이 변화된다.', () => {
     const { getByPlaceholderText } = render(<FormContainer />);
     const inputRestaurantName = getByPlaceholderText('이름');
