9a77ccbf5f085ff99cf714f583eed83c23075db9
saseungmin <dbd02169@naver.com>
2020-10-17 22:39:56 +0900

    App 컴포넌트를 수정한다.

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,16 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
 
-import Page from './Page';
-
-import { addRestaurant, updateRestaurant } from './actions';
+import InputContainer from './InputContainer';
+import ListContainer from './ListContainer';
 
 export default function App() {
-  const dispatch = useDispatch();
-
-  const { restaurant, restaurants } = useSelector((state) => ({
-    restaurant: state.restaurant,
-    restaurants: state.restaurant,
-  }));
-
-  function handleChangeRestaurant(event) {
-    const { value, name } = event.target;
-
-    dispatch(updateRestaurant({ value, name }));
-  }
-
-  function handleClickSubmit() {
-    dispatch(addRestaurant());
-  }
-
   return (
     <>
-      <Page
-        restaurant={restaurant}
-        restaurants={restaurants}
-        onChangeRestaurant={handleChangeRestaurant}
-        onClickSubmit={handleClickSubmit}
-      />
+      <div>
+        Restaurants
+      </div>
+      <ListContainer />
+      <InputContainer />
     </>
   );
 }
