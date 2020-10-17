cc1c0de2b5d9e3c9a1972e57431a54ba0bac7aba
saseungmin <dbd02169@naver.com>
2020-10-17 17:39:54 +0900

    Fixed Item test code

--- src/Item.test.jsx
+++ src/Item.test.jsx
@@ -5,29 +5,19 @@ import { render } from '@testing-library/react';
 import Item from './Item';
 
 describe('Item', () => {
-  const itemRender = (restaurants) => render((
+  const itemRender = (restaurant) => render((
     <Item
-      restaurants={restaurants}
+      restaurant={restaurant}
     />
   ));
 
-  const restaurants = [
-    {
-      id: 1, name: '마녀주방', type: '한식', address: '서울시 강남구',
-    },
-    {
-      id: 2, name: '시카고피자', type: '양식', address: '이태원동',
-    },
-    {
-      id: 3, name: '키와미', type: '일식', address: '분당구 정자동',
-    },
-  ];
+  const restaurant = {
+    id: 1, name: '마녀주방', type: '한식', address: '서울시 강남구',
+  };
 
   it('"restaurant"이 보인다.', () => {
-    const { getByText } = itemRender(restaurants);
+    const { getByText } = itemRender(restaurant);
 
-    restaurants.forEach(({ name }) => {
-      expect(getByText(name)).not.toBeNull();
-    });
+    expect(getByText(/마녀주방/)).not.toBeNull();
   });
 });
