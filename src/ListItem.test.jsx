c993bb688ce87a247e57d59a30f80e19c1b0b997
davidyang2149 <davidyang2149@gmail.com>
2020-10-17 22:06:04 +0900

    [Refactor] Update ListItem.test.jsx - Add object param
    4강 리덕스 - 4주 차 과제 2
    - ListItem.test.jsx 수정(Update).
    - ListItem.test object param 케이스 추가.

--- src/ListItem.test.jsx
+++ src/ListItem.test.jsx
@@ -6,8 +6,15 @@ import ListItem from './ListItem';
 
 describe('ListItem Component', () => {
   it('Show single item(Restaurant)', () => {
+    const restaurant = {
+      id: 101,
+      name: '마녀주방',
+      category: '한식',
+      address: '서울시 강남구',
+    };
+
     const { getByText } = render((
-      <ListItem />
+      <ListItem restaurant={restaurant} />
     ));
 
     expect(getByText('마녀주방 | 한식 | 서울시 강남구')).not.toBeNull();
