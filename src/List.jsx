84d4b3554b970af38dfb6ae39b8d7d747ac04bd7
davidyang2149 <davidyang2149@gmail.com>
2020-10-17 18:14:04 +0900

    [Green] Add List.jsx for Implement
    4강 리덕스 - 4주 차 과제 2
    - List.jsx 생성.
    - List 추가 및 테스트용 UI 구현.

---
+++ src/List.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function List() {
+  return (
+    <li>
+      <ul>
+        마녀주방 | 한식 | 서울시 강남구
+      </ul>
+      <ul>
+        시카고피자 | 양식 | 이태원동
+      </ul>
+    </li>
+  );
+}
