e10a8e5fa1994c23452816676503efb45ab62141
davidyang2149 <davidyang2149@gmail.com>
2020-10-17 20:31:35 +0900

    [Refactor] Update App.test.jsx - ListContainer test case add
    4강 리덕스 - 4주 차 과제 2
    - App.test.jsx 수정.
    - ListContainer 컴포넌트 추가에 따른 테스트 코드 추가.
    - 일부 테스트 문구 수정.

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -5,7 +5,7 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 describe('App', () => {
-  it('exist title, input, submit', () => {
+  it('exist title, input, button', () => {
     const { getByText, getByPlaceholderText } = render((
       <App />
     ));
@@ -16,5 +16,16 @@ describe('App', () => {
     expect(getByPlaceholderText('분류')).not.toBeNull();
     expect(getByPlaceholderText('주소')).not.toBeNull();
     expect(getByText('등록')).not.toBeNull();
+  });
+
+  context('when render ListContainer', () => {
+    it('show restaurant items ', () => {
+      const { getByText } = render((
+        <App />
+      ));
+
+      expect(getByText('마녀주방 | 한식 | 서울시 강남구')).not.toBeNull();
+      expect(getByText('시카고피자 | 양식 | 이태원동')).not.toBeNull();
+    });
   });
 });
