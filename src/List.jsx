4e7c0597ff01caff78b75aa693fb46ad5e5e0bc5
saseungmin <dbd02169@naver.com>
2020-10-17 18:04:20 +0900

    Fixed Item test, List test

--- src/List.jsx
+++ src/List.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 import Item from './Item';
 
 const List = ({ restaurants }) => (
-  <ol>
+  <ul>
     {restaurants.map((restaurant) => (
       <Item key={restaurant.id} restaurant={restaurant} />
     ))}
-  </ol>
+  </ul>
 );
 
 export default List;
--- src/List.test.jsx
+++ src/List.test.jsx
@@ -20,7 +20,7 @@ describe('List', () => {
         id: 2, name: '시카고피자', type: '양식', address: '이태원동',
       },
       {
-        id: 2, name: '키와미', type: '일식', address: '분당구 정자동',
+        id: 3, name: '키와미', type: '일식', address: '분당구 정자동',
       },
     ];
 
