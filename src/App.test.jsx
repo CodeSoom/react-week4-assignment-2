ad0895121b8d5ed3d5d128ffe58823978cd4df19
davidyang2149 <davidyang2149@gmail.com>
2020-10-17 15:50:53 +0900

    [Red] Add App.test.jsx for TDD
    4강 리덕스 - 4주 차 과제 2
    - App.test.jsx 추가.
    - App 테스트 코드 작성(Red).

---
+++ src/App.test.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import App from './App';
+
+describe('App', () => {
+  it('exist title, input, submit', () => {
+    const { getByText, getByPlaceholderText } = render((
+      <App />
+    ));
+
+    expect(getByText('Restaurants')).not.toBeNull();
+
+    expect(getByPlaceholderText('이름')).not.toBeNull();
+    expect(getByPlaceholderText('분류')).not.toBeNull();
+    expect(getByPlaceholderText('주소')).not.toBeNull();
+    expect(getByText('등록')).not.toBeNull();
+  });
+});
