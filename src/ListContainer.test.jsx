6067669e884d2322418a433ab87f9400d5f279f7
davidyang2149 <davidyang2149@gmail.com>
2020-10-17 20:09:30 +0900

    [Red] Add ListContainer.test.jsx for TDD
    4강 리덕스 - 4주 차 과제 2
    - ListContainer.test.jsx 추가.
    - ListContainer 테스트 코드 작성(Red).

---
+++ src/ListContainer.test.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import ListContainer from './ListContainer';
+
+describe('ListContainer Component', () => {
+  it('Show restaurant items', () => {
+    const { getByText } = render((
+      <ListContainer />
+    ));
+
+    expect(getByText('마녀주방 | 한식 | 서울시 강남구')).not.toBeNull();
+    expect(getByText('시카고피자 | 양식 | 이태원동')).not.toBeNull();
+  });
+});
