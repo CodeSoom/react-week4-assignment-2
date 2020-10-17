3d4f5526b95c8f6cfd8082754bc4fb0a0dc93284
juunini <juuni.ni.i@gmail.com>
2020-10-17 20:24:30 +0900

    [RED] Add component InputReservation

---
+++ src/components/__test__/InputReservation.test.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+
+import InputReservation from '../InputReservation';
+
+describe('InputReservation', () => {
+  const handleChangeName = jest.fn();
+  const handleChangeCategory = jest.fn();
+  const handleChangeAddress = jest.fn();
+  const handleClick = jest.fn();
+
+  const renderInputReservation = () => render((
+    <InputReservation
+      onChangeName={handleChangeName}
+      onChangeCategory={handleChangeCategory}
+      onChangeAddress={handleChangeAddress}
+      onClick={handleClick}
+    />
+  ));
+
+  it('input과 button 확인', () => {
+    renderInputReservation();
+
+    const { getByPlaceholderText, getByRole } = screen;
+
+    expect(getByPlaceholderText('이름')).toBeInTheDocument();
+    expect(getByPlaceholderText('분류')).toBeInTheDocument();
+    expect(getByPlaceholderText('주소')).toBeInTheDocument();
+    expect(getByRole('button', { name: '등록' })).toBeInTheDocument();
+  });
+
+  it('onChangeName 테스트', () => {
+    const { getByPlaceholderText } = screen;
+    const input = getByPlaceholderText('이름');
+
+    expect(handleChangeName).not.toBeCalled();
+
+    fireEvent.change(input, { target: { value: '마녀주방' } });
+
+    expect(handleChangeName).toBeCalled();
+  });
+
+  it('onChangeCategory 테스트', () => {
+    const { getByPlaceholderText } = screen;
+    const input = getByPlaceholderText('분류');
+
+    expect(handleChangeCategory).not.toBeCalled();
+
+    fireEvent.change(input, { target: { value: '한식' } });
+
+    expect(handleChangeCategory).toBeCalled();
+  });
+
+  it('onChangeAddress 테스트', () => {
+    const { getByPlaceholderText } = screen;
+    const input = getByPlaceholderText('주소');
+
+    expect(handleChangeAddress).not.toBeCalled();
+
+    fireEvent.change(input, { target: { value: '서울시 강남구' } });
+
+    expect(handleChangeAddress).toBeCalled();
+  });
+
+  it('onClick 테스트', () => {
+    const { getByRole } = screen;
+    const button = getByRole('button', { name: '등록' });
+
+    expect(handleClick).not.toBeCalled();
+
+    fireEvent.click(button);
+
+    expect(handleClick).toBeCalled();
+  });
+});
