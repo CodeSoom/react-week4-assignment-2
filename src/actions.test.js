4536958b06c7ffa19e368f9e6270cf249ffb171b
wjy5446 <wjy5446@gmail.com>
2020-10-17 13:53:10 +0900

    refac) initialStatet의 listRestaurantInfo 배열을 초기화 하는 테스트 코드 리팩토링

--- src/actions.test.js
+++ src/actions.test.js
@@ -1,4 +1,4 @@
-import { updateRestaurantName } from './actions';
+import { updateRestaurantName, updateRestaurantType, updateRestaurantLocation, addRestaurantInfo } from './actions';
 
 describe('updateRestaurantName', () => {
   it('restaurantInfoName action을 반환합니다.', () => {
@@ -8,5 +8,44 @@ describe('updateRestaurantName', () => {
 
     expect(action.type).toBe('updateRestaurantName');
     expect(action.payload).toBe(restaurantName);
+  });
+});
+
+describe('updateRestaurantType', () => {
+  it('restaurantInfoType action을 반환합니다.', () => {
+    const restaurantType = '한식';
+
+    const action = updateRestaurantType(restaurantType);
+
+    expect(action.type).toBe('updateRestaurantType');
+    expect(action.payload).toBe(restaurantType);
+  });
+});
+
+describe('updateRestaurantLocation', () => {
+  it('restaurantInfoLocation action을 반환합니다.', () => {
+    const restaurantLocation = '서울시 강남구';
+
+    const action = updateRestaurantLocation(restaurantLocation);
+
+    expect(action.type).toBe('updateRestaurantLocation');
+    expect(action.payload).toBe(restaurantLocation);
+  });
+});
+
+describe('addRestaurantInfo', () => {
+  it('addRestaurantInfo action을 반환합니다.', () => {
+    const restaurantInfo = {
+      name: '마녀주방',
+      type: '일식',
+      location: '서울시 강남구',
+    };
+
+    const action = addRestaurantInfo(restaurantInfo);
+
+    expect(action.type).toBe('addRestaurantInfo');
+    Object.keys(restaurantInfo).forEach((key) => {
+      expect(action.payload[key]).toBe(restaurantInfo[key]);
+    });
   });
 });
