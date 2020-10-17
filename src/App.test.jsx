78e1477b04940679c9a1c67a5eba035e1a7229fb
saseungmin <dbd02169@naver.com>
2020-10-17 18:48:51 +0900

    App 컴포넌트에 대한 테스트를 구현한다.

---
+++ src/App.test.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import { fireEvent, render } from '@testing-library/react';
+
+import App from './App';
+
+describe('App', () => {
+  const appRender = () => render((
+    <App />
+  ));
+
+  const inputType = [
+    { placeholderName: '이름', inputName: 'name', value: '마녀주방' },
+    { placeholderName: '분류', inputName: 'type', value: '한식' },
+    { placeholderName: '주소', inputName: 'address', value: '서울시 강남구' },
+  ];
+
+  it('"input" 입력 후 등록 버튼 클릭 후 화면에 나타나는지 확인한다.', () => {
+    const { getByPlaceholderText, getByText, container } = appRender();
+
+    inputType.forEach(({ placeholderName, inputName, value }) => {
+      fireEvent.change(getByPlaceholderText(placeholderName), {
+        target: { value, name: inputName },
+      });
+
+      expect(getByPlaceholderText(placeholderName)).toHaveDisplayValue(value);
+    });
+
+    fireEvent.click(getByText(/등록/));
+
+    inputType.forEach(({ placeholderName, value }) => {
+      expect(getByPlaceholderText(placeholderName)).toHaveDisplayValue('');
+      expect(container).toHaveTextContent(value);
+    });
+  });
+});
