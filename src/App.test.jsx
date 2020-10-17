83c945a879e4226e773b622e5a411611592fba15
thkim <thkim2017@gmail.com>
2020-10-17 20:51:31 +0900

    make App.test.jsx

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import { render } from 'react-dom';
+import { getByPlaceholderText, render } from '@testing-library/react';
 
 import App from './App';
 
 test('App', () => {
-  render((
+  const { getByText } = render((
     <App />
   ));
+
+  expect(getByText(/Restaurants/)).not.toBeNull();
+  expect(getByPlaceholderText(/이름/)).not.toBeNull();
+  expect(getByPlaceholderText(/분류/)).not.toBeNull();
+  expect(getByPlaceholderText(/주소/)).not.toBeNull();
+  expect(getByText(/등록/)).not.toBeNull();
 });
