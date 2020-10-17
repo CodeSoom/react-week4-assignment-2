30a67ea013dfe22d777e02169802f8b07796b5a9
davidyang2149 <davidyang2149@gmail.com>
2020-10-17 20:38:04 +0900

    [Refactor] Update List.jsx - link ListItem
    4강 리덕스 - 4주 차 과제 2
    - List.jsx 업데이트.
    - ListItem 컴포넌트 연결.

--- src/List.jsx
+++ src/List.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 
+import ListItem from './ListItem';
+
 export default function List() {
   return (
-    <li>
-      <ul>
-        마녀주방 | 한식 | 서울시 강남구
-      </ul>
-      <ul>
-        시카고피자 | 양식 | 이태원동
-      </ul>
-    </li>
+    <ul>
+      <ListItem />
+    </ul>
   );
 }
