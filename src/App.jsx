f151b0ce656ab20133a83627b8f17795aa465090
juunini <juuni.ni.i@gmail.com>
2020-10-17 22:24:41 +0900

    [REFACTORING] Add App to InputReservationContainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import ListContainer from './containers/ListContainer';
+import InputReservationContainer from './containers/InputReservationContainer';
 
 export default function App() {
   return (
     <div>
       <h1>Restaurants</h1>
       <ListContainer />
+      <InputReservationContainer />
     </div>
   );
 }
