591d70c7f9276b6c4927c623aa3e68b43c9fc8fc
saseungmin <dbd02169@naver.com>
2020-10-17 20:48:49 +0900

    reducer 'addRestaurant'의 테스트를 구현한다.

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,6 +1,6 @@
 import reducer from './reducer';
 
-import { updateRestaurant } from './actions';
+import { updateRestaurant, addRestaurant } from './actions';
 
 describe('reducer', () => {
   describe('updateRestaurant', () => {
@@ -21,6 +21,42 @@ describe('reducer', () => {
         }, updateRestaurant({ value, name }));
 
         expect(restaurant[name]).toBe(value);
+      });
+    });
+  });
+
+  describe('addRestaurant', () => {
+    const addRestaurantReducer = (restaurant) => reducer({
+      id: 1,
+      restaurant,
+      restaurants: [],
+    }, addRestaurant());
+
+    context('with restaurant', () => {
+      const restaurantInfo = [
+        { name: '마녀주방', type: '한식', address: '서울시 강남구' },
+        { name: '시카고피자', type: '양식', address: '이태원동' },
+        { name: '키와미', type: '일식', address: '분당구 정자동' },
+      ];
+
+      it('레스토랑에 대한 정보를 등록한다.', () => {
+        restaurantInfo.forEach((item, index) => {
+          const { restaurant, restaurants } = addRestaurantReducer(item);
+
+          expect(restaurants[0].id).not.toBeUndefined();
+          expect(restaurants).toHaveLength(index + 1);
+          expect(restaurants[restaurants.length - 1].id).toBe(index);
+          expect(restaurant).toBe({ name: '', type: '', address: '' });
+        });
+      });
+    });
+
+    context('without restaurant', () => {
+      const restaurantInfo = { name: '', type: '', address: '' };
+      it('할 일이 추가되지 않는다.', () => {
+        const { restaurants } = addRestaurantReducer(restaurantInfo);
+
+        expect(restaurants).toHaveLength(0);
       });
     });
   });
