88588cc7ccbae4b1b2980284978b21da4e2c16e5
Woong <37302935+TaeWoongKim2@users.noreply.github.com>
2020-10-18 07:42:10 +0900

    [Red] App에 대한 테스트 추가
    redux 적용을 위한 App 테스트 변경

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,15 +2,44 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import { useSelector } from 'react-redux';
+
 import App from './App';
 
+jest.mock('react-redux');
+
 test('Restaurant App', () => {
-  const { getByText, getByPlaceholderText } = render((
+  const restaurants = [
+    {
+      id: 100,
+      name: '마녀주방',
+      type: '양식',
+      address: '강남구 서현동',
+    },
+    {
+      id: 101,
+      name: '시카고피자',
+      type: '양식',
+      address: '용산구 이태원동',
+    },
+    {
+      id: 102,
+      name: '카와미',
+      type: '일식',
+      address: '분당구 정자동',
+    },
+  ];
+
+  useSelector.mockImplementation((selector) => selector({
+    restaurants,
+  }));
+
+  const { container, getByText, getByPlaceholderText } = render((
     <App />
   ));
 
   // App name
-  expect(getByText('Restaurants')).not.toBeNull();
+  expect(container).toHaveTextContent('Restaurants');
 
   // Input List
   expect(getByPlaceholderText('이름')).not.toBeNull();
