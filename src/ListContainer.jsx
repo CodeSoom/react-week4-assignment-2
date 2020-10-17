6f8413a450ae19d20a8ca77c1ea868889cd80d02
Dong A Kim <dongakim90@gmail.com>
2020-10-17 10:13:05 +0900

    ListContainer 구현
    redux-store 연결

--- src/ListContainer.jsx
+++ src/ListContainer.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import { useSelector } from 'react-redux';
+
 export default function ListContainer() {
+  const { restaurants } = useSelector((state) => state);
+
   return (
     <ul>
-      <li>a restaurant</li>
+      {restaurants.map((restaurant) => <li>{restaurant}</li>)}
     </ul>
   );
 }
