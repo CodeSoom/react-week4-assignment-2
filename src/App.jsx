13b3365aadc8d87163a103183fcd946f6dd33628
juunini <juuni.ni.i@gmail.com>
2020-10-17 22:24:08 +0900

    [REFACTORING] Add App to ListContainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import ListContainer from './containers/ListContainer';
+
 export default function App() {
   return (
     <div>
       <h1>Restaurants</h1>
+      <ListContainer />
     </div>
   );
 }
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import Item from './Item';
 
 export default function List({ reservations }) {
+  if (!reservations) return (<></>);
+
   return (
     <ul>
       {
