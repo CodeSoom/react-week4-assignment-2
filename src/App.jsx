e5a4b6869dc9868489ba57cc659e07b1d6bf25f0
Dong A Kim <dongakim90@gmail.com>
2020-10-17 10:14:42 +0900

    ListContainer 적용
    App 컴포넌트에 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import InputContainer from './InputContainer';
+import ListContainer from './ListContainer';
 
 export default function App() {
   return (
     <div>
       <h1>Restaurants</h1>
+      <ListContainer />
       <InputContainer />
     </div>
   );
