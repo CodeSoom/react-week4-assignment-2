4226c5ddf41f6c455c8ac6d80cf65e26e9051aeb
davidyang2149 <davidyang2149@gmail.com>
2020-10-18 14:44:15 +0900

    [Change] ListItem.test.jsx -> Item.test.jsx
    4강 리덕스 - 4주 차 과제 2
    - 파일 이름 변경.
    - ListItem.test.jsx -> Item.test.jsx.

---
+++ src/Item.test.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import Item from './Item';
+
+describe('Item Component', () => {
+  it('Show single item(Restaurant)', () => {
+    const restaurant = {
+      id: 101,
+      name: '마녀주방',
+      category: '한식',
+      address: '서울시 강남구',
+    };
+
+    const { getByText } = render((
+      <Item restaurant={restaurant} />
+    ));
+
+    expect(getByText('마녀주방 | 한식 | 서울시 강남구')).not.toBeNull();
+  });
+});
