bbf7fe751c3ccf8eefe21fae8e93db90142a0975
thkim <thkim2017@gmail.com>
2020-10-18 16:03:19 +0900

    [red] Create a test for List.jsx

---
+++ src/List.test.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import List from './List';
+
+test('List', () => {
+  const reservations = [
+    { name: '대천부양꼬치', type: '중식', address: '경기도 남양주시' },
+    { name: '바토스', type: '양식', address: '이태원동' },
+  ];
+
+  const { getByText } = render((
+    <List reservations={reservations} />
+  ));
+
+  expect(getByText('대천부양꼬치 | 중식 | 경기도 남양주시')).not.toBeNull();
+  expect(getByText('바토스 | 양식 | 이태원동')).not.toBeNull();
+});
