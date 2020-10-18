f65d9c6c4851091549f792f90142d1ba804a6bd9
JamieShin0201 <65442049+JamieShin0201@users.noreply.github.com>
2020-10-18 13:06:00 +0900

    Add test List.test.jsx

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -4,29 +4,45 @@ import { render } from '@testing-library/react';
 
 import List from './List';
 
-test('List', () => {
-  const restaurants = [
-    {
-      name: '마녀주방',
-      classification: '한식',
-      location: '서울시 강남구',
-    },
-    {
-      name: '시카고피자',
-      classification: '양식',
-      location: '이태원동',
-    },
-  ];
+describe('List', () => {
+  function renderList(restaurants) {
+    return render(
+      <List restaurants={restaurants} />,
+    );
+  }
 
-  const { container } = render((
-    <List
-      restaurants={restaurants}
-    />
-  ));
+  context('with restaurants', () => {
+    const restaurants = [
+      {
+        name: '마녀주방',
+        classification: '한식',
+        location: '서울시 강남구',
+      },
+      {
+        name: '시카고피자',
+        classification: '양식',
+        location: '이태원동',
+      },
+    ];
 
-  restaurants.forEach(({ name, classification, location }) => {
-    expect(container).toHaveTextContent(name);
-    expect(container).toHaveTextContent(classification);
-    expect(container).toHaveTextContent(location);
+    it('renders restaurants', () => {
+      const { container } = renderList(restaurants);
+
+      restaurants.forEach(({ name, classification, location }) => {
+        expect(container).toHaveTextContent(name);
+        expect(container).toHaveTextContent(classification);
+        expect(container).toHaveTextContent(location);
+      });
+    });
+  });
+
+  context('without restaurants', () => {
+    const restaurants = [];
+
+    it('renders empty', () => {
+      const { container } = renderList(restaurants);
+
+      expect(container).toHaveTextContent('');
+    });
   });
 });
