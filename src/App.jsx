1473fad0709e018c4e6e0275aaa57267a2fa342b
thkim <thkim2017@gmail.com>
2020-10-18 12:31:19 +0900

    make Input.jsx be passed

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,12 @@
 import React from 'react';
 
+import Input from './Input';
+
 export default function App() {
   return (
     <div>
       <h1>Restaurants</h1>
-      <input
-        id="input-restaurant-name"
-        type="text"
-        placeholder="이름"
-      />
-      <input
-        id="input-restaurant-type"
-        type="text"
-        placeholder="분류"
-      />
-      <input
-        id="input-restaurant-address"
-        type="text"
-        placeholder="주소"
-      />
-      <button type="button">
-        등록
-      </button>
+      <Input />
     </div>
   );
 }
