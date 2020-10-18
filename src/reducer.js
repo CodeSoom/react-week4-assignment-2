d609590e7194c59d1b3b10e013f333597dee5ec2
Seungmin Lee <seungmin@uxplus.dev>
2020-10-18 22:05:01 +0900

    Refactor reducer
    Refactor reducer to create pure functions.

--- src/reducer.js
+++ src/reducer.js
@@ -6,54 +6,53 @@ const initialState = {
   location: '',
 };
 
+const reducers = {
+  changeName: (state, { payload }) => ({
+    ...state,
+    name: payload.name,
+  }),
+  changeCuisine: (state, { payload }) => ({
+    ...state,
+    cuisine: payload.cuisine,
+  }),
+  changeLocation: (state, { payload }) => ({
+    ...state,
+    location: payload.location,
+  }),
+  clickAddRestaurant: (state) => {
+    const {
+      newId,
+      name,
+      cuisine,
+      location,
+      restaurants,
+    } = state;
+
+    return {
+      ...state,
+      newId: state.id + 1,
+      restaurants: [
+        ...restaurants,
+        {
+          id: newId,
+          name,
+          cuisine,
+          location,
+        },
+      ],
+      name: '',
+      cuisine: '',
+      location: '',
+    };
+  },
+};
+
 export default function reducer(state = initialState, action) {
   if (!action || action.type.indexOf('@@') > -1) {
     return state;
   }
 
-  const { newId, restaurants } = state;
-  const { type, payload } = action;
+  const { type } = action;
 
-  function returnNextState() {
-    return {
-      changeName: () => ({
-        ...state,
-        name: payload.name,
-      }),
-      changeCuisine: () => ({
-        ...state,
-        cuisine: payload.cuisine,
-      }),
-      changeLocation: () => ({
-        ...state,
-        location: payload.location,
-      }),
-      clickAddRestaurant: () => {
-        const {
-          name,
-          cuisine,
-          location,
-        } = state;
-
-        return {
-          ...state,
-          newId: state.id + 1,
-          restaurants: [
-            ...restaurants,
-            {
-              id: newId,
-              name,
-              cuisine,
-              location,
-            },
-          ],
-          name: '',
-          cuisine: '',
-          location: '',
-        };
-      },
-    }[type]();
-  }
-
-  return returnNextState();
+  return reducers[type](state, action);
 }
