4bc25c6dae61a64cb516c7e26d48899be6a562bc
Seungmin Lee <seungmin@uxplus.dev>
2020-10-18 22:13:41 +0900

    Remove unused code
    Remove unused code to declutter.

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,27 +2,9 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
-import { useSelector, useDispatch } from 'react-redux';
-
 import App from './App';
 
-jest.mock('react-redux');
-
 describe('App', () => {
-  const restaurants = [
-    {
-      id: 1,
-      name: '마녀주방',
-      cuisine: '한식',
-      location: '서울시 강남구',
-    },
-  ];
-
-  const dispatch = jest.fn();
-
-  useSelector.mockImplementation((selector) => selector(restaurants));
-  useDispatch.mockReturnValue(dispatch);
-
   it('renders', () => {
     const { container } = render(<App />);
 
