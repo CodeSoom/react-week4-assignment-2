df508b92f01e6c5b2e15627484df1d050414aaac
hyeonhyejin <dev.hyejin04@gmail.com>
2020-10-18 21:04:33 +0900

    Implement onChange for rastaurant category input element

--- src/InputContainer.jsx
+++ src/InputContainer.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 
-import { addRestaurant, updateRestaurantName } from './actions';
+import {
+  addRestaurant,
+  updateRestaurantCategory,
+  updateRestaurantName,
+} from './actions';
 
 export default function InputContainer() {
   const dispatch = useDispatch();
@@ -14,10 +18,14 @@ export default function InputContainer() {
     dispatch(updateRestaurantName(event.target.value));
   }
 
+  function handleChangeCategory(event) {
+    dispatch(updateRestaurantCategory(event.target.value));
+  }
+
   return (
     <div>
       <input placeholder="이름" onChange={handleChangeName} />
-      <input placeholder="분류" />
+      <input placeholder="분류" onChange={handleChangeCategory} />
       <input placeholder="주소" />
       <button type="button" onClick={handleClickAddRestaurant}>
         등록
