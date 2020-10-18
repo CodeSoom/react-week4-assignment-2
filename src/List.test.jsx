62ac96648b5e9fdc74b021e3537359abd0ea5cbf
Seungmin Lee <seungmin@uxplus.dev>
2020-10-18 21:44:34 +0900

    Add test code
    Add test code to move redux related code from List to ListContainer.

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -2,31 +2,21 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
-import { useSelector, useDispatch } from 'react-redux';
-
 import List from './List';
 
-jest.mock('react-redux');
-
 describe('List', () => {
-  const dispatch = jest.fn();
-
-  beforeEach(() => {
-    useSelector.mockImplementation((selector) => selector({
-      restaurants: [
+  it('renders', () => {
+    const { container } = render(<List restaurants={
+      [
         {
           id: 1,
           name: '마녀주방',
           cuisine: '한식',
           location: '서울시 강남구',
         },
-      ],
-    }));
-    useDispatch.mockReturnValue(dispatch);
-  });
-
-  it('renders', () => {
-    const { container } = render(<List />);
+      ]
+    }
+    />);
 
     expect(container).not.toBeNull();
     expect(container).toContainHTML('<li');
--- src/ListContainer.test.jsx
+++ src/ListContainer.test.jsx
@@ -2,9 +2,28 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import { useSelector } from 'react-redux';
+
 import ListContainer from './ListContainer';
 
+jest.mock('react-redux');
+
 describe('ListContainer', () => {
+  beforeEach(() => {
+    useSelector.mockImplementation((selector) => selector(
+      {
+        restaurants: [
+          {
+            id: 1,
+            name: '마녀주방',
+            cuisine: '한식',
+            location: '서울시 강남구',
+          },
+        ],
+      },
+    ));
+  });
+
   it('renders List', () => {
     const { container } = render(<ListContainer />);
 
