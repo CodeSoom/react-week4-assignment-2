5423a986c8e09a6c2061af3e3a48ed4494ed2f4a
thkim <thkim2017@gmail.com>
2020-10-18 15:36:13 +0900

    삽질 후 다시 시작- [red] Create a test for App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
 
-import InputContainer from './InputContainer';
-
 export default function App() {
   return (
     <div>
       <h1>Restaurants</h1>
-      <InputContainer />
     </div>
   );
 }
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -4,9 +4,16 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 test('App', () => {
-  const { getByText } = render((
+  const { getByText, getByPlaceholderText } = render((
     <App />
   ));
 
+  expect(getByText('대천부양꼬치 | 양식 | 경기도 남양주시')).not.toBeNull();
+  expect(getByText('바토스 | 양식 | 이태원동')).not.toBeNull();
+
   expect(getByText(/Restaurants/)).not.toBeNull();
+  expect(getByPlaceholderText(/이름/)).not.toBeNull();
+  expect(getByPlaceholderText(/분류/)).not.toBeNull();
+  expect(getByPlaceholderText(/주소/)).not.toBeNull();
+  expect(getByText(/등록/)).not.toBeNull();
 });
