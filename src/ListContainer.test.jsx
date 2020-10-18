918aec34fc1f1f65782c15bc4abe14dc05dc6a00
davidyang2149 <davidyang2149@gmail.com>
2020-10-18 15:17:01 +0900

    [Refactor] Update ListContainer.test.jsx
    4강 리덕스 - 4주 차 과제 2
    - ListContainer.test.jsx 수정(Update).
    - 변수의 위치 변경.

--- src/ListContainer.test.jsx
+++ src/ListContainer.test.jsx
@@ -7,28 +7,28 @@ import { useSelector } from 'react-redux';
 import ListContainer from './ListContainer';
 
 describe('ListContainer Component', () => {
-  it('Show List(restaurants)', () => {
-    const restaurants = [
-      {
-        id: 101,
-        name: '마녀주방',
-        category: '한식',
-        address: '서울시 강남구',
-      },
-      {
-        id: 102,
-        name: '시카고피자',
-        category: '양식',
-        address: '이태원동',
-      },
-      {
-        id: 103,
-        name: '카와미',
-        category: '일식',
-        address: '분당구 정자동',
-      },
-    ];
+  const restaurants = [
+    {
+      id: 101,
+      name: '마녀주방',
+      category: '한식',
+      address: '서울시 강남구',
+    },
+    {
+      id: 102,
+      name: '시카고피자',
+      category: '양식',
+      address: '이태원동',
+    },
+    {
+      id: 103,
+      name: '카와미',
+      category: '일식',
+      address: '분당구 정자동',
+    },
+  ];
 
+  it('Show List(restaurants)', () => {
     useSelector.mockImplementation((selector) => selector({
       restaurants,
     }));
