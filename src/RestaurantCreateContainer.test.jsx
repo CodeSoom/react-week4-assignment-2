f40e5c3bebcad542e2f279b153ec2b76730e0fd8
Woong <37302935+TaeWoongKim2@users.noreply.github.com>
2020-10-18 10:22:58 +0900

    [Red] RestaurantCreateContainer에 대한 테스트 작성
    1st, render restaurant form.
    2nd, change inputs about restaurant information.
    3rd, click add button.

--- src/RestaurantCreateContainer.test.jsx
+++ src/RestaurantCreateContainer.test.jsx
@@ -6,24 +6,87 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import RestaurantCreateContainer from './RestaurantCreateContainer';
 
+import {
+  updateRestaurantName,
+  updateRestaurantCategory,
+  updateRestaurantAddress,
+  addRestaurant,
+} from './actions';
+
 jest.mock('react-redux');
 
 describe('RestaurantCreateContainer', () => {
   const dispatch = jest.fn();
 
-  useDispatch.mockImplementation(() => dispatch);
+  beforeEach(() => {
+    jest.clearAllMocks();
+    useDispatch.mockImplementation(() => dispatch);
+  });
 
-  describe('create restaurant', () => {
-    const restaurant = {
+  const initialRestaurants = [];
+  const initialRestaurant = {
+    name: '',
+    category: '',
+    address: '',
+  };
 
-    };
+  it('render restaurant form', () => {
+    const { getByPlaceholderText, getByText } = render(<RestaurantCreateContainer />);
 
-    it('', () => {
+    expect(getByPlaceholderText('이름')).not.toBeNull();
+    expect(getByPlaceholderText('분류')).not.toBeNull();
+    expect(getByPlaceholderText('주소')).not.toBeNull();
+
+    expect(getByText('등록')).not.toBeNull();
+  });
+
+  describe('input restaurant information', () => {
+    it('changes a name of restaurant', () => {
       useSelector.mockImplementation((selector) => selector({
-        restaurant,
+        restaurant: initialRestaurant,
       }));
 
-      const { container } = render(<RestaurantCreateContainer />);
+      const { getByPlaceholderText } = render(<RestaurantCreateContainer />);
+
+      fireEvent.change(getByPlaceholderText('이름'), { target: { value: '마녀주방' } });
+
+      expect(dispatch).toBeCalledWith(updateRestaurantName('마녀주방'));
     });
+
+    it('changes a category of restaurant', () => {
+      useSelector.mockImplementation((selector) => selector({
+        restaurant: initialRestaurant,
+      }));
+
+      const { getByPlaceholderText } = render(<RestaurantCreateContainer />);
+
+      fireEvent.change(getByPlaceholderText('분류'), { target: { value: '양식' } });
+
+      expect(dispatch).toBeCalledWith(updateRestaurantCategory('양식'));
+    });
+
+    it('changes a address of restaurant', () => {
+      useSelector.mockImplementation((selector) => selector({
+        restaurant: initialRestaurant,
+      }));
+
+      const { getByPlaceholderText } = render(<RestaurantCreateContainer />);
+
+      fireEvent.change(getByPlaceholderText('주소'), { target: { value: '강남구 서현동' } });
+
+      expect(dispatch).toBeCalledWith(updateRestaurantAddress('강남구 서현동'));
+    });
+  });
+
+  it('click add button', () => {
+    useSelector.mockImplementation((selector) => selector({
+      initialRestaurants,
+    }));
+
+    const { getByText } = render(<RestaurantCreateContainer />);
+
+    fireEvent.click(getByText('등록'));
+
+    expect(dispatch).toBeCalledWith(addRestaurant());
   });
 });
