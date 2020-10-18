223c508004fc1580529586f334fb1689eed41895
davidyang2149 <davidyang2149@gmail.com>
2020-10-18 15:15:23 +0900

    [Refactor] Update List.test.jsx
    4강 리덕스 - 4주 차 과제 2
    - List.test.jsx 수정(Update).
    - 변수의 위치 변경.

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -5,28 +5,28 @@ import { render } from '@testing-library/react';
 import List from './List';
 
 describe('List Component', () => {
-  it('Show List(restaurants)', () => {
-    const restaurants = [
-      {
-        id: 101,
-        name: '마녀주방',
-        category: '한식',
-        address: '서울시 강남구',
-      },
-      {
-        id: 102,
-        name: '시카고피자',
-        category: '양식',
-        address: '이태원동',
-      },
-      {
-        id: 103,
-        name: '카와미',
-        category: '일식',
-        address: '분당구 정자동',
-      },
-    ];
+  const restaurants = [
+    {
+      id: 101,
+      name: '마녀주방',
+      category: '한식',
+      address: '서울시 강남구',
+    },
+    {
+      id: 102,
+      name: '시카고피자',
+      category: '양식',
+      address: '이태원동',
+    },
+    {
+      id: 103,
+      name: '카와미',
+      category: '일식',
+      address: '분당구 정자동',
+    },
+  ];
 
+  it('Show List(restaurants)', () => {
     const { getByText } = render((
       <List restaurants={restaurants} />
     ));
