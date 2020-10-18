62a386fac2324c3a121afbb92fe94dff759c9635
davidyang2149 <davidyang2149@gmail.com>
2020-10-18 14:17:21 +0900

    [Refactor] Update App.jsx - FormContainer add
    4강 리덕스 - 4주 차 과제 2
    - App.jsx 수정(Update).
    - FormContainer 추가.
    - 기존 input 제거.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,14 @@
 import React from 'react';
 
-import ListContainer from './ListContainer'
+import ListContainer from './ListContainer';
+import FormContainer from './FormContainer';
 
 export default function App() {
   return (
     <div>
       <h1>Restaurants</h1>
       <ListContainer />
-      <p>
-        <input type="text" placeholder="이름" />
-        <input type="text" placeholder="분류" />
-        <input type="text" placeholder="주소" />
-        <button type="button">
-          등록
-      </button>
-      </p>
+      <FormContainer />
     </div>
   );
 }
