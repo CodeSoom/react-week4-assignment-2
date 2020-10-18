f04725de062dfb80d1ea2736d5364f6eefd8aef1
Woong <37302935+TaeWoongKim2@users.noreply.github.com>
2020-10-18 13:19:04 +0900

    [Green] reducer에 대한 테스트 추가
    1st, init state.
    2nd, unknown reducer. 정의되지 않은 reducer 호출시 처리.

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -8,33 +8,46 @@ import {
 } from './actions';
 
 describe('reducer', () => {
+  const initialState = {
+    newId: 100,
+    restaurant: {
+      name: '',
+      category: '',
+      address: '',
+    },
+    restaurants: [],
+  };
+
+  it('init state', () => {
+    const state = reducer();
+    expect(state).toStrictEqual(initialState);
+  });
+
+  it('unknown reducer', () => {
+    const state = reducer(initialState, {
+      type: 'deleteRestaurant',
+      payload: {
+        name: '마녀주방',
+      },
+    });
+    expect(initialState).toBe(state);
+  });
+
   describe('update restaurant', () => {
     it('changes a name of restaurant', () => {
-      const state = reducer({
-        restaurant: {
-          name: '',
-        },
-      }, updateRestaurantName('마녀주방'));
+      const state = reducer(initialState, updateRestaurantName('마녀주방'));
 
       expect(state.restaurant.name).toBe('마녀주방');
     });
 
     it('changes a category of restaurant', () => {
-      const state = reducer({
-        restaurant: {
-          category: '',
-        },
-      }, updateRestaurantCategory('양식'));
+      const state = reducer(initialState, updateRestaurantCategory('양식'));
 
       expect(state.restaurant.category).toBe('양식');
     });
 
     it('changes a address of restaurant', () => {
-      const state = reducer({
-        restaurant: {
-          address: '',
-        },
-      }, updateRestaurantAddress('강남구 서현동'));
+      const state = reducer(initialState, updateRestaurantAddress('강남구 서현동'));
 
       expect(state.restaurant.address).toBe('강남구 서현동');
     });
@@ -43,17 +56,18 @@ describe('reducer', () => {
   describe('add a new restaurant', () => {
     function reduceAddRestaurant(restaurant) {
       return reducer({
-        newId: 100,
+        ...initialState,
         restaurant,
-        restaurants: [],
       }, addRestaurant());
     }
 
     context('with all information of restaurant', () => {
+      const completeRestaurantInformation = {
+        name: '마녀주방', category: '양식', address: '강남구 서현동',
+      };
+
       it('adds a new restaurant', () => {
-        const state = reduceAddRestaurant({
-          name: '마녀주방', category: '양식', address: '강남구 서현동',
-        });
+        const state = reduceAddRestaurant(completeRestaurantInformation);
 
         expect(state.restaurants).toHaveLength(1);
         expect(state.restaurants[0].id).not.toBeUndefined();
@@ -65,10 +79,12 @@ describe('reducer', () => {
     });
 
     context('without one of information of restaurant', () => {
+      const incompleteRestaurantInformation1 = {
+        name: '', category: '', address: '',
+      };
+
       it('nothing happens', () => {
-        const state = reduceAddRestaurant({
-          name: '', category: '', address: '',
-        });
+        const state = reduceAddRestaurant(incompleteRestaurantInformation1);
 
         expect(state.restaurants).toHaveLength(0);
       });
