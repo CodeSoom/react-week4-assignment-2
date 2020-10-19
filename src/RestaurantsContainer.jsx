9ebed92963939fe679166175e16b30296b74eec3
thkim <thkim2017@gmail.com>
2020-10-19 20:30:03 +0900

    [red] Create test for RestaurantsContainer plus Implement RestaurantsContainer

--- src/RestaurantsContainer.jsx
+++ src/RestaurantsContainer.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+
+import { useSelector } from 'react-redux';
+
 import Restaurants from './Restaurants';
 
 export default function RestaurantsContainer() {
-  const restaurants = [
-    { id: 1, name: '김밥제국', category: '분식', address: '서울시 강남구 역삼동' },
-  ];
+  const { restaurants } = useSelector((state) => ({
+    restaurant: state.restaurnt,
+  }));
   return (
     <div>
       <Restaurants restaurants={restaurants} />
--- src/RestaurantsContainer.test.jsx
+++ src/RestaurantsContainer.test.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 
+import { useSelector } from 'react-redux';
+
 import RestaurantsContainer from './RestaurantsContainer';
 
 import { render } from '@testing-library/react';
 
-test('App', () => {
-const { getByText} = render((
-  <RestaurantsContainer />
-));
+test('RestaurantsContainer', () => {
+  useSelector.mockImplementation((selector) => selector({
+    restaurants: [
+      {
+        id: 1, name: '김밥제국', category: '분식', address: '서울시 강남구 역삼동',
+      },
+    ],
+  }));
 
-expect(getByText(/김밥제국/)).not.toBeNull();
+  const { getByText } = render((
+    <RestaurantsContainer />
+  ));
+
+  expect(getByText(/김밥제국/)).not.toBeNull();
 });
