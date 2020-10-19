8497ceccf4c3de3f19db9d4a69afa4b1fa4961c1
thkim <thkim2017@gmail.com>
2020-10-19 23:48:51 +0900

    [refactor] Add reducer, actions

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import RestaurantsContainer from './RestaurantsContainer';
 
 export default function App() {
+  //TODO: 초기 실행
+  //restaurants에 데이터 넣기 => dispatch로 => store가 바뀜
   return (
     <div>
       <h1>Restaurants</h1>
---
+++ src/reducer.test.js
@@ -0,0 +1,11 @@
+import reducer from './reducer';
+
+import {
+
+} from './actions';
+
+describe('reducer', () => {
+  it('', () => {
+    
+  });
+});
