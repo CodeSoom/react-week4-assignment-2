2d5529b29b03f07206784a545912f975634d3056
thkim <thkim2017@gmail.com>
2020-10-19 21:50:33 +0900

    [refactor] apply restaurants.js in fixtures

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
+
 import Restaurants from './Restaurants';
 
+import restaurants from '../fixtures/restaurants'
+
 export default function App() {
-  const restaurants = [
-    { id: 1, name: '김밥제국', category: '분식', address: '서울시 강남구 역삼동' },
-  ];
   return (
     <div>
       <h1>Restaurants</h1>
--- src/RestaurantsContainer.test.jsx
+++ src/RestaurantsContainer.test.jsx
@@ -6,13 +6,11 @@ import RestaurantsContainer from './RestaurantsContainer';
 
 import { render } from '@testing-library/react';
 
+import restaurants from '../fixtures/restaurants';
+
 test('RestaurantsContainer', () => {
   useSelector.mockImplementation((selector) => selector({
-    restaurants: [
-      {
-        id: 1, name: '김밥제국', category: '분식', address: '서울시 강남구 역삼동',
-      },
-    ],
+    restaurants,
   }));
 
   const { getByText } = render((
