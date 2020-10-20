0ac6f8fa08b28adb05cb88f36e29479e6c6151b5
thkim <thkim2017@gmail.com>
2020-10-20 19:58:41 +0900

    [green] Implement RestaurantCreateContainer

---
+++ src/RestaurantCreateContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function RestaurantCreateContainer() {
+  return (
+    <div>
+      <button type="button">
+        등록
+      </button>
+    </div>
+  );
+}
