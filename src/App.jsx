57cf8e25a429777d7385f46d3b146f5d144f4a24
comejtome508 <youngnoh.goh@gmail.com>
2020-10-21 08:27:00 +0900

    restaurants 컴포넌트 따로 뺌

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import Restaurants from './Restaurants';
+
 export default function App() {
   return (
     <div>
       <h1>Restaurants</h1>
+      <Restaurants />
     </div>
   );
 }
