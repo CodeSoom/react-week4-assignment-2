7554a6a6a1df458603ee226b6935cdcbcacc47ee
comejtome508 <youngnoh.goh@gmail.com>
2020-10-21 09:47:05 +0900

    useEffect에 dispatch 추가, action에 초기데이터 추

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import { useDispatch } from 'react-redux';
 
 import RestaurantsContainer from './RestaurantsContainer';
 
+import {
+  setRestaurants,
+} from './actions';
+
 export default function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setRestaurants(restaurants));
+  }, []);
   return (
     <div>
       <h1>Restaurants</h1>
