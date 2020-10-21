42edc2ad086c98d78ad3500b881148534024e28e
thkim <thkim2017@gmail.com>
2020-10-21 19:54:48 +0900

    [green] Apply changeRestaurantField to reducer

--- src/reducer.js
+++ src/reducer.js
@@ -12,5 +12,16 @@ export default function reducer(state = initialState, action) {
     };
   }
 
+  if (action.type === 'changeRestaurantField') {
+    const { name, value } = action.payload;
+    return {
+      ...state,
+      restaurant: {
+        ...state.restaurant,
+        [name]: value,
+      },
+    };
+  }
+
   return state;
 }
