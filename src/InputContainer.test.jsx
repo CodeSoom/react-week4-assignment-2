176ce54027928e00810defa118b1ce177c5a65f0
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-17 18:02:59 +0900

    :white_check_mark: Red-Test: Add value input test

--- src/InputContainer.test.jsx
+++ src/InputContainer.test.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
+import { useDispatch } from 'react-redux';
 import InputContainer from './InputContainer';
+
+jest.mock('react-redux');
 
 describe('InputContainer', () => {
   it('should render three inputs and one button', () => {
@@ -11,5 +14,20 @@ describe('InputContainer', () => {
     expect(getByPlaceholderText('분류')).toBeInTheDocument();
     expect(getByPlaceholderText('주소')).toBeInTheDocument();
     expect(getByText('등록')).toBeInTheDocument();
+  });
+
+  it('restaurantInfo should be able to be entered.', () => {
+    const dispatch = jest.fn();
+    useDispatch.mockImplementationOnce(() => dispatch);
+    const { getByPlaceholderText } = render(<InputContainer />);
+
+    fireEvent.change(getByPlaceholderText('이름'), { target: { value: '선정릉역 깐부치킨' } });
+    expect(dispatch).toHaveBeenCalledWith({ type: 'UPDATE_RESTAURANTINFO', payload: '선정릉역 깐부치킨' });
+
+    fireEvent.change(getByPlaceholderText('분류'), { target: { value: '한식' } });
+    expect(dispatch).toHaveBeenCalledWith({ type: 'UPDATE_RESTAURANTINFO', payload: '한식' });
+
+    fireEvent.change(getByPlaceholderText('주소'), { target: { value: '서울 강남구 삼성동 37 깐부치킨 선정릉역점' } });
+    expect(dispatch).toHaveBeenCalledWith({ type: 'UPDATE_RESTAURANTINFO', payload: '서울 강남구 삼성동 37 깐부치킨 선정릉역점' });
   });
 });
