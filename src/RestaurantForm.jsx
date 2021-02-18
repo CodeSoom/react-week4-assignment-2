2c640194e9e92789b07f330f95a6a5a1e21e4b15
dodoongtak <themighty.tak@gmail.com>
2021-02-18 19:38:31 +0900

    🟢 RestaurantForm을 구현한다.

--- src/RestaurantForm.jsx
+++ src/RestaurantForm.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-export default function RestaurantForm({ restaurantInformation = {} }) {
+export default function RestaurantForm({ restaurantInformation = {}, handleChange }) {
   const { name, category, address } = restaurantInformation;
 
   return (
     <div>
-      <input placeholder="이름" value={name} />
-      <input placeholder="분류" value={category} />
-      <input placeholder="주소" value={address} />
+      <input placeholder="이름" value={name} onChange={handleChange} />
+      <input placeholder="분류" value={category} onChange={handleChange} />
+      <input placeholder="주소" value={address} onChange={handleChange} />
       <button type="button">등록</button>
     </div>
   );
