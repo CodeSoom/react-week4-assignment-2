e43e8a71aa7e48fe2f9e6c34d13bdf0c5e4da97f
shinhyogeun <tls1gy2rms3@naver.com>
2021-02-18 20:43:17 +0900

    App에 음식종류와 주소에 대한 test 추가

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -22,4 +22,28 @@ describe('App', () => {
 
     expect(queryByDisplayValue('마녀 주방')).toBeInTheDocument();
   });
+
+  it('음식 종류을 입력하면 종류가 화면에 보인다.', () => {
+    const { queryByDisplayValue, queryByPlaceholderText } = render(<App />);
+
+    fireEvent.change(queryByPlaceholderText('종류'), {
+      target: {
+        value: '한식',
+      },
+    });
+
+    expect(queryByDisplayValue('한식')).toBeInTheDocument();
+  });
+
+  it('주소를 입력하면 주소가 화면에 보인다.', () => {
+    const { queryByDisplayValue, queryByPlaceholderText } = render(<App />);
+
+    fireEvent.change(queryByPlaceholderText('주소'), {
+      target: {
+        value: '서울시 강남구',
+      },
+    });
+
+    expect(queryByDisplayValue('서울시 강남구')).toBeInTheDocument();
+  });
 });
