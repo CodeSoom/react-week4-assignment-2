d290e78a3bccd9c45508791a53bd5c7848f162ff
EL <ickyicky@gmail.com>
2021-02-18 20:35:47 +0900

    REFACTOR: 띄어쓰기 적용

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ export default function App() {
   return (
     <div>
       <h1>Restaurants</h1>
-      마녀주방|한식|서울시 강남구
+      <ul>
+        <li>마녀주방 | 한식 | 서울시 강남구</li>
+      </ul>
     </div>
   );
 }
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -5,12 +5,12 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 describe('App', () => {
-  it('Restaurants, 예약 목록을 보여준다.', () => {
+  it('제목 Restaurants, 예약 목록을 보여준다.', () => {
     const { container } = render((
       <App />
     ));
 
     expect(container).toHaveTextContent(/Restaurants/);
-    expect(container).toHaveTextContent(/마녀주방|한식|서울시 강남구/);
+    expect(container).toHaveTextContent(/마녀주방 | 한식 | 서울시 강남구/);
   });
 });
