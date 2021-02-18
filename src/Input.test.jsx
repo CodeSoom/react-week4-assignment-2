7143bbbdc513b340cbfd1343338ce14dead6615f
datalater <the7mincheol@gmail.com>
2021-02-18 15:50:16 +0900

    입력 테스트 코드를 리팩토링하라
    하나의 테스트 블락에 포함된 두 개의 테스트를 각각 분리한다.
    중복되는 render 함수를 분리해서 재사용한다.

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -5,12 +5,22 @@ import { render, fireEvent } from '@testing-library/react';
 import Input from './Input';
 
 describe('Input', () => {
-  it('renders restaurant inputs', () => {
-    const { getByPlaceholderText } = render((
+  function renderInput() {
+    return render((
       <Input />
     ));
+  }
+
+  it('renders restaurant inputs', () => {
+    const { getByPlaceholderText } = renderInput();
 
     expect(getByPlaceholderText(/이름/)).not.toBeNull();
+    expect(getByPlaceholderText(/분류/)).not.toBeNull();
+    expect(getByPlaceholderText(/주소/)).not.toBeNull();
+  });
+
+  it('changes inputs', () => {
+    const { getByPlaceholderText } = renderInput();
 
     fireEvent.change(getByPlaceholderText(/이름/), {
       target: { value: '마녀주방' },
@@ -18,15 +28,11 @@ describe('Input', () => {
 
     expect(getByPlaceholderText(/이름/).value).toBe('마녀주방');
 
-    expect(getByPlaceholderText(/분류/)).not.toBeNull();
-
     fireEvent.change(getByPlaceholderText(/분류/), {
       target: { value: '한식' },
     });
 
     expect(getByPlaceholderText(/분류/).value).toBe('한식');
-
-    expect(getByPlaceholderText(/주소/)).not.toBeNull();
 
     fireEvent.change(getByPlaceholderText(/주소/), {
       target: { value: '서울시 강남구' },
