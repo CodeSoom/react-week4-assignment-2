4fe34429154d4729e3772577823a82bf141a70e9
Yoon <yoonhe@gmail.com>
2021-02-18 20:57:02 +0900

    [red] ListContainer 컴포넌트에 대한 테스트를 추가한다

--- src/ListContainer.test.jsx
+++ src/ListContainer.test.jsx
@@ -2,14 +2,32 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import { useSelector } from 'react-redux';
 import ListContainer from './ListContainer';
 
 /**
  * redux store에서 state르 받아와 화면에 렌더하는지?
  */
 
+jest.mock();
+
 describe('ListContainer', () => {
+  beforeEach(() => {
+    useSelector.mockImplementation((selector) => selector({
+      list: [{
+        id: 1,
+        name: '마녀주방',
+        category: '한식',
+        address: '서울시 강남구',
+      }],
+    }));
+  });
+
   it('renders List', () => {
     const { getByText } = render(<ListContainer />);
+
+    expect(getByText('마녀주방')).not.toBeNull();
+    expect(getByText('한식')).not.toBeNull();
+    expect(getByText('서울시 강남구')).not.toBeNull();
   });
 });
