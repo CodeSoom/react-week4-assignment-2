8f603554d4fbce8b7a6eb4b96aa2a10169e2f468
dodoongtak <themighty.tak@gmail.com>
2021-02-18 15:48:09 +0900

    🔵 RestaurantForm을 리팩토링한다

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import ReastaurantForm from './RestaurantForm';
+
 export default function App() {
   return (
     <div>
       <h1>Restaurants</h1>
+      <ReastaurantForm />
     </div>
   );
 }
