8c74a2f77ce8b70f012c7f95213aeef71faf13eb
datalater <the7mincheol@gmail.com>
2021-02-18 18:41:20 +0900

    등록 버튼을 누르면 정보가 추가되도록 구현하라

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,57 @@ import React, { useState } from 'react';
 
 import Input from './Input';
 
+function List({ restaurants }) {
+  return (
+    <div>
+      { JSON.stringify(restaurants) }
+    </div>
+  );
+}
+
 export default function App() {
-  const [state, setstate] = useState({
+  const [state, setState] = useState({
     name: '',
     type: '',
     address: '',
+    restaurants: [],
   });
 
   const {
     name,
     type,
     address,
+    restaurants,
   } = state;
 
   function handleChange(event) {
     const { name: targetName, value } = event.target;
-    setstate({
+    setState({
       ...state,
       [targetName]: value,
     });
   }
 
   function handleClick() {
-
+    setState({
+      ...state,
+      restaurants: [
+        ...state.restaurants,
+        {
+          name: state.name,
+          type: state.type,
+          address: state.address,
+        },
+      ],
+    });
   }
 
   return (
     <div>
       <h1>Restaurants</h1>
+      <List
+        restaurants={restaurants}
+      />
       <Input
         name={name}
         type={type}
