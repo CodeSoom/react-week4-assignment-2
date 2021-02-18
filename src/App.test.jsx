62497862dc113aa3cf8a9c52d8d228853d46d635
shinhyogeun <tls1gy2rms3@naver.com>
2021-02-18 20:22:30 +0900

    app에 대한 test(이름을 입력하면 이름이 화면에 보인다.)추가

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import App from './App';
 
@@ -9,5 +9,17 @@ describe('App', () => {
     const { queryByText } = render(<App />);
 
     expect(queryByText('Restaurants')).toBeInTheDocument();
+  });
+
+  it('이름을 입력하면 이름이 화면에 보인다.', () => {
+    const { queryByDisplayValue, queryByPlaceholderText } = render(<App />);
+
+    fireEvent.change(queryByPlaceholderText('이름'), {
+      target: {
+        value: '마녀 주방',
+      },
+    });
+
+    expect(queryByDisplayValue('마녀 주방')).toBeInTheDocument();
   });
 });
