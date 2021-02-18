30e36149e68a412c0f08d334d2c75f6b9c0b18a7
EL <ickyicky@gmail.com>
2021-02-18 21:13:19 +0900

    RED: App의 input 요소에 대한 테스트 추가

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -5,12 +5,31 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 describe('App', () => {
-  it('제목 Restaurants, 예약 목록을 보여준다.', () => {
-    const { container } = render((
+  it('제목 Restaurants, input, button, 예약 목록을 보여준다.', () => {
+    const tasks = [
+      {
+        title: '마녀주방 | 한식 | 서울시 강남구',
+        id: 1,
+      },
+      {
+        title: '시카고피자 | 양식 | 이태원동',
+        id: 2,
+      },
+    ];
+
+    const { container, getByText, getByPlaceholderText } = render((
       <App />
     ));
 
     expect(container).toHaveTextContent(/Restaurants/);
-    expect(container).toHaveTextContent(/마녀주방 | 한식 | 서울시 강남구/);
+    tasks.forEach((task) => {
+      expect(container).toHaveTextContent(task.title);
+    });
+
+    getByPlaceholderText(/이름/);
+    getByPlaceholderText(/분류/);
+    getByPlaceholderText(/주소/);
+
+    getByText(/등록/);
   });
 });
