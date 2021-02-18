8b941310f0f2ce6fa8092a7d5dbf165ffb2c25f8
Blueberry <54464320+lee01097@users.noreply.github.com>
2021-02-18 20:49:36 +0900

    Refactoring) index.jsx 수정 및 상태 관리 위해 store.js 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
 
+import { useSelector } from 'react-redux';
+
+function handleButtonClick() {}
+
 export default function App() {
-  function handleButtonClick() {}
+  const registers = useSelector((state) => state.registers);
+
+  if (registers.length === 0) {
+    return (
+      <div>
+        <h1>Restaurant</h1>
+        <button type="button" onClick={handleButtonClick}>
+          등록
+        </button>
+      </div>
+    );
+  }
 
   return (
     <div>
       <h1>Restaurant</h1>
       <ul>
         <li>
-          마녀식당 | 한식 | 서울시 강남구
+          {registers.information}
         </li>
       </ul>
       <button type="button" onClick={handleButtonClick}>
