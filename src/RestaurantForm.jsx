1c24acb8badc393688407e85124dcde22a82e70a
dodoongtak <themighty.tak@gmail.com>
2021-02-18 19:12:42 +0900

    🔵 RestaurantForm을 리팩토링한다

--- src/RestaurantForm.jsx
+++ src/RestaurantForm.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 export default function RestaurantForm({ restaurantInformation = {} }) {
+  const { name, category, address } = restaurantInformation;
+
   return (
     <div>
-      <input placeholder="이름" value={restaurantInformation.name} />
-      <input placeholder="분류" value={restaurantInformation.category} />
-      <input placeholder="주소" value={restaurantInformation.address} />
+      <input placeholder="이름" value={name} />
+      <input placeholder="분류" value={category} />
+      <input placeholder="주소" value={address} />
       <button type="button">등록</button>
     </div>
   );
