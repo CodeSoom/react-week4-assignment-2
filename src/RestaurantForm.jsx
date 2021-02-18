0a302ce5bd01a740d4f6b3083b643bd455bcd26d
dodoongtak <themighty.tak@gmail.com>
2021-02-18 15:46:45 +0900

    🟢 RestaurantForm을 구현한다

--- src/RestaurantForm.jsx
+++ src/RestaurantForm.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 
 export default function RestaurantForm() {
   return (
-    <div />
+    <div>
+      <input placeholder="이름" />
+      <input placeholder="분류" />
+      <input placeholder="주소" />
+      <button type="button">등록</button>
+    </div>
   );
 }
