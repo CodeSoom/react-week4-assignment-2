5d6a4a5356c410749ae1d325140c4adc0a05be05
shinhyogeun <tls1gy2rms3@naver.com>
2021-02-18 20:52:43 +0900

    App test(음식 종류와 주소에 대한) 통과

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,33 @@ import InputForm from './InputForm';
 export default function App() {
   const [state, setState] = useState({
     name: '',
+    category: '',
+    address: '',
   });
 
-  const { name } = state;
+  const { name, category, address } = state;
 
-  function updateName(event) {
-    setState({
-      ...state,
-      name: event.target.value,
-    });
+  function updateInput(event) {
+    if (event.target.name === '이름') {
+      setState({
+        ...state,
+        name: event.target.value,
+      });
+    }
+
+    if (event.target.name === '종류') {
+      setState({
+        ...state,
+        category: event.target.value,
+      });
+    }
+
+    if (event.target.name === '주소') {
+      setState({
+        ...state,
+        address: event.target.value,
+      });
+    }
   }
 
   return (
@@ -21,9 +39,9 @@ export default function App() {
       <h1>Restaurants</h1>
       <InputForm
         nameValue={name}
-        categoryValue
-        addressValue
-        onChange={updateName}
+        categoryValue={category}
+        addressValue={address}
+        onChange={updateInput}
         onClick
       />
     </div>
--- src/InputForm.jsx
+++ src/InputForm.jsx
@@ -9,9 +9,9 @@ export default function InputForm({
 }) {
   return (
     <div>
-      <input value={nameValue} placeholder="이름" onChange={onChange} />
-      <input value={categoryValue} placeholder="종류" onChange={onChange} />
-      <input value={addressValue} placeholder="주소" onChange={onChange} />
+      <input value={nameValue} name="이름" placeholder="이름" onChange={onChange} />
+      <input value={categoryValue} name="종류" placeholder="종류" onChange={onChange} />
+      <input value={addressValue} name="주소" placeholder="주소" onChange={onChange} />
       <button type="button" onClick={onClick}>등록</button>
     </div>
   );
