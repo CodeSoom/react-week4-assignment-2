c7b51d480c114dc2b5786e54fc91632283905239
EL <ickyicky@gmail.com>
2021-02-18 20:31:03 +0900

    Red: App에 테스트 추

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -5,11 +5,12 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 describe('App', () => {
-  it('예약 목록을 보여준다.', () => {
+  it('Restaurants, 예약 목록을 보여준다.', () => {
     const { container } = render((
       <App />
     ));
 
+    expect(container).toHaveTextContent(/Restaurants/);
     expect(container).toHaveTextContent(/마녀주방|한식|서울시 강남구/);
   });
 });
