53f5671cbb566ff5c562a45cb276edf95586b880
Blueberry <54464320+lee01097@users.noreply.github.com>
2021-02-18 16:33:25 +0900

    Refactoring) App.test.jsx: 테스트 케이스 추가 및 오류 확인

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -4,17 +4,33 @@ import { render } from '@testing-library/react';
 
 import App from './App';
 
-test('App', () => {
+describe('App', () => {
   const useSelector = jest.fn();
-  useSelector.mockImplementation((selector) => selector({
-    registers: [{
-      id: 1,
-      information: '마녀식당 | 한식 | 서울시 강남구',
-    }],
-  }));
 
-  const { queryByText } = render((<App />));
+  context('with registers', () => {
+    it('shows all registers', () => {
+      useSelector.mockImplementation((selector) => selector({
+        registers: [{
+          id: 1,
+          information: '마녀식당 | 한식 | 서울시 강남구',
+        }],
+      }));
 
-  expect(queryByText(/마녀식당 | 한식 | 서울시 강남구/)).not.toBeNull();
-  expect(queryByText(/등록/)).not.toBeNull();
+      const { queryByText } = render((<App />));
+
+      expect(queryByText(/마녀식당 | 한식 | 서울시 강남구/)).not.toBeNull();
+      expect(queryByText(/등록/)).not.toBeNull();
+    });
+  });
+
+  context('with no registers', () => {
+    it('nothing shows', () => {
+      useSelector.mockImplementation((selector) => selector({ registers: [] }));
+
+      const { queryByText } = render((<App />));
+
+      expect(queryByText(/마녀식당 | 한식 | 서울시 강남구/)).toBeNull();
+      expect(queryByText(/등록/)).not.toBeNull();
+    });
+  });
 });
