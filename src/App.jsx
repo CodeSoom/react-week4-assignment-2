ca9d5a3e2324e611933bddc65ecb2ff9d86c0191
EL <ickyicky@gmail.com>
2021-02-18 21:04:24 +0900

    REFACTOR: App 구조 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 
+import List from './List';
+
 export default function App() {
+  const tasks = [
+    {
+      title: '마녀주방 | 한식 | 서울시 강남구',
+      id: 1,
+    },
+    {
+      title: '시카고피자 | 양식 | 이태원동',
+      id: 2,
+    },
+  ];
+
   return (
     <div>
       <h1>Restaurants</h1>
-      <ul>
-        <li>마녀주방 | 한식 | 서울시 강남구</li>
-      </ul>
+      <List tasks={tasks} />
     </div>
   );
 }
