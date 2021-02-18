8881aa59680cd5f2078e857bafb491d2407bdca4
최창현 <chchoi_einz@einz.co.kr>
2021-02-18 15:19:36 +0900

    Red-Test: make App test

---
+++ src/App.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { render } from '@testing-library/react'
+
+import App from './App';
+
+describe('App', () => {
+  it('예약 목록을 보여준다.', () => {
+    const { container } = render((
+      <App />
+    ));
+
+    expect(container).toHaveTextContent(/마녀주방|한식|서울시 강남구/);
+  })
+})
