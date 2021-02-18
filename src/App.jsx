102bbc3e8bbba5c431acaba62ba63dc99b09f167
Blueberry <54464320+lee01097@users.noreply.github.com>
2021-02-18 16:17:46 +0900

    Green) App.jsx: App.jsx 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 
 export default function App() {
+  function handleButtonClick() {}
+
   return (
     <div>
-      Hello
+      <h1>Restaurant</h1>
+      <ul>
+        <li>
+          마녀식당 | 한식 | 서울시 강남구
+        </li>
+      </ul>
+      <button type="button" onClick={handleButtonClick}>
+        등록
+      </button>
     </div>
   );
 }
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -16,4 +16,5 @@ test('App', () => {
   const { queryByText } = render((<App />));
 
   expect(queryByText(/마녀식당 | 한식 | 서울시 강남구/)).not.toBeNull();
+  expect(queryByText(/등록/)).not.toBeNull();
 });
