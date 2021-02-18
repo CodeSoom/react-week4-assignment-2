9d2e1c56ca7e61ffeb9f793bd03c1ac862f7b25d
dodoongtak <themighty.tak@gmail.com>
2021-02-18 19:21:25 +0900

    🔴 RestaurantForm에 대한 테스트를 추가한다

--- src/RestaurantForm.test.jsx
+++ src/RestaurantForm.test.jsx
@@ -1,21 +1,28 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import RestaurantForm from './RestaurantForm';
 
 describe('RestaurantForm', () => {
-  const renderRestaurantForm = (restaurantInformation) => render((
-    <RestaurantForm
-      restaurantInformation={restaurantInformation}
-    />
-  ));
+  const handleChange = jest.fn();
 
   const restaurantInformation = {
     name: '베이징',
     category: '중식',
     address: '파주시',
   };
+
+  const informations = Object.values(restaurantInformation);
+
+  const renderRestaurantForm = () => render((
+    <RestaurantForm
+      restaurantInformation={restaurantInformation}
+      handleChange={handleChange}
+    />
+  ));
+
+  beforeEach(() => jest.clearAllMocks());
 
   it('renders input elemetns', () => {
     const { getByPlaceholderText } = renderRestaurantForm();
@@ -36,12 +43,22 @@ describe('RestaurantForm', () => {
   });
 
   it('renders values of the input elements ', () => {
-    const { getByDisplayValue } = renderRestaurantForm(restaurantInformation);
-
-    const informations = Object.values(restaurantInformation);
+    const { getByDisplayValue } = renderRestaurantForm();
 
     informations.forEach((information) => {
       expect(getByDisplayValue(information)).not.toBeNull();
     });
+  });
+
+  it('calls handleChange function upon changing value of the input elements', () => {
+    const { getByDisplayValue } = renderRestaurantForm();
+
+    informations.forEach((information) => {
+      fireEvent.change(getByDisplayValue(information), {
+        target: { value: '본즈패밀리' },
+      });
+    });
+
+    expect(handleChange).toBeCalled();
   });
 });
