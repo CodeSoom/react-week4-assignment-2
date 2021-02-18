1a86507e078ba410ed54016628883d924d4de6ec
datalater <the7mincheol@gmail.com>
2021-02-18 18:29:20 +0900

    등록 버튼을 테스트하라

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import Input from './Input';
 
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
 describe('Input', () => {
+  const handleChange = jest.fn();
+  const handleClick = jest.fn();
+
   function renderInput() {
     return render((
-      <Input />
+      <Input
+        name=""
+        type=""
+        address=""
+        onChange={handleChange}
+        onClick={handleClick}
+      />
     ));
   }
 
@@ -19,5 +32,13 @@ describe('Input', () => {
     expect(getByPlaceholderText(/주소/)).not.toBeNull();
 
     expect(getByText(/등록/)).not.toBeNull();
+  });
+
+  it('adds restaurant infromation', () => {
+    const { getByText } = renderInput();
+
+    fireEvent.click(getByText(/등록/));
+
+    expect(handleClick).toBeCalled();
   });
 });
