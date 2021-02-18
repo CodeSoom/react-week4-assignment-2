d276b12019ce6fadb5b8175c3d406e23b1fea4d1
Yoon <yoonhe@gmail.com>
2021-02-18 19:26:50 +0900

    [green] AddForm 컴포넌트를 구현한다

--- src/AddForm.jsx
+++ src/AddForm.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 
 function AddForm() {
   return (
-    <div />
+    <form>
+      <input type="text" placeholder="이름" />
+      <input type="text" placeholder="분류" />
+      <input type="text" placeholder="주소" />
+    </form>
   );
 }
 
