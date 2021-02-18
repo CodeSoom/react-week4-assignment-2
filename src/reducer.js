aac1f963ad184c01c093fb02a8b12d8d1ea95280
Yoon <yoonhe@gmail.com>
2021-02-18 20:56:40 +0900

    redux store 연결

---
+++ src/reducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  inputTitles: {
+    name: '',
+    category: '',
+    address: '',
+  },
+  list: [],
+};
+
+export default function reducer(state = initialState, action = { type: '' }) {
+  return state;
+}
