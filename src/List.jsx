e691d4fd1d13297fe50a7bb0b8a64946b4f7481e
datalater <the7mincheol@gmail.com>
2021-02-19 20:53:23 +0900

    레스토랑 목록이 없는 경우 없다는 메시지가 나오도록 구현한다

--- src/List.jsx
+++ src/List.jsx
@@ -3,6 +3,14 @@ import React from 'react';
 import Restaurant from './Restaurant';
 
 export default function List({ restaurants }) {
+  if (restaurants.length === 0) {
+    return (
+      <div>
+        입력된 레스토랑 정보가 없습니다.
+      </div>
+    );
+  }
+
   return (
     <div>
       { restaurants.map(({
