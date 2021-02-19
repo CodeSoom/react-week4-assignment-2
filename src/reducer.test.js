24ed119127668a33b721e855f790609b6df6b411
Yoon <yoonhe@gmail.com>
2021-02-19 18:05:15 +0900

    [red] reducer에 대한 테스트를 추가한다

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -8,4 +8,54 @@ describe('reducer', () => {
       expect(state).toBe(initialState);
     });
   });
+
+  describe('changeInput', () => {
+    it('changes title', () => {
+      const state = reducer({
+        inputs: {
+          title: '',
+        },
+      }, {
+        type: 'changeInput',
+        payload: {
+          name: 'title',
+          value: '마녀주방',
+        },
+      });
+
+      expect(state.inputs.title).toBe('마녀주방');
+    });
+
+    it('changes category', () => {
+      const state = reducer({
+        inputs: {
+          category: '',
+        },
+      }, {
+        type: 'changeInput',
+        payload: {
+          name: 'category',
+          value: '한식',
+        },
+      });
+
+      expect(state.inputs.category).toBe('한식');
+    });
+
+    it('changes address', () => {
+      const state = reducer({
+        inputs: {
+          address: '',
+        },
+      }, {
+        type: 'changeInput',
+        payload: {
+          name: 'address',
+          value: '서울시 강남구',
+        },
+      });
+
+      expect(state.inputs.address).toBe('서울시 강남구');
+    });
+  });
 });
