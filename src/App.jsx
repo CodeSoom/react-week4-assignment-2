f4383b4f841fcaea3cfcde7d3ec3ee5e2cf7179f
Blueberry <54464320+lee01097@users.noreply.github.com>
2021-02-19 18:55:54 +0900

    App.jsx, App.test.jsx, index.jsx: 수정 및 state 사용 제거

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,19 @@
 import React from 'react';
 
-import { useSelector } from 'react-redux';
-
 function handleButtonClick() {}
 
 export default function App() {
-  const registers = useSelector((state) => state.registers);
-
-  if (registers.length === 0) {
-    return (
-      <div>
-        <h1>Restaurant</h1>
-        <button type="button" onClick={handleButtonClick}>
-          등록
-        </button>
-      </div>
-    );
-  }
+  const registers = [{
+    id: 1,
+    information: '마녀식당 | 한식 | 서울시 강남구',
+  }];
 
   return (
     <div>
       <h1>Restaurant</h1>
       <ul>
         <li>
-          {registers.information}
+          {registers[0].information}
         </li>
       </ul>
       <button type="button" onClick={handleButtonClick}>
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -4,33 +4,9 @@ import { render } from '@testing-library/react';
 
 import App from './App';
 
-describe('App', () => {
-  const useSelector = jest.fn();
+test('App', () => {
+  const { queryByText } = render((<App />));
 
-  context('with registers', () => {
-    it('shows all registers', () => {
-      useSelector.mockImplementation((selector) => selector({
-        registers: [{
-          id: 1,
-          information: '마녀식당 | 한식 | 서울시 강남구',
-        }],
-      }));
-
-      const { queryByText } = render((<App />));
-
-      expect(queryByText(/마녀식당 | 한식 | 서울시 강남구/)).not.toBeNull();
-      expect(queryByText(/등록/)).not.toBeNull();
-    });
-  });
-
-  context('with no registers', () => {
-    it('nothing shows', () => {
-      useSelector.mockImplementation((selector) => selector({ registers: [] }));
-
-      const { queryByText } = render((<App />));
-
-      expect(queryByText(/마녀식당 | 한식 | 서울시 강남구/)).toBeNull();
-      expect(queryByText(/등록/)).not.toBeNull();
-    });
-  });
+  expect(queryByText(/마녀식당 | 한식 | 서울시 강남구/)).not.toBeNull();
+  expect(queryByText(/등록/)).not.toBeNull();
 });
