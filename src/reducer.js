a2416584645a9c71740a3caae0221bdd8e03002e
shinhyogeun <tls1gy2rms3@naver.com>
2021-02-19 19:12:13 +0900

    reducer test 통과

---
+++ src/reducer.js
@@ -0,0 +1,31 @@
+function reducer(state, action) {
+  const { type, payload } = action;
+
+  if (type === 'updateInput') {
+    return {
+      ...state,
+      [payload.placeholder]: payload.input,
+    };
+  }
+
+  if (type === 'addRestaurants' && !Object.values(state).includes('')) {
+    const {
+      id, name, category, address, restaurantsList,
+    } = state;
+
+    return {
+      ...state,
+      id: id + 1,
+      name: '',
+      category: '',
+      address: '',
+      restaurantsList: [...restaurantsList, {
+        id, name, category, address,
+      }],
+    };
+  }
+
+  return state;
+}
+
+export default reducer;
