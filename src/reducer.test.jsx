c0d9498ab43435e7e09b1d618f5e6f1212a5061d
datalater <the7mincheol@gmail.com>
2021-02-19 22:04:17 +0900

    초기 상태값을 사용하는지 테스트하라

--- src/reducer.test.jsx
+++ src/reducer.test.jsx
@@ -21,37 +21,52 @@ describe('reducer', () => {
 
   context('with registered action type', () => {
     describe('updateRestaurantInformation', () => {
-      it('updates restaurant name', () => {
-        const state = reducer(previousState, {
-          type: 'updateRestaurantName',
-          payload: {
-            name: '마녀주방',
-          },
-        });
+      context('without previous state', () => {
+        it('updates restaurant name using initial state', () => {
+          const state = reducer(undefined, {
+            type: 'updateRestaurantName',
+            payload: {
+              name: '마녀주방',
+            },
+          });
 
-        expect(state.name).toBe('마녀주방');
+          expect(state.name).toBe('마녀주방');
+        });
       });
 
-      it('updates restaurant category', () => {
-        const state = reducer(previousState, {
-          type: 'updateRestaurantCategory',
-          payload: {
-            category: '한식',
-          },
+      context('with previous state', () => {
+        it('updates restaurant name', () => {
+          const state = reducer(previousState, {
+            type: 'updateRestaurantName',
+            payload: {
+              name: '마녀주방',
+            },
+          });
+
+          expect(state.name).toBe('마녀주방');
         });
 
-        expect(state.category).toBe('한식');
-      });
+        it('updates restaurant category', () => {
+          const state = reducer(previousState, {
+            type: 'updateRestaurantCategory',
+            payload: {
+              category: '한식',
+            },
+          });
 
-      it('updates restaurant address', () => {
-        const state = reducer(previousState, {
-          type: 'updateRestaurantAddress',
-          payload: {
-            address: '서울시 강남구',
-          },
+          expect(state.category).toBe('한식');
         });
 
-        expect(state.address).toBe('서울시 강남구');
+        it('updates restaurant address', () => {
+          const state = reducer(previousState, {
+            type: 'updateRestaurantAddress',
+            payload: {
+              address: '서울시 강남구',
+            },
+          });
+
+          expect(state.address).toBe('서울시 강남구');
+        });
       });
     });
   });
