ede484cb98ed2b8bb4a632c1138e4244de3bc406
dodoongtak <themighty.tak@gmail.com>
2021-02-19 13:18:24 +0900

    🟢 Reducer에 UpdateRestaurant 구현

--- src/reducer.js
+++ src/reducer.js
@@ -1,1 +1,19 @@
-export default function reducer() {}
+const initialState = {
+  newId: 100,
+  restaurant: {
+    name: '',
+    category: '',
+    restaurants: [],
+  },
+};
+
+const reducers = {
+  updateRestaurant: (state, { payload: { restaurant } }) => ({
+    ...state, restaurant: { ...state.restaurant, ...restaurant },
+  }),
+
+};
+
+export default function reducer(state = initialState, action = { type: undefined }) {
+  return reducers[action.type] ? reducers[action.type](state, action) : state;
+}
