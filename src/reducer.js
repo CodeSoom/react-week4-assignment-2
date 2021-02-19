cd273f4d944f6b5e95f96181cb1a901b6f43e6c2
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-19 14:50:07 +0900

    :recycle: Refactor: reducer
    - UPDATE_RESTAURANTINFO

--- src/reducer.js
+++ src/reducer.js
@@ -7,14 +7,7 @@ const initialState = {
 
 export default function reducer(state = initialState, action) {
   if (action.type === UPDATE_RESTAURANTINFO) {
-    return {
-      ...state,
-      restaurantInfo: {
-        name: '선정릉역 깐부치킨',
-        catagory: '한식',
-        address: '서울 강남구 삼성동 37 깐부치킨 선정릉역점',
-      },
-    };
+    return { ...state, restaurantInfo: { ...state.restaurantInfo, ...action.payload } };
   }
   return state;
 }
