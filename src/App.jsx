af243a5cfb6d681e39b34b8a8078d1ab40dfd156
Blueberry <54464320+lee01097@users.noreply.github.com>
2021-02-19 23:01:57 +0900

    Green) App.jsx: 수정

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,11 @@ import Input from './Input';
 import List from './List';
 
 export default function App() {
+  const registers = [{ id: 1, information: '마녀식당 | 한식 | 서울시 강남구' }];
   return (
     <div>
       <h1>Restaurant</h1>
-      <List />
+      <List registers={registers} />
       <Input />
     </div>
   );
