eb64efcc2652ed46ee2ec3e94726797da0c4cf33
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-19 17:33:02 +0900

    :construction: Green-Working: Make RestaurantInput pass the test

--- src/RestaurantInput.jsx
+++ src/RestaurantInput.jsx
@@ -6,17 +6,17 @@ function RestaurantInput({ onChange }) {
       <input
         type="text"
         placeholder="이름"
-        onChange={() => { onChange({ type: 'UPDATE_RESTAURANTINFO', payload: '선정릉역 깐부치킨' }); }}
+        onChange={() => { onChange({ type: 'UPDATE_RESTAURANTINFO', payload: { name: '선정릉역 깐부치킨' } }); }}
       />
       <input
         type="text"
         placeholder="분류"
-        onChange={() => { onChange({ type: 'UPDATE_RESTAURANTINFO', payload: '한식' }); }}
+        onChange={() => { onChange({ type: 'UPDATE_RESTAURANTINFO', payload: { category: '한식' } }); }}
       />
       <input
         type="text"
         placeholder="주소"
-        onChange={() => { onChange({ type: 'UPDATE_RESTAURANTINFO', payload: '서울 강남구 삼성동 37 깐부치킨 선정릉역점' }); }}
+        onChange={() => { onChange({ type: 'UPDATE_RESTAURANTINFO', payload: { address: '서울 강남구 삼성동 37 깐부치킨 선정릉역점' } }); }}
       />
       <button type="button">등록</button>
     </div>
