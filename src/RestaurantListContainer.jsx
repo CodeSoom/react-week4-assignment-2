432e15ac85f5f356cdfdf99d32ccfc69de63fc15
dodoongtak <themighty.tak@gmail.com>
2021-02-19 17:54:13 +0900

    🟢 RestaurantListContainer:  render 구현

--- src/RestaurantListContainer.jsx
+++ src/RestaurantListContainer.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
+import { useSelector } from 'react-redux';
+
+import RestaurantList from './RestaurantList';
+
 export default function RestaurantListContainer() {
+  const { restaurants } = useSelector((state) => ({
+    restaurants: state.restaurants,
+  }));
+
   return (
-    <div />
+    <RestaurantList
+      restaurants={restaurants}
+    />
   );
 }
