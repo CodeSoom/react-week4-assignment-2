384ff01842d0f1de9d86f7f82a4d6cdf47a08220
Yoon <yoonhe@gmail.com>
2021-02-19 17:47:10 +0900

    [red] FormContainer 컴포넌트에 대한 테스트를 추가한다

--- src/FormContainer.test.jsx
+++ src/FormContainer.test.jsx
@@ -1,29 +1,54 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import FormContainer from './FormContainer';
 
-jest.mock();
+jest.mock('react-redux');
 
 describe('FormContainer', () => {
+  const dispatch = jest.fn();
+
   beforeEach(() => {
+    jest.resetAllMocks();
+
     useSelector.mockImplementation((selector) => selector({
       inputs: {
-        name: '마녀주방',
-        category: '한식',
-        address: '서울시 강남구',
+        title: '',
+        category: '',
+        address: '',
       },
     }));
+
+    useDispatch.mockImplementation(() => dispatch);
   });
 
   it('renders inputs', () => {
-    const { queryByDisplayValue } = render(<FormContainer />);
+    const { queryByPlaceholderText } = render(<FormContainer />);
 
-    expect(queryByDisplayValue('마녀주방')).not.toBeNull();
-    expect(queryByDisplayValue('한식')).not.toBeNull();
-    expect(queryByDisplayValue('서울시 강남구')).not.toBeNull();
+    expect(queryByPlaceholderText('이름')).not.toBeNull();
+    expect(queryByPlaceholderText('분류')).not.toBeNull();
+    expect(queryByPlaceholderText('주소')).not.toBeNull();
+  });
+
+  it('listens name input change event', () => {
+    const { getByPlaceholderText } = render(<FormContainer />);
+
+    fireEvent.change(getByPlaceholderText('이름'), {
+      target: {
+        name: 'name',
+        value: '마녀주방',
+      },
+    });
+
+    expect(dispatch).toBeCalledWith({
+      type: 'changeInput',
+      payload: {
+        name: 'name',
+        value: '마녀주방',
+      },
+    });
   });
 
   it('renders "등록" button', () => {
