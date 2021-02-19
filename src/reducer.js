a5e44dc0f1a2ac9b6a7904ab744719ae705b2bca
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-19 14:04:46 +0900

    :recycle: Refactor: reducer
    - 초기상태 미리지정

--- src/reducer.js
+++ src/reducer.js
@@ -1,6 +1,8 @@
-export default function reducer() {
-  return {
-    restaurantList: [],
-    restaurantInfo: { name: '', category: '', address: '' },
-  };
+const initialState = {
+  restaurantList: [],
+  restaurantInfo: { name: '', category: '', address: '' },
+};
+
+export default function reducer(state = initialState, action) {
+  return state;
 }
