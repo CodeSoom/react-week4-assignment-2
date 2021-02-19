12bdeae6fc7add85697afd5927d542f7e0572590
Blueberry <54464320+lee01097@users.noreply.github.com>
2021-02-19 22:06:18 +0900

    Red) List.test.jsx : 테스트 케이스 작성

---
+++ src/List.test.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import List from './List';
+
+describe('List', () => {
+  function renderList(registers) {
+    return render((
+      <List registers={registers} />
+    ));
+  }
+
+  context('with registers', () => {
+    const registers = [{ id: 1, information: '마녀식당 | 한식 | 서울시 강남구' }];
+
+    it('shows registers', () => {
+      const { queryByText } = renderList(registers);
+
+      expect(queryByText('마녀식당 | 한식 | 서울시 강남구')).not.toBeNull();
+    });
+  });
+
+  context('without registers', () => {
+    const registers = [];
+
+    it('shows nothing', () => {
+      const { queryByText } = renderList(registers);
+      expect(queryByText('마녀식당 | 한식 | 서울시 강남구')).toBeNull();
+    });
+  });
+});
