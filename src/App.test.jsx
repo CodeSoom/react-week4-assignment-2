e2368a218c537a33f597757f493786b47be7f2d3
Yoon <yoonhe@gmail.com>
2021-02-19 14:48:22 +0900

    [red] App 컴포넌트에 대한 테스트를 추가한다

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import App from './App';
 
@@ -9,13 +9,43 @@ import App from './App';
  */
 
 describe('App', () => {
-  context('without input value', () => {
-    it('renders input placeholder', () => {
-      const { getByPlaceholderText } = render(<App />);
+  it('renders input placeholder', () => {
+    const { getByPlaceholderText } = render(<App />);
 
-      expect(getByPlaceholderText('이름')).not.toBeNull();
-      expect(getByPlaceholderText('분류')).not.toBeNull();
-      expect(getByPlaceholderText('주소')).not.toBeNull();
+    expect(getByPlaceholderText('이름')).not.toBeNull();
+    expect(getByPlaceholderText('분류')).not.toBeNull();
+    expect(getByPlaceholderText('주소')).not.toBeNull();
+  });
+
+  it('listens change events', () => {
+    const { getByDisplayValue, getByPlaceholderText } = render(<App />);
+
+    fireEvent.change(getByPlaceholderText('이름'), {
+      target: {
+        value: '마녀주방',
+      },
     });
+
+    fireEvent.change(getByPlaceholderText('분류'), {
+      target: {
+        value: '한식',
+      },
+    });
+
+    fireEvent.change(getByPlaceholderText('주소'), {
+      target: {
+        value: '서울시 강남구',
+      },
+    });
+
+    expect(getByDisplayValue('마녀주방')).not.toBeNull();
+    expect(getByDisplayValue('한식')).not.toBeNull();
+    expect(getByDisplayValue('서울시 강남구')).not.toBeNull();
+  });
+
+  it('renders button', () => {
+    const { getByText } = render(<App />);
+
+    expect(getByText('등록')).not.toBeNull();
   });
 });
