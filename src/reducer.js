5fb1375793fcb8d006e514f54ced55bb8e9165ff
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-19 18:12:05 +0900

    :recycle: Refactor: reducer
    - ADD_RESTAURANT

--- src/reducer.js
+++ src/reducer.js
@@ -10,14 +10,7 @@ export default function reducer(state = initialState, action) {
     return { ...state, restaurantInfo: { ...state.restaurantInfo, ...action.payload } };
   }
   if (action.type === ADD_RESTAURANT) {
-    return {
-      ...state,
-      restaurantList: [{
-        name: '선정릉역 깐부치킨',
-        category: '한식',
-        address: '서울 강남구 삼성동 37 깐부치킨 선정릉역점',
-      }],
-    };
+    return { ...state, restaurantList: [...state.restaurantList, state.restaurantInfo] };
   }
   return state;
 }
