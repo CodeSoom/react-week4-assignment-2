a0ef2301f115246f5a35b230913920f2b813e902
dodoongtak <themighty.tak@gmail.com>
2021-02-19 17:17:57 +0900

    🟢 Reducer : addRestaurant 구현

--- src/reducer.js
+++ src/reducer.js
@@ -5,13 +5,23 @@ const initialState = {
     category: '',
     address: '',
   },
+  restaurants: [],
 };
 
 const reducers = {
   updateRestaurant: (state, { payload: { restaurant } }) => ({
-    ...state, restaurant: { ...state.restaurant, ...restaurant },
+    ...state,
+    restaurant: { ...state.restaurant, ...restaurant },
   }),
+  addRestaurant: (state) => {
+    const { newId, restaurant, restaurants } = state;
 
+    return {
+      newId: newId + 1,
+      restaurant: {},
+      restaurants: [...restaurants, { id: newId, restaurant }],
+    };
+  },
 };
 
 export default function reducer(state = initialState, action = { type: undefined }) {
