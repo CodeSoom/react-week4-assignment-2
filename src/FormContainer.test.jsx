922a38c839f80c5f54b4deba012564128f34d5dc
Yoon <yoonhe@gmail.com>
2021-02-19 16:43:30 +0900

    [red] FormContainer 컴포넌트에 대한 테스트를 추가한다

--- src/FormContainer.test.jsx
+++ src/FormContainer.test.jsx
@@ -7,7 +7,17 @@ import FormContainer from './FormContainer';
 jest.mock();
 
 describe('FormContainer', () => {
-  it('renders form', () => {
+  it('renders inputs', () => {
     const { queryByPlaceholderText } = render(<FormContainer />);
+
+    expect(queryByPlaceholderText('이름')).not.toBeNull();
+    expect(queryByPlaceholderText('분류')).not.toBeNull();
+    expect(queryByPlaceholderText('주소')).not.toBeNull();
+  });
+
+  it('renders "등록" button', () => {
+    const { queryByText } = render(<FormContainer />);
+
+    expect(queryByText('등록')).not.toBeNull();
   });
 });
