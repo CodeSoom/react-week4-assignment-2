09d0044121618eb7585d78aff0adf2d42bce1c3f
Yoon <yoonhe@gmail.com>
2021-02-19 17:08:28 +0900

    [red] FormContainer 컴포넌트에 대한 테스트를 추가한다

--- src/FormContainer.test.jsx
+++ src/FormContainer.test.jsx
@@ -2,17 +2,28 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import { useSelector } from 'react-redux';
 import FormContainer from './FormContainer';
 
 jest.mock();
 
 describe('FormContainer', () => {
-  it('renders inputs', () => {
-    const { queryByPlaceholderText } = render(<FormContainer />);
+  beforeEach(() => {
+    useSelector.mockImplementation((selector) => selector({
+      inputs: {
+        name: '마녀주방',
+        category: '한식',
+        address: '서울시 강남구',
+      },
+    }));
+  });
 
-    expect(queryByPlaceholderText('이름')).not.toBeNull();
-    expect(queryByPlaceholderText('분류')).not.toBeNull();
-    expect(queryByPlaceholderText('주소')).not.toBeNull();
+  it('renders inputs', () => {
+    const { queryByDisplayValue } = render(<FormContainer />);
+
+    expect(queryByDisplayValue('마녀주방')).not.toBeNull();
+    expect(queryByDisplayValue('한식')).not.toBeNull();
+    expect(queryByDisplayValue('서울시 강남구')).not.toBeNull();
   });
 
   it('renders "등록" button', () => {
