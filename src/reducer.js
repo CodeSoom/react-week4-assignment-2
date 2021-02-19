f20aa23adef5db62bb6c365faae0e350f1296af2
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-19 14:49:12 +0900

    :construction: Green-Working: Make reducer pass the test
    - UPDATE_RESTAURANTINFO 테스트 통과처리

--- src/reducer.js
+++ src/reducer.js
@@ -1,8 +1,20 @@
+import { UPDATE_RESTAURANTINFO } from './actions';
+
 const initialState = {
   restaurantList: [],
   restaurantInfo: { name: '', category: '', address: '' },
 };
 
 export default function reducer(state = initialState, action) {
+  if (action.type === UPDATE_RESTAURANTINFO) {
+    return {
+      ...state,
+      restaurantInfo: {
+        name: '선정릉역 깐부치킨',
+        catagory: '한식',
+        address: '서울 강남구 삼성동 37 깐부치킨 선정릉역점',
+      },
+    };
+  }
   return state;
 }
