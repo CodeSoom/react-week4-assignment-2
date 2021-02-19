d63089cd2bae025f62bb33b4f707ea36f3eb4613
EL <ickyicky@gmail.com>
2021-02-19 21:31:37 +0900

    Green: 테스트 통과

---
+++ src/AddForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function AddForm() {
+  return (
+    <form>
+      <input type="text" placeholder="이름" />
+      <input type="text" placeholder="분류" />
+      <input type="text" placeholder="주소" />
+      <button type="submit">등록</button>
+    </form>
+  );
+}
