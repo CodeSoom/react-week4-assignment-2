d906cee7a176cf1ae8acab8842ce26b011cc87fd
Yoon <yoonhe@gmail.com>
2021-02-19 16:44:03 +0900

    [green] FormContainer 컴포넌트를 구현한다

--- src/FormContainer.jsx
+++ src/FormContainer.jsx
@@ -2,7 +2,21 @@ import React from 'react';
 
 function FormContainer() {
   return (
-    <div />
+    <form>
+      <input
+        type="text"
+        placeholder="이름"
+      />
+      <input
+        type="text"
+        placeholder="분류"
+      />
+      <input
+        type="text"
+        placeholder="주소"
+      />
+      <button type="submit">등록</button>
+    </form>
   );
 }
 
