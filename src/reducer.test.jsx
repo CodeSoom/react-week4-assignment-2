c9ed610de31661bff8e35cb00f5addc3598dacc7
datalater <the7mincheol@gmail.com>
2021-02-19 21:50:38 +0900

    등록되지 않은 액션 타입을 테스트하라

--- src/reducer.test.jsx
+++ src/reducer.test.jsx
@@ -1,45 +1,58 @@
 import reducer from './reducer';
 
 describe('reducer', () => {
-  describe('updateRestaurantInformation', () => {
-    const previousState = {
-      id: 0,
-      name: '',
-      category: '',
-      address: '',
-      restaurants: [],
-    };
-    it('updates restaurant name', () => {
+  const previousState = {
+    id: 0,
+    name: '',
+    category: '',
+    address: '',
+    restaurants: [],
+  };
+
+  context('without registered action type', () => {
+    it('does nothing', () => {
       const state = reducer(previousState, {
-        type: 'updateRestaurantName',
-        payload: {
-          name: '마녀주방',
-        },
+        type: undefined,
       });
 
-      expect(state.name).toBe('마녀주방');
+      expect(state).toEqual(previousState);
     });
+  });
 
-    it('updates restaurant category', () => {
-      const state = reducer(previousState, {
-        type: 'updateRestaurantCategory',
-        payload: {
-          category: '한식',
-        },
+  context('with registered action type', () => {
+    describe('updateRestaurantInformation', () => {
+      it('updates restaurant name', () => {
+        const state = reducer(previousState, {
+          type: 'updateRestaurantName',
+          payload: {
+            name: '마녀주방',
+          },
+        });
+
+        expect(state.name).toBe('마녀주방');
       });
 
-      expect(state.category).toBe('한식');
-    });
+      it('updates restaurant category', () => {
+        const state = reducer(previousState, {
+          type: 'updateRestaurantCategory',
+          payload: {
+            category: '한식',
+          },
+        });
 
-    it('updates restaurant address', () => {
-      const state = reducer(previousState, {
-        type: 'updateRestaurantAddress',
-        payload: {
-          address: '서울시 강남구',
-        },
+        expect(state.category).toBe('한식');
       });
 
-      expect(state.address).toBe('서울시 강남구');
+      it('updates restaurant address', () => {
+        const state = reducer(previousState, {
+          type: 'updateRestaurantAddress',
+          payload: {
+            address: '서울시 강남구',
+          },
+        });
+
+        expect(state.address).toBe('서울시 강남구');
+      });
     });
   });
 });
