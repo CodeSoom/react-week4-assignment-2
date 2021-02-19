fce4ac9e1bdc53b13278db1739acc14f02f44149
Najeong-Kim <atpeoe1023@gmail.com>
2021-02-19 16:27:20 +0900

    Input에 이름, 분류, 주소 구현하기

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import Input from './Input';
+
 export default function App() {
   return (
     <div>
       <h1>Restaurants</h1>
+      <Input />
     </div>
   );
 }
--- src/Input.jsx
+++ src/Input.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 
 export default function input() {
   return (
-    <div />
+    <div>
+      <input type="text" placeholder="이름" />
+      <input type="text" placeholder="분류" />
+      <input type="text" placeholder="주소" />
+    </div>
   )
 }
