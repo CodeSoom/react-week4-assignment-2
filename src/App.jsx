704019732d03aded930ca7c26940582a3ba3133f
Yoon <yoonhe@gmail.com>
2021-02-19 14:48:44 +0900

    [green] App 컴포넌트를 구현한다

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function App() {
+  const [state, setState] = useState({
+    inputs: {
+      name: '',
+      category: '',
+      address: '',
+    },
+  });
+
+  const { name, category, address } = state.inputs;
+
+  const handleChangeInput = (event) => {
+    const { value, name: eventName } = event.target;
+
+    setState({
+      ...state,
+      inputs: {
+        ...state.inputs,
+        [eventName]: value,
+      },
+    });
+  };
+
   return (
     <div>
       <form>
-        <input type="text" name="name" placeholder="이름" />
-        <input type="text" name="category" placeholder="분류" />
-        <input type="text" name="address" placeholder="주소" />
+        <input
+          type="text"
+          name="name"
+          placeholder="이름"
+          value={name}
+          onChange={handleChangeInput}
+        />
+        <input
+          type="text"
+          name="category"
+          placeholder="분류"
+          value={category}
+          onChange={handleChangeInput}
+        />
+        <input
+          type="text"
+          name="address"
+          placeholder="주소"
+          value={address}
+          onChange={handleChangeInput}
+        />
       </form>
     </div>
   );
