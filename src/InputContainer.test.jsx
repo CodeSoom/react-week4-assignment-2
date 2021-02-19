182a83dbaea42cc9486e4d9bf9afde69568daae9
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-19 14:52:54 +0900

    :white_check_mark: Red-Test: Refactor inputContainer.test.jsx
    - reducer test 와 일치하도록 수정

--- src/InputContainer.test.jsx
+++ src/InputContainer.test.jsx
@@ -21,13 +21,13 @@ describe('InputContainer', () => {
     useDispatch.mockImplementationOnce(() => dispatch);
     const { getByPlaceholderText } = render(<InputContainer />);
 
-    fireEvent.change(getByPlaceholderText('이름'), { target: { value: '선정릉역 깐부치킨' } });
-    expect(dispatch).toHaveBeenCalledWith({ type: 'UPDATE_RESTAURANTINFO', payload: '선정릉역 깐부치킨' });
+    fireEvent.change(getByPlaceholderText('이름'), { target: { name: 'name', value: '선정릉역 깐부치킨' } });
+    expect(dispatch).toHaveBeenCalledWith({ type: 'UPDATE_RESTAURANTINFO', payload: { name: '선정릉역 깐부치킨' } });
 
-    fireEvent.change(getByPlaceholderText('분류'), { target: { value: '한식' } });
-    expect(dispatch).toHaveBeenCalledWith({ type: 'UPDATE_RESTAURANTINFO', payload: '한식' });
+    fireEvent.change(getByPlaceholderText('분류'), { target: { name: 'category', value: '한식' } });
+    expect(dispatch).toHaveBeenCalledWith({ type: 'UPDATE_RESTAURANTINFO', payload: { category: '한식' } });
 
-    fireEvent.change(getByPlaceholderText('주소'), { target: { value: '서울 강남구 삼성동 37 깐부치킨 선정릉역점' } });
-    expect(dispatch).toHaveBeenCalledWith({ type: 'UPDATE_RESTAURANTINFO', payload: '서울 강남구 삼성동 37 깐부치킨 선정릉역점' });
+    fireEvent.change(getByPlaceholderText('주소'), { target: { name: 'address', value: '서울 강남구 삼성동 37 깐부치킨 선정릉역점' } });
+    expect(dispatch).toHaveBeenCalledWith({ type: 'UPDATE_RESTAURANTINFO', payload: { address: '서울 강남구 삼성동 37 깐부치킨 선정릉역점' } });
   });
 });
