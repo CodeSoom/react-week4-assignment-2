ca66d1ec7db4fe8840494b1de0de84ab3ae4cc03
Yoon <yoonhe@gmail.com>
2021-02-19 14:09:08 +0900

    [red] App 컴포넌트에 대한 테스트를 추가한다

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -4,8 +4,18 @@ import { render } from '@testing-library/react';
 
 import App from './App';
 
+/**
+ * input이 렌더링 되는지?
+ */
+
 describe('App', () => {
-  it('renders inputs', () => {
-    const { getByText } = render(<App />);
+  context('without input value', () => {
+    it('renders input placeholder', () => {
+      const { getByPlaceholderText } = render(<App />);
+
+      expect(getByPlaceholderText('이름')).not.toBeNull();
+      expect(getByPlaceholderText('분류')).not.toBeNull();
+      expect(getByPlaceholderText('주소')).not.toBeNull();
+    });
   });
 });
