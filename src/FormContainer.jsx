a2cef6a02336ff53d5571ed3bb992fac64abfbd6
Yoon <yoonhe@gmail.com>
2021-02-19 17:09:15 +0900

    [green] FormContainer 컴포넌트를 구현한다

--- src/FormContainer.jsx
+++ src/FormContainer.jsx
@@ -1,19 +1,31 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 function FormContainer() {
+  const {
+    inputs,
+  } = useSelector((state) => ({
+    inputs: state.inputs,
+  }));
+
+  const { name, category, address } = inputs;
+
   return (
     <form>
       <input
         type="text"
         placeholder="이름"
+        value={name}
       />
       <input
         type="text"
         placeholder="분류"
+        value={category}
       />
       <input
         type="text"
         placeholder="주소"
+        value={address}
       />
       <button type="submit">등록</button>
     </form>
