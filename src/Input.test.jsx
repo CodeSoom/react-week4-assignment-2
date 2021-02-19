cb03f13b432aea232a45d8de3f8e87e0aae02d35
EL <ickyicky@gmail.com>
2021-02-19 21:39:29 +0900

    Red: Input 테스트 작성

---
+++ src/Input.test.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import Input from './Input';
+
+describe('Input', () => {
+  function renderInput({ placeholder }) {
+    return render((
+      <Input placeholder={placeholder} />
+    ));
+  }
+
+  it('placeholder를 보여준다.', () => {
+    const placeholder = '이름';
+
+    const { getByPlaceholderText } = renderInput({ placeholder });
+
+    expect(getByPlaceholderText(/placeholder/)).not.toBeNull();
+
+  });
+});
