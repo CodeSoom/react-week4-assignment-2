155100d64a567c3dc20ea92da3ab543d6b5d3cc7
Blueberry <54464320+lee01097@users.noreply.github.com>
2021-02-20 00:02:58 +0900

    Red) List.test.jsx : 테스트 케이스 변경

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -12,12 +12,16 @@ describe('List', () => {
   }
 
   context('with registers', () => {
-    const registers = [{ id: 1, information: '마녀식당 | 한식 | 서울시 강남구' }];
+    const registers = [
+      { id: 1, information: '마녀식당 | 한식 | 서울시 강남구' },
+      { id: 2, information: '소녀식당 | 소식 | 소울시 강남구' },
+    ];
 
     it('shows registers', () => {
       const { queryByText } = renderList(registers);
 
       expect(queryByText('마녀식당 | 한식 | 서울시 강남구')).not.toBeNull();
+      expect(queryByText('소녀식당 | 소식 | 소울시 강남구')).not.toBeNull();
     });
   });
 
