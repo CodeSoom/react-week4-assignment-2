18fdd500d7b385b3122d0b2750559063af9bab56
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-19 14:47:26 +0900

    :white_check_mark: Red-Test: Add reducer test
    - 레스토랑 정보 입력시 테스트 추가

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,6 +1,15 @@
+import { updateRestaurantInfo } from './actions';
 import reducer from './reducer';
 
 describe('reducer', () => {
+  const initialState = {
+    restaurantList: [],
+    restaurantInfo: { name: '', category: '', address: '' },
+  };
+
+  function changeState(action) {
+    return reducer(initialState, action);
+  }
   describe('dafault state', () => {
     it('shold have default state', () => {
       const defaultState = reducer(undefined, '');
@@ -9,6 +18,20 @@ describe('reducer', () => {
       expect(defaultState.restaurantInfo.name).toBe('');
       expect(defaultState.restaurantInfo.category).toBe('');
       expect(defaultState.restaurantInfo.address).toBe('');
+    });
+  });
+  describe('UPDATE_RESTAURANTINFO', () => {
+    it('should change restaurantInfo', () => {
+      const restaurantInfo = {
+        name: '선정릉역 깐부치킨',
+        catagory: '한식',
+        address: '서울 강남구 삼성동 37 깐부치킨 선정릉역점',
+      };
+      const changedState = changeState(updateRestaurantInfo(restaurantInfo));
+
+      expect(changedState.restaurantInfo.name).toBe(restaurantInfo.name);
+      expect(changedState.restaurantInfo.catagory).toBe(restaurantInfo.catagory);
+      expect(changedState.restaurantInfo.address).toBe(restaurantInfo.address);
     });
   });
 });
