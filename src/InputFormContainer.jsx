e842351b334c6e0a22c10ff6c3d7cde348fae392
shinhyogeun <tls1gy2rms3@naver.com>
2021-02-19 15:36:06 +0900

    InputFormContainer test 통과

--- src/InputFormContainer.jsx
+++ src/InputFormContainer.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
 
+import { useSelector, useDispatch } from 'react-redux';
+
+import InputForm from './InputForm';
+
 export default function InputFormContainer() {
+  const dispatch = useDispatch();
+
+  const { name, category, address } = useSelector((state) => ({
+    name: state.name,
+    category: state.category,
+    address: state.address,
+  }));
+
   return (
-    <div />
+    <InputForm
+      nameValue={name}
+      categoryValue={category}
+      addressValue={address}
+      onChange={dispatch}
+      onClick={dispatch}
+    />
   );
 }
