ba5082971c6a9a85a025ae381d4f5d372d71036b
Yoon <yoonhe@gmail.com>
2021-02-19 14:09:37 +0900

    [green] App 컴포넌트를 구현한다

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 
 export default function App() {
   return (
-    <div />
+    <div>
+      <form>
+        <input type="text" name="name" placeholder="이름" />
+        <input type="text" name="category" placeholder="분류" />
+        <input type="text" name="address" placeholder="주소" />
+      </form>
+    </div>
   );
 }
