6d783e20d33fea7daffd8255a7d2baf1fcafddd1
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-02-20 16:56:52 +0900

    Input을 테스트하는데, 이름의 onchange를 테스트합니다.

--- src/Input.jsx
+++ src/Input.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
-export default function Input() {
+export default function Input({ value, onChange }) {
   return (
     <input
       type="text"
       placeholder="이름"
+
     />
   );
 }
--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import Input from './Input';
 
 describe('Input', () => {
+  const handleChange = jest.fn();
+
   it('입력값을 확인합니다.', () => {
-    const { getByPlaceholderText } = render((<Input />));
+    const { getByPlaceholderText, getByDisplayValue } = render((
+      <Input
+        value="기존 식당이름"
+        onChange={handleChange}
+      />));
 
     expect(getByPlaceholderText(/이름/)).not.toBeNull();
+
+    expect(getByDisplayValue(/기존 식당이름/)).not.toBeNull();
+    expect(handleChange).not.toBeCalled();
+    fireEvent.change(getByPlaceholderText(/이름/), {
+      target: { value: '마녀식당' },
+    });
+    expect(handleChange).toBeCalled();
   });
 });
