368b06c890003ac05adcea3eb77b7abc1d5067d7
EL <ickyicky@gmail.com>
2021-02-20 15:50:06 +0900

    Green: 커버리지

--- src/Restaurants.jsx
+++ src/Restaurants.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function Restaurants({ restaurants = [] }) {
+export default function Restaurants({ restaurants}) {
   return (
     <ul>
       {restaurants.map(({
