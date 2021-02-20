a3e42e48f54ef99a67de6a028ed0342af25814e0
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-20 13:35:20 +0900

    :white_check_mark: Red-Test: Update reducer test

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -2,14 +2,12 @@ import { addRestaurant, updateRestaurantInfo } from './actions';
 import reducer from './reducer';
 
 describe('reducer', () => {
-  const initialState = {
-    restaurantList: [],
-    restaurantInfo: { name: '', category: '', address: '' },
+  const restaurantInfo = {
+    id: 0,
+    name: '선정릉역 깐부치킨',
+    catagory: '한식',
+    address: '서울 강남구 삼성동 37 깐부치킨 선정릉역점',
   };
-
-  function changeState(state, action) {
-    return reducer(state, action);
-  }
   describe('dafault state', () => {
     it('shold have default state', () => {
       const defaultState = reducer(undefined, '');
@@ -22,12 +20,7 @@ describe('reducer', () => {
   });
   describe('UPDATE_RESTAURANTINFO', () => {
     it('should change restaurantInfo', () => {
-      const restaurantInfo = {
-        name: '선정릉역 깐부치킨',
-        catagory: '한식',
-        address: '서울 강남구 삼성동 37 깐부치킨 선정릉역점',
-      };
-      const changedState = changeState(initialState, updateRestaurantInfo(restaurantInfo));
+      const changedState = reducer(undefined, updateRestaurantInfo(restaurantInfo));
 
       expect(changedState.restaurantInfo.name).toBe(restaurantInfo.name);
       expect(changedState.restaurantInfo.catagory).toBe(restaurantInfo.catagory);
@@ -38,16 +31,13 @@ describe('reducer', () => {
     it('should be able to add a restaurant list', () => {
       const previousState = {
         restaurantList: [],
-        restaurantInfo: {
-          name: '선정릉역 깐부치킨',
-          category: '한식',
-          address: '서울 강남구 삼성동 37 깐부치킨 선정릉역점',
-        },
+        restaurantInfo,
       };
 
-      const changedState = changeState(previousState, addRestaurant());
+      const changedState = reducer(previousState, addRestaurant());
 
       expect(changedState.restaurantList).toHaveLength(1);
+      expect(changedState.restaurantList[0].id).toBe(1);
       expect(changedState.restaurantList[0].name).toBe(previousState.restaurantInfo.name);
       expect(changedState.restaurantList[0].category).toBe(previousState.restaurantInfo.category);
       expect(changedState.restaurantList[0].address).toBe(previousState.restaurantInfo.address);
