37cc763e37ace51d8557a96d046fc57de8a683b1
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-02-20 17:07:13 +0900

    Input을 구현하는데 분류항목의 element를 구현합니다.

--- src/Input.jsx
+++ src/Input.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 
-export default function Input({ value, onChange }) {
+export default function Input({ value, category, onChange }) {
   return (
     <div>
       <input
         type="text"
         placeholder="이름"
         value={value}
+        onChange={onChange}
+      />
+      <input
+        type="text"
+        placeholder="분류"
+        value={category}
         onChange={onChange}
       />
     </div>
