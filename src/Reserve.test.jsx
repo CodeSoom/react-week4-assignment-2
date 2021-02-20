eca5615ae82e425e60c5b5ed7ee5df0d64c735b1
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-02-21 06:19:10 +0900

    Reserve를 리팩토링하여 getBy를 queryBy로 대체하고 필요한 개행 추가합니다

--- src/Reserve.test.jsx
+++ src/Reserve.test.jsx
@@ -11,54 +11,62 @@ describe('Input', () => {
   beforeEach(() => jest.clearAllMocks());
 
   it('이름 항목의 elemnet들을 확인합니다.', () => {
-    const { getByPlaceholderText, getByDisplayValue } = render((
+    const { queryByPlaceholderText, queryByDisplayValue, getByPlaceholderText } = render((
       <Reserve
         value="기존 식당이름"
         onChange={onChange}
       />));
 
-    expect(getByPlaceholderText(/이름/)).not.toBeNull();
+    expect(queryByPlaceholderText(/이름/)).not.toBeNull();
 
-    expect(getByDisplayValue(/기존 식당이름/)).not.toBeNull();
+    expect(queryByDisplayValue(/기존 식당이름/)).not.toBeNull();
 
     expect(onChange).not.toBeCalled();
+
     fireEvent.change(getByPlaceholderText(/이름/), {
       target: { value: '마녀식당' },
     });
+
     expect(onChange).toBeCalled();
   });
 
   it('분류 항목의 elemnet들을 확인합니다.', () => {
-    const { getByPlaceholderText, getByDisplayValue } = render((
+    const { queryByPlaceholderText, queryAllByDisplayValue, getByPlaceholderText } = render((
       <Reserve
         value="기존 식당분류"
         onChange={onChange}
       />));
 
-    expect(getByPlaceholderText(/분류/)).not.toBeNull();
+    expect(queryByPlaceholderText(/분류/)).not.toBeNull();
 
-    expect(getByDisplayValue(/기존 식당분류/)).not.toBeNull();
+    expect(queryAllByDisplayValue(/기존 식당분류/)).not.toBeNull();
+
     expect(onChange).not.toBeCalled();
+
     fireEvent.change(getByPlaceholderText(/분류/), {
       target: { value: '한식' },
     });
+
     expect(onChange).toBeCalled();
   });
 
   it('주소 항목의 elemnet들을 확인합니다.', () => {
-    const { getByPlaceholderText, getByDisplayValue } = render((
+    const { queryByPlaceholderText, queryAllByDisplayValue, getByPlaceholderText } = render((
       <Reserve
         value="기존 식당주소"
         onChange={onChange}
       />));
 
-    expect(getByPlaceholderText(/주소/)).not.toBeNull();
+    expect(queryByPlaceholderText(/주소/)).not.toBeNull();
 
-    expect(getByDisplayValue(/기존 식당주소/)).not.toBeNull();
+    expect(queryAllByDisplayValue(/기존 식당주소/)).not.toBeNull();
+
     expect(onChange).not.toBeCalled();
+
     fireEvent.change(getByPlaceholderText(/주소/), {
       target: { value: '서울시 강남구' },
     });
+
     expect(onChange).toBeCalled();
   });
 
@@ -69,7 +77,9 @@ describe('Input', () => {
       />));
 
     expect(onClick).not.toBeCalled();
+
     fireEvent.click(getByText(/등록/));
+
     expect(onClick).toBeCalled();
   });
 });
