f9894e92b74fc44790c31c57c72f3146b6c9d6ff
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-20 13:25:51 +0900

    :recycle: Refactor: RestaurantList

--- src/RestaurantList.jsx
+++ src/RestaurantList.jsx
@@ -9,12 +9,7 @@ function RestaurantList({ restaurantList }) {
 
   return (
     <ul>
-      {[{
-        id: 0,
-        name: '선정릉역 깐부치킨',
-        category: '한식',
-        address: '서울 강남구 삼성동 37 깐부치킨 선정릉역점',
-      }].map((restaurantInfo) => (
+      {restaurantList.map((restaurantInfo) => (
         <Item
           key={restaurantInfo.id}
           restaurantInfo={restaurantInfo}
