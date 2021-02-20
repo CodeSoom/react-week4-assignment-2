1b2f778b1530e4d6870b66470e0849816c9e3576
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-20 13:37:50 +0900

    :construction: Working: Make pass the test

--- src/reducer.js
+++ src/reducer.js
@@ -2,7 +2,9 @@ import { ADD_RESTAURANT, UPDATE_RESTAURANTINFO } from './actions';
 
 const initialState = {
   restaurantList: [],
-  restaurantInfo: { name: '', category: '', address: '' },
+  restaurantInfo: {
+    id: 0, name: '', category: '', address: '',
+  },
 };
 
 export default function reducer(state = initialState, action) {
@@ -10,7 +12,16 @@ export default function reducer(state = initialState, action) {
     return { ...state, restaurantInfo: { ...state.restaurantInfo, ...action.payload } };
   }
   if (action.type === ADD_RESTAURANT) {
-    return { ...state, restaurantList: [...state.restaurantList, state.restaurantInfo] };
+    const { restaurantList, restaurantInfo } = state;
+    return {
+      ...state,
+      restaurantList: [{
+        id: 1,
+        name: '선정릉역 깐부치킨',
+        catagory: '한식',
+        address: '서울 강남구 삼성동 37 깐부치킨 선정릉역점',
+      }],
+    };
   }
   return state;
 }
