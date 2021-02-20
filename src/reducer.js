b536f8f8a534457a9c73612bb7ae745ba510f49f
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-20 13:38:17 +0900

    :recycle: Refactor: reducer

--- src/reducer.js
+++ src/reducer.js
@@ -15,12 +15,7 @@ export default function reducer(state = initialState, action) {
     const { restaurantList, restaurantInfo } = state;
     return {
       ...state,
-      restaurantList: [{
-        id: 1,
-        name: '선정릉역 깐부치킨',
-        catagory: '한식',
-        address: '서울 강남구 삼성동 37 깐부치킨 선정릉역점',
-      }],
+      restaurantList: [...restaurantList, { ...restaurantInfo, id: restaurantList.length + 1 }],
     };
   }
   return state;
