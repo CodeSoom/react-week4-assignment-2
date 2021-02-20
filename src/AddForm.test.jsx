d679bc537af49ea1c1c361bd1cdce149fd6fadc9
EL <ickyicky@gmail.com>
2021-02-20 15:26:50 +0900

    Red: 테스트 생성

--- src/AddForm.test.jsx
+++ src/AddForm.test.jsx
@@ -1,22 +1,56 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import AddForm from './AddForm';
 
 describe('AddForm', () => {
-  function renderAddForm() {
+  const onChange = jest.fn();
+
+  function renderAddForm({ restaurant }) {
     return render((
-      <AddForm />
+      <AddForm
+        restaurant={restaurant}
+      />
     ));
   }
 
   it('input, button을 보여준다.', () => {
-    const { queryByText, getByPlaceholderText } = renderAddForm();
+    const restaurant = {
+      name: '',
+      category: '',
+      address: '',
+    };
+
+    const { queryByText, getByPlaceholderText } = renderAddForm({ restaurant });
 
     expect(getByPlaceholderText(/이름/)).not.toBeNull();
     expect(getByPlaceholderText(/분류/)).not.toBeNull();
     expect(getByPlaceholderText(/주소/)).not.toBeNull();
+
+    expect(queryByText(/등록/)).not.toBeNull();
+  });
+
+  it('값을 변경하면, 상태값이 변경된다.', () => {
+    const restaurant = {
+      name: '',
+      category: '',
+      address: '',
+    };
+
+    const { queryByText, getByPlaceholderText } = renderAddForm({ restaurant });
+
+    const input = getByPlaceholderText('이름');
+
+    fireEvent.change(input, {
+      target: {
+        value: '인풋 작성',
+      },
+    });
+
+    expect(onChange).toBeCalled();
+
+    expect(input.value).toBe('인풋 작성');
 
     expect(queryByText(/등록/)).not.toBeNull();
   });
