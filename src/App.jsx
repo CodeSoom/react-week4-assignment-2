6cd3dd283d4de6ac0ebdb23816b3af8c0878f616
daniel <daniel@innosonian.com>
2021-02-22 19:08:43 +0900

    Restaurant 생성해서 출력부 구조 잡아주고 데이터 출력

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 
+import Restaurants from './Restaurants';
+
 export default function App() {
+  const restaurants = [
+    {
+      id: 1, name: '김밥천국', category: '분식', address: '서울시 서초구 방배동',
+    },
+  ];
   return (
     <div>
-      Hello
+      <h1>Restaurants</h1>
+      <Restaurants restaurants={restaurants} />
     </div>
   );
 }
