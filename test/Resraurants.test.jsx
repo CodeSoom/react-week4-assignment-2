feeb1574dee6d95313155c2ef66d93fb5d51c78e
jjkim01 <jaen6563@gmail.com>
2021-02-24 00:34:34 +0900

    fixtures를 통해 중복되는 데이터 한곳으로 통합

--- test/Resraurants.test.jsx
+++ test/Resraurants.test.jsx
@@ -4,13 +4,9 @@ import { render } from '@testing-library/react';
 
 import Restaurants from '../src/Restaurants';
 
-test('Restaurants', () => {
-  const restaurants = [
-    {
-      id: 1, name: '김밥천국', category: '분식', address: '서울시 서초구 방배동',
-    },
-  ];
+import restaurants from '../fixtures/restaurants';
 
+test('Restaurants', () => {
   const { getByText } = render((
     <Restaurants restaurants={restaurants} />
   ));
--- test/RestaurantsContainer.test.jsx
+++ test/RestaurantsContainer.test.jsx
@@ -6,15 +6,13 @@ import { render } from '@testing-library/react';
 
 import RestaurantsContainer from '../src/RestaurantsContainer';
 
+import restaurants from '../fixtures/restaurants';
+
 jest.mock('react-redux');
 
 test('RestaurantsContainer', () => {
   useSelector.mockImplementation((selector) => selector({
-    restaurants: [
-      {
-        id: 1, name: '김밥천국', category: '분식', address: '서울시 서초구 방배동',
-      },
-    ],
+    restaurants,
   }));
 
   const { getByText } = render((
