c5b02bf9aa0ee3572d95634610a2aee88f4d79c7
jjkim01 <jaen6563@gmail.com>
2021-02-24 00:53:33 +0900

    App에서 RestaurantsContainer 사용하도록 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
 import React from 'react';
 
-import Restaurants from './Restaurants';
+import RestaurantsContainer from './RestaurantsContainer';
 
 export default function App() {
-  const restaurants = [
-    {
-      id: 1, name: '김밥천국', category: '분식', address: '서울시 서초구 방배동',
-    },
-  ];
   return (
     <div>
       <h1>Restaurants</h1>
-      <Restaurants restaurants={restaurants} />
+      <RestaurantsContainer />
     </div>
   );
 }
