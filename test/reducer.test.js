c39a9d78e165e17d6feda5b43759ef9a3015ffa8
jjkim01 <jaen6563@gmail.com>
2021-02-24 02:19:20 +0900

    test: reducer를 통해 changeRestaurant 동작 검증

--- test/reducer.test.js
+++ test/reducer.test.js
@@ -2,6 +2,8 @@ import reducer from '../src/reducer';
 
 import {
   setRestaurants,
+  changeRestaurantField,
+  addRestaurants,
 } from '../src/actions';
 import restaurants from '../fixtures/restaurants';
 
@@ -15,6 +17,25 @@ describe('reducer', () => {
       const state = reducer(initalState, setRestaurants(restaurants));
 
       expect(state.restaurants).not.toHaveLength(0);
+    });
+  });
+
+  describe('changeRestaurants', () => {
+    it('changes restaurants form', () => {
+      const initalState = {
+        restaurant: {
+          name: '이름',
+          category: '분류',
+          address: '주소',
+        },
+      };
+
+      const state = reducer(initalState, changeRestaurantField({
+        name: 'address',
+        value: '서울시 강남구 역삼동',
+      }));
+
+      expect(state.restaurant.address).toBe('서울시 강남구 역삼동');
     });
   });
 });
