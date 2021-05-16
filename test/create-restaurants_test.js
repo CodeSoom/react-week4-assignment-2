4e1b97131dc2a92dcb9ba94ab4e8e88ad39db9ec
hannut91 <hannut91@gmail.com>
2021-05-16 18:38:01 +0900

    Upgrade dependencies
    Upgrade React to 17.
    Upgrade Webpack to v5.

--- test/create-restaurants_test.js
+++ test/create-restaurants_test.js
@@ -6,7 +6,7 @@ const restaurant = {
   address: '서울시 강남구',
 };
 
-Scenario('이름, 분류 그리고 주소를 입력한 후 확인을 누르면 레스토랑이 추가가 됩니다.', (I) => {
+Scenario('이름, 분류 그리고 주소를 입력한 후 확인을 누르면 레스토랑이 추가가 됩니다.', ({ I }) => {
   I.amOnPage('/');
 
   const { name, category, address } = restaurant;
