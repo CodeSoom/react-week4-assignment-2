599ea6bec789ee8bc1efd9b88acbf368e498ea5e
yujonglee <yujonglee@g.skku.edu>
2021-05-24 18:37:50 +0900

    App과 그 테스트에 리덕스를 도입하라

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
+import { useDispatch, useSelector } from 'react-redux';
 import List from './List';
 import InputBox from './InputBox';
 
 export default function App() {
+  const { restaurants } = useSelector((state) => state);
+  const dispatch = useDispatch();
+
   return (
     <>
       <h1>
         Restaurants
       </h1>
-      <List restaurants={[]} />
+      <List restaurants={restaurants} />
 
       <InputBox name="이름" />
       <InputBox name="분류" />
       <InputBox name="주소" />
+      <button type="button" onClick={() => dispatch({})}>
+        등록
+      </button>
     </>
   );
 }
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,5 +1,14 @@
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { useDispatch, useSelector } from 'react-redux';
+
 import App from './App';
+
+jest.mock('react-redux');
+
+useSelector.mockImplementation((selector) => selector({
+  restaurants: [],
+}));
 
 it('renders header', () => {
   const { container } = render(<App />);
@@ -9,4 +18,15 @@ it('renders header', () => {
 it('renders three inputbox', () => {
   const { getAllByRole } = render(<App />);
   expect(getAllByRole('textbox')).toHaveLength(3);
+});
+
+it('renders 등록 button', () => {
+  const dispatch = jest.fn();
+  useDispatch.mockImplementation(() => dispatch);
+
+  const { getByRole } = render(<App />);
+  expect(getByRole('button')).toBeInTheDocument();
+
+  userEvent.click(getByRole('button'));
+  expect(dispatch).toBeCalled();
 });
