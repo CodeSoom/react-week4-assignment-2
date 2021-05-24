9b3d2458374a24c9059a418de31e68f8cf5a0279
yujonglee <yujonglee@g.skku.edu>
2021-05-24 17:42:19 +0900

    App컴포넌트에서 List와 InputBox컴포넌트를 사용하라

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,17 @@
+import List from './List';
+import InputBox from './InputBox';
+
 export default function App() {
   return (
     <>
       <h1>
         Restaurants
       </h1>
+      <List restaurants={[]} />
 
-      <label htmlFor="input-name">
-        이름
-      </label>
-      <input
-        id="name"
-        type="text"
-        placeholder="이름"
-      />
-
-      <label htmlFor="input-category">
-        분류
-      </label>
-      <input
-        id="category"
-        type="text"
-        placeholder="분류"
-      />
-
-      <label htmlFor="input-address">
-        주소
-      </label>
-      <input
-        id="address"
-        type="text"
-        placeholder="주소"
-      />
+      <InputBox name="이름" />
+      <InputBox name="분류" />
+      <InputBox name="주소" />
     </>
   );
 }
