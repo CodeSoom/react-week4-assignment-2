27d50fbc8a8d64f23179575fe09cc7fe112bfa6c
yujonglee <yujonglee@g.skku.edu>
2021-05-24 18:16:48 +0900

    리듀서 테스트를 작성하라

--- src/reducer.test.jsx
+++ src/reducer.test.jsx
@@ -1,5 +1,18 @@
 import reducer from './reducer';
+import { addRestaurant } from './actions';
 
-it('', () => {
+it('updates title of task', () => {
+  const newRestaurant = {
+    id: 1, name: '마녀주방', category: '한식', address: '서울시 강남구',
+  };
 
+  const newState = reducer(undefined, addRestaurant(newRestaurant));
+
+  expect(newState).toEqual({
+    restaurants: [
+      {
+        id: 1, name: '마녀주방', category: '한식', address: '서울시 강남구',
+      },
+    ],
+  });
 });
