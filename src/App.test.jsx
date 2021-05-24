fdaa1c716ea837c28869321f9eb9751c1e1938fe
yujonglee <yujonglee@g.skku.edu>
2021-05-24 17:23:31 +0900

    App테스트에서 List테스트를 분리하라

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -10,10 +10,3 @@ it('renders three inputbox', () => {
   const { getAllByRole } = render(<App />);
   expect(getAllByRole('textbox')).toHaveLength(3);
 });
-
-it('renders restaurants', () => {
-  const { container } = render(<App />);
-  expect(container).toHaveTextContent('마녀주방');
-  expect(container).toHaveTextContent('한식');
-  expect(container).toHaveTextContent('서울시 강남구');
-});
---
+++ src/List.test.jsx
@@ -0,0 +1,21 @@
+import { render } from '@testing-library/react';
+import List from './List';
+
+const data = [
+  {
+    id: 1, name: '마녀주방', category: '한식', address: '서울시 강남구',
+  },
+  // {
+  //   id: 2, name: '시카고피자', category: '양식', address: '이태원동',
+  // },
+  // {
+  //   id: 2, name: '키와미', category: '일식', address: '분당구 정자동',
+  // },
+];
+
+it('renders restaurants', () => {
+  const { container } = render(<List restaurants={data} />);
+  expect(container).toHaveTextContent('마녀주방');
+  expect(container).toHaveTextContent('한식');
+  expect(container).toHaveTextContent('서울시 강남구');
+});
