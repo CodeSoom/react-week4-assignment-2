13cbb1512e14ce8341e59d20b6cc3290d7920ad6
yujonglee <yujonglee@g.skku.edu>
2021-05-24 17:21:07 +0900

    App 컴포넌트를 작성하라

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,55 @@
 export default function App() {
+  const data = [
+    {
+      id: 1, name: '마녀주방', category: '한식', address: '서울시 강남구',
+    },
+    // {
+    //   id: 2, name: '시카고피자', category: '양식', address: '이태원동',
+    // },
+    // {
+    //   id: 2, name: '키와미', category: '일식', address: '분당구 정자동',
+    // },
+  ];
   return (
-    <div>
-      Hello
-    </div>
+    <>
+      <h1>
+        Restaurants
+      </h1>
+
+      <label htmlFor="input-name">
+        이름
+      </label>
+      <input
+        id="name"
+        type="text"
+        placeholder="이름"
+      />
+
+      <label htmlFor="input-category">
+        분류
+      </label>
+      <input
+        id="category"
+        type="text"
+        placeholder="분류"
+      />
+
+      <label htmlFor="input-address">
+        주소
+      </label>
+      <input
+        id="address"
+        type="text"
+        placeholder="주소"
+      />
+
+      <ul>
+        <li>
+          {data[0].name}
+          {data[0].category}
+          {data[0].address}
+        </li>
+      </ul>
+    </>
   );
 }
