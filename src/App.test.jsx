aa4eb64a66d87de6e7eeccc42c963082e8d44765
최한울 <hanul.choi95@nts-corp.com>
2021-05-25 23:52:42 +0900

    App 컴포넌트 : 입력창과 등록버튼에 대한 테스트를 추가한다

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -19,4 +19,27 @@ describe('App', () => {
     expect(getByText('마녀주방 | 한식 | 서울시 강남구')).toHaveTextContent('마녀주방 | 한식 | 서울시 강남구');
     expect(getByText('시카고피자 | 양식 | 이태원동')).toHaveTextContent('시카고피자 | 양식 | 이태원동');
   });
+
+  it('renders 입력창과 등록 버튼', () => {
+    const { getByText, getByPlaceholderText } = render(
+      (
+        <div>
+          <h1>Restaurants</h1>
+          <ul>
+            <li>마녀주방 | 한식 | 서울시 강남구</li>
+            <li>시카고피자 | 양식 | 이태원동</li>
+          </ul>
+          <input placeholder="이름"/>
+          <input placeholder="분류"/>
+          <input placeholder="주소"/>
+          <button type="button">등록</button>
+        </div>
+      ),
+    );
+
+    expect(getByPlaceholderText(/이름/)).not.toBeNull();
+    expect(getByPlaceholderText(/분류/)).not.toBeNull();
+    expect(getByPlaceholderText(/주소/)).not.toBeNull();
+    expect(getByText(/등록/)).not.toBeNull();
+  })
 });
