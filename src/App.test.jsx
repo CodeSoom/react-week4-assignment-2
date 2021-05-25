6b30abe7710897efdd7a65a58a9373288cf22671
최한울 <hanul.choi95@nts-corp.com>
2021-05-25 23:40:29 +0900

    App 컴포넌트 : 타이틀을 리팩토링한다 2
    - App 컴포넌트를 임포트하여 테스트 코드에서 사용

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
 
+import App from "./App";
+
 import { render } from '@testing-library/react';
 
 describe('App', () => {
   it('renders title', () => {
-    const { getByText } = render(
-      (
-        <div>
-          <h1>Restaurants</h1>
-        </div>
-      ),
-    );
+    const { getByText } = render(<App />);
 
     const title = (getByText(/Restaurants/));
 
