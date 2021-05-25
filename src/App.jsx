26a71f796616c9a63cc460a27b7efade7d695ba5
최한울 <hanul.choi95@nts-corp.com>
2021-05-25 23:46:19 +0900

    App 컴포넌트 : 레스토랑 정보를 구현한다

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import React from "react";
+
 export default function App() {
   return (
     <div>
       <h1>Restaurants</h1>
+      <ol>
+        <li>마녀주방 | 한식 | 서울시 강남구</li>
+        <li>시카고피자 | 양식 | 이태원동</li>
+      </ol>
     </div>
   );
 }
