5983200da484dffdcc3560a1de2013261725bc16
최한울 <hanul.choi95@nts-corp.com>
2021-05-25 23:44:36 +0900

    App 컴포넌트 - 레스토랑 정보에 대한 테스트를 추가한다
    - 레스토랑 정보에 대한 간단한 테스트 작성

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -12,4 +12,21 @@ describe('App', () => {
 
     expect(title).not.toBeNull();
   });
+
+  it('renders 레스토랑 정보', () => {
+    const { getByText } = render(
+      (
+        <div>
+          <h1>Restaurants</h1>
+          <ol>
+            <li>마녀주방 | 한식 | 서울시 강남구</li>
+            <li>시카고피자 | 양식 | 이태원동</li>
+          </ol>
+        </div>
+      ),
+    );
+
+    expect(getByText('마녀주방 | 한식 | 서울시 강남구')).toHaveTextContent('마녀주방 | 한식 | 서울시 강남구');
+    expect(getByText('시카고피자 | 양식 | 이태원동')).toHaveTextContent('시카고피자 | 양식 | 이태원동');
+  });
 });
