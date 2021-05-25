31ba4701a7a1ff4643a0bc08ba5b95f3f9836ffb
최한울 <hanul.choi95@nts-corp.com>
2021-05-26 00:34:35 +0900

    App 컴포넌트 : 입력창 변경을 구현한다

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ export default function App() {
     console.log('click!!!');
   };
 
+  const handleChangeInputElement = () => {
+    console.log('change!!!');
+  };
+
   return (
     <div>
       <h1>Restaurants</h1>
@@ -12,9 +16,9 @@ export default function App() {
         <li>마녀주방 | 한식 | 서울시 강남구</li>
         <li>시카고피자 | 양식 | 이태원동</li>
       </ul>
-      <input placeholder="이름" />
-      <input placeholder="분류" />
-      <input placeholder="주소" />
+      <input placeholder="이름" onChange={handleChangeInputElement} />
+      <input placeholder="분류" onChange={handleChangeInputElement} />
+      <input placeholder="주소" onChange={handleChangeInputElement} />
       <button
         type="button"
         onClick={handleClickAddRestaurants}
