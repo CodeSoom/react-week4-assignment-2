a2a24b10cb7382fd490d758925595fcd906093fc
yujonglee <yujonglee@g.skku.edu>
2021-05-26 19:55:29 +0900

    DataInput테스트들을 describe블럭으로 묶어라

--- src/components/DataInput.test.jsx
+++ src/components/DataInput.test.jsx
@@ -6,26 +6,28 @@ import { addRestaurant } from '../redux/actions';
 
 jest.mock('react-redux');
 
-it('renders 등록 button', () => {
-  const restaurantData = {
-    id: 1,
-    name: '마녀주방',
-    category: '한식',
-    address: '서울시 강남구',
-  };
+describe('DataInput', () => {
+  it('renders 등록 button', () => {
+    const restaurantData = {
+      id: 1,
+      name: '마녀주방',
+      category: '한식',
+      address: '서울시 강남구',
+    };
 
-  const dispatch = jest.fn();
-  useDispatch.mockImplementation(() => dispatch);
-  useSelector.mockImplementation((selector) => selector(restaurantData));
+    const dispatch = jest.fn();
+    useDispatch.mockImplementation(() => dispatch);
+    useSelector.mockImplementation((selector) => selector(restaurantData));
 
-  const { getByRole } = render(<DataInput />);
-  expect(getByRole('button')).toBeInTheDocument();
+    const { getByRole } = render(<DataInput />);
+    expect(getByRole('button')).toBeInTheDocument();
 
-  fireEvent.click(getByRole('button', { name: '등록' }));
-  expect(dispatch).toBeCalledWith(addRestaurant(restaurantData));
-});
+    fireEvent.click(getByRole('button', { name: '등록' }));
+    expect(dispatch).toBeCalledWith(addRestaurant(restaurantData));
+  });
 
-it('renders three inputbox', () => {
-  const { getAllByRole } = render(<DataInput />);
-  expect(getAllByRole('textbox')).toHaveLength(3);
+  it('renders three inputbox', () => {
+    const { getAllByRole } = render(<DataInput />);
+    expect(getAllByRole('textbox')).toHaveLength(3);
+  });
 });
