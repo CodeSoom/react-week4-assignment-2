a982feb48879522de599947c0ef66baa973f5dfe
yujonglee <yujonglee@g.skku.edu>
2021-05-27 03:38:44 +0900

    useUpdateInfo 훅 테스트를 작성하라

---
+++ src/hooks/useUpdateInfo.test.js
@@ -0,0 +1,23 @@
+import { renderHook, act } from '@testing-library/react-hooks';
+import { useDispatch } from 'react-redux';
+
+import useUpdateInfo from './useUpdateInfo';
+import { updateField } from '../redux/actions';
+
+jest.mock('react-redux');
+
+it('returns updateInfo funtion', () => {
+  const dispatch = jest.fn();
+  useDispatch.mockImplementation(() => dispatch);
+
+  const { result } = renderHook(() => useUpdateInfo());
+
+  act(() => {
+    result.current.updateInfo('name', '마녀주방');
+  });
+
+  expect(dispatch).toBeCalledWith(updateField({
+    field: 'name',
+    value: '마녀주방',
+  }));
+});
