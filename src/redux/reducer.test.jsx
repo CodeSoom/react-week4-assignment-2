591498ec5c19a64b2358b9253f163926b0d21288
yujonglee <yujonglee@g.skku.edu>
2021-05-26 11:06:17 +0900

    잘못 복사된 테스트 설명을 수정하라

--- src/redux/reducer.test.jsx
+++ src/redux/reducer.test.jsx
@@ -1,7 +1,7 @@
 import reducer from './reducer';
 import { addRestaurant } from './actions';
 
-it('updates title of task', () => {
+it('adds new restaurant and updates id', () => {
   const newRestaurant = {
     id: 1, name: '마녀주방', category: '한식', address: '서울시 강남구',
   };
