853158e1e86a96c27052ecda34f9cbe16b88a91c
yujonglee <yujonglee@g.skku.edu>
2021-05-26 10:50:58 +0900

    DataInput의 등록 버튼 테스트에서 디스패치 호출을 테스트하게하라

--- src/components/DataInput.test.jsx
+++ src/components/DataInput.test.jsx
@@ -1,4 +1,4 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { fireEvent, render } from '@testing-library/react';
 import DataInput from './DataInput';
 import { addRestaurant } from '../redux/actions';
@@ -6,14 +6,22 @@ import { addRestaurant } from '../redux/actions';
 jest.mock('react-redux');
 
 it('renders 등록 button', () => {
+  const restaurantData = {
+    id: 1,
+    name: '마녀주방',
+    category: '한식',
+    address: '서울시 강남구',
+  };
+
   const dispatch = jest.fn();
   useDispatch.mockImplementation(() => dispatch);
+  useSelector.mockImplementation((selector) => selector(restaurantData));
 
   const { getByRole } = render(<DataInput />);
   expect(getByRole('button')).toBeInTheDocument();
 
   fireEvent.click(getByRole('button', { name: '등록' }));
-  expect(dispatch).toBeCalled();
+  expect(dispatch).toBeCalledWith(addRestaurant(restaurantData));
 });
 
 it('renders three inputbox', () => {
