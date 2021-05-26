a38968b38ce0602269135a4f00b18cb817b74b40
yujonglee <yujonglee@g.skku.edu>
2021-05-26 19:46:06 +0900

    잘못된 액션이 dispatch될 때에 대한 테스트를 추가하라

--- src/redux/reducer.test.jsx
+++ src/redux/reducer.test.jsx
@@ -2,33 +2,49 @@ import reducer from './reducer';
 import { addRestaurant, updateField } from './actions';
 
 describe('reducer', () => {
-  it('adds new restaurant and updates id', () => {
-    const newRestaurant = {
-      id: 1, name: '마녀주방', category: '한식', address: '서울시 강남구',
-    };
+  context('with invaild action', () => {
+    it('returns same state', () => {
+      const newState = reducer(undefined, { type: 'invaild' });
 
-    const newState = reducer(undefined, addRestaurant(newRestaurant));
-
-    expect(newState).toEqual({
-      id: 2,
-      name: '',
-      category: '',
-      address: '',
-      restaurants: [newRestaurant],
+      expect(newState).toEqual({
+        id: 1,
+        name: '',
+        category: '',
+        address: '',
+        restaurants: [],
+      });
     });
   });
 
-  it('updates field with input value', () => {
-    const newState = reducer(undefined, updateField(
-      { field: 'name', value: '마녀 주방' },
-    ));
+  context('with vaild action', () => {
+    it('adds new restaurant and updates id', () => {
+      const newRestaurant = {
+        id: 1, name: '마녀주방', category: '한식', address: '서울시 강남구',
+      };
 
-    expect(newState).toEqual({
-      id: 1,
-      name: '마녀 주방',
-      category: '',
-      address: '',
-      restaurants: [],
+      const newState = reducer(undefined, addRestaurant(newRestaurant));
+
+      expect(newState).toEqual({
+        id: 2,
+        name: '',
+        category: '',
+        address: '',
+        restaurants: [newRestaurant],
+      });
+    });
+
+    it('updates field with input value', () => {
+      const newState = reducer(undefined, updateField(
+        { field: 'name', value: '마녀 주방' },
+      ));
+
+      expect(newState).toEqual({
+        id: 1,
+        name: '마녀 주방',
+        category: '',
+        address: '',
+        restaurants: [],
+      });
     });
   });
 });
