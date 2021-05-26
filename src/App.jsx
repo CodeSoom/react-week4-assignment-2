ff98abc4259f20f0e49be20b7e82be0d301e0ef6
yujonglee <yujonglee@g.skku.edu>
2021-05-26 11:28:36 +0900

    App컴포넌트에서 수정된 List와 새로만든 DataInput를 사용하게하라

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,14 @@
-import { useSelector } from 'react-redux';
-
 import List from './components/List';
-import InputBox from './components/InputBox';
+import DataInput from './components/InputBox';
 
 export default function App() {
-  const { restaurants } = useSelector((state) => state);
-
   return (
     <>
       <h1>
         Restaurants
       </h1>
-      <List restaurants={restaurants} />
-
-      <InputBox name="이름" />
-      <InputBox name="분류" />
-      <InputBox name="주소" />
+      <List />
+      <DataInput />
     </>
   );
 }
