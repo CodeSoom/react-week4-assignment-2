93441191c61dcc8fac57aac6a525b4bd18c7684d
yujonglee <yujonglee@g.skku.edu>
2021-05-26 11:14:48 +0900

    updateField액션에 대한 리듀서 테스트를 추가하라

--- src/redux/reducer.test.jsx
+++ src/redux/reducer.test.jsx
@@ -1,18 +1,34 @@
 import reducer from './reducer';
-import { addRestaurant } from './actions';
+import { addRestaurant, updateField } from './actions';
 
-it('adds new restaurant and updates id', () => {
-  const newRestaurant = {
-    id: 1, name: '마녀주방', category: '한식', address: '서울시 강남구',
-  };
+describe('reducer', () => {
+  it('adds new restaurant and updates id', () => {
+    const newRestaurant = {
+      id: 1, name: '마녀주방', category: '한식', address: '서울시 강남구',
+    };
 
-  const newState = reducer(undefined, addRestaurant(newRestaurant));
+    const newState = reducer(undefined, addRestaurant(newRestaurant));
 
-  expect(newState).toEqual({
-    id: 2,
-    name: '',
-    category: '',
-    address: '',
-    restaurants: [newRestaurant],
+    expect(newState).toEqual({
+      id: 2,
+      name: '',
+      category: '',
+      address: '',
+      restaurants: [newRestaurant],
+    });
+  });
+
+  it('updates field with input value', () => {
+    const newState = reducer(undefined, updateField(
+      { field: 'name', value: '마녀 주방' },
+    ));
+
+    expect(newState).toEqual({
+      id: 1,
+      name: '마녀 주방',
+      category: '',
+      address: '',
+      restaurants: [],
+    });
   });
 });
