f1cc6a7b77f49680faf9eed671f42b94c650b4e6
yujonglee <yujonglee@g.skku.edu>
2021-05-26 10:36:08 +0900

    입력박스렌더링 테스트를 통과하도록 DataInput을 수정하라

--- src/components/DataInput.jsx
+++ src/components/DataInput.jsx
@@ -1,10 +1,17 @@
 import { useDispatch } from 'react-redux';
 
+import InputBox from './InputBox';
+
 export default function DataInput() {
   const dispatch = useDispatch();
   return (
-    <button type="button" onClick={() => dispatch({})}>
-      등록
-    </button>
+    <>
+      <InputBox name="이름" />
+      <InputBox name="분류" />
+      <InputBox name="주소" />
+      <button type="button" onClick={() => dispatch({})}>
+        등록
+      </button>
+    </>
   );
 }
