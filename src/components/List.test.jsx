9365f8ede98fc0e18b935ceb3e54bcc91cb4183a
yujonglee <yujonglee@g.skku.edu>
2021-05-26 11:25:01 +0900

    List컴포넌트가 props대신 redux에서 정보를 가져오는 것으로 테스트 인터페이스를 수정하라

--- src/components/List.test.jsx
+++ src/components/List.test.jsx
@@ -1,20 +1,27 @@
 import { render } from '@testing-library/react';
+import { useSelector } from 'react-redux';
 
 import List from './List';
 
-const data = [
-  {
-    id: 1, name: '마녀주방', category: '한식', address: '서울시 강남구',
-  },
-  // {
-  //   id: 2, name: '시카고피자', category: '양식', address: '이태원동',
-  // },
-  // {
-  //   id: 2, name: '키와미', category: '일식', address: '분당구 정자동',
-  // },
-];
+jest.mock('react-redux');
+const data = {
+  restaurants: [
+    {
+      id: 1, name: '마녀주방', category: '한식', address: '서울시 강남구',
+    },
+    {
+      id: 2, name: '시카고피자', category: '양식', address: '이태원동',
+    },
+    {
+      id: 3, name: '키와미', category: '일식', address: '분당구 정자동',
+    },
+  ],
+};
 
 it('renders restaurants', () => {
-  const { container } = render(<List restaurants={data} />);
+  useSelector.mockImplementation((selector) => selector(data));
+  const { container } = render(<List />);
   expect(container).toHaveTextContent('마녀주방 | 한식 | 서울시 강남구');
+  expect(container).toHaveTextContent('시카고피자 | 양식 | 이태원동');
+  expect(container).toHaveTextContent('키와미 | 일식 | 분당구 정자동');
 });
