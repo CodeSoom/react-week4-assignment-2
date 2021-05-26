3cf602f73f29914a174ae7bc782d8e3e79d1bd91
최한울 <hanul.choi95@nts-corp.com>
2021-05-27 00:53:39 +0900

    리듀서 작성 : initialState를 구현한다

---
+++ src/reducer.js
@@ -0,0 +1,31 @@
+const initialState = {
+  newId: 100,
+  restaurants: [
+    {
+      id: 1,
+      name: '뽀식이네 감자탕',
+      type: '한식',
+      address: '경기도 양평군',
+    },
+  ],
+};
+
+export default function reducer(state = initialState, action) {
+  if (action.type === 'addRestaurants') {
+    return state;
+  }
+
+  if (action.type === 'changeName') {
+    return state;
+  }
+
+  if (action.type === 'changeType') {
+    return state;
+  }
+
+  if (action.type === 'changeAddress') {
+    return state;
+  }
+
+  return state;
+}
--- src/store.js
+++ src/store.js
@@ -1,37 +1,7 @@
 import { createStore } from 'redux';
 
-const initialState = {
-  newId: 100,
-  restaurants: [
-    {
-      id: 1,
-      name: '뽀식이네 감자탕',
-      type: '한식',
-      address: '경기도 양평군',
-    },
-  ],
-};
+import reducer from './reducer';
 
-function reducer(state = initialState, action) {
-  if (action.type === 'addRestaurants') {
-    return state;
-  }
-
-  if (action.type === 'changeName') {
-    return state;
-  }
-
-  if (action.type === 'changeType') {
-    return state;
-  }
-
-  if (action.type === 'changeAddress') {
-    return state;
-  }
-
-  return state;
-}
-
-const store = createStore(reducer, initialState);
+const store = createStore(reducer);
 
 export default store;
