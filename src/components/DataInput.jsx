0f4b5c5a533bdd4524c0958fcd78607bdeda49ab
yujonglee <yujonglee@g.skku.edu>
2021-05-26 10:51:18 +0900

    DataInput를 디스패치 테스트가 통과하도록 수정하라

--- src/components/DataInput.jsx
+++ src/components/DataInput.jsx
@@ -1,15 +1,27 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import InputBox from './InputBox';
+import { addRestaurant } from '../redux/actions';
 
 export default function DataInput() {
   const dispatch = useDispatch();
+  const {
+    id, name, category, address,
+  } = useSelector((state) => state);
+
   return (
     <>
       <InputBox name="이름" />
       <InputBox name="분류" />
       <InputBox name="주소" />
-      <button type="button" onClick={() => dispatch({})}>
+      <button
+        type="button"
+        onClick={() => dispatch(
+          addRestaurant({
+            id, name, category, address,
+          }),
+        )}
+      >
         등록
       </button>
     </>
