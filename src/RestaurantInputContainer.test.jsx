aa62e37c7781adfb51c3e2328af30882b24c86ce
최한울 <hanul.choi95@nts-corp.com>
2021-05-27 02:01:34 +0900

    input 엘리먼트의 value 속성값에 대한 로직을 구현한다

--- src/RestaurantInputContainer.test.jsx
+++ src/RestaurantInputContainer.test.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { render, fireEvent } from '@testing-library/react';
 
-import { useDispatch } from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 
 import RestaurantInputContainer from './RestaurantInputContainer';
 
@@ -12,6 +12,23 @@ describe('RestaurantInputContainer', () => {
   const dispatch = jest.fn();
 
   useDispatch.mockImplementation(() => dispatch);
+
+  useSelector.mockImplementation((selector) => selector({
+    restaurants: [
+      {
+        id: 1,
+        name: '마녀주방',
+        type: '한식',
+        address: '서울시 강남구',
+      },
+      {
+        id: 2,
+        name: '시카고피자',
+        type: '양식',
+        address: '이태원역',
+      },
+    ],
+  }));
 
   it('renders 입력창과 등록 버튼', () => {
     const { getByText, getByPlaceholderText } = render(<RestaurantInputContainer />);
