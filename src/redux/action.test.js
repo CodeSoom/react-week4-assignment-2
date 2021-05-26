4058a8fed1fb0ab2868aa2d11313919e580cf686
yujonglee <yujonglee@g.skku.edu>
2021-05-26 10:56:21 +0900

    field를 업데이트하는 액션생성자 함수 테스트를 추가하라

--- src/redux/action.test.js
+++ src/redux/action.test.js
@@ -1,17 +1,29 @@
-import { addRestaurant } from './actions';
+import { addRestaurant, updateField } from './actions';
 
-test('addRestaurant action creator', () => {
-  const newRestaurant = {
-    id: 2, name: '시카고피자', category: '양식', address: '이태원동',
-  };
+describe('actions', () => {
+  it('creates action for addRestaurant', () => {
+    const newRestaurant = {
+      id: 2, name: '시카고피자', category: '양식', address: '이태원동',
+    };
 
-  expect(addRestaurant(newRestaurant)).toEqual({
-    type: 'AddRestaurant',
-    payload: {
-      id: 2,
-      name: '시카고피자',
-      category: '양식',
-      address: '이태원동',
-    },
+    expect(addRestaurant(newRestaurant)).toEqual({
+      type: 'AddRestaurant',
+      payload: {
+        id: 2,
+        name: '시카고피자',
+        category: '양식',
+        address: '이태원동',
+      },
+    });
+  });
+
+  it('creates action for updateField', () => {
+    expect(updateField({ field: 'name', value: '마녀 주방' })).toEqual({
+      type: 'restaurant/updateField',
+      payload: {
+        field: 'name',
+        value: '마녀 주방',
+      },
+    });
   });
 });
