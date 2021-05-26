8ce44df1dc9a1ec9c677caeac2f8a9ed4537c578
yujonglee <yujonglee@g.skku.edu>
2021-05-26 10:50:07 +0900

    테스트가 통과하도록 리듀서를 수정하라

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,9 +1,17 @@
 const initialState = {
+  id: 1,
+  name: '',
+  category: '',
+  address: '',
   restaurants: [],
 };
 
 export default function reducer(state = initialState, action) {
   return {
+    id: state.id + 1,
+    name: '',
+    category: '',
+    address: '',
     restaurants: [...state.restaurants, action.payload],
   };
 }
