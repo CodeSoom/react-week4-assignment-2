2e796bf5f4fbdbe4a53f638d2fcb58d7ca542c5c
seong-dodo <dyrnxm52@hanmail.net>
2021-05-27 10:53:05 +0900

    App 컴포넌트 기능 구현2
    - App 컴포넌트 화면 관련 기능구현2

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ export default function App() {
   return (
     <div>
       <h1>Restaurants</h1>
+      <input type="text" placeholder="이름" />
+      <input type="text" placeholder="분류" />
+      <input type="text" placeholder="주소" />
+      <button type="button">등록</button>
     </div>
   );
 }
