2b674bac32597a2ac2c463b0138fdd3b2716bf93
seong-dodo <dyrnxm52@hanmail.net>
2021-05-27 10:45:12 +0900

    App 컴포넌트 테스트 추가2
    - App 컴포넌트 화면 관련 테스트 추가2

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -6,8 +6,12 @@ describe('App', () => {
   it('App 화면', () => {
     // given
     // when
-    const { getByText } = render(<App />);
+    const { getByText, getByPlaceholderText } = render(<App />);
     // then
     expect(getByText(/Restaurants/)).not.toBeNull();
+    expect(getByPlaceholderText(/이름/)).not.toBeNull();
+    expect(getByPlaceholderText(/분류/)).not.toBeNull();
+    expect(getByPlaceholderText(/주소/)).not.toBeNull();
+    expect(getByText(/등록/)).not.toBeNull();
   });
 });
