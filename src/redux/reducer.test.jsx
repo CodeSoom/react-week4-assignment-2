ce4d64a73471d228d36488e216f027f20074558f
yujonglee <yujonglee@g.skku.edu>
2021-05-27 10:07:40 +0900

    let-beforeEach대신 각 테스트에 상태변수를 도입하라

--- src/redux/reducer.test.jsx
+++ src/redux/reducer.test.jsx
@@ -2,34 +2,32 @@ import reducer, { initialState } from './reducer';
 import { addRestaurant, updateField } from './actions';
 
 describe('reducer', () => {
-  let oldState;
-
-  beforeEach(() => {
-    oldState = {
-      id: 1,
-      name: '',
-      category: '',
-      address: '',
-      restaurants: [],
-    };
-  });
-
   context('with invaild action', () => {
     it('returns same state', () => {
-      const newState = reducer(oldState, { type: 'invaild' });
-
-      expect(newState).toEqual({
+      const oldState = {
         id: 1,
         name: '',
         category: '',
         address: '',
         restaurants: [],
-      });
+      };
+
+      const newState = reducer(oldState, { type: 'invaild' });
+
+      expect(newState).toEqual(oldState);
     });
   });
 
   context('with vaild action', () => {
     it('adds new restaurant and updates id', () => {
+      const oldState = {
+        id: 1,
+        name: '',
+        category: '',
+        address: '',
+        restaurants: [],
+      };
+
       const newRestaurant = {
         id: 1, name: '마녀주방', category: '한식', address: '서울시 강남구',
       };
@@ -46,6 +44,14 @@ describe('reducer', () => {
     });
 
     it('updates field with input value', () => {
+      const oldState = {
+        id: 1,
+        name: '',
+        category: '',
+        address: '',
+        restaurants: [],
+      };
+
       const newState = reducer(oldState, updateField(
         { field: 'name', value: '마녀 주방' },
       ));
