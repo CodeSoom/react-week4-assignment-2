b8c2201fa46d9f76522893fcdf98ade76b9fbb83
dngwoodo <dngwoodo@gmail.com>
2021-05-28 21:32:27 +0900

    fix: Fix Input props
    오타 수정, onChange에 인자 전달

--- src/presentational/Input.jsx
+++ src/presentational/Input.jsx
@@ -1,12 +1,36 @@
 import React from 'react';
 
-export default function Input() {
+import {
+  updateRestaurantAddress,
+  updateRestaurantCategory,
+  updateRestaurantName,
+} from '../actions/actions';
+
+export default function Input({
+  name, category, address, onChange,
+  onClickAddRestaurant,
+}) {
   return (
     <>
-      <input type="text" placeholder="이름" />
-      <input type="text" placeholder="분류" />
-      <input type="text" placeholder="주소" />
-      <button type="button">등록</button>
+      <input
+        type="text"
+        placeholder="이름"
+        onChange={(event) => onChange(updateRestaurantName(event.target.value))}
+        value={name}
+      />
+      <input
+        type="text"
+        placeholder="분류"
+        onChange={(event) => onChange(updateRestaurantCategory(event.target.value))}
+        value={category}
+      />
+      <input
+        type="text"
+        placeholder="주소"
+        onChange={(event) => onChange(updateRestaurantAddress(event.target.value))}
+        value={address}
+      />
+      <button type="button" onClick={onClickAddRestaurant}>등록</button>
     </>
   );
 }
