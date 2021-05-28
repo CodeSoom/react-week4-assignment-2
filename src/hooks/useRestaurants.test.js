be199b93bca14e76f32df24bdbb0cfa2868ee5a3
yujonglee <yujonglee@g.skku.edu>
2021-05-28 11:09:20 +0900

    state변수를 beforeEach바깥으로 빼라

--- src/hooks/useRestaurants.test.js
+++ src/hooks/useRestaurants.test.js
@@ -7,19 +7,18 @@ import { addNewRestaurant } from '../redux/actions';
 jest.mock('react-redux');
 
 describe('useRestaurants', () => {
+  const state = {
+    newRestaurant: {
+      id: 1,
+      name: '마녀주방',
+      category: '한식',
+      address: '서울시 강남구',
+    },
+    restaurants: [],
+  };
+
   const dispatch = jest.fn();
-
   beforeEach(() => {
-    const state = {
-      newRestaurant: {
-        id: 1,
-        name: '마녀주방',
-        category: '한식',
-        address: '서울시 강남구',
-      },
-      restaurants: [],
-    };
-
     dispatch.mockClear();
 
     useDispatch.mockImplementation(() => dispatch);
