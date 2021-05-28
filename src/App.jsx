501be4957155fc29cd2b999cb9576df2e3abcb7d
hanvit7 <hanvit7@gmail.com>
2021-05-28 13:53:42 +0900

    리팩토링 - App list 부분 리팩토링
    - restaurant 데이터 리팩토링
    - list 데이터 테스트 리팩토링

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
 export default function App() {
+  const restaurants = [
+    {
+      id: 1, name: '마녀주방', category: '한식', location: '서울시 강남구',
+    },
+    {
+      id: 2, name: '시카고피자', category: '양식', location: '이태원동',
+    },
+  ];
+
   return (
     <div>
       <h2>Restaurants</h2>
-      <ol>
-        <li>마녀주방 | 한식 | 서울시 강남구</li>
-        <li>시카고피자| 양식 | 이태원동</li>
-      </ol>
+      <ul>
+        {restaurants.map((item) => (
+          <li key={item.id}>
+            {item.name}
+            {' '}
+            |
+            {' '}
+            {item.category}
+            {' '}
+            |
+            {' '}
+            {item.location}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -9,11 +9,29 @@ describe('App', () => {
     expect(getByText(/Restaurants/)).not.toBeNull();
   });
 
-  it('has a list', () => {
-    const { getAllByRole, getByText } = render(<App />);
+  describe('List', () => {
+    it('has a list', () => {
+      const { getAllByRole } = render(<App />);
 
-    expect(getAllByRole('listitem')).toHaveLength(2);
-    expect(getByText(/마녀주방 | 한식 | 서울시 강남구/)).not.toBeNull();
-    expect(getByText(/시카고피자| 양식 | 이태원동/)).not.toBeNull();
+      expect(getAllByRole('listitem')).toHaveLength(2);
+    });
+
+    it('has a restaurant information', () => {
+      const restaurants = [
+        { name: '마녀주방', category: '한식', location: '서울시 강남구' },
+        { name: '시카고피자', category: '양식', location: '이태원동' },
+      ];
+
+      const { getAllByRole } = render(<App />);
+
+      const listitem = getAllByRole('listitem');
+
+      expect(listitem[0]).toHaveTextContent(
+        `${restaurants[0].name} | ${restaurants[0].category} | ${restaurants[0].location}`,
+      );
+      expect(listitem[1]).toHaveTextContent(
+        `${restaurants[1].name} | ${restaurants[1].category} | ${restaurants[1].location}`,
+      );
+    });
   });
 });
