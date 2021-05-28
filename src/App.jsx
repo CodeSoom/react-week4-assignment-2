95dcdd3897b19510ad8ce59fb0d0f5bb1ab06f20
dngwoodo <dngwoodo@gmail.com>
2021-05-28 19:42:12 +0900

    feat: Setup initial restaurants
    TDD하기전에 useEffect를 구현 해주었는데 맞는진 모르겠음

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import { useDispatch } from 'react-redux';
+import { setRestaurants } from './actions/actions';
 
 import Input from './presentational/Input';
 import RestaurantsContainer from './container/RestaurantsContainer';
 
 export default function App() {
+  const dispatch = useDispatch();
+
+  const restaurants = [
+    {
+      id: 1,
+      name: '마녀주방',
+      category: '한식',
+      address: '서울시 강남구',
+    },
+    {
+      id: 2,
+      name: '시카고피자',
+      category: '양식',
+      address: '이태원동',
+    },
+  ];
+
+  useEffect(() => {
+    dispatch(setRestaurants(restaurants));
+  }, []);
+
   return (
     <div>
       <h1>Restaurants</h1>
