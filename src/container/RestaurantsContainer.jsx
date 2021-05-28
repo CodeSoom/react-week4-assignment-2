0afc76ef077b9e3ae9b9e3f660310dd060d2e2f2
dngwoodo <dngwoodo@gmail.com>
2021-05-28 19:46:16 +0900

    refactor: Get data from redux

--- src/container/RestaurantsContainer.jsx
+++ src/container/RestaurantsContainer.jsx
@@ -1,21 +1,11 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Restaurants from '../presentational/Restaurants';
 
 export default function RestaurantsContainer() {
-  const restaurants = [
-    {
-      id: 1,
-      name: '마녀주방',
-      category: '한식',
-      address: '서울시 강남구',
-    },
-    {
-      id: 2,
-      name: '시카고피자',
-      category: '양식',
-      address: '이태원동',
-    },
-  ];
+  const { restaurants } = useSelector((state) => ({
+    restaurants: state.restaurants,
+  }));
 
   return <Restaurants restaurants={restaurants} />;
 }
