0e14befb05164119a68492fa5b76b26bc642b88d
dngwoodo <dngwoodo@gmail.com>
2021-05-28 17:15:10 +0900

    refactor: Add props for Restaurants

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,25 @@ import Input from './presentational/Input';
 import Restaurants from './presentational/Restaurants';
 
 export default function App() {
+  const restaurants = [
+    {
+      id: 1,
+      name: '마녀주방',
+      category: '한식',
+      address: '서울시 강남구',
+    },
+    {
+      id: 2,
+      name: '시카고피자',
+      category: '양식',
+      address: '이태원동',
+    },
+  ];
+
   return (
     <div>
       <h1>Restaurants</h1>
-      <Restaurants />
+      <Restaurants restaurants={restaurants} />
       <Input />
     </div>
   );
--- src/presentational/Restaurants.jsx
+++ src/presentational/Restaurants.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 
-export default function Restaurants() {
+export default function Restaurants({ restaurants }) {
   return (
     <ul>
-      <li>마녀주방 | 한식 | 서울시 강남구 </li>
-      <li>시카고피자 | 양식 | 이태원동</li>
+      {restaurants.map((restaurant) => (
+        <li key={restaurant.id}>
+          {restaurant.name}
+          {' '}
+          |
+          {' '}
+          {restaurant.category}
+          {' '}
+          |
+          {' '}
+          {restaurant.address}
+        </li>
+      ))}
     </ul>
   );
 }
