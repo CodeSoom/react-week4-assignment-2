17c85c24e628c42766169fa551e12fddad37021f
hanvit7 <hanvit7@gmail.com>
2021-05-28 12:37:14 +0900

    테스트 - App에 레스토랑 리스트 테스트 추가
    - App / has a list : 2개의 레스토랑 리스트 테스트 추가

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -8,4 +8,12 @@ describe('App', () => {
 
     expect(getByText(/Restaurants/)).not.toBeNull();
   });
+
+  it('has a list', () => {
+    const { getAllByRole, getByText } = render(<App />);
+
+    expect(getAllByRole('listitem')).toHaveLength(2);
+    expect(getByText(/마녀주방 | 한식 | 서울시 강남구/)).not.toBeNull();
+    expect(getByText(/시카고피자| 양식 | 이태원동/)).not.toBeNull();
+  });
 });
