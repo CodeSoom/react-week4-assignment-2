a2c78e81e60fb0db84338121ef32b7416904fb5c
dngwoodo <dngwoodo@gmail.com>
2021-05-28 16:44:03 +0900

    green: Add Restaurants component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 
 import Input from './presentational/Input';
+import Restaurants from './presentational/Restaurants';
 
 export default function App() {
   return (
     <div>
       <h1>Restaurants</h1>
-      <ul>
-        <li>마녀주방 | 한식 | 서울시 강남구 </li>
-        <li>시카고피자 | 양식 | 이태원동</li>
-      </ul>
+      <Restaurants />
       <Input />
     </div>
   );
