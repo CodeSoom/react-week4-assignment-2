f7538e7a5423d896170f6d9eceb6cba5e62a31be
dngwoodo <dngwoodo@gmail.com>
2021-05-28 20:17:03 +0900

    env: Setup actions

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -7,4 +7,29 @@ export function setRestaurants(restaurants) {
   };
 }
 
-export default {};
+export function addRestaurant() {
+  return {
+    type: 'restaurants/addRestaurant',
+  };
+}
+
+export function updateRestaurantName(name) {
+  return {
+    type: 'restaurant/updateRestaurantName',
+    payload: { name },
+  };
+}
+
+export function updateRestaurantCategory(category) {
+  return {
+    type: 'restaurant/updateRestaurantCategory',
+    payload: { category },
+  };
+}
+
+export function updateRestaurantAddress(address) {
+  return {
+    type: 'restaurant/updateRestaurantAddress',
+    payload: { address },
+  };
+}
