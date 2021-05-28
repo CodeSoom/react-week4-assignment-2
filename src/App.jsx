19cf3afe766b13d144a06d42c11e5e38425040b6
dngwoodo <dngwoodo@gmail.com>
2021-05-28 16:29:53 +0900

    green: Add App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,15 @@ import React from 'react';
 export default function App() {
   return (
     <div>
-      Hello
+      <h1>Restaurants</h1>
+      <ul>
+        <li>마녀주방 | 한식 | 서울시 강남구 </li>
+        <li>시카고피자 | 양식 | 이태원동</li>
+      </ul>
+      <input type="text" placeholder="이름" />
+      <input type="text" placeholder="분류" />
+      <input type="text" placeholder="주소" />
+      <button type="button">등록</button>
     </div>
   );
 }
