14266be270a19a7bef205f1b2df28e43b52ce8ce
hanvit7 <hanvit7@gmail.com>
2021-05-28 12:38:27 +0900

    구현 - 테스트 - App에 레스토랑 리스트 구현

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ export default function App() {
   return (
     <div>
       <h2>Restaurants</h2>
+      <ol>
+        <li>마녀주방 | 한식 | 서울시 강남구</li>
+        <li>시카고피자| 양식 | 이태원동</li>
+      </ol>
     </div>
   );
 }
