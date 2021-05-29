cdea019d4e4f57013e8a502bfc8e66eee48ca3e1
seong-dodo <dyrnxm52@hanmail.net>
2021-05-30 03:36:56 +0900

    InputContainer 컴포넌트 분리
    - InputContainer 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,15 @@
 import React from 'react';
 
-import { useDispatch } from 'react-redux';
-
 import ListContainer from './ListContainer';
 
 import InputContainer from './InputContainer';
 
-import {
-  addRestaurantInformation,
-} from './actions';
-
 export default function App() {
-  const dispatch = useDispatch();
-
-  function handleClickAddButton() {
-    dispatch(addRestaurantInformation());
-  }
-
   return (
     <div>
       <h1>Restaurants</h1>
       <ListContainer />
       <InputContainer />
-      <button type="button" onClick={handleClickAddButton}>등록</button>
     </div>
   );
 }
--- src/InputContainer.jsx
+++ src/InputContainer.jsx
@@ -6,6 +6,7 @@ import {
   updateName,
   updateCategory,
   updateAddress,
+  addRestaurantInformation,
 } from './actions';
 
 export default function InputContainer() {
@@ -22,11 +23,16 @@ export default function InputContainer() {
     dispatch(updateAddress(event.target.value));
   }
 
+  function handleClickAddButton() {
+    dispatch(addRestaurantInformation());
+  }
+
   return (
     <div>
       <input type="text" placeholder="이름" onChange={handleChangeName} />
       <input type="text" placeholder="분류" onChange={handleChangeCategory} />
       <input type="text" placeholder="주소" onChange={handleChangeAddress} />
+      <button type="button" onClick={handleClickAddButton}>등록</button>
     </div>
   );
 }
