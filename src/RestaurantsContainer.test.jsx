7fc97bee27750ba201c4eee331ffffc65ebd1ec7
hxunz <guswl2843@gmail.com>
2021-05-30 00:58:16 +0900

    RestaurantsContainer test 작성

--- src/RestaurantsContainer.test.jsx
+++ src/RestaurantsContainer.test.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 
+import {useSelector} from 'react-redux';
+
 import {render}from '@trsting-library/react';
 
 import RestaurantsContainer from './RestaurantsContainer';
 
 test('RestaurantsContainer', () => {
+  useSelector.mockImplememtation((selector) => selector({
+    restaurants: [
+      {
+        id: 1, name: '김밥제국', category: '분식', address: '서울시 강남구 역삼동',
+      },
+    ],
+  }));
+
   const {getByText} = render((
     <RestaurantsContainer/>
   ));
