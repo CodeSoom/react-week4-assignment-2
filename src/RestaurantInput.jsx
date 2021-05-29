2f6348aa8f03242ab89214da830de85154a2b273
최한울 <hanul.choi95@nts-corp.com>
2021-05-30 00:54:23 +0900

    인수 테스트 통과
    - 테스트를 통과하기 위해서 input 엘리먼트의 name attribute 추가

--- src/RestaurantInput.jsx
+++ src/RestaurantInput.jsx
@@ -7,16 +7,19 @@ export default function RestaurantInput({
   return (
     <div>
       <input
+        name="name"
         value={name}
         placeholder="이름"
         onChange={(event) => onChangeName(event.target.value)}
       />
       <input
+        name="category"
         value={type}
         placeholder="분류"
         onChange={(event) => onChangeType(event.target.value)}
       />
       <input
+        name="address"
         value={address}
         placeholder="주소"
         onChange={(event) => onChangeAddress(event.target.value)}
