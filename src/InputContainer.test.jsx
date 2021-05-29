5f4846b62242d9e917e099583fd6b73451cfdd2c
seong-dodo <dyrnxm52@hanmail.net>
2021-05-30 03:33:16 +0900

    InputContainer 테스트 코드 추가
    - InputContainer 테스트 코드 추가: AddressaddRestaurantInformation

--- src/InputContainer.test.jsx
+++ src/InputContainer.test.jsx
@@ -8,6 +8,7 @@ import {
   updateAddress,
   updateCategory,
   updateName,
+  addRestaurantInformation,
 } from './actions';
 
 jest.mock('react-redux');
@@ -68,5 +69,24 @@ describe('InputContainer 관련 테스트', () => {
     );
 
     expect(dispatch).toBeCalledWith(updateAddress());
+  });
+
+  it('handleClickAddButton이 호출되는지 확인하는 테스트입니다', () => {
+    const dispatch = jest.fn();
+
+    useDispatch.mockImplementation(() => dispatch);
+
+    useSelector.mockImplementation((selector) => selector({
+      name: '시카고피자',
+      category: '양식',
+      address: '강남구',
+      restaurants: [],
+    }));
+
+    const { getByText } = render(<InputContainer />);
+
+    fireEvent.click(getByText(/등록/));
+
+    expect(dispatch).toBeCalledWith(addRestaurantInformation());
   });
 });
