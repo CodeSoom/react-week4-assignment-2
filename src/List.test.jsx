bf97846865758eac9f9a813610ecda0c2b4cf753
seong-dodo <dyrnxm52@hanmail.net>
2021-05-30 03:12:23 +0900

    InputContainer 테스트 코드 추가
    - InputContainer 테스트 코드 추가: handleChangeName

---
+++ src/List.test.jsx
@@ -0,0 +1,39 @@
+import { render } from '@testing-library/react';
+
+import List from './List';
+
+describe('List', () => {
+  function renderList(restaurants) {
+    return render(<List restaurants={restaurants} />);
+  }
+
+  context('with restaurants', () => {
+    const restaurants = [
+      {
+        id: 1,
+        name: '마녀주방',
+        category: '한식',
+        address: '서울시 강남구',
+      },
+
+    ];
+
+    it('renders restaurants', () => {
+      const { getByText } = renderList(restaurants);
+
+      expect(getByText(/마녀주방/)).not.toBeNull();
+      expect(getByText(/한식/)).not.toBeNull();
+      expect(getByText(/서울시 강남구/)).not.toBeNull();
+    });
+  });
+
+  context('without restaurants', () => {
+    it('no change', () => {
+      const restaurants = [];
+
+      const restaurant = renderList(restaurants);
+
+      expect(restaurant.restaurants).toHaveLength(0);
+    });
+  });
+});
--- src/ListContainer.jsx
+++ src/ListContainer.jsx
@@ -2,22 +2,16 @@ import React from 'react';
 
 import { useSelector } from 'react-redux';
 
+import List from './List';
+
 export default function ListContainer() {
   const { restaurants } = useSelector((restaurant) => ({
     restaurants: restaurant.restaurants,
   }));
 
   return (
-    <div>
-      <ul>
-        {
-          restaurants.map((restaurant) => (
-            <li key={restaurant.id}>
-              {`${restaurant.name} | ${restaurant.category} | ${restaurant.address}`}
-            </li>
-          ))
-        }
-      </ul>
-    </div>
+    <List
+      restaurants={restaurants}
+    />
   );
 }
