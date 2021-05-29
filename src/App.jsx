ff0a4315659fe6ed605fe661b5dad8da60809a84
seong-dodo <dyrnxm52@hanmail.net>
2021-05-30 03:31:05 +0900

    InputContainer 컴포넌트 분리
    - InputContainer 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -7,26 +7,11 @@ import ListContainer from './ListContainer';
 import InputContainer from './InputContainer';
 
 import {
-  updateName,
-  updateCategory,
-  updateAddress,
   addRestaurantInformation,
 } from './actions';
 
 export default function App() {
   const dispatch = useDispatch();
-
-  function handleChangeName(event) {
-    dispatch(updateName(event.target.value));
-  }
-
-  function handleChangeCategory(event) {
-    dispatch(updateCategory(event.target.value));
-  }
-
-  function handleChangeAddress(event) {
-    dispatch(updateAddress(event.target.value));
-  }
 
   function handleClickAddButton() {
     dispatch(addRestaurantInformation());
@@ -37,8 +22,6 @@ export default function App() {
       <h1>Restaurants</h1>
       <ListContainer />
       <InputContainer />
-      <input type="text" placeholder="분류" onChange={handleChangeCategory} />
-      <input type="text" placeholder="주소" onChange={handleChangeAddress} />
       <button type="button" onClick={handleClickAddButton}>등록</button>
     </div>
   );
--- src/InputContainer.jsx
+++ src/InputContainer.jsx
@@ -4,6 +4,8 @@ import { useDispatch } from 'react-redux';
 
 import {
   updateName,
+  updateCategory,
+  updateAddress,
 } from './actions';
 
 export default function InputContainer() {
@@ -12,10 +14,19 @@ export default function InputContainer() {
   function handleChangeName(event) {
     dispatch(updateName(event.target.value));
   }
+  function handleChangeCategory(event) {
+    dispatch(updateCategory(event.target.value));
+  }
+
+  function handleChangeAddress(event) {
+    dispatch(updateAddress(event.target.value));
+  }
 
   return (
     <div>
       <input type="text" placeholder="이름" onChange={handleChangeName} />
+      <input type="text" placeholder="분류" onChange={handleChangeCategory} />
+      <input type="text" placeholder="주소" onChange={handleChangeAddress} />
     </div>
   );
 }
