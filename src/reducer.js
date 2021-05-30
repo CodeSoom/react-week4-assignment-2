37a03e46e8e38c452067779d73b3a72868da6d4a
hanvit7 <hanvit7@gmail.com>
2021-05-30 16:16:37 +0900

    구현 - restaurants 초기화를 위한 setRestaurants 구현
    - reducer, action에 setRestaurants 구현

--- src/reducer.js
+++ src/reducer.js
@@ -1,7 +1,17 @@
-import restaurants from '../fixtures/restaurants';
+// import restaurants from '../fixtures/restaurants';
 
-const initailState = restaurants;
+const initailState = {
+  restaurants: [],
+};
 
 export default function reducer(state = initailState, action) {
+  if (action === 'setRestaurants') {
+    const { restaurants } = state;
+
+    return {
+      ...state,
+      restaurants,
+    };
+  }
   return state;
 }
