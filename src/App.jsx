c45eea5db91c156f87ca2e7dff93f10ee4bf6920
hanvit7 <hanvit7@gmail.com>
2021-05-30 16:15:40 +0900

    테스트 - App에서 restaurants 초기화 부분 추가
    - useEffect로 setRestaurants로 restaurants 초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
+import { useEffect } from 'react';
+
+import { useDispatch } from 'react-redux';
+
+import { setRestaurants } from './actions';
+
 import RestaurantsContainer from './RestaurantsContainer';
 
+import restaurants from '../fixtures/restaurants';
+
 export default function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setRestaurants(restaurants));
+  }, []);
+
   return (
     <div>
       <h2>Restaurants</h2>
