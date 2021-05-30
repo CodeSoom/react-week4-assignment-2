4eae9ea71e8fded47ef3b0fe69c36c421be471ab
hanvit7 <hanvit7@gmail.com>
2021-05-30 23:43:32 +0900

    테스트 - RestaurantsCreateContainer 테스트 추가
    - 버튼 클릭 시 addRestaurant 호출 테스트 추가

--- src/RestaurantsCreateContainer.test.jsx
+++ src/RestaurantsCreateContainer.test.jsx
@@ -1,16 +1,27 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import RestaurantsCreateContainer from './RestaurantsCreateContainer';
 
 jest.mock('react-redux');
 
 test('RestaurantsCreateContainer', () => {
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
+
   useSelector.mockImplementation((selector) => selector({
+    restaurants: [],
   }));
 
-  const { container } = render(<RestaurantsCreateContainer />);
+  const { container, getByText } = render(<RestaurantsCreateContainer />);
 
   expect(container).toHaveTextContent('등록');
+
+  fireEvent.click(getByText('등록'));
+
+  expect(dispatch).toBeCalledWith({
+    type: 'addRestaurant',
+  });
 });
