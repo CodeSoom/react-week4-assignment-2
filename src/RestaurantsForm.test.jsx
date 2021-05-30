1506354963e3010a8e4a7c9318a77c5dd1baa7db
hanvit7 <hanvit7@gmail.com>
2021-05-31 00:21:04 +0900

    테스트 - RestaurantsForm input 테스트 추가

--- src/RestaurantsForm.test.jsx
+++ src/RestaurantsForm.test.jsx
@@ -5,11 +5,20 @@ import RestaurantsForm from './RestaurantsForm';
 test('RestaurantsForm', () => {
   const handleClick = jest.fn();
 
-  const { getByText } = render(
+  const { getByDisplayValue, getByText } = render(
     <RestaurantsForm
+      restaurant={{
+        name: '마녀',
+        category: '한식',
+        location: '서울시',
+      }}
       onClick={handleClick}
     />,
   );
+
+  expect(getByDisplayValue('마녀')).not.toBeNull();
+  expect(getByDisplayValue('한식')).not.toBeNull();
+  expect(getByDisplayValue('서울시')).not.toBeNull();
 
   fireEvent.click(getByText('등록'));
 
