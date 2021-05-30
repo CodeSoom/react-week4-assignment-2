9a21e089b50042727cb33e74008946cead2e6f2d
hanvit7 <hanvit7@gmail.com>
2021-05-30 22:20:28 +0900

    테스트 - reducer setRestaurants 테스트 추가

---
+++ src/reducer.test.jsx
@@ -0,0 +1,19 @@
+import reducer from './reducer';
+
+import { setRestaurants } from './actions';
+
+import restaurants from '../fixtures/restaurants';
+
+describe('reducer', () => {
+  describe('setRestaurants', () => {
+    it('changes restaurants array', () => {
+      const initialState = {
+        restaurants: [],
+      };
+
+      const state = reducer(initialState, setRestaurants(restaurants));
+
+      expect(state.restaurants).not.toHaveLength(0);
+    });
+  });
+});
