8caed0c98a21ea3b6fbcd913f7bca65eedb9c62b
hanvit7 <hanvit7@gmail.com>
2021-05-31 15:05:31 +0900

    리팩토링 - App에 임시로 넣은 초깃값 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,11 @@ import RestaurantsContainer from './RestaurantsContainer';
 
 import RestaurantsCreateContainer from './RestaurantsCreateContainer';
 
-import restaurants from '../fixtures/restaurants';
-
 export default function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(setRestaurants(restaurants));
+    dispatch(setRestaurants([]));
   }, []);
 
   return (
