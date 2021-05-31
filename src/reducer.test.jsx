d33abade8a34444ce20813676ad869d4f84d56a1
hanvit7 <hanvit7@gmail.com>
2021-05-31 14:07:45 +0900

    테스트 - changes restaurant form 테스트 추가 in reducer

--- src/reducer.test.jsx
+++ src/reducer.test.jsx
@@ -1,6 +1,9 @@
 import reducer from './reducer';
 
-import { setRestaurants } from './actions';
+import {
+  setRestaurants,
+  changeRestaurantField,
+} from './actions';
 
 import restaurants from '../fixtures/restaurants';
 
@@ -9,11 +12,32 @@ describe('reducer', () => {
     it('changes restaurants array', () => {
       const initialState = {
         restaurants: [],
+        restaurant: {},
       };
 
       const state = reducer(initialState, setRestaurants(restaurants));
 
       expect(state.restaurants).not.toHaveLength(0);
+    });
+  });
+
+  describe('changeRestaurantField', () => {
+    it('changes restaurant form', () => {
+      const initialState = {
+        restaurants,
+        restaurant: {
+          name: '이름',
+          category: '분류',
+          address: '주소',
+        },
+      };
+
+      const state = reducer(initialState, changeRestaurantField({
+        name: 'category',
+        value: '한식',
+      }));
+
+      expect(state.restaurant.category).toBe('한식');
     });
   });
 });
