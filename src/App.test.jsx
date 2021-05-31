5ee0a318694a98b40c0f0a46450b860431fe3764
hanvit7 <hanvit7@gmail.com>
2021-05-31 13:31:27 +0900

    리팩토링 - App에 RestaurantsCreateContainer 교체 전 테스트 정리

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -6,40 +6,16 @@ import App from './App';
 
 import restaurants from '../fixtures/restaurants';
 
-describe('App', () => {
+test('App', () => {
   const dispatch = jest.fn();
 
   useDispatch.mockImplementation(() => dispatch);
   useSelector.mockImplementation((selector) => selector({
     restaurants,
+    restaurant: {},
   }));
 
-  it('has a title', () => {
-    const { getByText } = render(<App />);
+  const { getByText } = render(<App />);
 
-    expect(getByText(/Restaurants/)).not.toBeNull();
-  });
-
-  describe('List', () => {
-    it('has a list', () => {
-      const { getAllByRole } = render(<App />);
-
-      expect(getAllByRole('listitem')).toHaveLength(restaurants.length);
-    });
-
-    it('has a restaurant information', () => {
-      const { getByText } = render(<App />);
-
-      expect(getByText(
-        `${restaurants[0].name} | ${restaurants[0].category} | ${restaurants[0].location}`,
-      )).not.toBeNull();
-    });
-  });
-
-  it('has three input and button', () => {
-    const { getAllByRole, getByRole } = render(<App />);
-
-    expect(getAllByRole('textbox')).toHaveLength(3);
-    expect(getByRole('button', { name: '등록' })).not.toBeNull();
-  });
+  expect(getByText(/Restaurants/)).not.toBeNull();
 });
