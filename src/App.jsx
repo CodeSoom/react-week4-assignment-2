be1ae7df115017311844dd63f1c6026f7422eab5
studioLuda <studio.luda.work@gmail.com>
2021-06-10 20:09:43 +0900

    Restaurant 페이지의 구성 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
 export default function App() {
+  const dummyItems = [
+    {
+      name: '마녀주방',
+      category: '한식',
+      address: '서울시 강남구',
+    },
+  ];
   return (
     <div>
-      Hello
+      <h1>Restaurants</h1>
+      <ul>
+        {dummyItems.map((restaurant) => (
+          <li key={restaurant.name}>
+            <p>
+              {restaurant.name}
+              {' '}
+              |
+              {' '}
+              {restaurant.category}
+              {' '}
+              |
+              {' '}
+              {restaurant.address}
+              {' '}
+            </p>
+          </li>
+        ))}
+      </ul>
+      <div>
+        <input placeholder="이름" />
+        <input placeholder="분류" />
+        <input placeholder="주소" />
+        <button type="button">등록</button>
+      </div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -3,8 +3,10 @@ import App from './App';
 
 describe('App', () => {
   it('renders restaurants page', () => {
-    const { container, getByText } = render(App);
-
+    const { container, getByText } = render(<App />);
+    screen.debug();
     expect(container).toHaveTextContent('Restaurants');
+    expect(container).toHaveTextContent('마녀주방 | 한식 | 서울시 강남구');
+    expect(container).toHaveTextContent('등록');
   });
 });
