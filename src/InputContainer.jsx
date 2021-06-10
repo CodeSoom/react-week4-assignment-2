d6e25e15ed528d528a2e17b5fc9aedd853eb36b3
studioLuda <studio.luda.work@gmail.com>
2021-06-10 21:59:51 +0900

    InputCountainer onChange이벤트 테스트

--- src/InputContainer.jsx
+++ src/InputContainer.jsx
@@ -3,16 +3,25 @@ import { useDispatch, useSelector } from 'react-redux';
 import { addRestaurant } from './actions';
 
 export default function InputContainer() {
+  const { restaurantName, restaurantCategory, restaurantAddress } = useSelector(
+    (selector) => ({
+      restaurantName: selector.restaurantName,
+      restaurantCategory: selector.restaurantCategory,
+      restaurantAddress: selector.restaurantAddress,
+    }),
+  );
   const dispatch = useDispatch();
   function handleAddRestaurant() {
     dispatch(addRestaurant());
   }
   return (
     <div>
-      <input placeholder="이름" />
-      <input placeholder="분류" />
-      <input placeholder="주소" />
-      <button type="button" onClick={handleAddRestaurant}>등록</button>
+      <input placeholder="이름" value={restaurantName} />
+      <input placeholder="분류" value={restaurantCategory} />
+      <input placeholder="주소" value={restaurantAddress} />
+      <button type="button" onClick={handleAddRestaurant}>
+        등록
+      </button>
     </div>
   );
 }
--- src/InputContainer.test.jsx
+++ src/InputContainer.test.jsx
@@ -1,6 +1,7 @@
 import { render, fireEvent, screen } from '@testing-library/react';
 import { useSelector, useDispatch } from 'react-redux';
-import { addRestaurant } from './actions';
+import { addRestaurant, updateRestaurantName } from './actions';
+import { initialState } from './reducer';
 
 import InputContainer from './InputContainer';
 
@@ -11,6 +12,7 @@ describe('InputContainer', () => {
   useDispatch.mockImplementation(() => dispatch);
 
   it('renders inputs & button', () => {
+    useSelector.mockImplementation((selector) => selector(initialState));
     const { container, queryByPlaceholderText, getByText } = render(<InputContainer />);
     // screen.debug();
     expect(queryByPlaceholderText('이름'));
@@ -21,5 +23,18 @@ describe('InputContainer', () => {
     expect(dispatch).not.toBeCalledWith(addRestaurant());
     fireEvent.click(getByText('등록'));
     expect(dispatch).toBeCalledWith(addRestaurant());
+  });
+
+  it('renders updated input value', () => {
+    useSelector.mockImplementation((selector) => selector(initialState));
+    const { queryByPlaceholderText } = render(<InputContainer />);
+
+    expect(queryByPlaceholderText('이름')).toHaveDisplayValue('');
+
+    fireEvent.change(queryByPlaceholderText('이름'), {
+      target: { value: '이름' },
+    });
+
+    expect(dispatch).toBeCalledWith(updateRestaurantName('이름'));
   });
 });
