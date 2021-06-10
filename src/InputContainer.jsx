886e7fdaa2faea0763626e79e97181aa0c2a1377
studioLuda <studio.luda.work@gmail.com>
2021-06-10 22:05:11 +0900

    InputCountainer onChange이벤트 구현

--- src/InputContainer.jsx
+++ src/InputContainer.jsx
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from 'react-redux';
 
-import { addRestaurant } from './actions';
+import { addRestaurant, updateRestaurantName } from './actions';
 
 export default function InputContainer() {
   const { restaurantName, restaurantCategory, restaurantAddress } = useSelector(
@@ -14,9 +14,12 @@ export default function InputContainer() {
   function handleAddRestaurant() {
     dispatch(addRestaurant());
   }
+  function handleOnChangeInput(value) {
+    dispatch(updateRestaurantName(value));
+  }
   return (
     <div>
-      <input placeholder="이름" value={restaurantName} />
+      <input placeholder="이름" value={restaurantName} onChange={(e) => { handleOnChangeInput(e.target.value); }} />
       <input placeholder="분류" value={restaurantCategory} />
       <input placeholder="주소" value={restaurantAddress} />
       <button type="button" onClick={handleAddRestaurant}>
