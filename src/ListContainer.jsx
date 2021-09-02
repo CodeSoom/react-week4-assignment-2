3c773d68a7d5f9c8d04b5853702d3d116c9eb555
Aiden <rheech22@gmail.com>
2021-09-03 03:26:33 +0900

    Add a code to pass for ListContainer(GREEN)

--- src/ListContainer.jsx
+++ src/ListContainer.jsx
@@ -1,5 +1,13 @@
-export default function ListContainer() {
+export default function ListContainer({ restaurants }) {
   return (
-    <div />
+    <ul>
+      {restaurants.map((restaurant) => (
+        <li key={restaurant.id}>
+          {restaurant.name}
+          {restaurant.category}
+          {restaurant.address}
+        </li>
+      ))}
+    </ul>
   );
 }
