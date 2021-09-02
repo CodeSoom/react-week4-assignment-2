19a2eebb70439275958286a2ef8ec2b9e5d39d11
Aiden <rheech22@gmail.com>
2021-09-03 05:20:51 +0900

    Refactoring a test for InputContainer(1)
    test attributes of inputs

--- src/InputContainer.jsx
+++ src/InputContainer.jsx
@@ -1,9 +1,12 @@
-export default function InputContainer() {
+export default function InputContainer({
+  nameText, categoryText, addressText,
+  handleChangeName, handleChangeCategory, handleChangeAddress,
+}) {
   return ((
     <div>
-      <input name="name" placeholder="이름" />
-      <input name="category" placeholder="분류" />
-      <input name="address" placeholder="주소" />
+      <input name="name" placeholder="이름" value={nameText} onChange={handleChangeName} />
+      <input name="category" placeholder="분류" value={categoryText} onChange={handleChangeCategory} />
+      <input name="address" placeholder="주소" value={addressText} onChange={handleChangeAddress} />
       <button type="submit">등록</button>
     </div>
   ));
--- src/InputContainer.test.jsx
+++ src/InputContainer.test.jsx
@@ -1,14 +1,40 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import InputContainer from './InputContainer';
 
 test('InputContainer', () => {
+  const handleChangeName = jest.fn();
+  const handleChangeCategory = jest.fn();
+  const handleChangeAddress = jest.fn();
+
+  const { nameText, categoryText, addressText } = {
+    nameText: '성원각',
+    categoryText: '중식',
+    addressText: '서울시 동작구',
+  };
+
   const { getByText, getByPlaceholderText } = render((
-    <InputContainer />
+    <InputContainer
+      nameText={nameText}
+      categoryText={categoryText}
+      addressText={addressText}
+      handleChangeName={handleChangeName}
+      handleChangeCategory={handleChangeCategory}
+      handleChangeAddress={handleChangeAddress}
+    />
   ));
 
   expect(getByText(/등록/)).not.toBeNull();
   expect(getByPlaceholderText(/이름/)).not.toBeNull();
   expect(getByPlaceholderText(/분류/)).not.toBeNull();
   expect(getByPlaceholderText(/주소/)).not.toBeNull();
+
+  expect(getByPlaceholderText(/이름/)).toHaveDisplayValue(/성원각/);
+  expect(getByPlaceholderText(/분류/)).toHaveDisplayValue(/중식/);
+  expect(getByPlaceholderText(/주소/)).toHaveDisplayValue(/서울시 동작구/);
+
+  fireEvent.change(getByPlaceholderText(/이름/), {
+    target: { value: '시골순두부' },
+  });
+  expect(handleChangeName).toBeCalled();
 });
