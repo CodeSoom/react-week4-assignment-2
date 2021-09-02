51e080d6b158761c61754808a7f595ac9d493329
Aiden <rheech22@gmail.com>
2021-09-03 02:59:37 +0900

    Add a test for App.jsx(RED)

---
+++ src/App.test.jsx
@@ -0,0 +1,11 @@
+import { render } from '@testing-library/react';
+
+import App from './App';
+
+test('App', () => {
+  const { getByText } = render((
+    <App />
+  ));
+
+  expect(getByText(/Restaurant/)).toBeNull();
+});
