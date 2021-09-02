7d43fc483ffbd0c169b38df87195b9a6c2409da5
Aiden <rheech22@gmail.com>
2021-09-03 05:04:07 +0900

    Add a code to pass a test for InputContainer(GREEN)

--- src/InputContainer.jsx
+++ src/InputContainer.jsx
@@ -1,5 +1,10 @@
 export default function InputContainer() {
   return ((
-    <div />
+    <div>
+      <input name="name" placeholder="이름" />
+      <input name="category" placeholder="분류" />
+      <input name="address" placeholder="주소" />
+      <button type="submit">등록</button>
+    </div>
   ));
 }
