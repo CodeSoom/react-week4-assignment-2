edca18a2c79d51d1eadef108a0868b227cbcda92
Aiden <rheech22@gmail.com>
2021-09-03 06:27:48 +0900

    Refactoring a test for reducer

--- src/reducer.js
+++ src/reducer.js
@@ -7,5 +7,54 @@ const initialState = {
 };
 
 export default function reducer(state = initialState, action) {
+  if (action.type === 'updateNameText') {
+    return {
+      ...state,
+      nameText: action.payload.nameText,
+    };
+  }
+
+  if (action.type === 'updateCategoryText') {
+    return {
+      ...state,
+      categoryText: action.payload.categoryText,
+    };
+  }
+
+  if (action.type === 'updateAddressText') {
+    return {
+      ...state,
+      addressText: action.payload.addressText,
+    };
+  }
+
+  if (action.type === 'addRestaurant') {
+    const {
+      newId,
+      nameText,
+      categoryText,
+      addressText,
+      restaurants,
+    } = state;
+
+    if (!nameText || !categoryText || !addressText) {
+      return state;
+    }
+
+    return {
+      ...state,
+      newId: newId + 1,
+      nameText: '',
+      categoryText: '',
+      addressText: '',
+      restaurants: [...restaurants, {
+        id: newId,
+        name: nameText,
+        category: categoryText,
+        address: addressText,
+      }],
+    };
+  }
+
   return state;
 }
--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,5 +1,12 @@
 import reducer from './reducer';
 
+import {
+  addRestaurant,
+  updateAddressText,
+  updateCategoryText,
+  updateNameText,
+} from './actions';
+
 describe('reducer', () => {
   context('with an action not existed', () => {
     const notExistedAction = jest.fn();
@@ -16,6 +23,78 @@ describe('reducer', () => {
       expect(state.categoryText).toBe('');
       expect(state.addressText).toBe('');
       expect(state.restaurants).toHaveLength(0);
+    });
+  });
+
+  context('with an action existed', () => {
+    describe('updateNameText', () => {
+      it('changes a displayed name', () => {
+        const state = reducer({
+          nameText: '',
+        }, updateNameText('성원각'));
+
+        expect(state.nameText).toBe('성원각');
+      });
+    });
+
+    describe('updateCategoryText', () => {
+      it('changes a displayed category', () => {
+        const state = reducer({
+          categoryText: '',
+        }, updateCategoryText('중식'));
+
+        expect(state.categoryText).toBe('중식');
+      });
+    });
+
+    describe('updateAddressText', () => {
+      it('changes a displayed address', () => {
+        const state = reducer({
+          addressText: '',
+        }, updateAddressText('서울시 동작구'));
+
+        expect(state.addressText).toBe('서울시 동작구');
+      });
+    });
+
+    describe('addRestaurant', () => {
+      function reduceAddRestaurant(nameText, categoryText, addressText) {
+        return reducer({
+          newId: 100,
+          nameText,
+          categoryText,
+          addressText,
+          restaurants: [],
+        }, addRestaurant());
+      }
+
+      context('without restaurant infos', () => {
+        it('Nothing happens', () => {
+          const state = reduceAddRestaurant();
+
+          expect(state.restaurants).toHaveLength(0);
+        });
+      });
+
+      context('with restaurant infos', () => {
+        it('appends a new restaurant into a list', () => {
+          const state = reduceAddRestaurant('성원각', '중식', '서울시 동작구');
+
+          expect(state.restaurants).toHaveLength(1);
+          expect(state.restaurants[0].id).not.toBeUndefined();
+          expect(state.restaurants[0].name).toBe('성원각');
+          expect(state.restaurants[0].category).toBe('중식');
+          expect(state.restaurants[0].address).toBe('서울시 동작구');
+        });
+
+        it('clears input values', () => {
+          const state = reduceAddRestaurant('성원각', '중식', '서울시 동작구');
+
+          expect(state.nameText).toBe('');
+          expect(state.categoryText).toBe('');
+          expect(state.addressText).toBe('');
+        });
+      });
     });
   });
 });
