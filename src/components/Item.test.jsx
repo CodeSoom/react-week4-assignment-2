605c353223779042fc26d7e89f6f4d069913a5fa
seilylook <seilylook@naver.com>
2021-09-02 13:47:44 +0900

    refactor: item.test add

---
+++ src/components/Item.test.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import Item from './Item';
+
+describe('Item', () => {
+  it('show restaurants', () => {
+    const newRestaurant = {
+      id: '100',
+      name: '감성타코',
+      category: '양식',
+      address: '신사동 가로수길',
+    };
+
+    const { container } = render((
+      <Item restaurant={newRestaurant} />
+    ));
+
+    expect(container).toHaveTextContent('감성타코');
+    expect(container).toHaveTextContent('양식');
+    expect(container).toHaveTextContent('신사동 가로수길');
+  });
+});
