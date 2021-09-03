cf167816cb8096b508c62ee498d48233a5d4388d
passwd10 <parkinseo94@naver.com>
2021-09-03 19:06:20 +0900

    test: App 테스트를 추가한다

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -3,9 +3,39 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 describe('App', () => {
-  it('renders Restaurants', () => {
+  it('renders title', () => {
     const { container } = render(<App />);
 
     expect(container).toHaveTextContent('Restaurants');
+  });
+
+  it('renders name input field', () => {
+    const { getByLabelText } = render(<App />);
+
+    const restaurantName = getByLabelText('restaurantName');
+
+    expect(restaurantName).not.toBeNull();
+  });
+
+  it('renders category input field', () => {
+    const { getByLabelText } = render(<App />);
+
+    const restaurantCategory = getByLabelText('restaurantCategory');
+
+    expect(restaurantCategory).not.toBeNull();
+  });
+
+  it('renders address input field', () => {
+    const { getByLabelText } = render(<App />);
+
+    const restaurantAddress = getByLabelText('restaurantAddress');
+
+    expect(restaurantAddress).not.toBeNull();
+  });
+
+  it('renders 등록 button', () => {
+    const { container } = render(<App />);
+
+    expect(container).toHaveTextContent('등록');
   });
 });
