ee4ede19fcb1be7eef53ab46fee0ea252d1fc0f9
passwd10 <parkinseo94@naver.com>
2021-09-03 21:10:02 +0900

    test: reducer 테스트를 action별로 작성한다

--- src/store/reducer.test.js
+++ src/store/reducer.test.js
@@ -7,24 +7,30 @@ import {
 } from './actions';
 
 describe('reducer', () => {
-  it('changes restuarant name', () => {
-    const previousState = { restuarantName: '' };
-    const nextState = reducer(previousState, updateRestuarantName('시카고피자'));
+  describe('updateRestuarantName', () => {
+    it('changes restuarant name', () => {
+      const previousState = { restuarantName: '' };
+      const nextState = reducer(previousState, updateRestuarantName('시카고피자'));
 
-    expect(nextState.restuarantName).toBe('시카고피자');
+      expect(nextState.restuarantName).toBe('시카고피자');
+    });
   });
 
-  it('changes restuarant category', () => {
-    const previousState = { restuarantCategory: '' };
-    const nextState = reducer(previousState, updateRestuarantCategory('양식'));
+  describe('updateRestuarantCategory', () => {
+    it('changes restuarant category', () => {
+      const previousState = { restuarantCategory: '' };
+      const nextState = reducer(previousState, updateRestuarantCategory('양식'));
 
-    expect(nextState.restuarantCategory).toBe('양식');
+      expect(nextState.restuarantCategory).toBe('양식');
+    });
   });
 
-  it('changes restuarant address', () => {
-    const previousState = { restuarantAddress: '' };
-    const nextState = reducer(previousState, updateRestuarantAddress('서울시 강남구'));
+  describe('updateRestuarantAddress', () => {
+    it('changes restuarant address', () => {
+      const previousState = { restuarantAddress: '' };
+      const nextState = reducer(previousState, updateRestuarantAddress('서울시 강남구'));
 
-    expect(nextState.restuarantAddress).toBe('서울시 강남구');
+      expect(nextState.restuarantAddress).toBe('서울시 강남구');
+    });
   });
 });
