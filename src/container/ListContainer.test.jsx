004b907e8ec9d10cfcc1c6c539e20be4816057b4
passwd10 <parkinseo94@naver.com>
2021-09-04 01:24:06 +0900

    test: ListContainer 테스트를 추가한다

---
+++ src/container/ListContainer.test.jsx
@@ -0,0 +1,27 @@
+import { render } from '@testing-library/react';
+
+import { useSelector } from 'react-redux';
+
+import given2 from 'given2';
+
+import ListContainer from './ListContainer';
+
+describe('ListContainer', () => {
+  given2('restaurants', () => []);
+
+  useSelector.mockImplementation((selector) => selector({
+    restaurants: given2.restaurants,
+  }));
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('renders restaurant lists', () => {
+    given2('restaurants', () => ['맥도날드 | 양식 | 서울시 양천구']);
+
+    const { container } = render(<ListContainer />);
+
+    expect(container).toHaveTextContent('맥도날드 | 양식 | 서울시 양천구');
+  });
+});
