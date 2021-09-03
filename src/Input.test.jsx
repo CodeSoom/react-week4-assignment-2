39ad94d1436a623bb91d3dc390b792fac74d3a7d
lazy-sky <1cielo@naver.com>
2021-09-04 01:34:10 +0900

    red: Add listening value change case

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -1,4 +1,5 @@
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import Input from './Input';
 
@@ -27,5 +28,17 @@ describe('Input', () => {
     expect(getByPlaceholderText(/이름/)).not.toBeNull();
     expect(getByPlaceholderText(/분류/)).not.toBeNull();
     expect(getByPlaceholderText(/주소/)).not.toBeNull();
+  });
+
+  it('listens input value change', () => {
+    const { getByLabelText } = renderInput();
+
+    userEvent.type(getByLabelText(/이름/), '좋은 식당');
+    userEvent.type(getByLabelText(/분류/), '한식');
+    userEvent.type(getByLabelText(/주소/), '서울');
+
+    expect(handleChangeName).toBeCalled();
+    expect(handleChangeFood).toBeCalled();
+    expect(handleChangeLocation).toBeCalled();
   });
 });
