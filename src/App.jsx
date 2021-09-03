180729e4c57244895b01f27a4fb08726783c5132
passwd10 <parkinseo94@naver.com>
2021-09-03 19:10:00 +0900

    feat: App 컴포넌트를 구현한다

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
 export default function App() {
   return (
-    <h1>
-      Restaurants
-    </h1>
+    <>
+      <h1>
+        Restaurants
+      </h1>
+      <input
+        type="text"
+        placeholder="이름"
+        aria-label="restaurantName"
+      />
+      <input
+        type="text"
+        placeholder="분류"
+        aria-label="restaurantCategory"
+      />
+      <input
+        type="text"
+        placeholder="주소"
+        aria-label="restaurantAddress"
+      />
+      <button type="button">
+        등록
+      </button>
+    </>
   );
 }
