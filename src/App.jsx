4eb05008fa13427aa33e3a9eceaae3ea526be32b
passwd10 <parkinseo94@naver.com>
2021-09-03 22:00:26 +0900

    refactor: App 컴포넌트에서 InputContainer를 import한다

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,12 @@
-import { useState } from 'react';
-
-import Input from './presentational/Input';
+import InputContainer from './container/InputContainer';
 
 export default function App() {
-  const [name, setName] = useState('');
-  const [category, setCategory] = useState('');
-  const [address, setAddress] = useState('');
-
-  const handleChangeName = (event) => {
-    setName(event.target.value);
-  };
-
-  const handleChangeCategory = (event) => {
-    setCategory(event.target.value);
-  };
-
-  const handleChangeAddress = (event) => {
-    setAddress(event.target.value);
-  };
-
   return (
     <>
       <h1>
         Restaurants
       </h1>
-      <Input
-        ariaLabel="restaurantName"
-        value={name}
-        handleChange={handleChangeName}
-      />
-      <Input
-        ariaLabel="restaurantCategory"
-        value={category}
-        handleChange={handleChangeCategory}
-      />
-      <Input
-        ariaLabel="restaurantAddress"
-        value={address}
-        handleChange={handleChangeAddress}
-      />
+      <InputContainer />
       <button type="button">
         등록
       </button>
