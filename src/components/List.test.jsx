678141b84362449d797b756ae0915545e0a6680d
kooku <koomg9599@gmail.com>
2021-09-03 20:52:54 +0900

    List 테스트 작성

---
+++ src/components/List.test.jsx
@@ -0,0 +1,31 @@
+import { render } from '@testing-library/react';
+import List from './List';
+
+describe('List', () => {
+  const renderList = (restaurants) => render(<List restaurants={restaurants} />);
+
+  context('레스토랑들이 있다면', () => {
+    it('레스토랑들을 보여준다.', () => {
+      const restaurants = [
+        {
+          id: 1, name: '마녀주방', type: '한식', address: '서울시 강남구',
+        },
+        {
+          id: 2, name: '시카고피자', type: '양식', address: '이태원동',
+        },
+      ];
+      const { getByText } = renderList(restaurants);
+
+      expect(getByText('마녀주방 | 한식 | 서울시 강남구')).not.toBeUndefined();
+      expect(getByText('시카고피자 | 양식 | 이태원동')).not.toBeUndefined();
+    });
+  });
+
+  context('레스토랑들이 없다면', () => {
+    it('"레스토랑이 없습니다"를 출력한다.', () => {
+      const { getByText } = renderList([]);
+
+      expect(getByText(/레스토랑이 없습니다/)).not.toBeUndefined();
+    });
+  });
+});
---
+++ src/components/ListContainer.test.jsx
@@ -0,0 +1,21 @@
+import { render } from '@testing-library/react';
+import { useSelector } from '../../__mocks__/react-redux';
+import ListContainer from './ListContainer';
+
+test('ListContainer', () => {
+  useSelector.mockImplementation((selector) => selector({
+    restaurants: [
+      {
+        id: 1, name: '마녀주방', type: '한식', address: '서울시 강남구',
+      },
+      {
+        id: 2, name: '시카고피자', type: '양식', address: '이태원동',
+      },
+    ],
+  }));
+
+  const { getByText } = render(<ListContainer />);
+
+  expect(getByText('마녀주방 | 한식 | 서울시 강남구')).not.toBeUndefined();
+  expect(getByText('시카고피자 | 양식 | 이태원동')).not.toBeUndefined();
+});
