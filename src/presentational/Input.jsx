410d82b942039bf1deaf7efb0fc26db9fbe55521
passwd10 <parkinseo94@naver.com>
2021-09-04 01:50:36 +0900

    feat: Input props에 name을 추가한다

--- src/presentational/Input.jsx
+++ src/presentational/Input.jsx
@@ -1,9 +1,10 @@
 export default function Input({
-  ariaLabel, value, handleChange, placeholder,
+  ariaLabel, value, handleChange, placeholder, inputName,
 }) {
   return (
     <input
       type="text"
+      name={inputName}
       placeholder={placeholder}
       aria-label={ariaLabel}
       value={value}
