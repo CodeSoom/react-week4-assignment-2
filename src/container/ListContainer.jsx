4981b6e51fe002ce9a121557048eabb698c38abf
passwd10 <parkinseo94@naver.com>
2021-09-04 01:33:10 +0900

    refactor: ListContainer가 List를 사용한다

--- src/container/ListContainer.jsx
+++ src/container/ListContainer.jsx
@@ -1,11 +1,18 @@
 import { useSelector } from 'react-redux';
 
+import List from '../presentational/List';
+
 export default function ListContainer() {
   const { restaurants } = useSelector((state) => state);
 
   return (
     <>
-      {restaurants}
+      {restaurants.map((restaurant, index) => (
+        <List
+          key={index}
+          value={restaurant}
+        />
+      ))}
     </>
   );
 }
