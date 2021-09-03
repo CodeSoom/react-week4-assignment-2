6e1045025df5e3364342aa2075e225ece857b8ec
kooku <koomg9599@gmail.com>
2021-09-03 20:54:19 +0900

    Item 테스트 작성

---
+++ src/components/Item.test.jsx
@@ -0,0 +1,11 @@
+import { render } from '@testing-library/react';
+import Item from './Item';
+
+test('Item', () => {
+  const restaurant = {
+    id: 1, name: '마녀주방', type: '한식', address: '서울시 강남구',
+  };
+  const { getByText } = render(<Item restaurant={restaurant} />);
+
+  expect(getByText('마녀주방 | 한식 | 서울시 강남구')).not.toBeUndefined();
+});
