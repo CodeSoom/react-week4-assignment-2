f91396885367d9e7c351326a4af1be051c69c366
passwd10 <parkinseo94@naver.com>
2021-09-04 21:46:03 +0900

    refactor: 불필요한 given을 제거한다

--- src/container/ListContainer.test.jsx
+++ src/container/ListContainer.test.jsx
@@ -2,15 +2,14 @@ import { render } from '@testing-library/react';
 
 import { useSelector } from 'react-redux';
 
-import given2 from 'given2';
-
 import ListContainer from './ListContainer';
 
 describe('ListContainer', () => {
-  given2('restaurants', () => []);
-
   useSelector.mockImplementation((selector) => selector({
-    restaurants: given2.restaurants,
+    restaurants: [{
+      id: 0,
+      title: '맥도날드 | 양식 | 서울시 양천구',
+    }],
   }));
 
   beforeEach(() => {
@@ -18,11 +17,6 @@ describe('ListContainer', () => {
   });
 
   it('renders restaurant lists', () => {
-    given2('restaurants', () => [{
-      id: 0,
-      title: '맥도날드 | 양식 | 서울시 양천구',
-    }]);
-
     const { container } = render(<ListContainer />);
 
     expect(container).toHaveTextContent('맥도날드 | 양식 | 서울시 양천구');
--- src/presentational/Input.test.jsx
+++ src/presentational/Input.test.jsx
@@ -1,15 +1,9 @@
 import { render, fireEvent } from '@testing-library/react';
 
-import given from 'given2';
-
 import Input from './Input';
 
 describe('Input', () => {
   const onChange = jest.fn();
-
-  given('value', () => '');
-  given('ariaLabel', () => '');
-  given('placeholder', () => '');
 
   beforeEach(() => {
     jest.clearAllMocks();
@@ -18,16 +12,14 @@ describe('Input', () => {
   const renderInput = () => render((
     <Input
       onChange={onChange}
-      value={given.value}
-      ariaLabel={given.ariaLabel}
-      placeholder={given.placeholder}
+      value="시카고피자"
+      ariaLabel="restaurantName"
+      placeholder="이름"
       name="name"
     />
   ));
 
   it('renders input field', () => {
-    given('ariaLabel', () => 'restaurantName');
-
     const { getByLabelText } = renderInput();
 
     const restaurantName = getByLabelText('restaurantName');
@@ -36,27 +28,20 @@ describe('Input', () => {
   });
 
   it('renders input value', () => {
-    given('ariaLabel', () => 'restaurantName');
-    given('value', () => '시카고피자');
-    given('placeholder', () => '이름');
-
     const { getByLabelText } = renderInput();
 
     const restaurantName = getByLabelText('restaurantName');
 
     expect(restaurantName.value).toBe('시카고피자');
-    expect(restaurantName.name).toBe('name');
   });
 
   it('listens change event', () => {
-    given('ariaLabel', () => 'restaurantName');
-
     const { getByLabelText } = renderInput();
 
     expect(onChange).not.toBeCalled();
 
     fireEvent.change(getByLabelText('restaurantName'), {
-      target: { value: '시카고피자' },
+      target: { value: '맥도날드' },
     });
 
     expect(onChange).toBeCalled();
