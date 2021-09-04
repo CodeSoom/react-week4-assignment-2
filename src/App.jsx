1189e5e1ccec4ef6a6cc87d06eb5ffaad745067e
psy082 <zhappy114@gmail.com>
2021-09-05 00:37:27 +0900

    feat: 이름, 분류 주소를 위한 input 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
 export default function App() {
+  const handleChange = () => {
+
+  };
+
   return (
     <div>
       <h1>Restaurants</h1>
+      <div>
+        <input
+          id="input-name"
+          type="text"
+          placeholder="이름"
+          value=""
+          onChange={handleChange}
+        />
+        <input
+          id="input-classification"
+          type="text"
+          placeholder="분류"
+          value=""
+          onChange={handleChange}
+        />
+        <input
+          id="input-address"
+          type="text"
+          placeholder="주소"
+          value=""
+          onChange={handleChange}
+        />
+      </div>
     </div>
   );
 }
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import App from './App';
 
@@ -9,8 +9,21 @@ describe('App', () => {
         const { container } = render(
           <App />,
         );
+
         expect(container).not.toHaveTextContent('식당');
       });
+    });
+  });
+
+  describe('restaurant information inputs', () => {
+    it('are 3 for name, classification, and address', () => {
+      const { getByPlaceholderText } = render(
+        <App />,
+      );
+
+      expect(getByPlaceholderText('이름')).not.toBeNull();
+      expect(getByPlaceholderText('분류')).not.toBeNull();
+      expect(getByPlaceholderText('주소')).not.toBeNull();
     });
   });
 });
