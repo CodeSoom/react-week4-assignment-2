f268fa827affc4a92fb668cda96fe7dc3873f81c
passwd10 <parkinseo94@naver.com>
2021-09-04 22:05:25 +0900

    test: List 테스트를 추가한다

--- src/presentational/List.test.jsx
+++ src/presentational/List.test.jsx
@@ -3,11 +3,22 @@ import { render } from '@testing-library/react';
 import List from './List';
 
 describe('List', () => {
-  it('renders value of props', () => {
+  it('renders restaurants', () => {
+    const restaurants = [
+      {
+        id: 0,
+        title: '맥도날드 | 양식 | 서울시 양천구',
+      }, {
+        id: 1,
+        title: '맘스터치 | 양식 | 서울시 종로구',
+      },
+    ];
+
     const { container } = render((
-      <List value="ㅇㅇ | ㅇㅇ | ㅇㅇ" />
+      <List restaurants={restaurants} />
     ));
 
-    expect(container).toHaveTextContent('ㅇㅇ | ㅇㅇ | ㅇㅇ');
+    expect(container).toHaveTextContent('맥도날드 | 양식 | 서울시 양천구');
+    expect(container).toHaveTextContent('맘스터치 | 양식 | 서울시 종로구');
   });
 });
