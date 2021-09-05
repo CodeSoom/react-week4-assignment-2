349afcdf5d9639d5be244d71c660d2418f2caa4e
lazy-sky <1cielo@naver.com>
2021-09-05 17:19:57 +0900

    red: Testing to render restaurants information

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -3,8 +3,16 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 test('App', () => {
-  const { getByText } = render((<App />
+  const restaurants = [
+    {
+      id: 1, title: '마녀주방', category: '한식', address: '서울시 강남구',
+    },
+  ];
+
+  const { getByText } = render((<App restaurants={restaurants} />
   ));
 
   expect(getByText(/마녀주방/)).not.toBeNull();
+  expect(getByText(/한식/)).not.toBeNull();
+  expect(getByText(/서울시 강남구/)).not.toBeNull();
 });
