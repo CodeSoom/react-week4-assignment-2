0332586f97d5149191174f4a0428745643ba82a1
psy082 <zhappy114@gmail.com>
2021-09-06 01:15:36 +0900

    test: RegisterContainer test 추가
    - input 입력 시 updateRestaurantInfo action을 호출하는 지 test
    - 등록 버튼 클릭 시 addRestaurant action을 호출하는 지 test

--- src/Container/Register/RegisterContainer.test.jsx
+++ src/Container/Register/RegisterContainer.test.jsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -25,5 +25,66 @@ describe('RegisterContainer', () => {
     expect(getByDisplayValue(/마녀주방/)).not.toBeNull();
     expect(getByDisplayValue(/분식/)).not.toBeNull();
     expect(getByDisplayValue(/서울시 강남구/)).not.toBeNull();
+  });
+
+  it('calls updateRestaurantInfo action', () => {
+    useSelector.mockImplementation((selector) => selector({
+      name: '',
+      category: '',
+      address: '',
+    }));
+
+    const { getByPlaceholderText } = render((
+      <RegisterContainer />
+    ));
+
+    expect(getByPlaceholderText('이름')).toHaveValue('');
+    expect(getByPlaceholderText('분류')).toHaveValue('');
+    expect(getByPlaceholderText('주소')).toHaveValue('');
+
+    fireEvent.change(getByPlaceholderText('이름'), {
+      target: { value: '마녀주방' },
+    });
+
+    expect(dispatch).toBeCalledWith({
+      type: 'updateRestaurantInfo',
+      payload: { infoType: 'name', value: '마녀주방' },
+    });
+
+    fireEvent.change(getByPlaceholderText('분류'), {
+      target: { value: '분식' },
+    });
+
+    expect(dispatch).toBeCalledWith({
+      type: 'updateRestaurantInfo',
+      payload: { infoType: 'category', value: '분식' },
+    });
+
+    fireEvent.change(getByPlaceholderText('주소'), {
+      target: { value: '서울시 강남구' },
+    });
+
+    expect(dispatch).toBeCalledWith({
+      type: 'updateRestaurantInfo',
+      payload: { infoType: 'address', value: '서울시 강남구' },
+    });
+  });
+
+  it('calls addRestaurant action', () => {
+    useSelector.mockImplementation((selector) => selector({
+      name: '마녀주방',
+      category: '분식',
+      address: '서울시 강남구',
+    }));
+
+    const { getByText } = render((
+      <RegisterContainer />
+    ));
+
+    expect(getByText('등록')).not.toBeNull();
+
+    fireEvent.click(getByText('등록'));
+
+    expect(dispatch).toBeCalledWith({ type: 'addRestaurant' });
   });
 });
