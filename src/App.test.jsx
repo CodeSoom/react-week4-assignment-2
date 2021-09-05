3c5c5f04b2a6565a604c0e22453be553cb3db5f9
hanabyhana <yaong1002@gmail.com>
2021-09-05 18:15:45 +0900

    RestaurantsContainer 구현, App 테스트 수정

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,7 +1,23 @@
 import { render, fireEvent } from '@testing-library/react';
 
+import { useDispatch, useSelector } from 'react-redux';
+
 import App from './App';
 
-describe('App', () => {
+jest.mock('react-redux');
 
+test('App', () => {
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
+
+  useSelector.mockImplementation((selector) => selector({
+    restaurants: [],
+    restaurant: {
+    },
+  }));
+
+  render((
+    <App />
+  ));
 });
