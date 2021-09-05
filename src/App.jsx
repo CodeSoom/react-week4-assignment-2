5b0c0866d2257750b8e78b2a1e54b80b75562873
lazy-sky <1cielo@naver.com>
2021-09-05 17:27:55 +0900

    green: Rendering restaurants

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 export default function App() {
+  const restaurants = [
+    {
+      id: 1, name: '마녀주방', category: '한식', address: '서울시 강남구',
+    },
+  ];
+
   return (
     <div>
       <h1>Restaurants</h1>
       <ul>
-        <li>마녀주방</li>
+        {restaurants.map((restaurant) => (
+          <li key={restaurant.id}>
+            {restaurant.name}
+            {restaurant.category}
+            {restaurant.address}
+          </li>
+        ))}
       </ul>
     </div>
   );
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -5,7 +5,7 @@ import App from './App';
 test('App', () => {
   const restaurants = [
     {
-      id: 1, title: '마녀주방', category: '한식', address: '서울시 강남구',
+      id: 1, name: '마녀주방', category: '한식', address: '서울시 강남구',
     },
   ];
 
