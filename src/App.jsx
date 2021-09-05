6b2f89199ed986a70725c87c4e4b5a8ee73189d5
lazy-sky <1cielo@naver.com>
2021-09-05 23:20:42 +0900

    refact: Rendering container in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
-import Restaurants from './Restaurants';
+import RestaurantsContainer from './RestaurantsContainer';
 
 export default function App() {
-  const restaurants = [
-    {
-      id: 1, name: '마녀주방', category: '한식', address: '서울시 강남구',
-    },
-  ];
-
   return (
     <div>
       <h1>Restaurants</h1>
-      <Restaurants restaurants={restaurants} />
+      <RestaurantsContainer />
     </div>
   );
 }
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,13 +1,17 @@
 import { render } from '@testing-library/react';
 
+import { useSelector } from 'react-redux';
+
 import App from './App';
 
+import restaurants from '../fixtures/restaurants';
+
+jest.mock('react-redux');
+
 test('App', () => {
-  const restaurants = [
-    {
-      id: 1, name: '마녀주방', category: '한식', address: '서울시 강남구',
-    },
-  ];
+  useSelector.mockImplementation((selector) => selector({
+    restaurants,
+  }));
 
   const { getByText } = render((<App restaurants={restaurants} />
   ));
