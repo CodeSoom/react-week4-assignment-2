d0775c500af867af2974e4946650ea41b4a45d0d
lazy-sky <1cielo@naver.com>
2021-09-06 01:08:12 +0900

    red: Testing reducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
+import { useEffect } from 'react';
+
+import { usseDispatch } from 'react-redux';
+
 import RestaurantsContainer from './RestaurantsContainer';
 
+import { setRestaurants } from './actions';
+
 export default function App() {
+  const dispatch = usseDispatch();
+
+  // TODO: 초기 실행
+  // restaurants에 데이터 넣기 => dispatch
+  useEffect(() => {
+    dispatch(setRestaurants);
+  }, []);
+
   return (
     <div>
       <h1>Restaurants</h1>
--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,1 +1,17 @@
 import reducer from './reducer';
+
+import { setRestaurants } from './actions';
+
+import restaurants from '../fixtures/restaurants';
+
+describe('reducer', () => {
+  describe('setRestaurants', () => {
+    const initialState = {
+      restaurants: [],
+    };
+
+    const state = reducer(initialState, setRestaurants(restaurants));
+
+    expect(state).not.toHaveLength(0);
+  });
+});
