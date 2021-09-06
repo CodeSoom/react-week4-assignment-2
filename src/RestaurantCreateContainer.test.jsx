6760ae4f4823a15631f614703c934bc4c475cd45
lazy-sky <1cielo@naver.com>
2021-09-06 17:15:11 +0900

    red: test addRestaurant dispatcher

--- src/RestaurantCreateContainer.test.jsx
+++ src/RestaurantCreateContainer.test.jsx
@@ -1,6 +1,6 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import RestaurantCreateContainer from './RestaurantCreateContainer';
 
@@ -9,6 +9,10 @@ import restaurants from '../fixtures/restaurants';
 jest.mock('react-redux');
 
 test('RestaurantCreateContainer', () => {
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
+
   useSelector.mockImplementation((selector) => selector({
     restaurants,
   }));
@@ -17,4 +21,10 @@ test('RestaurantCreateContainer', () => {
   ));
 
   expect(getByText(/등록/)).not.toBeNull();
+
+  fireEvent.click(getByText(/등록/));
+
+  expect(dispatch).toBeCalledWith({
+    type: 'addRestaurant',
+  });
 });
