1eda65b94df6db6c7ab2c4c71c85978a56c52eea
GUAJEGUICHAN <qkr135qkr@gmail.com>
2021-12-22 19:31:33 +0900

    [refactor] store.js에서 reducer.js를 분리

---
+++ src/reducer.js
@@ -0,0 +1,42 @@
+const initialState = {
+  name: '',
+  category: '',
+  address: '',
+  data: [],
+};
+
+export default function reducer(state = initialState, action) {
+  if (action.type === 'handleChangeNameValue') {
+    return {
+      ...state,
+      name: action.payload.name,
+    };
+  }
+  if (action.type === 'handleChangeCategroryValue') {
+    return {
+      ...state,
+      category: action.payload.category,
+    };
+  }
+  if (action.type === 'handleChangeAddressValue') {
+    return {
+      ...state,
+      address: action.payload.address,
+    };
+  }
+  if (action.type === 'saveData') {
+    console.log(action.payload.data);
+    return {
+      ...state,
+      name: '',
+      category: '',
+      address: '',
+      data: [
+        ...state.data,
+        action.payload.data,
+      ],
+    };
+  }
+
+  return state;
+}
--- src/store.js
+++ src/store.js
@@ -1,47 +1,5 @@
 import { createStore } from 'redux';
-
-const initialState = {
-  name: '',
-  category: '',
-  address: '',
-  data: [],
-};
-
-function reducer(state = initialState, action) {
-  if (action.type === 'handleChangeNameValue') {
-    return {
-      ...state,
-      name: action.payload.name,
-    };
-  }
-  if (action.type === 'handleChangeCategroryValue') {
-    return {
-      ...state,
-      category: action.payload.category,
-    };
-  }
-  if (action.type === 'handleChangeAddressValue') {
-    return {
-      ...state,
-      address: action.payload.address,
-    };
-  }
-  if (action.type === 'saveData') {
-    console.log(action.payload.data);
-    return {
-      ...state,
-      name: '',
-      category: '',
-      address: '',
-      data: [
-        ...state.data,
-        action.payload.data,
-      ],
-    };
-  }
-
-  return state;
-}
+import reducer from './reducer';
 
 const store = createStore(reducer);
 
