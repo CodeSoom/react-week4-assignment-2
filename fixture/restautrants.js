cafc9f6c6b3988d5b0d91025edd59d019bcf136f
KIMSEUNGGYU <45627868+KIMSEUNGGYU@users.noreply.github.com>
2021-12-22 23:30:55 +0900

    [test] input 입력 테스트 기능 구현

---
+++ fixture/restautrants.js
@@ -0,0 +1,23 @@
+export const restautrant = {
+  name: "마녀 식당",
+  type: "한식",
+  address: "서울시 강남구",
+};
+
+export const restautrants = [
+  {
+    name: "마녀주방",
+    type: "한식",
+    address: "서울시 강남구",
+  },
+  {
+    name: "시카고피자",
+    type: "양식",
+    address: "이태원동",
+  },
+  {
+    name: "키와미",
+    type: "일식",
+    address: "분당구 정자동",
+  },
+];
--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -1,12 +1,19 @@
-import { render } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 
 import Input from "./Input";
 
+import { restautrant } from "../fixture/restautrants";
+
 describe("Input", () => {
-  it("render", () => {
-    const { getByPlaceholderText, getByRole } = render(
-      <Input restautrant={{}} />
+  const handleChangeRestautrantInfo = jest.fn();
+
+  const renderComponent = (restautrant) =>
+    render(
+      <Input restautrant={restautrant} onChange={handleChangeRestautrantInfo} />
     );
+
+  it("render", () => {
+    const { getByPlaceholderText, getByRole } = renderComponent({});
 
     expect(getByPlaceholderText(/이름/)).toBeInTheDocument();
     expect(getByPlaceholderText(/분류/)).toBeInTheDocument();
@@ -14,27 +21,65 @@ describe("Input", () => {
     expect(getByRole("button", { name: /추가/ })).toBeInTheDocument();
   });
 
-  it("각각의 입렵 란은 전달 받은 값을 렌더링한다.", () => {
-    const restautrant = {
-      name: "마녀 식당",
-      type: "한식",
-      address: "서울시 강남구",
-    };
-    const { getByPlaceholderText } = render(
-      <Input restautrant={restautrant} />
-    );
+  context("각각의 입련 란은", () => {
+    beforeEach(() => {
+      jest.clearAllMocks();
+    });
 
-    expect(getByPlaceholderText(/이름/)).toHaveAttribute(
-      "value",
-      restautrant.name
-    );
-    expect(getByPlaceholderText(/분류/)).toHaveAttribute(
-      "value",
-      restautrant.type
-    );
-    expect(getByPlaceholderText(/주소/)).toHaveAttribute(
-      "value",
-      restautrant.address
-    );
+    it("전달 받은 값을 렌더링한다.", () => {
+      const { getByPlaceholderText } = renderComponent(restautrant);
+
+      expect(getByPlaceholderText(/이름/)).toHaveAttribute(
+        "value",
+        restautrant.name
+      );
+      expect(getByPlaceholderText(/분류/)).toHaveAttribute(
+        "value",
+        restautrant.type
+      );
+      expect(getByPlaceholderText(/주소/)).toHaveAttribute(
+        "value",
+        restautrant.address
+      );
+    });
+
+    it("이름 입력창에 입력 시, handleChangeRestautrantInfo 를 name 과 입력 값을 함께 호출한다", () => {
+      const { getByPlaceholderText } = renderComponent(restautrant);
+
+      fireEvent.change(getByPlaceholderText(/이름/), {
+        target: { value: restautrant.name },
+      });
+
+      expect(handleChangeRestautrantInfo).toBeCalledWith(
+        "name",
+        restautrant.name
+      );
+    });
+
+    it("종류 입력창에 입력 시, handleChangeRestautrantInfo 를 type 과 입력 값을 함께 호출한다", () => {
+      const { getByPlaceholderText } = renderComponent(restautrant);
+
+      fireEvent.change(getByPlaceholderText(/종류/), {
+        target: { value: restautrant.type },
+      });
+
+      expect(handleChangeRestautrantInfo).toBeCalledWith(
+        "type",
+        restautrant.type
+      );
+    });
+
+    it("주소 입력창에 입력 시, handleChangeRestautrantInfo 를 address 와 입력 값을 함께 호출한다", () => {
+      const { getByPlaceholderText } = renderComponent(restautrant);
+
+      fireEvent.change(getByPlaceholderText(/주소/), {
+        target: { value: restautrant.address },
+      });
+
+      expect(handleChangeRestautrantInfo).toBeCalledWith(
+        "address",
+        restautrant.address
+      );
+    });
   });
 });
