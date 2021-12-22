0b76962e9fa621b092e9724d9d1c449c05d1de66
emewjin <emewjin@gmail.com>
2021-12-22 22:54:11 +0900

    up: 헤딩 텍스트를 추가하고 ul 태그 추가

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import ListContainer from './ListContainer';
 export default function App() {
   return (
     <>
+      <h1>맛집 목록</h1>
       <InputContainer />
       <ListContainer />
     </>
--- src/ListContainer.jsx
+++ src/ListContainer.jsx
@@ -1,17 +1,15 @@
 import { useSelector } from 'react-redux';
 
 export default function ListContainer() {
-  const {
-    restaurants,
-  } = useSelector((state) => ({
+  const { restaurants } = useSelector((state) => ({
     restaurants: state.restaurants,
   }));
 
   return (
-    restaurants.map(({
-      id, title, address, category,
-    }) => (
-      <li key={id}>{`${title} | ${address} | ${category}`}</li>
-    ))
+    <ul>
+      {restaurants.map(({ id, title, address, category }) => (
+        <li key={id}>{`${title} | ${address} | ${category}`}</li>
+      ))}
+    </ul>
   );
 }
