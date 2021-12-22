5e49d8d718bd9101df2c49e9e34a0602ef2d1597
emewjin <emewjin@gmail.com>
2021-12-22 22:43:37 +0900

    up: ListContainer의 정상 렌더링을 확인한다

--- src/ListContainer.test.jsx
+++ src/ListContainer.test.jsx
@@ -1,4 +1,21 @@
-test('ListContainer', () => {
-  const x = 1;
-  expect(x).toBe(1);
+import { useSelector } from 'react-redux';
+import { render } from '@testing-library/react';
+
+import ListContainer from './ListContainer';
+
+jest.mock('react-redux');
+
+describe('ListContainer', () => {
+  useSelector.mockImplementation((selector) => selector({
+    restaurants: [{
+      id: 1, title: '알단테', address: '광교', category: '양식',
+    }],
+  }));
+
+  it('ListContainer 컴포넌트가 잘 렌더링 된다', () => {
+    const { getByText } = render(<ListContainer />);
+    expect(getByText(/알단테/)).not.toBeNull();
+    expect(getByText(/광교/)).not.toBeNull();
+    expect(getByText(/양식/)).not.toBeNull();
+  });
 });
