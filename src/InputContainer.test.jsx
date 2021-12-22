38ea477905756679fe129ee2b8d0453fcc310f52
emewjin <emewjin@gmail.com>
2021-12-22 22:23:36 +0900

    up: updateRestaurantInfo가 잘 호출되는지 테스트하는 코드 추가

--- src/InputContainer.test.jsx
+++ src/InputContainer.test.jsx
@@ -17,8 +17,22 @@ describe('InputContainer', () => {
     },
   }));
 
-  it('updateRestaurantInfo가 호출된다', () => {
+  it('InputContainer 컴포넌트가 잘 렌더링 된다', () => {
     const { getByText } = render(<InputContainer />);
     expect(getByText(/등록/)).not.toBeNull();
+  });
+
+  it('updateRestaurantInfo가 호출된다', () => {
+    const { getByPlaceholderText } = render(<InputContainer />);
+
+    fireEvent.change(getByPlaceholderText(/이름/), { target: { value: '맥도날드' } });
+
+    expect(dispatch).toHaveBeenCalledWith({
+      type: 'updateRestaurantInfo',
+      payload: {
+        name: 'title',
+        value: '맥도날드',
+      },
+    });
   });
 });
