f19797ff84d780190df7f510202809ea2d2a7b39
KIMSEUNGGYU <45627868+KIMSEUNGGYU@users.noreply.github.com>
2021-12-22 20:17:44 +0900

    [feat] input 컴포넌트 value 추 가

--- src/Input.jsx
+++ src/Input.jsx
@@ -1,9 +1,11 @@
-export default function Input() {
+export default function Input({ restautrant }) {
+  const { name, type, address } = restautrant;
+
   return (
     <>
-      <input placeholder="이름" />
-      <input placeholder="분류" />
-      <input placeholder="주소" />
+      <input placeholder="이름" value={name} />
+      <input placeholder="분류" value={type} />
+      <input placeholder="주소" value={address} />
       <button>추가</button>
     </>
   );
