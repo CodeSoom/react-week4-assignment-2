7d228c4c6f5152de7aa0f4dadd2adff7618abb93
KIMSEUNGGYU <45627868+KIMSEUNGGYU@users.noreply.github.com>
2021-12-22 20:12:17 +0900

    [feat] Input 컴포넌트 UI 기능 구현

---
+++ src/Input.jsx
@@ -0,0 +1,10 @@
+export default function Input() {
+  return (
+    <>
+      <input placeholder="이름" />
+      <input placeholder="분류" />
+      <input placeholder="주소" />
+      <button>추가</button>
+    </>
+  );
+}
