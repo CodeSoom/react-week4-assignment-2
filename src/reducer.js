a70b3e6640292c6bc07dbe9cf6902195de83fbd8
emewjin <emewjin@gmail.com>
2021-12-22 23:07:31 +0900

    up: reducer 리팩토링 (함수분리)

--- src/reducer.js
+++ src/reducer.js
@@ -8,37 +8,46 @@ const initialState = {
   },
 };
 
+function addRestaurants(state) {
+  const values = Object.values(state.restaurantInfo);
+  const isAllValuesEmpty = values.some((value) => value === '');
+
+  if (isAllValuesEmpty) {
+    return state;
+  }
+
+  return {
+    ...state,
+    newId: state.newId + 1,
+    restaurants: [
+      ...state.restaurants,
+      { id: state.newId, ...state.restaurantInfo },
+    ],
+    restaurantInfo: {
+      title: '',
+      address: '',
+      category: '',
+    },
+  };
+}
+
+function updateRestaurantInfo(state, action) {
+  return {
+    ...state,
+    restaurantInfo: {
+      ...state.restaurantInfo,
+      [action.payload.name]: action.payload.value,
+    },
+  };
+}
+
 export default function reducer(state = initialState, action) {
   if (action.type === 'addRestaurants') {
-    const values = Object.values(state.restaurantInfo);
-    const isAllValuesEmpty = values.some((value) => value === '');
-    if (isAllValuesEmpty) {
-      return state;
-    }
-
-    return {
-      ...state,
-      newId: state.newId + 1,
-      restaurants: [
-        ...state.restaurants,
-        { id: state.newId, ...state.restaurantInfo },
-      ],
-      restaurantInfo: {
-        title: '',
-        address: '',
-        category: '',
-      },
-    };
+    return addRestaurants(state);
   }
 
   if (action.type === 'updateRestaurantInfo') {
-    return {
-      ...state,
-      restaurantInfo: {
-        ...state.restaurantInfo,
-        [action.payload.name]: action.payload.value,
-      },
-    };
+    return updateRestaurantInfo(state, action);
   }
   return state;
 }
