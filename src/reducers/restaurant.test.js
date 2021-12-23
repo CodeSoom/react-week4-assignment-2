6ae417693a23b9c283db8477c88bc370c2839285
wallababa <ansrlansrl@naver.com>
2021-12-23 21:38:43 +0900

    restaurantReducer의 action을 전달하지 않는 경우의 테스트와 구현

--- src/reducers/restaurant.test.js
+++ src/reducers/restaurant.test.js
@@ -4,4 +4,11 @@ describe('restaurantReducer', () => {
   it('아무 값도 전달하지 않는다면 initialState를 반환합니다.', () => {
     expect(restaurantReducer()).toEqual(initialState);
   });
+
+  it('action을 전달하지 않는다면 이전 상태를 반환합니다.', () => {
+    const prevState = restaurantReducer();
+    const nextState = restaurantReducer(prevState);
+
+    expect(nextState).toEqual(prevState);
+  });
 });
