28830aacbabaf785091dcff850c9a4a4b43cbc59
jeongseop.lee <sjl0614@naver.com>
2021-12-23 21:42:06 +0900

    :white_check_mark: App input 테스트 통과

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ export default function App() {
   return (
     <div>
       <h1>Restaurant</h1>
+      <input type="text" placeholder="이름" />
+      <input type="text" placeholder="분류" />
+      <input type="text" placeholder="주소" />
     </div>
   );
 }
