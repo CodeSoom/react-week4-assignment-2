6fce383eca9ffff038a9ef5a930bfa84023b3ff9
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2021-12-23 19:42:36 +0900

    [test] Input 컨테이너 테스트 추가 - 입력한 경우 - 추가 버튼 클릭한 경우

--- src/InputsContainer.test.jsx
+++ src/InputsContainer.test.jsx
@@ -1,17 +1,12 @@
-// 여러 개의 정보를 입력할 수 있다. => dispatch 요청
-// => ? 얘는 여러개인데..
-// "등록" 버튼을 클릭 할 수 있다. => dispatch 요청
-
-// 이름 / 분류 / 주소
-// 마녀주방 / 한식 / 서울시 강남구
-
 import { fireEvent, render } from '@testing-library/react';
 
 import { useDispatch } from 'react-redux';
 
 import InputsContainer from './InputsContainer';
 
-import { updateRestaurantInfo } from './actions';
+import { updateRestaurantInfo, addRestaurant } from './actions';
+
+import { restautrant } from '../fixture/restautrants';
 
 jest.mock('react-redux');
 
@@ -19,18 +14,45 @@ describe('InputsContainer', () => {
   const dispatch = jest.fn();
   useDispatch.mockImplementation(() => dispatch);
 
-  it('여러 input 의 값을 입력하면, 입력한 input 의 key 와 value 와 함께 dispatch 를 요청한다.', () => {
-    const { getByPlaceholderText } = render(<InputsContainer />);
+  const renderComponent = () => render(
+    <InputsContainer />,
+  );
 
-    const nameInput = getByPlaceholderText(/이름/);
-    const typeInput = getByPlaceholderText(/분류/);
-    const addressInput = getByPlaceholderText(/주소/);
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
 
-    // 마녀주방 / 한식 / 서울시 강남구
-    fireEvent.change(nameInput, { target: { value: '마녀주방' } });
-    fireEvent.change(typeInput, { target: { value: '한식' } });
-    fireEvent.change(addressInput, { target: { value: '서울시 강남구' } });
+  context('여러 input 의 값을 입력하면', () => {
+    it('name input 의 key 와 value 와 함께 dispatch 를 요청한다.', () => {
+      const { getByPlaceholderText } = renderComponent();
 
-    expect(dispatch).toBeCalledWith(updateRestaurantInfo('key', 'value'));
+      fireEvent.change(getByPlaceholderText(/이름/), { target: { value: restautrant.name } });
+
+      expect(dispatch).toBeCalledWith(updateRestaurantInfo('name', restautrant.name));
+    });
+
+    it('type input 의 key 와 value 와 함께 dispatch 를 요청한다.', () => {
+      const { getByPlaceholderText } = renderComponent();
+
+      fireEvent.change(getByPlaceholderText(/분류/), { target: { value: restautrant.type } });
+
+      expect(dispatch).toBeCalledWith(updateRestaurantInfo('type', restautrant.type));
+    });
+
+    it('type input 의 key 와 value 와 함께 dispatch 를 요청한다.', () => {
+      const { getByPlaceholderText } = renderComponent();
+
+      fireEvent.change(getByPlaceholderText(/주소/), { target: { value: restautrant.address } });
+
+      expect(dispatch).toBeCalledWith(updateRestaurantInfo('address', restautrant.address));
+    });
+  });
+
+  it('"추가" 버튼을 클릭 하면 디스패치를 이용하여 addRestaurant 을 요청한다', () => {
+    const { getByRole } = renderComponent();
+
+    fireEvent.click(getByRole('button', { name: '추가' }));
+
+    expect(dispatch).toBeCalled(addRestaurant());
   });
 });
