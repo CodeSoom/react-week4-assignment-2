510274c0451ba0b13c972f5a1ddbd4dec7d61bf4
PAPICO <ocipap0531@gmail.com>
2021-12-23 12:52:17 +0900

    리스트 페이지 식당 목록 렌더링 테스트 작성

--- test/ListContainer.test.jsx
+++ test/ListContainer.test.jsx
@@ -1,10 +1,28 @@
+import { useSelector } from 'react-redux';
+import { render } from '@testing-library/react';
 import ListContainer from '../src/ListContainer';
 
+jest.mock('react-redux');
+
 describe('ListContainer', () => {
-  const renderComponent = () => <ListContainer />;
+  useSelector.mockImplementation((selector) => selector({
+    restaurants: [{
+      name: '마녀주방',
+      category: '한식',
+      address: '경기도',
+    }],
+  }));
+
+  const renderComponent = () => render(<ListContainer />);
   it('ListContainer 가 렌더링 된다.', () => {
     const { container } = renderComponent();
 
     expect(container).not.toBeNull();
+  });
+
+  it('식당 목록이 있다면, 식당 목록이 화면에 뿌려진다.', () => {
+    const { getAllByRole } = renderComponent();
+
+    expect(getAllByRole('listitem')).toHaveLength(1);
   });
 });
