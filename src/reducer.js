949681fe347c8a265faef87f0bc958c33763d2cb
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2021-12-23 20:20:01 +0900

    [feat] reducer 레스토랑 추가 기능 구현

--- src/reducer.js
+++ src/reducer.js
@@ -1,4 +1,4 @@
-const initialState = {
+export const initialState = {
   id: 100,
   restautrants: [],
   restautrant: {
@@ -9,7 +9,7 @@ const initialState = {
 };
 
 // TODO: updateRestautrantInfo 리듀서 기능 정의
-export default function useReducer(state = initialState, action = {}) {
+export default function reducer(state = initialState, action = {}) {
   if (action.type === 'updateRestautrantInfo') {
     const { key, value } = action.payload;
 
@@ -19,6 +19,21 @@ export default function useReducer(state = initialState, action = {}) {
         ...state.restautrant,
         [key]: value,
       },
+    };
+  }
+
+  if (action.type === 'addRestaurant') {
+    const { restautrants, restautrant } = state;
+    const { name, type, address } = restautrant;
+
+    if (!(name && type && address)) {
+      return state;
+    }
+
+    return {
+      ...state,
+      restautrants: restautrants.concat(restautrant),
+      restautrant: initialState.restautrant,
     };
   }
 
