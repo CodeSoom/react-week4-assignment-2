b82b6d73086c223fa09ed4113961a40472ea9520
wallababa <ansrlansrl@naver.com>
2021-12-23 23:21:27 +0900

    RestaurantPage 값을 다 채우지 않은 경우 테스트 작성

---
+++ src/pages/RestaurantPage.test.jsx
@@ -0,0 +1,15 @@
+import { render, fireEvent } from '@testing-library/react';
+import RestaurantPage from './RestaurantPage';
+
+describe('RestaurantPage', () => {
+  it('레스토랑의 이름, 분류, 주소 중 하나 이상이 비어있다면 alert를 호출합니다.', () => {
+    const spy = jest.spyOn(window, 'alert');
+    const sut = render(<RestaurantPage />);
+
+    fireEvent.change(sut.getByLabelText('이름'), { target: { value: 'foo' } });
+    fireEvent.change(sut.getByLabelText('분류'), { target: { value: 'foo' } });
+    fireEvent.click(sut.getByRole('button', { name: '등록' }));
+
+    expect(spy).toBeCalledTimes(1);
+  });
+});
