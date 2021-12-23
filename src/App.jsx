136baeba0835fd960e2f7570d0e5a7896b215426
CheolWoong Choi <bluesky4381@naver.com>
2021-12-24 00:13:37 +0900

    Input 테스트를 통과시키도록 App, Input 컴포넌트 코드 작성

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
+import { useState } from 'react';
+import Input from './Input';
+
 export default function App() {
+  const [restaurant, setRestaurant] = useState({
+    name: '',
+    category: '',
+    address: '',
+  });
+
+  const handleChangeRestaurant = (e) => {
+    setRestaurant({
+      ...restaurant,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  const handleClickAddButton = () => {
+
+  }
+
   return (
     <div>
       <h1>Restaurants</h1>
+      <Input
+        restaurant={restaurant}
+        onChange={handleChangeRestaurant}
+        onClick={handleClickAddButton}
+      />
     </div>
   );
 }
--- src/Input.jsx
+++ src/Input.jsx
@@ -1,10 +1,32 @@
-export default function Input() {
+export default function Input({
+  restaurant, onChange, onClick,
+}) {
+  const { name, category, address } = restaurant;
+
   return (
     <div>
-      <input type="text" placeholder="이름" />
-      <input type="text" placeholder="분류" />
-      <input type="text" placeholder="주소" />
-      <button type="button">등록</button>
+      <input
+        name="name"
+        type="text"
+        placeholder="이름"
+        onChange={onChange}
+        value={name}
+      />
+      <input
+        name="category"
+        type="text"
+        placeholder="분류"
+        onChange={onChange}
+        value={category}
+      />
+      <input
+        name="address"
+        type="text"
+        placeholder="주소"
+        value={address}
+        onChange={onChange}
+      />
+      <button type="button" onClick={onClick}>등록</button>
     </div>
   );
 }
