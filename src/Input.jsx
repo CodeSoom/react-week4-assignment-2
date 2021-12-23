1fec2b2d62ea7f24e5a4fa767e6e94f7605ea073
GUAJEGUICHAN <qkr135qkr@gmail.com>
2021-12-23 15:19:07 +0900

    [chore] 불필요한 줄 제거

--- src/Input.jsx
+++ src/Input.jsx
@@ -12,7 +12,6 @@ export default function RestaurantInfoInput({
         onChange={onNameChange}
         value={name}
       />
-
       <input
         type="text"
         name="category"
@@ -21,7 +20,6 @@ export default function RestaurantInfoInput({
         onChange={onCategoryChange}
         value={category}
       />
-
       <input
         type="text"
         name="address"
@@ -30,7 +28,6 @@ export default function RestaurantInfoInput({
         onChange={onAddressChange}
         value={address}
       />
-
       <input
         type="button"
         value="등록"
