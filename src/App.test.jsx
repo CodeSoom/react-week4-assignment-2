50c8e8b4e7644f9f3c5edbb0f918e874d22d609b
jeongseop.lee <sjl0614@naver.com>
2021-12-23 21:38:39 +0900

    :sparkles: App input 테스트 추가

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -3,9 +3,12 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 test('App', () => {
-  const { getByText } = render((
+  const { getByText, getByPlaceholderText } = render((
     <App />
   ));
 
   expect(getByText(/Restaurant/)).not.toBeNull();
+  expect(getByPlaceholderText(/이름/)).not.toBeNull();
+  expect(getByPlaceholderText(/분류/)).not.toBeNull();
+  expect(getByPlaceholderText(/주소/)).not.toBeNull();
 });
