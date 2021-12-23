df3f96eabda31c9d9f81706620e2f7cd2bf2c73c
wallababa <ansrlansrl@naver.com>
2021-12-23 21:57:16 +0900

    restaurantReducer의 addRestaurant action 적용 및 구현

--- src/reducers/restaurant.js
+++ src/reducers/restaurant.js
@@ -1,10 +1,21 @@
+import { TYPE_ADD_RESTAURANT } from '../actions/restaurant';
+
 export const initialState = {
   lastId: -1,
   restaurants: [],
 };
 
-const restaurantReducer = () => {
-  return initialState;
+const restaurantReducer = (state = initialState, action = {}) => {
+  if (action.type === TYPE_ADD_RESTAURANT) {
+    const newId = state.lastId + 1;
+    return {
+      ...state,
+      lastId: newId,
+      restaurants: [...state.restaurants, action.payload],
+    };
+  }
+
+  return state;
 };
 
 export default restaurantReducer;
