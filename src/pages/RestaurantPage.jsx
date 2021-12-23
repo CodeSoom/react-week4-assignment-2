626c3df194ed7d0317eb84f8d1e59f1e437adcbe
wallababa <ansrlansrl@naver.com>
2021-12-23 23:29:21 +0900

    RestaurantPage 값을 다 채우지 않은 경우 구현 및 테스트 리팩토링

--- src/pages/RestaurantPage.jsx
+++ src/pages/RestaurantPage.jsx
@@ -1,11 +1,42 @@
+/* eslint-disable no-alert */
+import { useState } from 'react';
 import RestaurantInput from '../components/RestaurantInput';
 import RestaurantList from '../components/RestaurantList';
 
 const RestaurantPage = () => {
+  const [name, setName] = useState('');
+  const handleChangeName = ({ target: { value } }) => {
+    setName(value);
+  };
+
+  const [category, setCategory] = useState('');
+  const handleChangeCategory = ({ target: { value } }) => {
+    setCategory(value);
+  };
+
+  const [address, setAddress] = useState('');
+  const handleChangeAddressName = ({ target: { value } }) => {
+    setAddress(value);
+  };
+
+  const handleClick = () => {
+    if (!name || !category || !address) {
+      alert('');
+    }
+  };
+
   return (
     <div>
       <h1>Restaurants</h1>
-      <RestaurantInput />
+      <RestaurantInput
+        valueName={name}
+        onChangeName={handleChangeName}
+        valueCategory={category}
+        onChangeCategory={handleChangeCategory}
+        valueAddress={address}
+        onChangeAddress={handleChangeAddressName}
+        onClick={handleClick}
+      />
       <RestaurantList />
     </div>
   );
--- src/pages/RestaurantPage.test.jsx
+++ src/pages/RestaurantPage.test.jsx
@@ -4,6 +4,7 @@ import RestaurantPage from './RestaurantPage';
 describe('RestaurantPage', () => {
   it('레스토랑의 이름, 분류, 주소 중 하나 이상이 비어있다면 alert를 호출합니다.', () => {
     const spy = jest.spyOn(window, 'alert');
+    spy.mockImplementation(() => {}); // Error: Not implemented: window.alert를 해결하기 위해
     const sut = render(<RestaurantPage />);
 
     fireEvent.change(sut.getByLabelText('이름'), { target: { value: 'foo' } });
