65118b0d1bd1dde90cd6bf44376439944461fd9c
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2021-12-23 19:26:52 +0900

    [feat] input change 핸들 기능 구현

--- src/Input.jsx
+++ src/Input.jsx
@@ -1,24 +1,35 @@
-export default function Input({ restautrant, onChange }) {
+export default function Input({ restautrant, onChange, onClick }) {
   const { name, type, address } = restautrant;
 
+  const handleChange = ({ target }) => {
+    onChange(target.name, target.value);
+  };
+
   return (
-    <>
+    <p>
       <input
+        id="input-task-title"
+        name="name"
+        type="text"
         placeholder="이름"
         value={name}
-        onChange={onChange}
+        onChange={handleChange}
       />
       <input
+        type="text"
+        name="type"
         placeholder="분류"
         value={type}
-        onChange={onChange}
+        onChange={handleChange}
       />
       <input
+        type="text"
+        name="address"
         placeholder="주소"
         value={address}
-        onChange={onChange}
+        onChange={handleChange}
       />
-      <button type="button">추가</button>
-    </>
+      <button type="button" onClick={onClick}>추가</button>
+    </p>
   );
 }
