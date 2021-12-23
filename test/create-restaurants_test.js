c3b076f046a1b40d4b95b817914307c41a897893
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2021-12-23 23:01:24 +0900

    [test] e2e 테스트 코드 수정 - 개발한 내용과 e2e 테스트의 속성 정보가 달라 수정

--- test/create-restaurants_test.js
+++ test/create-restaurants_test.js
@@ -2,22 +2,22 @@ Feature('Create restaurant');
 
 const restaurant = {
   name: '마녀주방',
-  category: '한식',
+  type: '한식',
   address: '서울시 강남구',
 };
 
 Scenario('이름, 분류 그리고 주소를 입력한 후 확인을 누르면 레스토랑이 추가가 됩니다.', ({ I }) => {
   I.amOnPage('/');
 
-  const { name, category, address } = restaurant;
+  const { name, type, address } = restaurant;
 
   I.fillField('input[name=name]', name);
-  I.fillField('input[name=category]', category);
+  I.fillField('input[name=type]', type);
   I.fillField('input[name=address]', address);
 
-  I.click('등록');
+  I.click('추가');
 
   I.see(name);
-  I.see(category);
+  I.see(type);
   I.see(address);
 });
