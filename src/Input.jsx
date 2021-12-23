64f5b68e5451ae6fae1903f42d0be2b85835710c
CheolWoong Choi <bluesky4381@naver.com>
2021-12-23 23:21:22 +0900

    Input 테스트 코드 작성 #1
    - placeholder 텍스트를 가진 input 유무 테스트
    - 특정 텍스트를 가진 버튼 유무 테스트

--- src/Input.jsx
+++ src/Input.jsx
@@ -1,5 +1,10 @@
 export default function Input() {
   return (
-    <div>Input</div>
+    <div>
+      <input type="text" placeholder="이름" />
+      <input type="text" placeholder="분류" />
+      <input type="text" placeholder="주소" />
+      <button type="button">등록</button>
+    </div>
   );
 }
--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -2,9 +2,27 @@ import { render } from '@testing-library/react';
 import Input from './Input';
 
 describe('Input', () => {
-  it('it renders "Input" text', () => {
+  it('has a input element with "이름" placeholder', () => {
+    const { getByPlaceholderText } = render(<Input />);
+
+    expect(getByPlaceholderText('이름')).not.toBeNull();
+  });
+
+  it('has a input element with "분류" placeholder', () => {
+    const { getByPlaceholderText } = render(<Input />);
+
+    expect(getByPlaceholderText('분류')).not.toBeNull();
+  });
+
+  it('has a input element with "주소" placeholder', () => {
+    const { getByPlaceholderText } = render(<Input />);
+
+    expect(getByPlaceholderText('주소')).not.toBeNull();
+  });
+
+  it('has a button with "등록" text', () => {
     const { getByText } = render(<Input />);
 
-    expect(getByText('Input')).not.toBeNull();
+    expect(getByText('등록')).not.toBeNull();
   });
 });
