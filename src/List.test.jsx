bd4e0fada4b9eb8c18522f938e83e07e84799f59
shongs27 <shongs27@naver.com>
2021-12-24 21:53:26 +0900

    Test : List.test - 리스트 state의 유무에 따라 화면 렌더링 테스트

---
+++ src/List.test.jsx
@@ -0,0 +1,30 @@
+import { render } from "@testing-library/react";
+
+import List from "./List";
+
+describe("List", () => {
+  context("without list", () => {
+    it("show a blank sheet", () => {
+      const state = [];
+      const { getByText } = render(<List lists={state} />);
+
+      expect(getByText(/Restaurant/).firstElementChild).toBeNull();
+    });
+  });
+
+  context("with list", () => {
+    it("show state lists", () => {
+      const state = [
+        {
+          name: "칠암아나고",
+          type: "수산업",
+          address: "부산시 기장군",
+        },
+      ];
+
+      const { getByText } = render(<List lists={state} />);
+
+      expect(getByText(/아나고/)).not.toBeNull();
+    });
+  });
+});
--- src/ListContainer.test.jsx
+++ src/ListContainer.test.jsx
@@ -13,35 +13,21 @@ jest.mock("react-redux", () => ({
 }));
 
 describe("ListContainer", () => {
-  context("without list", () => {
-    it("show a blank sheet", () => {
-      useSelector.mockImplementation((selector) =>
-        selector({ registered: [] })
-      );
+  it("render list content", () => {
+    useSelector.mockImplementation((selector) =>
+      selector({
+        registered: [
+          {
+            name: "칠암아나고",
+            type: "수산업",
+            address: "부산시 기장군",
+          },
+        ],
+      })
+    );
 
-      const { getByText, debug } = render(<ListContainer />);
+    const { getByText } = render(<ListContainer />);
 
-      expect(getByText(/Restaurant/).firstElementChild).toBeNull();
-    });
-  });
-
-  context("with list", () => {
-    it("render ", () => {
-      useSelector.mockImplementation((selector) =>
-        selector({
-          registered: [
-            {
-              name: "칠암아나고",
-              type: "수산업",
-              address: "부산시 기장군",
-            },
-          ],
-        })
-      );
-
-      const { getByText } = render(<ListContainer />);
-
-      expect(getByText(/아나고/)).not.toBeNull();
-    });
+    expect(getByText(/아나고/)).not.toBeNull();
   });
 });
