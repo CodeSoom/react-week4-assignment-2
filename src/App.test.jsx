cbac478eb25bd258fd55930d124a2cbd5af3296d
Ravi <ravi@inflearn.com>
2021-12-25 00:49:14 +0900

    테스트 코드 수정

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,10 +1,17 @@
 import { render } from '@testing-library/react';
+import { useSelector } from 'react-redux';
 
 import App from './App';
 
 describe('App', () => {
+  useSelector.mockImplementation((selector) => selector({
+    restaurants: [],
+  }));
+
   it('renders title', () => {
     const { getByRole } = render(<App />);
     expect(getByRole('heading')).toHaveTextContent('Restaurants');
   });
+
+  // TODO: 통합 테스트
 });
--- src/RestaurantListContainer/RestaurantList.test.jsx
+++ src/RestaurantListContainer/RestaurantList.test.jsx
@@ -7,7 +7,7 @@ describe('RestaurantList', () => {
     const restaurant = {
       id: 1, name: '김밥천국', category: '분식', address: '서울시 서초구 서초대로',
     };
-    const { getByText } = render(<RestaurantList list={[restaurant]} />);
+    const { getByText } = render(<RestaurantList restaurants={[restaurant]} />);
 
     expect(getByText(restaurant.name)).toBeInTheDocument();
     expect(getByText(restaurant.category)).toBeInTheDocument();
