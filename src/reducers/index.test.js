743b0aaad9c6ae9e5b1fe10d1378ab8020642fdb
CheolWoong Choi <bluesky4381@naver.com>
2021-12-24 22:51:23 +0900

    reducer 테스트 코드 작성해보기
    - TDD 사이클에서 RED에 해당

--- src/reducers/index.test.js
+++ src/reducers/index.test.js
@@ -1,5 +1,41 @@
+import reducer from './index';
+
+import {
+  updateRestaurant,
+  addRestaurant,
+} from '../actions';
+
 describe('reducer', () => {
-  it('', () => {
-    expect();
+  const initialState = {
+    restaurants: [],
+    restaurant: {
+      name: '',
+      category: '',
+      address: '',
+    },
+  };
+
+  context('updateRestaurant', () => {
+    it('식당 이름을 입력하면, name에 값이 적용된다.', () => {
+      const state = reducer(initialState, updateRestaurant);
+    });
+
+    it('식당 분류를 입력하면, category에 값이 적용된다.', () => {
+      const state = reducer(initialState, updateRestaurant);
+    });
+
+    it('식당 주소를 입력하면, address에 값이 적용된다.', () => {
+      const state = reducer(initialState, updateRestaurant);
+    });
+  });
+
+  context('addRestaurant', () => {
+    it('식당 정보를 입력하면, restaurants에 해당 정보가 추가된다.', () => {
+      const state = reducer(initialState, addRestaurant);
+    });
+
+    it('식당 정보를 입력하면, restaurants 배열 길이가 1만큼 증가한다.', () => {
+      const state = reducer(initialState, addRestaurant);
+    });
   });
 });
