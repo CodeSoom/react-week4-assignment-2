be9fc70955e486f496907ad950e8c16afe2f350c
PAPICO <ocipap0531@gmail.com>
2021-12-24 20:07:02 +0900

    Input props 구현

--- src/Input.jsx
+++ src/Input.jsx
@@ -1,9 +1,9 @@
-export default function Input() {
+export default function Input({ name, category, address }) {
   return (
     <div>
-      <input type="text" name="name" placeholder="이름" />
-      <input type="text" name="category" placeholder="분류" />
-      <input type="text" name="address" placeholder="주소" />
+      <input type="text" name="name" placeholder="이름" value={name} readOnly />
+      <input type="text" name="category" placeholder="분류" value={category} readOnly />
+      <input type="text" name="address" placeholder="주소" value={address} readOnly />
       <button type="button">등록</button>
     </div>
   );
