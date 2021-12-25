e9284815e88b391eaa2462c368920c379e32190a
jeongseop.lee <sjl0614@naver.com>
2021-12-25 20:41:10 +0900

    RestaurantCreatecontainer dispatch 테스트 작성

--- src/RestaurantCreateContainer.test.jsx
+++ src/RestaurantCreateContainer.test.jsx
@@ -1,12 +1,16 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import RestaurantCreateContainer from './RestaurantCreateContainer';
 
 jest.mock('react-redux');
 
 test('RestaurantCreateContainer', () => {
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
+
   useSelector.mockImplementation((selector) => selector({
   }));
 
@@ -15,4 +19,10 @@ test('RestaurantCreateContainer', () => {
   ));
 
   expect(getByText(/등록/)).not.toBeNull();
+
+  fireEvent.click(getByText(/등록/));
+
+  expect(dispatch).toBeCalled({
+    type: 'addRestaurant',
+  });
 });
