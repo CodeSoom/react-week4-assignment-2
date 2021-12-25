1c012b1b5dcf1a134339c79bac1f9e31496eef6a
CheolWoong Choi <bluesky4381@naver.com>
2021-12-25 22:02:21 +0900

    ListContainer 테스트 코드 작성

--- src/containers/ListContainer.test.jsx
+++ src/containers/ListContainer.test.jsx
@@ -1,30 +1,39 @@
-import { render, fireEvent } from '@testing-library/react';
-import { useSelector, useDispatch } from 'react-redux';
+import { render } from '@testing-library/react';
+import { useSelector } from 'react-redux';
 
 import ListContainer from './ListContainer';
 
 jest.mock('react-redux');
 
 describe('ListContainer', () => {
-  const dispatch = jest.fn();
-
+  const restaurants = [];
   const renderListContainer = () => render(<ListContainer />);
 
-  beforeEach(() => {
-    jest.clearAllMocks();
+  useSelector.mockImplementation((selector) => selector({
+    restaurants,
+  }));
 
-    useSelector.mockImplementation((selector) => selector({
-      restaurants: [],
-      restaurant: {
-        name: '',
-        category: '',
-        address: '',
-      },
-    }));
-    useDispatch.mockImplementation(() => dispatch);
+  context('식당 정보가 없으면', () => {
+    it('아무 것도 화면에 나타나지 않는다.', () => {
+      const { container } = renderListContainer();
+
+      expect(container).toHaveTextContent('');
+    });
   });
 
-  it('', () => {
+  context('식당 정보가 있으면', () => {
+    it('식당 정보들이 화면에 나타난다.', () => {
+      restaurants.push({
+        name: '이름-1',
+        category: '분류-1',
+        address: '주소-1',
+      });
 
+      const { queryByText } = renderListContainer();
+
+      expect(queryByText(/이름-1/)).not.toBeNull();
+      expect(queryByText(/분류-1/)).not.toBeNull();
+      expect(queryByText(/주소-1/)).not.toBeNull();
+    });
   });
 });
