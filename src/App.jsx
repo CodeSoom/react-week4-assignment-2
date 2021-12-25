78d790d19734a778de9b51a194e8d98971ca804a
padawanR0k <padawanR0k@gmail.com>
2021-12-25 13:44:01 +0900

    new: 기본구조 퍼블리싱

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 export default function App() {
   return (
     <div>
-      Hello
+      <h1>Restaurants</h1>
+      <ul />
+      <div>
+        <input id="title" type="text" />
+        <input id="menu" type="text" />
+        <input id="address" type="text" />
+        <button type="button">등록</button>
+      </div>
     </div>
   );
 }
