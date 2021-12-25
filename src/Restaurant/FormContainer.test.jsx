3a9cb49ede13ceeaad6f8b41e2c97bc9793f87fa
padawanR0k <padawanR0k@gmail.com>
2021-12-25 19:13:45 +0900

    new: FormContainer test작성

---
+++ src/Restaurant/FormContainer.test.jsx
@@ -0,0 +1,34 @@
+import { render, fireEvent } from '@testing-library/react';
+import { useDispatch } from 'react-redux';
+
+import FormContainer from './FormContainer';
+import { addRestaurant } from '../actions';
+
+jest.mock('react-redux');
+
+describe('FormContainer', () => {
+  test('레스토랑을 추가한다.', () => {
+    const dispatch = jest.fn();
+    useDispatch.mockImplementation(() => dispatch);
+    const { getByText } = render(<FormContainer />);
+
+    fireEvent.click(getByText('등록'));
+    expect(dispatch).toBeCalledWith(addRestaurant(1));
+  });
+
+  test('레스토랑을 작성폼을 수정한다.', () => {
+    // const onChange = jest.fn();
+    // const { getByDisplayValue } = render(
+    //   <Form
+    //     onChange={onChange}
+    //     title="홍반장"
+    //     menu="중국집"
+    //     address="판교"
+    //   />,
+    // );
+
+    // expect(getByDisplayValue('홍반장')).not.toBeNull();
+    // expect(getByDisplayValue('중국집')).not.toBeNull();
+    // expect(getByDisplayValue('판교')).not.toBeNull();
+  });
+});
