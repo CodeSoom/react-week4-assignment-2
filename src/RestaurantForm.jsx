8daf182118216d61550ffefde4d88c64c78c6dd2
jeongseop.lee <sjl0614@naver.com>
2021-12-25 20:38:46 +0900

    RestaurantForm 테스트 통과

---
+++ src/RestaurantForm.jsx
@@ -0,0 +1,12 @@
+export default function RestaurantForm({ onClick }) {
+  return (
+    <div>
+      <button
+        type="button"
+        onClick={onClick}
+      >
+        등록
+      </button>
+    </div>
+  );
+}
