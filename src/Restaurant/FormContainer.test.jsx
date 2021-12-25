4fd07c296cf748f9b403d5434ec2b616872448ee
padawanR0k <padawanR0k@gmail.com>
2021-12-25 19:47:36 +0900

    fix: FormContainer 잘못된 테스트 수정

--- src/Restaurant/FormContainer.test.jsx
+++ src/Restaurant/FormContainer.test.jsx
@@ -2,22 +2,25 @@ import { render, fireEvent } from '@testing-library/react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import FormContainer from './FormContainer';
-import { addRestaurant } from '../actions';
+import { addRestaurant, changeForm } from '../actions';
 import { restaurant } from '../fixtures/restaurant';
 
 jest.mock('react-redux');
 
 describe('FormContainer', () => {
-  test('레스토랑을 추가한다.', () => {
-    const dispatch = jest.fn();
+  const dispatch = jest.fn();
+  beforeEach(() => {
     useDispatch.mockImplementation(() => dispatch);
     useSelector.mockImplementation((selector) => selector({
       restaurant,
     }));
+  });
+
+  test('레스토랑을 추가한다.', () => {
     const { getByText } = render(<FormContainer />);
 
     fireEvent.click(getByText('등록'));
-    expect(dispatch).toBeCalledWith(addRestaurant(1));
+    expect(dispatch).toBeCalledWith(addRestaurant(restaurant));
   });
 
   test('레스토랑을 작성폼을 수정한다.', () => {
