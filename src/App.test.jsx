21f173cd51bbde08e636a16a2397fdb0fcd1c14e
jeongseop.lee <sjl0614@naver.com>
2021-12-25 18:31:44 +0900

    :sparkles: App 레스토랑 제목 테스트 작성

---
+++ src/App.test.jsx
@@ -0,0 +1,11 @@
+import { render } from '@testing-library/react';
+
+import App from './App';
+
+test('App', () => {
+  const { getByText } = render((
+    <App />
+  ));
+
+  expect(getByText(/김밥제국/)).not.toBeNull();
+});
