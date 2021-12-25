af7c40be4b9d754246c46ed6fd0225c821b71e57
jeongseop.lee <sjl0614@naver.com>
2021-12-25 21:17:22 +0900

    reducer changeReataurantField 테스트 추가

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -2,6 +2,8 @@ import reducer from './reducer';
 
 import {
   setRestaurants,
+  addRestaurant,
+  changeRestaurantField,
 } from './actions';
 
 import restaurants from '../fixtures/restaurants';
@@ -16,6 +18,25 @@ describe('reducer', () => {
       const state = reducer(initialState, setRestaurants(restaurants));
 
       expect(state.restaurants).not.toHaveLength(0);
+    });
+  });
+
+  describe('changeRestaurantField', () => {
+    it('changes restaurants form', () => {
+      const initialState = {
+        restaurnat: {
+          name: '이름',
+          category: '분류',
+          address: '주소',
+        },
+      };
+
+      const state = reducer(initialState, changeRestaurantField({
+        name: 'name',
+        value: '마법사주방',
+      }));
+
+      expect(state.restaurant.name).toBe('마법사주방');
     });
   });
 });
