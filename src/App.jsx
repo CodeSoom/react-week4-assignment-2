c85904c1f8c39518279a0710a6f88e44c4312653
padawanR0k <padawanR0k@gmail.com>
2021-12-25 18:22:50 +0900

    up: Form 컴포넌트 분리

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
+import Form from './Restaurant/Form';
+
 export default function App() {
   return (
     <div>
       <h1>Restaurants</h1>
       <ul />
-      <div>
-        <input id="title" type="text" />
-        <input id="menu" type="text" />
-        <input id="address" type="text" />
-        <button type="button">등록</button>
-      </div>
+      <Form />
     </div>
   );
 }
---
+++ src/Restaurant/Form.jsx
@@ -0,0 +1,10 @@
+const Form = () => (
+  <div>
+    <input id="title" type="text" />
+    <input id="menu" type="text" />
+    <input id="address" type="text" />
+    <button type="button">등록</button>
+  </div>
+);
+
+export default Form;
