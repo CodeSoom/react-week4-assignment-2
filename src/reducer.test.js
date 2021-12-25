92b8de84ddb792c90394b76ea5c24b6995b90256
padawanR0k <padawanR0k@gmail.com>
2021-12-25 20:53:00 +0900

    new: reducer 테스트 추가

---
+++ src/reducer.test.js
@@ -0,0 +1,33 @@
+import reducer from './reducer';
+import { restaurant } from './fixtures/restaurant';
+import { addRestaurant, changeForm } from './actions';
+
+describe('reducer', () => {
+  test('addRestaurant', () => {
+    const initialState = {
+      newId: 1,
+      restaurants: [],
+    };
+
+    const state = reducer(initialState, addRestaurant({
+      title: '홍콩반점',
+      menu: '중식',
+      address: '서울',
+    }));
+
+    expect(state.restaurants).toHaveLength(1);
+    expect(state.restaurants[0].id).toBe(initialState.newId + 1);
+  });
+
+  test('changeForm', () => {
+    const initialState = {
+      restaurant,
+    };
+
+    const state = reducer(initialState, changeForm({
+      title: '뉴욕반점',
+    }));
+
+    expect(state.restaurant.title).toBe('뉴욕반점');
+  });
+});
