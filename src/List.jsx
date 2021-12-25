e9b642661a3b75135a1b8e8596206c1b683c1ab2
PAPICO <ocipap0531@gmail.com>
2021-12-25 17:35:50 +0900

    List 컴포넌트 제거 리펙토링

--- src/List.jsx
+++
@@ -1,9 +0,0 @@
-import Item from './Item';
-
-export default function List({ restaurants }) {
-  return (
-    <ul>
-      {restaurants.map((restaurant) => <Item key={restaurant.id} restaurant={restaurant} />)}
-    </ul>
-  );
-}
--- src/ListContainer.jsx
+++ src/ListContainer.jsx
@@ -1,11 +1,13 @@
 import { useSelector } from 'react-redux';
-import List from './List';
+import Item from './Item';
 
 export default function ListContainer() {
   const restaurants = useSelector((state) => state.restaurants);
   return (
     <>
-      <List restaurants={restaurants} />
+      <ul>
+        {restaurants.map((restaurant) => <Item key={restaurant.id} restaurant={restaurant} />)}
+      </ul>
     </>
   );
 }
