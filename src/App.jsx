728f982df0286f203d51dc0c658ed79949f95ba7
jeongseop.lee <sjl0614@naver.com>
2021-12-25 19:07:01 +0900

    :recycle: App 레스토랑 리스트 리팩터링

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 export default function App() {
+  const restaurants = [
+    {
+      id: 1, name: '김밥제국', category: '분식', address: '서울 강남구 역삼동',
+    },
+  ];
+
   return (
     <div>
       <h1>Restaurants</h1>
       <ul>
-        <li>
-          김밥제국
-        </li>
+        {
+          restaurants.map((restaurant) => (
+            <li key={restaurant.id}>
+              {restaurant.name}
+            </li>
+          ))
+        }
       </ul>
     </div>
   );
