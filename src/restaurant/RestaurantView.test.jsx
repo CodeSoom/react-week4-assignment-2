3bee45787278ca18ae8e71a8428e31354d8477ba
jojoldu <jojoldu@gmail.com>
2021-12-26 20:29:33 +0900

    RestaurantView 테스트 코드 추가

---
+++ src/restaurant/RestaurantView.test.jsx
@@ -0,0 +1,43 @@
+import { render, fireEvent } from '@testing-library/react';
+
+describe('RestaurantView', () => {
+  let onChangeName;
+  let onChangeCategory;
+  let onChangeAddress;
+  let onClickAdd;
+
+  beforeEach(() => {
+    onChangeName = jest.fn();
+    onChangeCategory = jest.fn();
+    onChangeAddress = jest.fn();
+    onClickAdd = jest.fn();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  function renderTestComponent(name = '', category = '', address = '') {
+    render((
+      <RestaurantView
+        name={name}
+        category={category}
+        address={address}
+        onChangeName={onChangeName}
+        onChangeCategory={onChangeCategory}
+        onChangeAddress={onChangeAddress}
+        onClickAdd={onClickAdd}
+      />
+    ));
+  }
+
+  it('이름이 변경되면 onChangeName 이 1회 호출된다', () => {
+    const { getByRole } = renderTestComponent();
+
+    fireEvent.change(getByRole('textbox', { name: /name/i }), {
+      target: { value: 'test' },
+    });
+
+    expect(onChangeName).toBeCalledTimes(1);
+  });
+});
