ed284bd29a91d98ea742bae2e73d4fb22c816737
jojoldu <jojoldu@gmail.com>
2021-12-26 20:35:52 +0900

    RestaurantView onChangeCategory 구현

--- src/restaurant/RestaurantView.jsx
+++ src/restaurant/RestaurantView.jsx
@@ -10,6 +10,7 @@ export default function RestaurantView({
   return (
     <form aria-label="form">
       <input type="text" aria-label="name" id="name" name="name" placeholder="이름" value={name} onChange={onChangeName} />
+      <input type="text" aria-label="category" id="category" name="category" placeholder="분류" value={category} onChange={onChangeCategory} />
     </form>
   );
 }
