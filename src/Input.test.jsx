bc38a66430baa47b8b1fa461136410dcaa1bfad3
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2021-12-26 19:27:08 +0900

    [test] 테스트 코드 수정

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -33,18 +33,12 @@ describe('Input', () => {
     it('전달 받은 값을 렌더링한다.', () => {
       const { getByPlaceholderText } = renderComponent(restautrant);
 
-      expect(getByPlaceholderText(/이름/)).toHaveAttribute(
-        'value',
-        restautrant.name,
-      );
-      expect(getByPlaceholderText(/분류/)).toHaveAttribute(
-        'value',
-        restautrant.type,
-      );
-      expect(getByPlaceholderText(/주소/)).toHaveAttribute(
-        'value',
-        restautrant.address,
-      );
+      expect(getByPlaceholderText(/이름/))
+        .toHaveAttribute('value', restautrant.name);
+      expect(getByPlaceholderText(/분류/))
+        .toHaveAttribute('value', restautrant.type);
+      expect(getByPlaceholderText(/주소/))
+        .toHaveAttribute('value', restautrant.address);
     });
 
     it('이름 입력창에 입력 시, handleChangeRestautrantInfo 를 name 과 입력 값을 함께 호출한다', () => {
@@ -54,10 +48,8 @@ describe('Input', () => {
         target: { value: restautrant.name },
       });
 
-      expect(handleChangeRestautrantInfo).toBeCalledWith(
-        'name',
-        restautrant.name,
-      );
+      expect(handleChangeRestautrantInfo)
+        .toBeCalledWith('name', restautrant.name);
     });
 
     it('종류 입력창에 입력 시, handleChangeRestautrantInfo 를 type 과 입력 값을 함께 호출한다', () => {
@@ -67,10 +59,8 @@ describe('Input', () => {
         target: { value: restautrant.type },
       });
 
-      expect(handleChangeRestautrantInfo).toBeCalledWith(
-        'type',
-        restautrant.type,
-      );
+      expect(handleChangeRestautrantInfo)
+        .toBeCalledWith('type', restautrant.type);
     });
 
     it('주소 입력창에 입력 시, handleChangeRestautrantInfo 를 address 와 입력 값을 함께 호출한다', () => {
@@ -80,10 +70,8 @@ describe('Input', () => {
         target: { value: restautrant.address },
       });
 
-      expect(handleChangeRestautrantInfo).toBeCalledWith(
-        'address',
-        restautrant.address,
-      );
+      expect(handleChangeRestautrantInfo)
+        .toBeCalledWith('address', restautrant.address);
     });
   });
 
