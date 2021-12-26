a994004e34a7fcfe76a8509ebaecb769366542c7
syaring <sarah.k.serim@gmail.com>
2021-12-26 18:21:44 +0900

    Add store, reducerm and actions

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,25 @@
+import { useEffect } from 'react';
+
+import { useDispatch } from 'react-redux';
+
 import RestaurantContainer from './RestaurantsContainer';
 
+import {
+  setRestaurants,
+} from './actions';
+
+import restaurants from '../fixtures/restaurants';
+
 export default function App() {
+  // TODO:
+  // 초기 실행 => useEffect
+  // restaurants에 데이터 넣기 => dispatch
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setRestaurants(restaurants));
+  }, []);
+
   return (
     <div>
       <h1>Restautants</h1>
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { render } from '@testing-library/react';
 
@@ -11,11 +11,19 @@ import restaurants from '../fixtures/restaurants';
 jest.mock('react-redux');
 
 test('App', () => {
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
   useSelector.mockImplementation((selector) => selector({ restaurants }));
 
   const { getByText } = render((
     <App />
   ));
+
+  expect(dispatch).toBeCalledWith({ // App 이 렌더가 되면 dispatch가 무조건 실행 되어야 함
+    type: 'setRestaurants',
+    payload: { restaurants },
+  });
 
   expect(getByText(/김밥제국/)).not.toBeNull();
 });
