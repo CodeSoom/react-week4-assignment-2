065be7d4afbe19bacc58b4a4656a56a6aaacec74
jojoldu <jojoldu@gmail.com>
2021-12-26 20:36:55 +0900

    RestaurantView onChangeAddress, onClickAdd 구현

--- src/restaurant/RestaurantView.jsx
+++ src/restaurant/RestaurantView.jsx
@@ -11,6 +11,8 @@ export default function RestaurantView({
     <form aria-label="form">
       <input type="text" aria-label="name" id="name" name="name" placeholder="이름" value={name} onChange={onChangeName} />
       <input type="text" aria-label="category" id="category" name="category" placeholder="분류" value={category} onChange={onChangeCategory} />
+      <input type="text" aria-label="address" id="address" name="address" placeholder="주소" value={address} onChange={onChangeAddress} />
+      <button type="button" onClick={onClickAdd}>등록</button>
     </form>
   );
 }
