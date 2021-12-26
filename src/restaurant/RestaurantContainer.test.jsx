4a15ee128d550fe3944d520bb973f9f4da843341
jojoldu <jojoldu@gmail.com>
2021-12-26 21:06:51 +0900

    RestaurantContainer onChangeCategory 테스트 추가

--- src/restaurant/RestaurantContainer.test.jsx
+++ src/restaurant/RestaurantContainer.test.jsx
@@ -25,4 +25,14 @@ describe('RestaurantContainer', () => {
 
     expect(dispatch).toBeCalledTimes(1);
   });
+
+  it('카테고리가 변경되면 dispatch 가 1회 호출된다', () => {
+    const { getByRole } = render(<RestaurantContainer />);
+
+    fireEvent.change(getByRole('textbox', { name: /category/i }), {
+      target: { value: 'test' },
+    });
+
+    expect(dispatch).toBeCalledTimes(1);
+  });
 });
