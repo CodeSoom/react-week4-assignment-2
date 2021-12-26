2a1897d427325ae7a4cc2ffac3563c2814b6c287
CheolWoong Choi <bluesky4381@naver.com>
2021-12-26 20:00:42 +0900

    ListContainer 테스트 코드에서 테스트별로 식당 정보를 다르게 제공

--- src/containers/ListContainer.test.jsx
+++ src/containers/ListContainer.test.jsx
@@ -6,12 +6,15 @@ import ListContainer from './ListContainer';
 jest.mock('react-redux');
 
 describe('ListContainer', () => {
-  const restaurants = [];
   const renderListContainer = () => render(<ListContainer />);
 
-  useSelector.mockImplementation((selector) => selector({
-    restaurants,
-  }));
+  beforeEach(() => {
+    jest.clearAllMocks();
+
+    useSelector.mockImplementation((selector) => selector({
+      restaurants: [],
+    }));
+  });
 
   context('식당 정보가 없으면', () => {
     it('아무 것도 화면에 나타나지 않는다.', () => {
@@ -23,11 +26,13 @@ describe('ListContainer', () => {
 
   context('식당 정보가 있으면', () => {
     it('식당 정보들이 화면에 나타난다.', () => {
-      restaurants.push({
-        name: '이름-1',
-        category: '분류-1',
-        address: '주소-1',
-      });
+      useSelector.mockImplementation((selector) => selector({
+        restaurants: [{
+          name: '이름-1',
+          category: '분류-1',
+          address: '주소-1',
+        }],
+      }));
 
       const { queryByText } = renderListContainer();
 
