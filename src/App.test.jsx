1c64f5a3cb33917d056d6d01939ff6d63a8621bc
CheolWoong Choi <bluesky4381@naver.com>
2021-12-26 19:35:34 +0900

    App 테스트 코드에서 불필요한 테스트 제거
    - Input 컨테이너와 List 컨테이너의 테스트에서 테스트를 진행하고 있기 때문에 제거

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,16 +1,12 @@
-import { render, fireEvent } from '@testing-library/react';
-import { useSelector, useDispatch } from 'react-redux';
+import { render } from '@testing-library/react';
+import { useSelector } from 'react-redux';
 
 import App from './App';
 
 jest.mock('react-redux');
 
 describe('App', () => {
-  const dispatch = jest.fn();
-
   beforeEach(() => {
-    jest.clearAllMocks();
-
     useSelector.mockImplementation((selector) => selector({
       restaurants: [],
       restaurant: {
@@ -19,7 +15,6 @@ describe('App', () => {
         address: '',
       },
     }));
-    useDispatch.mockImplementation(() => dispatch);
   });
 
   const renderApp = () => render(<App />);
@@ -30,67 +25,5 @@ describe('App', () => {
 
       expect(getByText('Restaurants')).not.toBeNull();
     });
-  });
-
-  context('Input에 대한 기능 테스트', () => {
-    /* it('"이름", "분류", "장소" input에 값을 입력하면, 화면에 해당 값들이 나타난다.', () => {
-      const { getByPlaceholderText, getByDisplayValue } = renderApp();
-      const restaurant = {
-        name: '마녀주방',
-        category: '한식',
-        address: '서울시 강남구',
-      };
-      const { name, category, address } = restaurant;
-
-      fireEvent.change(getByPlaceholderText(/이름/), { target: { value: name } });
-      fireEvent.change(getByPlaceholderText(/분류/), { target: { value: category } });
-      fireEvent.change(getByPlaceholderText(/주소/), { target: { value: address } });
-
-      expect(getByDisplayValue(name)).not.toBeNull();
-      expect(getByDisplayValue(category)).not.toBeNull();
-      expect(getByDisplayValue(address)).not.toBeNull();
-    }); */
-
-    it('"등록" 버튼을 누르면, "이름", "분류", "장소" input의 값이 초기화된다.', () => {
-      const { getByText, getByPlaceholderText, queryByDisplayValue } = renderApp();
-      const restaurant = {
-        name: '마녀주방',
-        category: '한식',
-        address: '서울시 강남구',
-      };
-      const { name, category, address } = restaurant;
-
-      fireEvent.change(getByPlaceholderText(/이름/), { target: { value: name } });
-      fireEvent.change(getByPlaceholderText(/분류/), { target: { value: category } });
-      fireEvent.change(getByPlaceholderText(/주소/), { target: { value: address } });
-
-      fireEvent.click(getByText(/등록/));
-
-      expect(queryByDisplayValue(name)).toBeNull();
-      expect(queryByDisplayValue(category)).toBeNull();
-      expect(queryByDisplayValue(address)).toBeNull();
-    });
-  });
-
-  context('List에 대한 기능 테스트', () => {
-    /* it('식당 정보를 등록하면, 화면에 식당 "이름", "분류", "장소"가 나타난다.', () => {
-      const { getByText, getByPlaceholderText } = renderApp();
-      const restaurant = {
-        name: '마녀주방',
-        category: '한식',
-        address: '서울시 강남구',
-      };
-      const { name, category, address } = restaurant;
-
-      fireEvent.change(getByPlaceholderText(/이름/), { target: { value: name } });
-      fireEvent.change(getByPlaceholderText(/분류/), { target: { value: category } });
-      fireEvent.change(getByPlaceholderText(/주소/), { target: { value: address } });
-
-      fireEvent.click(getByText(/등록/));
-
-      expect(getByText(/마녀주방/)).not.toBeNull();
-      expect(getByText(/한식/)).not.toBeNull();
-      expect(getByText(/서울시 강남구/)).not.toBeNull();
-    }); */
   });
 });
