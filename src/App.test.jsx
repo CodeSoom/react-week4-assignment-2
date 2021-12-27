8073c94c0b1509c601135612ac0de12591309544
jojoldu <jojoldu@gmail.com>
2021-12-27 15:57:46 +0900

    App Restaurant 추가 될 경우 테스트 추가

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,6 +1,7 @@
 import { render } from '@testing-library/react';
 import { useSelector } from 'react-redux';
 import App from './App';
+import Restaurant from './domain/Restaurant';
 
 jest.mock('react-redux');
 
@@ -24,5 +25,21 @@ describe('App', () => {
 
     expect(getByText('Restaurants')).not.toBeNull();
     expect(getByText('등록')).not.toBeNull();
+  });
+
+  it('restaurant가 추가되면 목록에 나열된다', () => {
+    const expectName = 'New Name';
+    const expectCategory = 'New Category';
+    const expectAddress = 'New Address';
+
+    stubSelector([Restaurant.newJson(expectName, expectCategory, expectAddress)]);
+
+    const { getAllByRole } = render((
+      <App />
+    ));
+
+    const result = getAllByRole('listitem');
+    expect(result).toHaveLength(1);
+    expect(result[0]).toHaveTextContent(`${expectName} | ${expectCategory} | ${expectAddress}`);
   });
 });
