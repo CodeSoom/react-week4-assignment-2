735acf6c63ca2ccdeda76734150647b1b3df2f25
hdpyo <hudongpyo.dev@gmail.com>
2022-04-12 18:26:15 +0900

    refactor: App 컴포넌트의 input 에 대한 테스트 코드를 수정한다.
    - <label> 을 모두 제거하였으므로, queryByLabelText 를 사용할 수 없다.
      ㄴ queryByPlaceHolerText 를 사용하여 테스트한다.

--- src/App.test.js
+++ src/App.test.js
@@ -10,10 +10,10 @@ describe('App', () => {
   });
 
   it('shows three inputs', () => {
-    const { queryByLabelText } = render(<App />);
+    const { queryByPlaceholderText } = render(<App />);
 
-    expect(queryByLabelText(/이름/)).not.toBeNull();
-    expect(queryByLabelText(/분류/)).not.toBeNull();
-    expect(queryByLabelText(/주소/)).not.toBeNull();
+    expect(queryByPlaceholderText(/이름/)).not.toBeNull();
+    expect(queryByPlaceholderText(/분류/)).not.toBeNull();
+    expect(queryByPlaceholderText(/주소/)).not.toBeNull();
   });
 });
