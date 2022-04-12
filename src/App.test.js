4d708f5579c903126118d534683d64fa432c0ca6
hdpyo <hudongpyo.dev@gmail.com>
2022-04-12 18:11:00 +0900

    test: 실패하는 테스트 작성
    - App 컴포넌트는 Restaurants 라는 텍스트를 보여준다.

--- src/App.test.js
+++ src/App.test.js
@@ -3,9 +3,9 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 describe('App', () => {
-  it('shows Hello', () => {
-    const { getByText } = render(<App />);
+  it('shows "Restaurants"', () => {
+    const { queryByText } = render(<App />);
 
-    expect(getByText(/Hello/)).toBeVisible();
+    expect(queryByText(/Restaurants/)).not.toBeNull();
   });
 });
