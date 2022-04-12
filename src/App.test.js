f1e465f22e65399f02db066b78a5782269aeaa68
hdpyo <hudongpyo.dev@gmail.com>
2022-04-12 18:15:58 +0900

    test: App 컴포넌트는 3개의 input 요소를 보여준다.
    - 3개의 input 은 각각 '이름','분류','주소' <label>을 가지고 있다.

--- src/App.test.js
+++ src/App.test.js
@@ -8,4 +8,12 @@ describe('App', () => {
 
     expect(queryByText(/Restaurants/)).not.toBeNull();
   });
+
+  it('shows three inputs', () => {
+    const { queryByLabelText } = render(<App />);
+
+    expect(queryByLabelText(/이름/)).not.toBeNull();
+    expect(queryByLabelText(/분류/)).not.toBeNull();
+    expect(queryByLabelText(/주소/)).not.toBeNull();
+  });
 });
