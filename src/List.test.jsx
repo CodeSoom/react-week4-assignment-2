c7c245f53127c3b79f00a45e42dfea81b8c73a82
kjoobin <imkj1148@gmail.com>
2022-04-12 20:39:10 +0900

    Add renders restaurant list test-case in List

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -19,12 +19,17 @@ describe('List', () => {
       name: '마녀주방',
       category: '한식',
       address: '서울시 강남구',
+    }, {
+      name: '시카고 피자',
+      category: '양식',
+      address: '서울시 서초구',
     }];
 
-    it('renders restaurant', () => {
+    it('renders restaurant list', () => {
       const { container } = renderList({ restaurantList });
 
       expect(container).toHaveTextContent('마녀주방 | 한식 | 서울시 강남구');
+      expect(container).toHaveTextContent('시카고 피자 | 양식 | 서울시 서초구');
     });
   });
 });
