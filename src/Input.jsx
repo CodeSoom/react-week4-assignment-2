59655dd9dd6e7d11f57f0061f71c57fa818886e7
kjoobin <imkj1148@gmail.com>
2022-04-12 19:29:54 +0900

    Add implement placeholder in Input

--- src/Input.jsx
+++ src/Input.jsx
@@ -1,9 +1,9 @@
 export default function Input() {
   return (
     <div>
-      <input />
-      <input />
-      <input />
+      <input placeholder="이름" />
+      <input placeholder="분류" />
+      <input placeholder="주소" />
       <button type="button">등록</button>
     </div>
   );
