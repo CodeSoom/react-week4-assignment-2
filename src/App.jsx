326ed56efc5c5fdc591afd4ad74bbe2173dedf7b
hdpyo <hudongpyo.dev@gmail.com>
2022-04-12 18:23:37 +0900

    feat: App 컴포넌트에 input 요소 3개를 추가하였다.
    - 이름, 분류, 주소에 대한 input 을 생성하였다.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,25 @@ export default function App() {
   return (
     <div>
       <h1>Restaurants</h1>
+
+      <label htmlFor="input-restaurant-name">이름</label>
+      <input
+        id="input-restaurant-name"
+        type="text"
+        placeholder="이름"
+      />
+      <label htmlFor="input-restaurant-category">분류</label>
+      <input
+        id="input-restaurant-category"
+        type="text"
+        placeholder="분류"
+      />
+      <label htmlFor="input-restaurant-address">주소</label>
+      <input
+        id="input-restaurant-address"
+        type="text"
+        placeholder="주소"
+      />
     </div>
   );
 }
