5d0516031273fb1d9ca5bbffab2d9988b87176a0
hdpyo <hudongpyo.dev@gmail.com>
2022-04-12 18:25:14 +0900

    refactor: input 에 대한 <label>을 제거하였다.
    - 과제의 내용에 맞게 각 input 의 <label>을 제거한다.

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,18 @@ export default function App() {
     <div>
       <h1>Restaurants</h1>
 
-      <label htmlFor="input-restaurant-name">이름</label>
       <input
         id="input-restaurant-name"
         type="text"
         placeholder="이름"
       />
-      <label htmlFor="input-restaurant-category">분류</label>
+
       <input
         id="input-restaurant-category"
         type="text"
         placeholder="분류"
       />
-      <label htmlFor="input-restaurant-address">주소</label>
+
       <input
         id="input-restaurant-address"
         type="text"
