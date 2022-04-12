8b4891ed6f67327395da00fc7b1330e4e63bb09c
hdpyo <hudongpyo.dev@gmail.com>
2022-04-12 19:09:36 +0900

    chore: 불필요한 공백을 제거한다.

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,16 @@ export default function App() {
   return (
     <div>
       <h1>Restaurants</h1>
-
       <input
         id="input-restaurant-name"
         type="text"
         placeholder="이름"
       />
-
       <input
         id="input-restaurant-category"
         type="text"
         placeholder="분류"
       />
-
       <input
         id="input-restaurant-address"
         type="text"
