4a76e9d73b5aa9c59155983280627cdcb9c6d3da
hdpyo <hudongpyo.dev@gmail.com>
2022-04-13 15:32:40 +0900

    fix: 테스트 내용에 적절한 assertion 으로 수정한다.

--- src/RestaurantAddressInput.test.js
+++ src/RestaurantAddressInput.test.js
@@ -5,6 +5,6 @@ describe('RestaurantAddressInput 컴포넌트', () => {
   it('"주소"라는 placeholderText를 가진 input 요소를 가지고 있다.', () => {
     const { queryByPlaceholderText } = render((<RestaurantAddressInput />));
 
-    expect(queryByPlaceholderText('주소')).toBeVisible();
+    expect(queryByPlaceholderText('주소')).not.toBeNull();
   });
 });
--- src/RestaurantCategoryInput.test.js
+++ src/RestaurantCategoryInput.test.js
@@ -6,6 +6,6 @@ describe('RestaurantCategoryInput 컴포넌트', () => {
   it('"분류"이라는 placeholderText를 가진 input 요소를 가지고 있다.', () => {
     const { queryByPlaceholderText } = render((<RestaurantCategoryInput />));
 
-    expect(queryByPlaceholderText('분류')).toBeVisible();
+    expect(queryByPlaceholderText('분류')).not.toBeNull();
   });
 });
