28bdb204b777dbcc4a4565df321f67ed850aa96b
hdpyo <hudongpyo.dev@gmail.com>
2022-04-14 00:05:56 +0900

    feat: Page 컴포넌트 추가
    - Page 컴포넌트를 렌더링하는 테스트에 import문 추가

--- src/Page.test.jsx
+++ src/Page.test.jsx
@@ -1,5 +1,7 @@
 import { render } from '@testing-library/react';
 
+import Page from './Page';
+
 describe('Page', () => {
   it('Page 컴포넌트를 렌더링할 수 있다.', () => {
     const { container } = render(<Page />);
