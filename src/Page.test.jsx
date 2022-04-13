b3dd37939d564d3882fcdfa0a858d0a369dba681
hdpyo <hudongpyo.dev@gmail.com>
2022-04-14 00:15:22 +0900

    test: Page 컴포넌트를 렌더링할 수 있다. (테스트 내용 추가)

--- src/Page.test.jsx
+++ src/Page.test.jsx
@@ -2,8 +2,13 @@ import { render } from '@testing-library/react';
 
 import Page from './Page';
 
-describe('Page', () => {
-  it('Page 컴포넌트를 렌더링할 수 있다.', () => {
-    const { container } = render(<Page />);
+describe('Page 컴포넌트', () => {
+  it('Page 컴포넌트를 렌더링할 수 있다. ', () => {
+    const { queryByTestId, queryByPlaceholderText } = render(<Page />);
+
+    expect(queryByTestId('restaurants-list')).not.toBeNull();
+    expect(queryByPlaceholderText(/이름/)).not.toBeNull();
+    expect(queryByPlaceholderText(/분류/)).not.toBeNull();
+    expect(queryByPlaceholderText(/주소/)).not.toBeNull();
   });
 });
