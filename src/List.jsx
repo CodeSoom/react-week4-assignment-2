8c729d6e6997a55a602e563ccdb3e78ee4b963c1
kjoobin <imkj1148@gmail.com>
2022-04-13 23:23:02 +0900

    Add 레스토랑 목록이 없는경우 구현

--- src/List.jsx
+++ src/List.jsx
@@ -1,6 +1,9 @@
-export default function List({
-  restaurantList,
-}) {
+export default function List({ restaurantList }) {
+  if (!restaurantList.length) {
+    return (
+      <p>레스토랑 목록이 없습니다.</p>
+    );
+  }
   return (
     <ul>
       {restaurantList.map((restaurant) => {
