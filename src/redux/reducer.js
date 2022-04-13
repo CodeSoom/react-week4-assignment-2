89cdc032a1e0937cc2581057c9c7484c9e7fe31c
이찬호 <0407chan@naver.com>
2022-04-13 23:34:20 +0900

    green. deleteRestaurant 일단 통과하게 수정

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -34,5 +34,12 @@ export default function reducer({
     };
   }
 
+  if (action.type === 'deleteRestaurant') {
+    return {
+      ...state,
+      restaurants: [],
+    };
+  }
+
   return state;
 }
