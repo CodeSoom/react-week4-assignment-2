32960dace61111cadd6256ba185611b7e2cdac8e
Jiwoo Choi <heyjiwoo@annotation.ai.com>
2022-04-14 08:27:00 +0900

    Refactor reducer test
    Refactor reducer test.

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -10,9 +10,11 @@ describe('reducer', () => {
     context('with information', () => {
       it('changes the restaurant information', () => {
         const state = reducer({
-          name: '',
-          category: '',
-          address: '',
+          information: {
+            name: '',
+            category: '',
+            address: '',
+          },
         }, updateInformation({
           information: {
             name: 'New restaurant',
@@ -21,70 +23,78 @@ describe('reducer', () => {
           },
         }));
 
-        expect(state.name).toBe('New restaurant');
-        expect(state.category).toBe('New category');
-        expect(state.address).toBe('New address');
+        expect(state.information.name).toBe('New restaurant');
+        expect(state.information.category).toBe('New category');
+        expect(state.information.address).toBe('New address');
       });
     });
 
     context('without information', () => {
       it('does nothing', () => {
         const state = reducer({
-          name: '브루클린 버거',
-          category: '양식',
-          address: '서래마을',
+          information: {
+            name: '브루클린 버거',
+            category: '양식',
+            address: '서래마을',
+          },
         }, updateInformation({ review: 'The food is so good' }));
 
-        expect(state.name).toBe('브루클린 버거');
-        expect(state.category).toBe('양식');
-        expect(state.address).toBe('서래마을');
+        expect(state.information.name).toBe('브루클린 버거');
+        expect(state.information.category).toBe('양식');
+        expect(state.information.address).toBe('서래마을');
       });
     });
   });
 
   describe('addRestaurant', () => {
-    function reduceAddRestaurant({
-      name,
-      category,
-      address,
-    }) {
+    function reduceAddRestaurant({ information }) {
       return reducer({
         newId: 100,
-        name,
-        category,
-        address,
+        information,
         restaurants: [],
       }, addRestaurant());
     }
 
-    context('with restaurant name, category and address', () => {
-      const state = reduceAddRestaurant({
-        name: '브루클린 버거',
-        category: '양식',
-        address: '서래마을',
-      });
-
+    context('with restaurant name, category and address information', () => {
       it('adds a new restaurant', () => {
+        const state = reduceAddRestaurant({
+          information: {
+            name: '브루클린 버거',
+            category: '양식',
+            address: '서래마을',
+          },
+        });
+
         expect(state.restaurants).toHaveLength(1);
         expect(state.restaurants[0].id).not.toBeUndefined();
-        expect(state.restaurants[0].name).toBe('브루클린 버거');
-        expect(state.restaurants[0].category).toBe('양식');
-        expect(state.restaurants[0].address).toBe('서래마을');
+        expect(state.restaurants[0].information.name).toBe('브루클린 버거');
+        expect(state.restaurants[0].information.category).toBe('양식');
+        expect(state.restaurants[0].information.address).toBe('서래마을');
       });
 
       it('clears the task title', () => {
-        expect(state.name).toBe('');
-        expect(state.category).toBe('');
-        expect(state.address).toBe('');
+        const state = reduceAddRestaurant({
+          information: {
+            name: '브루클린 버거',
+            category: '양식',
+            address: '서래마을',
+          },
+        });
+
+        expect(state.information.name).toBe('');
+        expect(state.information.category).toBe('');
+        expect(state.information.address).toBe('');
       });
     });
 
     context('without restaurant name', () => {
       it('does nothing', () => {
         const state = reduceAddRestaurant({
-          name: '',
-          category: '양식',
-          address: '서래마을',
+          information: {
+            name: '',
+            category: '양식',
+            address: '서래마을',
+          },
         });
 
         expect(state.restaurants).toHaveLength(0);
@@ -94,9 +104,11 @@ describe('reducer', () => {
     context('without restaurant category', () => {
       it('does nothing', () => {
         const state = reduceAddRestaurant({
-          name: '브루클린 버거',
-          category: '',
-          address: '서래마을',
+          information: {
+            name: '브루클린 버거',
+            category: '',
+            address: '서래마을',
+          },
         });
 
         expect(state.restaurants).toHaveLength(0);
@@ -106,9 +118,11 @@ describe('reducer', () => {
     context('without restaurant address', () => {
       it('does nothing', () => {
         const state = reduceAddRestaurant({
-          name: '브루클린 버거',
-          category: '양식',
-          address: '',
+          information: {
+            name: '브루클린 버거',
+            category: '양식',
+            address: '',
+          },
         });
 
         expect(state.restaurants).toHaveLength(0);
@@ -117,12 +131,10 @@ describe('reducer', () => {
   });
 
   context('without actions', () => {
-    const someFunction = jest.fn(({ name, category, address }) => ({
+    const someFunction = jest.fn(({ information }) => ({
       type: 'someFunction',
       payload: {
-        name,
-        category,
-        address,
+        information,
       },
     }));
 
@@ -134,9 +146,9 @@ describe('reducer', () => {
       }));
 
       expect(state.newId).toBe(100);
-      expect(state.name).toBe('');
-      expect(state.category).toBe('');
-      expect(state.address).toBe('');
+      expect(state.information.name).toBe('');
+      expect(state.information.category).toBe('');
+      expect(state.information.address).toBe('');
       expect(state.restaurants).toHaveLength(0);
     });
   });
