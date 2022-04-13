a74a36ad311beee824e1b2ecced1332f76ed1bdb
hdpyo <hudongpyo.dev@gmail.com>
2022-04-13 16:26:28 +0900

    test: 새로운 주소를 입력했을 때 값을 변경하는 이벤트가 동작한다.

--- src/RestaurantAddressInput.test.js
+++ src/RestaurantAddressInput.test.js
@@ -1,4 +1,5 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
+
 import RestaurantAddressInput from './RestaurantAddressInput';
 
 describe('RestaurantAddressInput 컴포넌트', () => {
@@ -6,5 +7,21 @@ describe('RestaurantAddressInput 컴포넌트', () => {
     const { queryByPlaceholderText } = render((<RestaurantAddressInput />));
 
     expect(queryByPlaceholderText('주소')).not.toBeNull();
+  });
+
+  it('새로운 주소를 입력했을 때 값을 변경하는 이벤트가 동작한다.', () => {
+    const handleAddressChange = jest.fn();
+
+    const { queryByPlaceholderText } = render((
+      <RestaurantAddressInput
+        onChange={handleAddressChange}
+      />
+    ));
+
+    fireEvent.change(queryByPlaceholderText('주소'), {
+      target: { value: '서울시 송파구' },
+    });
+
+    expect(handleAddressChange).toBeCalled();
   });
 });
