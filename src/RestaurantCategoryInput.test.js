8723c26aa9f0d34667b048d8afdd19c583ae90f4
hdpyo <hudongpyo.dev@gmail.com>
2022-04-13 16:20:15 +0900

    test: 레스토랑 분류를 입력할 때 값을 변경하는 이벤트가 동작한다.

--- src/RestaurantCategoryInput.test.js
+++ src/RestaurantCategoryInput.test.js
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import RestaurantCategoryInput from './RestaurantCategoryInput';
 
@@ -7,5 +7,21 @@ describe('RestaurantCategoryInput 컴포넌트', () => {
     const { queryByPlaceholderText } = render((<RestaurantCategoryInput />));
 
     expect(queryByPlaceholderText('분류')).not.toBeNull();
+  });
+
+  it('새로운 분류를 입력했을 때 값을 변경하는 이벤트가 동작한다.', () => {
+    const handleCategoryChange = jest.fn();
+
+    const { queryByPlaceholderText } = render((
+      <RestaurantCategoryInput
+        onChange={handleCategoryChange}
+      />
+    ));
+
+    fireEvent.change(queryByPlaceholderText('분류'), {
+      target: { value: '한식' },
+    });
+
+    expect(handleCategoryChange).toBeCalled();
   });
 });
