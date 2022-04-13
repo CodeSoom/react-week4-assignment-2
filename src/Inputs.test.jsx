94bdfd5d2f9da1224c4375c110770419bd07872e
kjoobin <imkj1148@gmail.com>
2022-04-13 23:39:00 +0900

    Add 레스토랑 분류, 주소 업데이트 테스트

--- src/Inputs.test.jsx
+++ src/Inputs.test.jsx
@@ -29,4 +29,38 @@ describe('Inputs', () => {
 
     expect(onChangeName).toBeCalledWith('한우마을');
   });
+
+  it('listens for change event on update restaurant name', () => {
+    const onChangeCategory = jest.fn();
+
+    const { getByPlaceholderText } = render((
+      <Inputs
+        onChangeCategory={onChangeCategory}
+      />
+    ));
+
+    fireEvent.change(
+      getByPlaceholderText('분류'),
+      { target: { value: '한식' } },
+    );
+
+    expect(onChangeCategory).toBeCalledWith('한식');
+  });
+
+  it('listens for change event on update restaurant name', () => {
+    const onChangeAddress = jest.fn();
+
+    const { getByPlaceholderText } = render((
+      <Inputs
+        onChangeAddress={onChangeAddress}
+      />
+    ));
+
+    fireEvent.change(
+      getByPlaceholderText('주소'),
+      { target: { value: '경기도 수원시' } },
+    );
+
+    expect(onChangeAddress).toBeCalledWith('경기도 수원시');
+  });
 });
