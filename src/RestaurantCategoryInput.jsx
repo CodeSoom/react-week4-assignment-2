0a82b3203eda6e913d0f0cc60d253d204a62a0d0
hdpyo <hudongpyo.dev@gmail.com>
2022-04-13 15:07:13 +0900

    feat: RestaurantCategoryInput 컴포넌트를 작성한다.
    - App 컴포넌트의 '분류' input 을 분리한다.

---
+++ src/RestaurantCategoryInput.jsx
@@ -0,0 +1,9 @@
+export default function RestaurantCategoryInput() {
+  return (
+    <input
+      id="input-restaurant-category"
+      type="text"
+      placeholder="분류"
+    />
+  );
+}
--- src/RestaurantCategoryInput.test.js
+++ src/RestaurantCategoryInput.test.js
@@ -1,5 +1,7 @@
 import { render } from '@testing-library/react';
 
+import RestaurantCategoryInput from './RestaurantCategoryInput';
+
 describe('RestaurantCategoryInput 컴포넌트', () => {
   it('"분류"이라는 placeholderText를 가진 input 요소를 가지고 있다.', () => {
     const { queryByPlaceholderText } = render((<RestaurantCategoryInput />));
