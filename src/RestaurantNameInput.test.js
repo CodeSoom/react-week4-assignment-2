81fc18d8b30e8c12c765cb588fca0f7c47bd4889
hdpyo <hudongpyo.dev@gmail.com>
2022-04-13 15:10:26 +0900

    refactor: 테스트의 의미에 맞도록 assertion을 수정한다.
    - toBeVisible() 보다 tobeNull() 이 더 의미에 적절할 것 같다.

--- src/RestaurantNameInput.test.js
+++ src/RestaurantNameInput.test.js
@@ -6,6 +6,6 @@ describe('RestaurantNameInput 컴포넌트', () => {
   it('"이름"이라는 placeholderText를 가진 input 요소를 가지고 있다.', () => {
     const { queryByPlaceholderText } = render((<RestaurantNameInput />));
 
-    expect(queryByPlaceholderText('이름')).toBeVisible();
+    expect(queryByPlaceholderText('이름')).not.toBeNull();
   });
 });
