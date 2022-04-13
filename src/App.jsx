7cd4205a645b9c67a09a54654639e94aeca70e5d
kjoobin <imkj1148@gmail.com>
2022-04-13 23:09:44 +0900

    Refactoring 레스토랑 목록을 보여주기

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 export default function App() {
+  const restaurantList = [
+    {
+      id: 1, name: '시카고피자', category: '양식', address: '서울시 강남구',
+    },
+  ];
+
   return (
     <div>
       Hello
       <ol>
+        {restaurantList.map((restaurant) => {
+          const {
+            id, name, category, address,
+          } = restaurant;
+
+          return (
+            <ul key={id}>
+              {`${name} | ${category} | ${address}`}
+            </ul>
+          );
+        })}
         <ul>
           시카고피자 | 양식 | 서울시 강남구
         </ul>
