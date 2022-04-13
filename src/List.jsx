a814cb938bfbbb53090e4c17575262b3a5e32d83
kjoobin <imkj1148@gmail.com>
2022-04-13 23:14:40 +0900

    Refactoring 레스토랑 리스트 보여주기 추출

---
+++ src/List.jsx
@@ -0,0 +1,25 @@
+export default function List({
+  restaurantList,
+}) {
+  return (
+    <div>
+      Hello
+      <ol>
+        {restaurantList.map((restaurant) => {
+          const {
+            id, name, category, address,
+          } = restaurant;
+
+          return (
+            <ul key={id}>
+              {`${name} | ${category} | ${address}`}
+            </ul>
+          );
+        })}
+        <ul>
+          시카고피자 | 양식 | 서울시 강남구
+        </ul>
+      </ol>
+    </div>
+  );
+}
