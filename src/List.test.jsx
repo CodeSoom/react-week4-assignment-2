cea32beb480512022e7cdb8c06e1eab7add6a095
kjoobin <imkj1148@gmail.com>
2022-04-13 23:21:48 +0900

    Add 레스토랑 목록이 없을경우 보여주는 테스트

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -3,7 +3,21 @@ import { render } from '@testing-library/react';
 import List from './List';
 
 describe('List', () => {
-  it('renders restaurant', () => {
+  context('without restaurant', () => {
+    it('renders "레스토랑 목록이 없습니다."', () => {
+      const restaurantList = [];
+
+      const { container } = render((
+        <List
+          restaurantList={restaurantList}
+        />
+      ));
+
+      expect(container).toHaveTextContent('레스토랑 목록이 없습니다.');
+    });
+  });
+
+  it('renders restaurant list', () => {
     const restaurantList = [
       {
         id: 1, name: '시카고피자', category: '양식', address: '이태원구',
@@ -12,6 +26,7 @@ describe('List', () => {
         id: 2, name: '마녀식당', category: '한식', address: '서울시 강남구',
       },
     ];
+
     const { container } = render((
       <List
         restaurantList={restaurantList}
