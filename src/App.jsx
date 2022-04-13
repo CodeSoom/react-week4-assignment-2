68d5c00298f15d516a3e9826e2c1b32dadaf2c9b
kjoobin <imkj1148@gmail.com>
2022-04-13 23:29:03 +0900

    Add 레스토랑 입력을 위한 textbox 구현

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,39 @@ export default function App() {
     },
   ];
 
+  function handleChangeRestaurantName(event) {
+
+  }
+
+  function handleChangeRestaurantCategory(event) {
+
+  }
+
+  function handleChangeRestaurantAddress(event) {
+
+  }
+
   return (
     <div>
       <List restaurantList={restaurantList} />
+      <input
+        id="input-restaurant-name"
+        type="text"
+        placeholder="이름"
+        onChange={handleChangeRestaurantName}
+      />
+      <input
+        id="input-restaurant-category"
+        type="text"
+        placeholder="분류"
+        onChange={handleChangeRestaurantCategory}
+      />
+      <input
+        id="input-restaurant-address"
+        type="text"
+        placeholder="주소"
+        onChange={handleChangeRestaurantAddress}
+      />
     </div>
   );
 }
