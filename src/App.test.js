c7d72709669c49dd653891bf17316f5db72bae86
hdpyo <hudongpyo.dev@gmail.com>
2022-04-13 20:47:21 +0900

    refactor: App 컴포넌트 테스트 중 불필요한 테스트 제거

--- src/App.test.js
+++ src/App.test.js
@@ -9,12 +9,6 @@ describe('App', () => {
     expect(queryByText(/Restaurants/)).not.toBeNull();
   });
 
-  it('"등록" 버튼을 가지고 있다.', () => {
-    const { queryByText } = render(<App />);
-
-    expect(queryByText('등록')).not.toBeNull();
-  });
-
   it('RestaurantList 컴포넌트가 있다.', () => {
     const { queryByTestId } = render(<App />);
     expect(queryByTestId('restaurants-list')).not.toBeNull();
