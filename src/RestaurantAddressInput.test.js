2655797f4b28e803f843349af8e81e1d8a94e3d0
hdpyo <hudongpyo.dev@gmail.com>
2022-04-13 15:17:19 +0900

    test: RestaurantAddressInput 컴포넌트는 '주소' input 을 가지고 있다.
    - App 컴포넌트로부터 '주소' input 을 분리하는 것이 목적이다.

---
+++ src/RestaurantAddressInput.test.js
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/react';
+
+describe('RestaurantAddressInput 컴포넌트', () => {
+  it('"주소"라는 placeholderText를 가진 input 요소를 가지고 있다.', () => {
+    const { queryByPlaceholderText } = render((<RestaurantAddressInput />));
+
+    expect(queryByPlaceholderText('주소')).toBeVisible();
+  });
+});
