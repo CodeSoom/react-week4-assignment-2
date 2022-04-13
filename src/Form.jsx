ab548d907eb90eeb17bc34c3684a3646880ac4ed
Jiwoo Choi <heyjiwoo@annotation.ai.com>
2022-04-13 22:14:02 +0900

    Refactor
    Refactor.

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,51 +1,34 @@
+import RestaurantInput from './RestaurantInput';
+
 export default function Form({
   value,
   onChange,
   onClick,
 }) {
-  function handleChange(event) {
-    const { name, value: changedValue } = event.target;
-
-    if (name === 'name') {
-      onChange({ restaurantName: changedValue });
-    }
-
-    if (name === 'category') {
-      onChange({ restaurantCategory: changedValue });
-    }
-
-    if (name === 'address') {
-      onChange({ restaurantAddress: changedValue });
-    }
-  }
-
   const { name, category, address } = value;
 
   return (
     <p>
-      <input
+      <RestaurantInput
         id="input-restaurant-name"
         name="name"
-        type="text"
         placeholder="이름"
         value={name}
-        onChange={handleChange}
+        onChange={onChange}
       />
-      <input
+      <RestaurantInput
         id="input-restaurant-category"
         name="category"
-        type="text"
         placeholder="분류"
         value={category}
-        onChange={handleChange}
+        onChange={onChange}
       />
-      <input
+      <RestaurantInput
         id="input-restaurant-address"
         name="address"
-        type="text"
         placeholder="주소"
         value={address}
-        onChange={handleChange}
+        onChange={onChange}
       />
       <button type="button" onClick={onClick}>
         등록
--- src/RestaurantInput.jsx
+++ src/RestaurantInput.jsx
@@ -1,6 +1,22 @@
 export default function RestaurantInput({
   id, name, placeholder, value, onChange,
 }) {
+  function handleChange(event) {
+    const { name: inputName, value: changedValue } = event.target;
+
+    if (inputName === 'name') {
+      onChange({ restaurantName: changedValue });
+    }
+
+    if (inputName === 'category') {
+      onChange({ restaurantCategory: changedValue });
+    }
+
+    if (inputName === 'address') {
+      onChange({ restaurantAddress: changedValue });
+    }
+  }
+
   return (
     <input
       id={id}
@@ -8,7 +24,7 @@ export default function RestaurantInput({
       type="text"
       placeholder={placeholder}
       value={value}
-      onChange={onChange}
+      onChange={handleChange}
     />
   );
 }
