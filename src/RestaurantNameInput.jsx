55464b9c7fa8a6be4a2adfbc4bf47967b539b9bc
hdpyo <hudongpyo.dev@gmail.com>
2022-04-13 16:16:06 +0900

    refactor: event 객체 대신 값을 전달받는 형태로 수정한다.

--- src/RestaurantNameInput.jsx
+++ src/RestaurantNameInput.jsx
@@ -1,11 +1,15 @@
 export default function RestaurantNameInput({ value, onChange }) {
+  function handleChange(event) {
+    const { value: inputName } = event.target;
+    onChange(inputName);
+  }
   return (
     <input
       id="input-restaurant-name"
       type="text"
       placeholder="이름"
       value={value}
-      onChange={onChange}
+      onChange={handleChange}
     />
   );
 }
