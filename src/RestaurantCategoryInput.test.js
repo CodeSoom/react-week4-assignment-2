25c57b5c783ec1953e1dc24bf691f6c5964a89df
hdpyo <hudongpyo.dev@gmail.com>
2022-04-13 15:03:13 +0900

    test: RestaurantCategoryInput 컴포넌트는 '분류' input 을 가지고 있다.
    - App 컴포넌트에서 '분류' input 을 분리하는 것이 목표다.

---
+++ src/RestaurantCategoryInput.test.js
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/react';
+
+describe('RestaurantCategoryInput 컴포넌트', () => {
+  it('"분류"이라는 placeholderText를 가진 input 요소를 가지고 있다.', () => {
+    const { queryByPlaceholderText } = render((<RestaurantCategoryInput />));
+
+    expect(queryByPlaceholderText('분류')).toBeVisible();
+  });
+});
