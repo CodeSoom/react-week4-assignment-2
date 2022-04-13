f9f4683fc676a371260d7987cc60810d3ad265b0
kjoobin <imkj1148@gmail.com>
2022-04-13 23:25:21 +0900

    Add 레스토랑 추가를 위한 textbox 테스트

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -11,4 +11,14 @@ describe('App', () => {
     expect(container).toHaveTextContent('시카고피자 | 양식 | 이태원구');
     expect(container).toHaveTextContent('마녀식당 | 한식 | 서울시 강남구');
   });
+
+  it('renders restaurant inputs', () => {
+    const { queryByPlaceholderText } = render((
+      <App />
+    ));
+
+    expect(queryByPlaceholderText('이름')).not.toBeNull();
+    expect(queryByPlaceholderText('분류')).not.toBeNull();
+    expect(queryByPlaceholderText('주소')).not.toBeNull();
+  });
 });
