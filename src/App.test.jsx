a57858235e0b6ca7e963b617b67591d554ea422d
0hu <dladudgn123@naver.com>
2022-04-14 22:46:12 +0900

    app 테스트

---
+++ src/App.test.jsx
@@ -0,0 +1,15 @@
+import { render } from '@testing-library/react';
+
+import App from './App';
+
+test('App', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  const { queryByText } = render((
+    <App />
+  ));
+
+  expect(queryByText(/Hello/)).not.toBeNull();
+});
