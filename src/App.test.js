869718d8a1603644fdfa198a8a47eade2a5f76c3
hdpyo <hudongpyo.dev@gmail.com>
2022-04-14 10:52:30 +0900

    refactor: <App/> 테스트 코드 리팩토링
    - 초기 화면이 렌더링 될때의 테스트를 작성했다.
    - 불필요한 테스트 코드를 제거했다.

--- src/App.test.js
+++ src/App.test.js
@@ -3,36 +3,12 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 describe('App', () => {
-  it('"Restaurants" 타이틀이 보여야한다.', () => {
-    const { queryByText } = render(<App />);
+  it('초기 화면에는 이름|분류|주소 입력란, 등록 버튼이 있어야 한다.', () => {
+    const { queryByText, queryByPlaceholderText } = render(<App />);
 
     expect(queryByText(/Restaurants/)).not.toBeNull();
-  });
-
-  // TODO : 테스트 수정 필요
-  // it('RestaurantList 컴포넌트가 있다.', () => {
-  //   const { queryByTestId } = render(<App />);
-  //   expect(queryByTestId('restaurants-list')).not.toBeNull();
-  // });
-
-  // TODO : 테스트 수정 필요
-  it('RestaurantNameInput 컴포넌트가 있다.', () => {
-    const { queryByPlaceholderText } = render(<App />);
-
     expect(queryByPlaceholderText(/이름/)).not.toBeNull();
-  });
-
-  // TODO : 테스트 수정 필요
-  it('RestaurantCategoryInput 컴포넌트가 있다.', () => {
-    const { queryByPlaceholderText } = render(<App />);
-
     expect(queryByPlaceholderText(/분류/)).not.toBeNull();
-  });
-
-  // TODO : 테스트 수정 필요
-  it('RestaurantAddressInput 컴포넌트가 있다.', () => {
-    const { queryByPlaceholderText } = render(<App />);
-
     expect(queryByPlaceholderText(/주소/)).not.toBeNull();
   });
 });
