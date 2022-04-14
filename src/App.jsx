dd3797f91dab25aad4bb3f5a0e3b546d4476bdd2
0hu <dladudgn123@naver.com>
2022-04-14 22:56:35 +0900

    컨테이너 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import InputContainer from './InputContainer';
+import RestaurantsListContainer from './RestaurantsListContainer';
+
 export default function App() {
   return (
     <div>
       <h1>Restaurants</h1>
+      <RestaurantsListContainer />
+      <InputContainer />
     </div>
   );
 }
