4ee36121cc2579276a0e5f35ab3076ea07ae6799
이찬호 <0407chan@naver.com>
2022-04-14 20:33:11 +0900

    green. refactor. InputContainer render 구현

--- src/containers/InputContainer.jsx
+++ src/containers/InputContainer.jsx
@@ -1,14 +1,25 @@
 export default function InputContainer() {
   return (
     <div>
-      <label htmlFor="input-name">
-        이름
-      </label>
+      <label htmlFor="input-name">이름</label>
       <input
         id="input-name"
         type="text"
         placeholder="이름"
       />
+      <label htmlFor="input-category">분류</label>
+      <input
+        id="input-category"
+        type="text"
+        placeholder="분류"
+      />
+      <label htmlFor="input-address">주소</label>
+      <input
+        id="input-address"
+        type="text"
+        placeholder="주소"
+      />
+      <button type="button">등록</button>
     </div>
   );
 }
