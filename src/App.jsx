cd28a2863272f3613d98d906cc88bbdcce1c7fdc
free-ko <duddnr654@naver.com>
2022-04-14 21:17:13 +0900

    Feat: renders three input box when App component init

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 export default function App() {
   return (
-    <h1>
-      Restaurants
-    </h1>
+    <div>
+      <h1>
+        Restaurants
+      </h1>
+      <input
+        id="input-restaurants-name"
+        type="text"
+        placeholder="이름"
+      />
+      <input
+        id="input-restaurants-category"
+        type="text"
+        placeholder="분류"
+      />
+      <input
+        id="input-restaurants-address"
+        type="text"
+        placeholder="주소"
+      />
+    </div>
   );
 }
