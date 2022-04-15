bc92c4c9d3c657aed58f1edce4dc7de6bfcae7d2
free-ko <duddnr654@naver.com>
2022-04-15 21:10:21 +0900

    Fix: add input name

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,19 @@ export default function App() {
       <input
         id="input-restaurants-name"
         type="text"
+        name="name"
         placeholder="이름"
       />
       <input
         id="input-restaurants-category"
         type="text"
+        name="category"
         placeholder="분류"
       />
       <input
         id="input-restaurants-address"
         type="text"
+        name="address"
         placeholder="주소"
       />
       <button type="button">등록</button>
