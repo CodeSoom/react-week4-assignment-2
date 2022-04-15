aaed6956044c005aea71e93b642c80228360ddf2
0hu <dladudgn123@naver.com>
2022-04-15 11:12:30 +0900

    Implement - addRestaurant action type 구현

--- src/reducer.js
+++ src/reducer.js
@@ -1,13 +1,26 @@
 const initialState = {
+  newId: 100,
   restaurantsName: '',
   category: '',
   address: '',
+  restaurants: [],
 };
 
 export default function reducer(state = initialState, action) {
   if (action.type === 'addRestaurant') {
+    const {
+      newId, restaurantsName, category, address, restaurants,
+    } = state;
+
     return {
       ...state,
+      newId: newId + 1,
+      restaurantsName: '',
+      category: '',
+      address: '',
+      restaurants: [...restaurants, {
+        id: newId, name: restaurantsName, category, address,
+      }],
     };
   }
   return state;
--- src/reducer.test.js
+++ src/reducer.test.js
@@ -14,7 +14,7 @@ describe('reducer', () => {
         category,
         address,
         restaurants: [],
-      }, addRestaurant());
+      }, addRestaurant({ restaurantsName, category, address }));
     }
     it('appends a new name,category,address in restaurants', () => {
       const state = reduceAddRestaurant({
@@ -25,7 +25,7 @@ describe('reducer', () => {
 
       expect(state.restaurants).toHaveLength(1);
       expect(state.restaurants[0].id).not.toBeUndefined();
-      expect(state.restaurants[0].restaurantsName).toBe('쿠스쿠스');
+      expect(state.restaurants[0].name).toBe('쿠스쿠스');
     });
   });
 });
