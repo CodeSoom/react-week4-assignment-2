69aa0083f35ce45e6f115d4f8a7d639860309ec9
free-ko <duddnr654@naver.com>
2022-04-15 22:22:00 +0900

    Test: changes the name value

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import Input from './Input';
 
@@ -12,5 +12,21 @@ describe('Input', () => {
     expect(queryByPlaceholderText(/분류/)).not.toBeNull();
     expect(queryByPlaceholderText(/주소/)).not.toBeNull();
     expect(queryByText(/등록/)).not.toBeNull();
+  });
+
+  it('changes the name', () => {
+    const handleChangeName = jest.fn();
+
+    const { queryByPlaceholderText } = render((
+      <Input
+        onChange={handleChangeName}
+      />
+    ));
+
+    fireEvent.change(queryByPlaceholderText('이름'), {
+      target: { value: '고수닭갈비' },
+    });
+
+    expect(handleChangeName).toBeCalled();
   });
 });
