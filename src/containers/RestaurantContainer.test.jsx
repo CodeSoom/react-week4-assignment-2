f3cfa558b3c5ded59e2deddc1355f55be37daedc
이찬호 <0407chan@naver.com>
2022-04-15 19:12:43 +0900

    refactor. given2로 식당 유무 반영

--- src/containers/RestaurantContainer.test.jsx
+++ src/containers/RestaurantContainer.test.jsx
@@ -1,5 +1,7 @@
 import { render } from '@testing-library/react';
 import { useDispatch, useSelector } from 'react-redux';
+import given from 'given2';
+import 'given2/setup';
 
 import RestaurantContainer from './RestaurantContainer';
 
@@ -7,8 +9,18 @@ jest.mock('react-redux');
 
 describe('RestaurantContainer', () => {
   const dispatch = jest.fn();
-
   useDispatch.mockImplementation(() => dispatch);
+
+  given('restaurants', () => [{
+    id: 1,
+    name: '두향',
+    category: '한식',
+    address: '성남시 분당구',
+  }]);
+
+  useSelector.mockImplementation((selector) => selector({
+    restaurants: given.restaurants,
+  }));
 
   beforeEach(() => {
     jest.clearAllMocks();
@@ -18,23 +30,14 @@ describe('RestaurantContainer', () => {
     return render(<RestaurantContainer />);
   }
 
-  function returnUseSelector({ restaurants }) {
-    return useSelector.mockImplementation((selector) => selector({
-      restaurants,
-    }));
-  }
-
-  returnUseSelector({
-    restaurants: [{
-      id: 1,
-      name: '두향',
-      category: '한식',
-      address: '성남시 분당구',
-    }],
-  });
-
   context('with restaurants', () => {
     it('renders restaurants', () => {
+      given('restaurants', () => [{
+        id: 1,
+        name: '두향',
+        category: '한식',
+        address: '성남시 분당구',
+      }]);
       const { queryByText } = renderRestaurantContainer();
 
       expect(queryByText(/두향 | 한식 | 성남시 분당구/)).not.toBeNull();
@@ -43,7 +46,7 @@ describe('RestaurantContainer', () => {
 
   context('without restaurants', () => {
     it('renders "식당이 없어요!"', () => {
-      returnUseSelector([]);
+      given('restaurants', () => []);
 
       const { container } = renderRestaurantContainer();
       expect(container).toHaveTextContent(/식당이 없어요!/);
