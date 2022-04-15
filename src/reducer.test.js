5a0178f18aa95bf5d4472b0f3a4a35db942d7ece
0hu <dladudgn123@naver.com>
2022-04-15 11:39:54 +0900

    Test - 레스토랑 이름 카테고리 주소 초기화 테스트

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -16,16 +16,32 @@ describe('reducer', () => {
         restaurants: [],
       }, addRestaurant({ restaurantsName, category, address }));
     }
-    it('appends a new name,category,address in restaurants', () => {
-      const state = reduceAddRestaurant({
-        restaurantsName: '쿠스쿠스',
-        category: '모로코식',
-        address: '이태원동',
+    context(' with name, category, address', () => {
+      it('appends a new name,category,address in restaurants', () => {
+        const state = reduceAddRestaurant({
+          restaurantsName: '쿠스쿠스',
+          category: '모로코식',
+          address: '이태원동',
+        });
+
+        expect(state.restaurants).toHaveLength(1);
+        expect(state.restaurants[0].id).not.toBeUndefined();
+        expect(state.restaurants[0].name).toBe('쿠스쿠스');
+        expect(state.restaurants[0].category).toBe('모로코식');
+        expect(state.restaurants[0].address).toBe('이태원동');
       });
 
-      expect(state.restaurants).toHaveLength(1);
-      expect(state.restaurants[0].id).not.toBeUndefined();
-      expect(state.restaurants[0].name).toBe('쿠스쿠스');
+      it('clear name,category,address in restaurants', () => {
+        const state = reduceAddRestaurant({
+          restaurantsName: '쿠스쿠스',
+          category: '모로코식',
+          address: '이태원동',
+        });
+
+        expect(state.restaurants.name).toBe('');
+        expect(state.restaurants.category).toBe('');
+        expect(state.restaurants.address).toBe('');
+      });
     });
   });
 });
