bbee8ab970d9906af5462b9571ffe19b3e68e6ad
free-ko <duddnr654@naver.com>
2022-04-15 21:54:15 +0900

    Refactor: renders input test code

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -3,21 +3,14 @@ import { render } from '@testing-library/react';
 import Input from './Input';
 
 describe('Input', () => {
-  it('renders three input box', () => {
-    const { queryByPlaceholderText } = render((
+  it('renders three input box and button', () => {
+    const { queryByPlaceholderText, queryByText } = render((
       <Input />
     ));
 
     expect(queryByPlaceholderText(/이름/)).not.toBeNull();
     expect(queryByPlaceholderText(/분류/)).not.toBeNull();
     expect(queryByPlaceholderText(/주소/)).not.toBeNull();
-  });
-
-  it('renders button', () => {
-    const { queryByText } = render((
-      <Input />
-    ));
-
     expect(queryByText(/등록/)).not.toBeNull();
   });
 });
