0a3b4a38cf85e55366f8f03d3f834758481ed9fb
kjoobin <imkj1148@gmail.com>
2022-04-16 11:55:56 +0900

    App 테스트 에 react-redux mock 연결

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,18 @@
 import { render } from '@testing-library/react';
 
+import { useSelector } from 'react-redux';
+
 import App from './App';
 
+import restaurants from '../fixtures/restaurants';
+
+jest.mock('react-redux');
+
 describe('App', () => {
+  useSelector.mockImplementation((selector) => selector({
+    restaurants,
+  }));
+
   it('renders restaurant', () => {
     const { queryByText } = render((
       <App />
