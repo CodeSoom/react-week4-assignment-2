bf123345e89f2d3fcb391b61c105397c5301f413
kjoobin <imkj1148@gmail.com>
2022-04-16 14:16:37 +0900

    reducer changeRestaurantField 테스트

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,8 +1,9 @@
 import reducer from './reducer';
 
-import { setRestaurants } from './actions';
+import { changeRestaurantField, setRestaurants } from './actions';
 
 import restaurants from '../fixtures/restaurants';
+import restaurant from '../fixtures/restaurant';
 
 describe('Reducer', () => {
   beforeEach(() => {
@@ -18,6 +19,17 @@ describe('Reducer', () => {
       const state = reducer(initialState, setRestaurants(restaurants));
 
       expect(state.restaurants).not.toHaveLength(0);
+    });
+  });
+
+  describe('changeRestaurantField', () => {
+    const initialState = {
+      restaurant,
+    };
+    it('return change restaurant field value', () => {
+      const state = reducer(initialState, changeRestaurantField({ name: 'address', value: '서울시 종로구' }));
+
+      expect(state.restaurant.address).toBe('서울시 종로구');
     });
   });
 });
