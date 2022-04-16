5d83e9da465ccda3989e68581427abc11ed0c3f2
kjoobin <imkj1148@gmail.com>
2022-04-16 11:19:54 +0900

    레스토랑 목록 보이기 서비스 분리

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
+import Restaurants from './Restaurants';
+
 export default function App() {
   return (
     <div>
       <h1>Restaurans</h1>
-      <ul>
-        <li>
-          김밥제국
-        </li>
-      </ul>
+      <Restaurants />
+
     </div>
   );
 }
---
+++ src/Restaurants.jsx
@@ -0,0 +1,9 @@
+export default function Restaurants() {
+  return (
+    <ul>
+      <li>
+        김밥제국
+      </li>
+    </ul>
+  );
+}
