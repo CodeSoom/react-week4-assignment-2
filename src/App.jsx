6464c431b3bcb8fbfd1b428a730ea5181a0b7dc3
kjoobin <imkj1148@gmail.com>
2022-04-16 14:43:58 +0900

    오타 수정

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ export default function App() {
 
   return (
     <div>
-      <h1>Restaurans</h1>
+      <h1>Restaurants</h1>
       <RestaurantsContainer />
       <RestaurantCreateContainer />
     </div>
