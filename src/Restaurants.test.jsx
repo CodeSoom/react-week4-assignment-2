4f277971f15c68b6a0839f1a94fb888a1a1bd447
kjoobin <imkj1148@gmail.com>
2022-04-16 11:47:40 +0900

    restaurants fixture 생성

--- src/Restaurants.test.jsx
+++ src/Restaurants.test.jsx
@@ -2,14 +2,10 @@ import { render } from '@testing-library/react';
 
 import Restaurants from './Restaurants';
 
+import restaurants from '../fixtures/restaurants';
+
 describe('Restaurans', () => {
   it('renders restaurant', () => {
-    const restaurants = [
-      {
-        id: 1, title: '김밥제국', category: '분식', address: '서울시 강남구 역삼동',
-      },
-    ];
-
     const { queryByText } = render((
       <Restaurants Restaurants={restaurants} />
     ));
--- src/RestaurantsContainer.test.jsx
+++ src/RestaurantsContainer.test.jsx
@@ -4,15 +4,11 @@ import { useSelector } from 'react-redux';
 
 import RestaurantsContainer from './RestaurantsContainer';
 
+import restaurants from '../fixtures/restaurants';
+
 jest.mock('react-redux');
 
 describe('RestaurantsContainer', () => {
-  const restaurants = [
-    {
-      id: 1, name: '김밥제국', category: '분식', address: '서울시 강남구 역삼동',
-    },
-  ];
-
   useSelector.mockImplementation((selector) => selector({
     restaurants,
   }));
