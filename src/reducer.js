89475afe47cd119e1c71ec0309a0d28133c875bf
kjoobin <imkj1148@gmail.com>
2022-04-16 14:18:44 +0900

    changeRestaurantField 리듀서 구현

--- src/reducer.js
+++ src/reducer.js
@@ -1,4 +1,9 @@
 const initialState = {
+  restaurant: {
+    name: '마법사식당',
+    category: '이탈리안',
+    address: '서울시 강남구',
+  },
   restaurants: [
     {
       id: 1, name: '김밥제국', category: '분식', address: '서울시 강남구 역삼동',
@@ -15,5 +20,19 @@ export default function reducer(state = initialState, action) {
       restaurants,
     };
   }
+
+  if (action.type === 'changeRestaurantField') {
+    const { restaurant } = state;
+    const { name, value } = action.payload;
+
+    return {
+      ...state,
+      restaurant: {
+        ...restaurant,
+        [name]: value,
+      },
+    };
+  }
+
   return state;
 }
