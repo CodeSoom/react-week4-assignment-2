e926ddad814fad90ca88e460c4f9edda33781646
rukeni <rukeni@naver.com>
2022-05-24 17:10:47 +0900

    Test: 레스토랑 리스트에 이름, 분류, 주소가 나타나야 한다.

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -7,4 +7,13 @@ describe('App', () => {
     const element = getByRole('heading');
     expect(element).toHaveTextContent(/Restaurants/i);
   });
+
+  it('renders a list of restaurants', () => {
+    const { getByRole } = render(<App />);
+    const element = getByRole('list');
+
+    expect(element).toHaveTextContent(/마녀주방 | 한식 | 서울시 강남구/i);
+    expect(element).toHaveTextContent(/시카고피자 | 양식 | 이태원동/i);
+    expect(element).toHaveTextContent(/키와미 | 일식 | 분당구 정자동/i);
+  });
 });
