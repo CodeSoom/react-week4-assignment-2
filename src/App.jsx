7601a29536922fbb3472e8542369c6ea366b9d91
rukeni <rukeni@naver.com>
2022-05-24 17:11:09 +0900

    Add: 레스토랑 목록 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ export default function App() {
   return (
     <div>
       <h1>Restaurants</h1>
+      <ul>
+        <li>마녀주방 | 한식 | 서울시 강남구</li>
+        <li>시카고피자 | 양식 | 이태원동</li>
+        <li>키와미 | 일식 | 분당구 정자동</li>
+      </ul>
     </div>
   );
 }
