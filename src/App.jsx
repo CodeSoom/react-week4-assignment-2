fc0d8e440ba05d2f2e222fbf1e499b2fb502327d
rukeni <rukeni@naver.com>
2022-05-24 17:15:29 +0900

    Refactor: restaurants목록을 state로 관리

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,40 @@
+import { useState } from 'react';
+
 export default function App() {
+  const state = [{
+    id: 1,
+    name: '마녀주방',
+    address: '한식',
+    category: '서울시 강남구',
+  }, {
+    id: 2,
+    name: '시카고피자',
+    address: '양식',
+    category: '이태원동',
+  }, {
+    id: 3,
+    name: '키와미',
+    address: '일식',
+    category: '분당구 정자동',
+  }];
+  const [restaurants, setRestaurants] = useState(state);
+
   return (
     <div>
       <h1>Restaurants</h1>
       <ul>
-        <li>마녀주방 | 한식 | 서울시 강남구</li>
-        <li>시카고피자 | 양식 | 이태원동</li>
-        <li>키와미 | 일식 | 분당구 정자동</li>
+        {restaurants.map((restaurant) => (
+          <li key={restaurant.id}>
+            {restaurant.name}
+            |
+            {' '}
+            {restaurant.address}
+            {' '}
+            |
+            {' '}
+            {restaurant.category}
+          </li>
+        ))}
       </ul>
     </div>
   );
