b3a3b4c46998483d2d585edfd3882c89fbc9ba27
rukeni <rukeni@naver.com>
2022-05-26 16:59:56 +0900

    Test: textContent가 있는지 단언 -> text가 있는 element가 있는지 단언하도록 리팩토링

--- src/RestaurantList.test.jsx
+++ src/RestaurantList.test.jsx
@@ -1,37 +1,24 @@
 import { render } from '@testing-library/react';
+
 import RestaurantList from './RestaurantList';
+import restaurants from '../fixtures/restaurants';
 
 describe('RestaurantList', () => {
   context('without restaurants', () => {
     it("doesn't exist", () => {
-      const { queryByRole } = render(<RestaurantList restaurants={[]} />);
-      expect(queryByRole('list')).toBeNull();
+      const { getByRole } = render(<RestaurantList restaurants={[]} />);
+      expect(getByRole('list').children).toHaveLength(0);
     });
   });
   context('with restaurants', () => {
     it('renders a list of restaurants', () => {
-      const state = [{
-        id: 1,
-        name: '마녀주방',
-        address: '한식',
-        category: '서울시 강남구',
-      }, {
-        id: 2,
-        name: '시카고피자',
-        address: '양식',
-        category: '이태원동',
-      }, {
-        id: 3,
-        name: '키와미',
-        address: '일식',
-        category: '분당구 정자동',
-      }];
-      const { getByRole } = render(<RestaurantList restaurants={state} />);
-      const element = getByRole('list');
+      const { getByText, getByRole } = render(<RestaurantList restaurants={restaurants} />);
 
-      expect(element).toHaveTextContent(/마녀주방 | 한식 | 서울시 강남구/i);
-      expect(element).toHaveTextContent(/시카고피자 | 양식 | 이태원동/i);
-      expect(element).toHaveTextContent(/키와미 | 일식 | 분당구 정자동/i);
+      expect(getByText(/마녀주방 | 한식 | 서울시 강남구/i)).not.toBeNull();
+      expect(getByText(/시카고피자 | 양식 | 이태원동/i)).not.toBeNull();
+      expect(getByText(/키와미 | 일식 | 분당구 정자동/i)).not.toBeNull();
+
+      expect(getByRole('list').children).toHaveLength(3);
     });
   });
 });
