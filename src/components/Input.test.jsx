af7efd04135e8d1ab33ab7d5399eb20cefdbb3fa
jaeyooniverse <jaeyoon4819@naver.com>
2022-05-26 16:42:07 +0900

    ListContainer 테스트 코드 작성

--- src/components/Input.test.jsx
+++ src/components/Input.test.jsx
@@ -1,3 +1,16 @@
+import { render } from '@testing-library/react';
+
+import Input from './Input';
+
 describe('Input', () => {
-  it('이름, 분류, 주소 input을 렌더한다', () => {});
+  const renderInput = () => render(<Input />);
+
+  it('이름, 분류, 주소 input, 등록 버튼을 렌더한다', () => {
+    const { getByPlaceholderText, getByRole } = renderInput();
+
+    expect(getByPlaceholderText('이름')).toBeInTheDocument();
+    expect(getByPlaceholderText('분류')).toBeInTheDocument();
+    expect(getByPlaceholderText('주소')).toBeInTheDocument();
+    expect(getByRole('button')).toBeInTheDocument();
+  });
 });
---
+++ src/containers/ListContainer.test.jsx
@@ -0,0 +1,28 @@
+import { render } from '@testing-library/react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import ListContainer from './ListContainer';
+
+import restaurants from '../../fixtures/RestaurantsData';
+
+describe('ListContainer', () => {
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
+  useSelector.mockImplementation((selector) => selector({ restaurants }));
+
+  const renderListContainer = () => render(<ListContainer />);
+
+  context('restaurants가 있을 때', () => {
+    it('식당들을 출력한다', () => {
+      const { container } = renderListContainer();
+
+      expect(container).toHaveTextContent('마녀식당');
+      expect(container).toHaveTextContent('양식');
+      expect(container).toHaveTextContent('분당구 정자동');
+    });
+  });
+
+  // TODO: '등록된 식당이 없어요' 기능 추가가 필요할 때, 테스트 코드를 작성한다.
+  context('restaurants가 없을 때', () => {});
+});
