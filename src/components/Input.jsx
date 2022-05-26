9eb35c09f230f70021bbb4566daf34c0c7aec2a2
jaeyooniverse <jaeyoon4819@naver.com>
2022-05-26 16:01:07 +0900

    InputContainer 테스트, 코드 작성

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,11 +1,37 @@
 import React from 'react';
 
-export default function Input() {
+export default function Input({
+  nameValue,
+  categoryValue,
+  addressValue,
+  onChangeRestaurantName,
+  onChangeRestaurantCategory,
+  onChangeRestaurantAddress,
+  onClickAddRestaurant,
+}) {
   return (
-    <>
-      <input placeholder='이름' />
-      <input placeholder='분류' />
-      <input placeholder='주소' />
-    </>
+    <p>
+      <input
+        type='text'
+        placeholder='이름'
+        value={nameValue}
+        onChange={onChangeRestaurantName}
+      />
+      <input
+        type='text'
+        placeholder='분류'
+        value={categoryValue}
+        onChange={onChangeRestaurantCategory}
+      />
+      <input
+        type='text'
+        placeholder='주소'
+        value={addressValue}
+        onChange={onChangeRestaurantAddress}
+      />
+      <button type='button' onClick={onClickAddRestaurant}>
+        등록
+      </button>
+    </p>
   );
 }
