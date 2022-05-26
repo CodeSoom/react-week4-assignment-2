a09e058fea7229ff09ec39ec515ac390d6ee861c
rukeni <rukeni@naver.com>
2022-05-26 18:50:45 +0900

    Refactor: redux 적용하여 상태 로직 분리

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,34 @@
-import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
 import RestaurantList from './RestaurantList';
-
 import TextBox from './TextBox';
+import { updateRestaurantInfo, addRestaurant } from './actions';
 
-function updateRestaurantInfo(state, name, value) {
+function selector(state) {
   return {
-    ...state,
-    restaurantInfo: {
-      [name]: value,
-    },
-  };
-}
-
-function addRestaurant(state) {
-  const { restaurants, restaurantInfo } = state;
-  return {
-    restaurants: [
-      ...restaurants,
-      restaurantInfo,
-    ],
-    restaurantInfo: {
-      name: '',
-      category: '',
-      address: '',
-    },
+    restaurantInfo: state.restaurantInfo,
+    restaurants: state.restaurants,
   };
 }
 
 export default function App() {
-  const initialState = {
-    restaurantInfo: {
-      name: '',
-      address: '',
-      category: '',
-    },
-    restaurants: [{
-      id: 1,
-      name: '마녀주방',
-      address: '한식',
-      category: '서울시 강남구',
-    }, {
-      id: 2,
-      name: '시카고피자',
-      address: '양식',
-      category: '이태원동',
-    }, {
-      id: 3,
-      name: '키와미',
-      address: '일식',
-      category: '분당구 정자동',
-    }],
-  };
+  const dispatch = useDispatch();
 
-  const [state, setState] = useState(initialState);
+  const { restaurantInfo, restaurants } = useSelector(selector);
 
   const handleChange = (event) => {
     const { name, value } = event.target;
-    setState((previousState) => (updateRestaurantInfo(previousState, name, value)));
+    dispatch(updateRestaurantInfo(name, value));
   };
   const handleClick = () => {
-    setState((previousState) => (addRestaurant(previousState)));
+    dispatch((addRestaurant()));
   };
   return (
     <div>
       <h1>Restaurants</h1>
-      <RestaurantList restaurants={state.restaurants} />
+      <RestaurantList restaurants={restaurants} />
       <TextBox
-        restaurantInfo={state.restaurantInfo}
+        restaurantInfo={restaurantInfo}
         onChange={handleChange}
         onClick={handleClick}
       />
