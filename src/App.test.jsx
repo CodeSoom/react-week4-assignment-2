d8f71e4f3dab5f00a91e4a8f231ed970922251d6
rukeni <rukeni@naver.com>
2022-05-27 14:27:56 +0900

    Test: 하위에서 테스트하는 코드 제거

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,41 +1,27 @@
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
+
+import { useSelector, useDispatch } from 'react-redux';
 
 import App from './App';
+import restaurants from '../fixtures/restaurants';
 
-describe('App', () => {
-  it('renders headline', () => {
-    const { getByRole } = render(<App />);
-    const element = getByRole('heading');
-    expect(element).toHaveTextContent(/Restaurants/i);
-  });
+jest.mock('react-redux');
 
-  it('renders a list of restaurants', () => {
-    const { getByRole } = render(<App />);
-    const element = getByRole('list');
+const dispatch = useDispatch();
 
-    expect(element).toHaveTextContent(/마녀주방 | 한식 | 서울시 강남구/i);
-    expect(element).toHaveTextContent(/시카고피자 | 양식 | 이태원동/i);
-    expect(element).toHaveTextContent(/키와미 | 일식 | 분당구 정자동/i);
-  });
+test('App', () => {
+  useDispatch.mockImplementation(() => dispatch);
 
-  it('renders a TextBox', () => {
-    const { getByRole, getByPlaceholderText } = render(<App />);
+  useSelector.mockImplementation((selector) => selector({
+    restaurants,
+    restaurantInfo: {
+      name: '',
+      address: '',
+      phone: '',
+    },
+  }));
 
-    const list = getByRole('list');
-    const name = getByPlaceholderText('이름');
-    const category = getByPlaceholderText('분류');
-    const address = getByPlaceholderText('주소');
-
-    fireEvent.change(name, { target: { value: '마녀주방' } });
-    fireEvent.change(category, { target: { value: '한식' } });
-    fireEvent.change(address, { target: { value: '서울시 강남구' } });
-
-    expect(name).toHaveValue('마녀주방');
-    expect(category).toHaveValue('한식');
-    expect(address).toHaveValue('서울시 강남구');
-
-    fireEvent.click(getByRole('button'));
-
-    expect(list).toHaveTextContent(/마녀주방 | 한식 | 서울시 강남구/i);
-  });
+  const { getByText } = render(<App />);
+  const element = getByText(/Restaurants/);
+  expect(element).toBeInTheDocument();
 });
