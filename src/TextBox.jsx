9dc1119e6258c0ee3eee10f56d73b36972739d45
rukeni <rukeni@naver.com>
2022-05-27 14:52:42 +0900

    Refactor: input -> Input컴포넌트로 대체

--- src/TextBox.jsx
+++ src/TextBox.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import Input from './Input';
 
 export default function TextBox({ onChange, restaurantInfo, onClick }) {
+  const { name, category, address } = restaurantInfo;
   return (
     <>
-      <input type="text" name="name" placeholder="이름" onChange={onChange} defaultValue={restaurantInfo.name} />
-      <input type="text" name="category" placeholder="분류" onChange={onChange} defaultValue={restaurantInfo.category} />
-      <input type="text" name="address" placeholder="주소" onChange={onChange} defaultValue={restaurantInfo.address} />
+      <Input name="name" placeholder="이름" onChange={onChange} value={name} />
+      <Input name="category" placeholder="분류" onChange={onChange} value={category} />
+      <Input name="address" placeholder="주소" onChange={onChange} value={address} />
       <button type="button" onClick={onClick}>등록</button>
     </>
   );
