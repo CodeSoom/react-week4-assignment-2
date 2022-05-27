bf554849ac242ed7c58995ab96209e661b6227ea
rukeni <rukeni@naver.com>
2022-05-27 15:42:40 +0900

    Modify: initialState restaurants 빈배열로 변경

--- src/reducer.js
+++ src/reducer.js
@@ -4,25 +4,10 @@ const initialState = {
     address: '',
     category: '',
   },
-  restaurants: [{
-    id: 1,
-    name: '마녀주방',
-    address: '한식',
-    category: '서울시 강남구',
-  }, {
-    id: 2,
-    name: '시카고피자',
-    address: '양식',
-    category: '이태원동',
-  }, {
-    id: 3,
-    name: '키와미',
-    address: '일식',
-    category: '분당구 정자동',
-  }],
+  restaurants: [],
 };
 
-export default function reducer(state = initialState, action) {
+export default function reducer(state = initialState, action = '') {
   if (action.type === 'addRestaurant') {
     const { restaurants, restaurantInfo } = state;
     return {
@@ -42,6 +27,7 @@ export default function reducer(state = initialState, action) {
     return {
       ...state,
       restaurantInfo: {
+        ...state.restaurantInfo,
         [name]: value,
       },
     };
