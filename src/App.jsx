7942160ce4db9ea27c95b00bc3db3c40f459f1f6
rukeni <rukeni@naver.com>
2022-05-27 16:47:08 +0900

    Refactor: 상태처리 App에서 TextBox로 이동

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,12 @@
-import { useSelector, useDispatch } from 'react-redux';
-
 import RestaurantList from './RestaurantList';
 import TextBox from './TextBox';
-import { updateRestaurantInfo, addRestaurant } from './actions';
-
-function selector(state) {
-  return {
-    restaurantInfo: state.restaurantInfo,
-    restaurants: state.restaurants,
-  };
-}
 
 export default function App() {
-  const dispatch = useDispatch();
-
-  const { restaurantInfo, restaurants } = useSelector(selector);
-
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    dispatch(updateRestaurantInfo(name, value));
-  };
-  const handleClick = () => {
-    dispatch((addRestaurant()));
-  };
   return (
     <div>
       <h1>Restaurants</h1>
       <RestaurantList restaurants={restaurants} />
-      <TextBox
-        restaurantInfo={restaurantInfo}
-        onChange={handleChange}
-        onClick={handleClick}
-      />
+      <TextBox />
     </div>
   );
 }
--- src/TextBox.jsx
+++ src/TextBox.jsx
@@ -1,14 +1,31 @@
+import { useSelector, useDispatch } from 'react-redux';
+
+import { updateRestaurantInfo, addRestaurant } from './actions';
+
 import Input from './Input';
 import Button from './Button';
 
-export default function TextBox({ onChange, restaurantInfo, onClick }) {
+export default function TextBox() {
+  const { restaurantInfo } = useSelector((state) => ({
+    restaurantInfo: state.restaurantInfo,
+  }));
   const { name, category, address } = restaurantInfo;
+
+  const dispatch = useDispatch();
+
+  const handleChange = (event) => {
+    dispatch(updateRestaurantInfo(event.target.name, event.target.value));
+  };
+
+  const handleClick = () => {
+    dispatch((addRestaurant()));
+  };
   return (
     <>
-      <Input name="name" placeholder="이름" onChange={onChange} value={name} />
-      <Input name="category" placeholder="분류" onChange={onChange} value={category} />
-      <Input name="address" placeholder="주소" onChange={onChange} value={address} />
-      <Button onClick={onClick} />
+      <Input name="name" placeholder="이름" onChange={handleChange} value={name} />
+      <Input name="category" placeholder="분류" onChange={handleChange} value={category} />
+      <Input name="address" placeholder="주소" onChange={handleChange} value={address} />
+      <Button onClick={handleClick} />
     </>
   );
 }
