19756b99ad352490295f0cb2789b61af88259e9b
rukeni <rukeni@naver.com>
2022-05-29 01:54:22 +0900

    Test: 테스트 코드 수정

--- src/RestaurantList.test.jsx
+++ src/RestaurantList.test.jsx
@@ -13,7 +13,7 @@ describe('RestaurantList', () => {
     useDispatch.mockImplementation(() => dispatch);
   });
   context('without restaurants', () => {
-    it("doesn't exist", () => {
+    it("doesn't work", () => {
       useSelector.mockImplementation((selector) => selector({
         restaurants: [],
         restaurantInfo: {
@@ -22,8 +22,8 @@ describe('RestaurantList', () => {
           address: '서울시 강남구',
         },
       }));
-      const { getByRole } = render(<RestaurantList restaurants={[]} />);
-      expect(getByRole('list').children).toHaveLength(0);
+      const { queryAllByRole } = render(<RestaurantList restaurants={[]} />);
+      expect(queryAllByRole('listitem')).toHaveLength(0);
     });
   });
   context('with restaurants', () => {
@@ -46,13 +46,13 @@ describe('RestaurantList', () => {
           address: '서울시 강남구',
         },
       }));
-      const { getByText, getByRole } = render(<RestaurantList />);
+      const { getByText, getAllByRole } = render(<RestaurantList />);
 
       expect(getByText(/마녀주방 | 한식 | 서울시 강남구/i)).not.toBeNull();
       expect(getByText(/시카고피자 | 양식 | 이태원동/i)).not.toBeNull();
       expect(getByText(/키와미 | 일식 | 분당구 정자동/i)).not.toBeNull();
 
-      expect(getByRole('list').children).toHaveLength(3);
+      expect(getAllByRole('listitem')).toHaveLength(3);
     });
   });
 });
