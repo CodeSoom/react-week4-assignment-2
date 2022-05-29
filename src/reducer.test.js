a96ebde701457142f186ebb13784ad76bff258fd
Ga-Young Song <dolphin2440@naver.com>
2022-05-29 22:03:04 +0900

    Add reducer test case

---
+++ src/reducer.test.js
@@ -0,0 +1,22 @@
+import addRestaurant from "./actions";
+
+import reducer from "./reducer";
+
+describe('reducer', () => {
+  describe('addRestaurant', () => {
+    it('appends a new restaurant into restaurants', () => {
+      const restaurant = {
+        name: '마녀 주방',
+        category: '한식',
+        address: '서울시 강남구',
+      }
+      const state = reducer({
+        restaurant,
+      }, addRestaurant(restaurant));
+
+      expect(state.restaurant.name).toBe('마녀 주방');
+      expect(state.restaurant.category).toBe('한식');
+      expect(state.restaurant.address).toBe('서울시 강남구');
+    });
+  });
+});
