966604bcafc9f0e8adf30bf31e3e3203000370cb
Ga-Young Song <dolphin2440@naver.com>
2022-05-29 22:42:36 +0900

    Add test case of reducer

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -20,4 +20,34 @@ describe('reducer', () => {
       expect(state.restaurant.address).toBe('서울시 강남구');
     });
   });
+
+  describe('updateName', () => {
+    it('change the name of restaurant', () => {
+      const state = reducer({
+        restaurant: { name: '' },
+      }, updateName('시카고 피자'));
+
+      expect(state.restaurant.name).toBe('시카고 피자');
+    });
+  });
+
+  describe('updateCategory', () => {
+    it('change the category of restaurant', () => {
+      const state = reducer({
+        restaurant: { category: '' },
+      }, updateCategory('양식'));
+
+      expect(state.restaurant.category).toBe('양식');
+    });
+  });
+
+  describe('updateAddress', () => {
+    it('change the address of restaurant', () => {
+      const state = reducer({
+        restaurant: { address: '' },
+      }, updateAddress('이태원'));
+
+      expect(state.restaurant.address).toBe('이태원');
+    });
+  });
 });
