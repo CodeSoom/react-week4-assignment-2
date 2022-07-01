61d2339301df28d240dc28ceb42099f3ad8a6503
minsuk <alstjr0183@naver.com>
2022-07-01 20:50:14 +0900

    fix: restaurant-dummy 수정 restaurants 데이터의 adress 부분을 수정했습니다

--- src/fixtures/restaurant-dummy.js
+++ src/fixtures/restaurant-dummy.js
@@ -6,6 +6,6 @@ export const restaurantAdress = '주소';
 
 export const restaurants = [
   {
-    id: 1, restaurantName: '한식뷔페', restaurantType: '한식', restaurantAdress: '주소',
+    id: 1, restaurantName: '한식뷔페', restaurantType: '한식', restaurantAdress: '서울특별시 신림동',
   },
 ];
