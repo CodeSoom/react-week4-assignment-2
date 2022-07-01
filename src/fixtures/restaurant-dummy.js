bb749d0362ad5c1180b85f3c8941c66c5bab00bf
minsuk <alstjr0183@naver.com>
2022-07-01 20:33:17 +0900

    feat: fixtures - restaurant 셋팅 자주 쓸법한 테스트 변수들을 셋팅해주었습니다

---
+++ src/fixtures/restaurant-dummy.js
@@ -0,0 +1,11 @@
+export const restaurantName = '한식뷔페';
+
+export const restaurantType = '한식';
+
+export const restaurantAdress = '주소';
+
+export const restaurants = [
+  {
+    id: 1, restaurantName: '한식뷔페', restaurantType: '한식', restaurantAdress: '주소',
+  },
+];
