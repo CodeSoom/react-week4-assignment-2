1a731ae27cc48c09402d8f79589beb571ad8d288
Woo Dy <kjop7419@naver.com>
2022-07-01 22:47:26 +0900

    refactor(App) 테스트에 대해 더 명확한 표현 적용

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -3,13 +3,11 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 describe('App', () => {
-  context('App이 렌더링 될 때', () => {
-    it('헤더가 보여집니다.', () => {
-      const { getByText } = render((
-        <App />
-      ));
+  it('App이 렌더링 될 때, 헤더가 보여집니다.', () => {
+    const { getByText } = render((
+      <App />
+    ));
 
-      expect(getByText(/Restaurants/)).not.toBeNull();
-    });
+    expect(getByText(/Restaurants/)).not.toBeNull();
   });
 });
