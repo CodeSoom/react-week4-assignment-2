452dd31858e82cfa84af0de333b70570e06b6eac
minsuk <alstjr0183@naver.com>
2022-07-01 20:51:02 +0900

    feat: app test 코드 작성 app쪽에 렌더링 관련 테스트 코드를 작성하였습니다

---
+++ src/App.test.jsx
@@ -0,0 +1,49 @@
+import { render } from '@testing-library/react';
+
+import { useSelector } from 'react-redux';
+
+import App from './App';
+
+import { restaurants } from './fixtures/restaurant-dummy';
+
+jest.mock('react-redux');
+
+describe('App', () => {
+  useSelector.mockImplementation((selector) => selector({
+    Restaurants: restaurants,
+  }));
+
+  const renderApp = () => render((
+    <App />
+  ));
+
+  it('title을 렌더링한다', () => {
+    const { getByText } = renderApp();
+
+    expect(getByText(/Restaurants/)).not.toBeNull();
+  });
+
+  it('input-button을 렌더링한다', () => {
+    const { getByText } = renderApp();
+
+    expect(getByText(/완료/)).not.toBeNull();
+  });
+
+  it('restaurants-restaurantName을 렌더링한다', () => {
+    const { getByText } = renderApp();
+
+    expect(getByText(/한식 뷔페/)).not.toBeNull();
+  });
+
+  it('restaurants-restaurantType을 렌더링한다', () => {
+    const { getByText } = renderApp();
+
+    expect(getByText(/한식/)).not.toBeNull();
+  });
+
+  it('restaurants-restaurantAdress을 렌더링한다', () => {
+    const { getByText } = renderApp();
+
+    expect(getByText(/서울특별시 신림동/)).not.toBeNull();
+  });
+});
