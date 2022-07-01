ca4e7c15bbdab57b09fe74d150130aed29cfa3e3
Woo Dy <kjop7419@naver.com>
2022-07-01 23:56:20 +0900

    fix (Item) 테스트 통과

---
+++ src/components/Item.test.jsx
@@ -0,0 +1,21 @@
+import { render } from '@testing-library/react';
+
+import Item from './Item';
+
+describe('Item', () => {
+  it('렌더링시 레스토랑 목록이 보여집니다', () => {
+    const restaurants = {
+      id: 1, name: '마녀주방', category: '한식', address: '잠실',
+    };
+
+    const { container } = render((
+      <Item
+        restaurants={restaurants}
+      />
+    ));
+
+    expect(container).toHaveTextContent('마녀주방');
+    expect(container).toHaveTextContent('한식');
+    expect(container).toHaveTextContent('잠실');
+  });
+});
