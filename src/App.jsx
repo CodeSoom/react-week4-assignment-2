68cccfc75273023bd69096823cbbf18ca440858d
Woo Dy <kjop7419@naver.com>
2022-07-02 00:27:36 +0900

    test (Input) 테스트코드 작성

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
+import ListContainer from './container/ListContainer';
+
 export default function App() {
   return (
-    <h1>Restaurants</h1>
+    <>
+      <h1>Restaurants</h1>
+      <ListContainer />
+    </>
   );
 }
---
+++ src/components/Input.test.jsx
@@ -0,0 +1,104 @@
+import { render, fireEvent } from '@testing-library/react';
+
+import Input from './Input';
+
+describe('Input', () => {
+  const handleChangeName = jest.fn();
+  const handleChangeCategory = jest.fn();
+  const handleChangeAddress = jest.fn();
+  const handleSubmit = jest.fn();
+
+  const restaurant = {
+    name: '마녀주방',
+    category: '한식',
+    address: '잠실',
+  };
+
+  const renderInput = render((
+    <Input
+      restaurant={restaurant}
+      onChangeName={handleChangeName}
+      onChangeCategory={handleChangeCategory}
+      onChangeAddress={handleChangeAddress}
+      onSubmit={handleSubmit}
+    />
+  ));
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('name input이 보여집니다.', () => {
+    const { getByPlaceholderText } = renderInput();
+
+    expect(getByPlaceholderText('이름')).toBeInTheDocument();
+  });
+
+  it('category input이 보여집니다.', () => {
+    const { getByPlaceholderText } = renderInput();
+
+    expect(getByPlaceholderText('분류')).toBeInTheDocument();
+  });
+
+  it('address input이 보여집니다.', () => {
+    const { getByPlaceholderText } = renderInput();
+
+    expect(getByPlaceholderText('주소')).toBeInTheDocument();
+  });
+
+  it('button이 보여집니다.', () => {
+    const { getByText } = renderInput();
+
+    expect(getByText('등록')).toHaveTextContent();
+  });
+
+  it('name이 restaurant의 name과 같습니다', () => {
+    const { getByPlaceholderText } = renderInput();
+
+    expect(getByPlaceholderText('이름')).toHaveValue(restaurant.name);
+  });
+
+  it('category가 restaurant의 category와 같습니다', () => {
+    const { getByPlaceholderText } = renderInput();
+
+    expect(getByPlaceholderText('분류')).toHaveValue(restaurant.category);
+  });
+
+  it('address가 restaurant의 address와 같습니다', () => {
+    const { getByPlaceholderText } = renderInput();
+
+    expect(getByPlaceholderText('주소')).toHaveValue(restaurant.address);
+  });
+
+  it('name을 변경하면 handleChangeName이 호출됩니다.', () => {
+    const { getByPlaceholderText } = renderInput();
+
+    fireEvent.change(getByPlaceholderText('이름'), { target: { value: '김가네' } });
+
+    expect(handleChangeName).toBeCalled();
+  });
+
+  it('category를 변경하면 handleChangeCategory가 호출됩니다.', () => {
+    const { getByPlaceholderText } = renderInput();
+
+    fireEvent.change(getByPlaceholderText('분류'), { target: { value: '분식' } });
+
+    expect(handleChangeName).toBeCalled();
+  });
+
+  it('address를 변경하면 handleChangeAddress가 호출됩니다.', () => {
+    const { getByPlaceholderText } = renderInput();
+
+    fireEvent.change(getByPlaceholderText('주소'), { target: { value: '잠실' } });
+
+    expect(handleChangeName).toBeCalled();
+  });
+
+  it('버튼을 클릭하면 onSubmit이벤트가 호출됩니다.', () => {
+    const { getByText } = renderInput();
+
+    fireEvent.click(getByText('등록'));
+
+    expect(handleSubmit).toBeCalled();
+  });
+});
