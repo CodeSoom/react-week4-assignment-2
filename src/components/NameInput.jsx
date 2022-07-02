40aee9af8e55dcb98cdd7f13f8df1776006de749
seungwoo Kim <kyh196201@gmail.com>
2022-07-02 18:17:40 +0900

    NameInput 컴포넌트 props 구현

--- src/components/NameInput.jsx
+++ src/components/NameInput.jsx
@@ -1,5 +1,5 @@
-export default function NameInput() {
+export default function NameInput({ name, onChangeName }) {
   return (
-    <input type="text" placeholder="이름" />
+    <input type="text" placeholder="이름" value={name} onChange={onChangeName} />
   );
 }
