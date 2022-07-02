4c05dfcddc98f5fc13e4d24d54228dab38005543
seungwoo Kim <kyh196201@gmail.com>
2022-07-02 18:31:43 +0900

    NameInput 테스트 코드 리팩터링
    - render 시에 name props를 하드코딩으로 입력하는 부분을 given을 이용해서 개선했습니다.

--- src/components/NameInput.test.jsx
+++ src/components/NameInput.test.jsx
@@ -1,8 +1,12 @@
 import { render, fireEvent } from '@testing-library/react';
 
+import given from 'given2';
+
 import NameInput from './NameInput';
 
 describe('<NameInput />', () => {
+  given('restaurantName', () => '');
+
   const handleChange = jest.fn();
 
   beforeEach(() => {
@@ -12,40 +16,44 @@ describe('<NameInput />', () => {
   const renderNameInput = (name = '') => render((<NameInput name={name} onChangeName={handleChange} />));
 
   it('레스토랑 이름을 입력할 수 있는 input이 보인다.', () => {
-    const { getByPlaceholderText } = renderNameInput('');
+    const { getByPlaceholderText } = renderNameInput(given.restaurantName);
 
     expect(getByPlaceholderText('이름')).toBeInTheDocument();
   });
 
   context('입력된 이름이 없으면', () => {
     it('input이 비어있다.', () => {
-      const { getByPlaceholderText } = renderNameInput('');
+      const { getByPlaceholderText } = renderNameInput(given.restaurantName);
 
-      expect(getByPlaceholderText('이름')).toHaveDisplayValue('');
+      expect(getByPlaceholderText('이름')).toHaveDisplayValue(given.restaurantName);
     });
   });
 
   context('입력된 이름이 있으면', () => {
     it('입력된 이름이 보인다.', () => {
-      const { getByPlaceholderText } = renderNameInput('마녀주방');
+      given('restaurantName', () => '마녀주방');
 
-      expect(getByPlaceholderText('이름')).toHaveDisplayValue('마녀주방');
+      const { getByPlaceholderText } = renderNameInput(given.restaurantName);
+
+      expect(getByPlaceholderText('이름')).toHaveDisplayValue(given.restaurantName);
     });
   });
 
   describe('이름 입력', () => {
     it('handleChange이 호출된다.', () => {
+      given('restaurantName', () => '마녀주방');
+
       const { getByPlaceholderText } = renderNameInput('');
 
       expect(handleChange).not.toBeCalled();
 
       fireEvent.change(getByPlaceholderText('이름'), {
         target: {
-          value: '마녀주방',
+          value: given.restaurantName,
         },
       });
 
-      expect(handleChange).toBeCalledWith('마녀주방');
+      expect(handleChange).toBeCalledWith(given.restaurantName);
     });
   });
 });
