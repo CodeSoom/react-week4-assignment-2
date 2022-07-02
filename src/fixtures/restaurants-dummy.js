a4e87e16a40a75640332a26d227ca1fe42021de1
yerin512 <73337811+yerin512@users.noreply.github.com>
2022-07-02 23:08:23 +0900

    테스트에 쓸 식당 정보 dummy 데이터를 생성
    테스트에 쓸 restaurants dummy 데이터를 따로 생성했습니다.

---
+++ src/fixtures/restaurants-dummy.js
@@ -0,0 +1,10 @@
+const restaurants = [
+  {
+    id: 1, name: '김밥천국', category: '분식', address: '역삼동',
+  },
+  {
+    id: 2, name: '피자천국', category: '양식', address: '동동동',
+  },
+];
+
+export default restaurants;
