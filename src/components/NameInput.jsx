dc432f3950a7dc973f481045496b43d9c0722e2b
seungwoo Kim <kyh196201@gmail.com>
2022-07-02 18:27:40 +0900

    NameInput handleChange 함수 구현

--- src/components/NameInput.jsx
+++ src/components/NameInput.jsx
@@ -1,5 +1,7 @@
 export default function NameInput({ name, onChangeName }) {
+  const handleChange = (event) => onChangeName(event.target.value);
+
   return (
-    <input type="text" placeholder="이름" value={name} onChange={onChangeName} />
+    <input type="text" placeholder="이름" value={name} onChange={handleChange} />
   );
 }
