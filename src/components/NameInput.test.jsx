e0923519b0a4e79ab924b02f649aec50546967de
seungwoo Kim <kyh196201@gmail.com>
2022-07-02 18:25:32 +0900

    NameInput change 이벤트에 대한 테스트

--- src/components/NameInput.test.jsx
+++ src/components/NameInput.test.jsx
@@ -1,9 +1,15 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import NameInput from './NameInput';
 
 describe('<NameInput />', () => {
-  const renderNameInput = (name = '') => render((<NameInput name={name} />));
+  const handleChange = jest.fn();
+
+  beforeEach(() => {
+    handleChange.mockClear();
+  });
+
+  const renderNameInput = (name = '') => render((<NameInput name={name} onChangeName={handleChange} />));
 
   it('레스토랑 이름을 입력할 수 있는 input이 보인다.', () => {
     const { getByPlaceholderText } = renderNameInput('');
@@ -11,11 +17,35 @@ describe('<NameInput />', () => {
     expect(getByPlaceholderText('이름')).toBeInTheDocument();
   });
 
+  context('입력된 이름이 없으면', () => {
+    it('input이 비어있다.', () => {
+      const { getByPlaceholderText } = renderNameInput('');
+
+      expect(getByPlaceholderText('이름')).toHaveDisplayValue('');
+    });
+  });
+
   context('입력된 이름이 있으면', () => {
     it('입력된 이름이 보인다.', () => {
       const { getByPlaceholderText } = renderNameInput('마녀주방');
 
       expect(getByPlaceholderText('이름')).toHaveDisplayValue('마녀주방');
+    });
+  });
+
+  describe('이름 입력', () => {
+    it('handleChange이 호출된다.', () => {
+      const { getByPlaceholderText } = renderNameInput('');
+
+      expect(handleChange).not.toBeCalled();
+
+      fireEvent.change(getByPlaceholderText('이름'), {
+        target: {
+          value: '마녀주방',
+        },
+      });
+
+      expect(handleChange).toBeCalled();
     });
   });
 });
