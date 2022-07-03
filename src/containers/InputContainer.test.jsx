5cbd9278f09f19335ab6b2dadeb69a6be23fcec9
seungwoo Kim <kyh196201@gmail.com>
2022-07-03 23:31:00 +0900

    InputContainer 입력한 이름이 있을 경우에대한 테스트 코드 작성

--- src/containers/InputContainer.test.jsx
+++ src/containers/InputContainer.test.jsx
@@ -1,13 +1,35 @@
 import { render } from '@testing-library/react';
 
+import given from 'given2';
+
+import { useSelector } from 'react-redux';
+
 import InputContainer from './InputContainer';
 
+jest.mock('react-redux');
+
 describe('<InputContainer />', () => {
+  given('restaurantName', () => '');
+
+  useSelector.mockImplementation((selector) => selector({
+    name: given.restaurantName,
+  }));
+
   const renderInputContainer = () => render((<InputContainer />));
 
   it('레스토랑 이름을 입력할 수 있는 input이 보인다.', () => {
     const { getByPlaceholderText } = renderInputContainer();
 
     expect(getByPlaceholderText('이름')).toBeInTheDocument();
+  });
+
+  context('입력된 이름이 있으면', () => {
+    it('입력된 이름이 보인다.', () => {
+      given('restaurantName', () => '마녀주방');
+
+      const { getByPlaceholderText } = renderInputContainer();
+
+      expect(getByPlaceholderText('이름')).toHaveDisplayValue(given.restaurantName);
+    });
   });
 });
