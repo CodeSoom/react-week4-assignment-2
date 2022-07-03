eceaa7854da232889a21ee0a8efc742d38907074
seungwoo Kim <kyh196201@gmail.com>
2022-07-04 01:31:57 +0900

    App 컴포넌트 테스트 통과
    - ListContainer 렌더링

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import InputContainer from './containers/InputContainer';
+import ListContainer from './containers/ListContainer';
 
 export default function App() {
   return (
     <div>
       <h1>Restaurants</h1>
       <InputContainer />
+      <ListContainer />
     </div>
   );
 }
