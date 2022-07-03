a0ff744d34b990deecf61c16ad8c68d131749711
seungwoo Kim <kyh196201@gmail.com>
2022-07-03 23:24:14 +0900

    reducer 초기 상태 테스트 코드 리팩터링

--- src/store/reducer.test.js
+++ src/store/reducer.test.js
@@ -1,13 +1,6 @@
-import reducer from './reducer';
+import reducer, { initialState } from './reducer';
 
 describe('reducer', () => {
-  const initialState = {
-    name: '',
-    category: '',
-    address: '',
-    restaurants: [],
-  };
-
   context('state가 없으면', () => {
     it('initialState를 초기 상태로 가진다.', () => {
       const state = reducer();
