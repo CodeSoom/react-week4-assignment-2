bd8468b082565f051cd9b0616eb13e0b68f1130f
seungwoo Kim <kyh196201@gmail.com>
2022-07-04 01:48:44 +0900

    reducer 테스트 통과

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -6,6 +6,52 @@ export const initialState = {
   restaurants: [],
 };
 
-const reducer = (state = initialState) => (state);
+const reducer = (state = initialState, action) => {
+  if (action.type === 'updateName') {
+    return {
+      ...state,
+      name: action.payload.name,
+    };
+  }
+
+  if (action.type === 'updateCategory') {
+    return {
+      ...state,
+      category: action.payload.category,
+    };
+  }
+
+  if (action.type === 'updateAddress') {
+    return {
+      ...state,
+      address: action.payload.address,
+    };
+  }
+
+  if (action.type === 'addRestaurant') {
+    const {
+      name,
+      address,
+      category,
+      restaurants,
+      newId,
+    } = state;
+
+    return {
+      ...state,
+      newId: newId + 1,
+      name: '',
+      category: '',
+      address: '',
+      restaurants: [...restaurants, {
+        name,
+        address,
+        category,
+      }],
+    };
+  }
+
+  return state;
+};
 
 export default reducer;
