2efc80718a6b8796b67deab60f2b8c9d2992d630
seungwoo Kim <kyh196201@gmail.com>
2022-07-04 01:31:05 +0900

    ListContainer 컴포넌트 테스트 통과

--- src/containers/ListContainer.jsx
+++ src/containers/ListContainer.jsx
@@ -1,5 +1,13 @@
+import { useSelector } from 'react-redux';
+
+import Restaurants from '../components/Restaurants';
+
 export default function ListContainer() {
+  const { restaurants } = useSelector((state) => ({
+    restaurants: state.restaurants,
+  }));
+
   return (
-    <div />
+    <Restaurants restaurants={restaurants} />
   );
 }
