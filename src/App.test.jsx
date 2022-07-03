6d9020425ac79fcdbd44bc56da82971e95685ba5
seungwoo Kim <kyh196201@gmail.com>
2022-07-03 23:59:08 +0900

    App 컴포넌트에 NameInput이 렌더링되었는지 확인하는 테스트 코드 작성

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -3,9 +3,17 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 describe('<App />', () => {
+  const renderApp = () => render((<App />));
+
   it('앱의 제목이 보인다.', () => {
-    const { container } = render((<App />));
+    const { container } = renderApp();
 
     expect(container).toHaveTextContent('Restaurants');
+  });
+
+  it('레스토랑 이름을 입력할 수 있는 input이 보인다.', () => {
+    const { getByPlaceholderText } = renderApp();
+
+    expect(getByPlaceholderText('이름')).toBeInTheDocument();
   });
 });
