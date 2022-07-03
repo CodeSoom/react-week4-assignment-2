bb7680b461ec1d9057ae289edfa9c5ff51fc5314
minsuk <alstjr0183@naver.com>
2022-07-03 17:17:20 +0900

    feat: input.test 코드 작업 input을 테스트하는 코드를 작성하였습니다

---
+++ src/Input.test.jsx
@@ -0,0 +1,42 @@
+import { render } from '@testing-library/react';
+
+import Input from './Input';
+
+import { restaurantName, restaurantType, restaurantAdress } from './fixtures/restaurant';
+
+describe('Input', () => {
+  const handleChangeRestaurantName = jest.fn();
+  const handleChangeRestaurantType = jest.fn();
+  const handleChangeRestaurantAdress = jest.fn();
+  const handleClick = jest.fn();
+
+  const renderInput = () => render((
+    <Input
+      nameInput={restaurantName}
+      typeInput={restaurantType}
+      adressInput={restaurantAdress}
+      handleChangeRestaurantName={handleChangeRestaurantName}
+      handleChangeRestaurantType={handleChangeRestaurantType}
+      handleChangeRestaurantAdress={handleChangeRestaurantAdress}
+      handleClickAddRestaurant={handleClick}
+    />
+  ));
+
+  it('input-restaurantName을 렌더링한다', () => {
+    const { getByPlaceholderText } = renderInput();
+
+    expect(getByPlaceholderText('이름')).toHaveValue(restaurantName);
+  });
+
+  it('input-restaurantType을 렌더링한다', () => {
+    const { getByPlaceholderText } = renderInput();
+
+    expect(getByPlaceholderText('분류')).toHaveValue(restaurantType);
+  });
+
+  it('input-restaurantAdress을 렌더링한다', () => {
+    const { getByPlaceholderText } = renderInput();
+
+    expect(getByPlaceholderText('주소')).toHaveValue(restaurantAdress);
+  });
+});
