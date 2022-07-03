4c6ffe8916a530dcc529ecb0641e2ca21108d289
yerin512 <73337811+yerin512@users.noreply.github.com>
2022-07-03 10:31:35 +0900

    App.test.jsx에 dispatch 호출 테스트 코드작성
    디스패치 호출이 되었는지 테스트코드작성

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,4 +1,4 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import { render } from '@testing-library/react';
 
@@ -9,6 +9,10 @@ import restaurants from '../fixtures/restaurants';
 jest.mock('react-redux');
 
 describe('<App/>', () => {
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
+
   useSelector.mockImplementation((selector) => selector({
     restaurants,
   }));
@@ -16,6 +20,14 @@ describe('<App/>', () => {
   const renderApp = () => render((
     <App />
   ));
+
+  it('dispatch 호출', () => {
+    renderApp();
+    expect(dispatch).toBeCalledWith({
+      type: 'setRestaurants',
+      payload: { restaurants },
+    });
+  });
 
   it('타이틀이 보인다', () => {
     const { getByText } = renderApp();
