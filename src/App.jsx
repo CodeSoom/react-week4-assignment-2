0ff443db5e113ed194b2fb733f10c98b023b8afe
yerin512 <73337811+yerin512@users.noreply.github.com>
2022-07-03 10:30:38 +0900

    App.jsx 처음 렌더시 디스패치 호출
    App.jsx에 디스패치 호출을 useEffect를 이용함

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
 import FormContainer from './FormContainer';
 import ListContainer from './ListContainer';
 
+import restaurants from '../fixtures/restaurants';
+
+import { setRestaurants } from './store/actions';
+
 export default function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setRestaurants(restaurants));
+  }, []);
   return (
     <div>
       <h1>Restaurants</h1>
