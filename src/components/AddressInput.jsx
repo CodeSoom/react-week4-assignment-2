f7685de2983f5a936eb7b371340f810a8782da2c
seungwoo Kim <kyh196201@gmail.com>
2022-07-04 00:38:02 +0900

    AddressInput 테스트 코드 통과

--- src/components/AddressInput.jsx
+++ src/components/AddressInput.jsx
@@ -1,5 +1,7 @@
-export default function AddressInput() {
+export default function AddressInput({ address, onChange }) {
+  const handleChange = (event) => onChange(event.target.value);
+
   return (
-    <input type="text" placeholder="주소" />
+    <input type="text" placeholder="주소" value={address} onChange={handleChange} />
   );
 }
