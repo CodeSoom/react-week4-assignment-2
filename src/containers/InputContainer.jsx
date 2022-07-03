a36f5797d7786385b42581153d281c6840df36b1
seungwoo Kim <kyh196201@gmail.com>
2022-07-03 23:33:53 +0900

    InputContainer가 NameInput에 props를 전달하도록 수정

--- src/containers/InputContainer.jsx
+++ src/containers/InputContainer.jsx
@@ -1,7 +1,12 @@
+import { useSelector } from 'react-redux';
 import NameInput from '../components/NameInput';
 
 export default function InputContainer() {
+  const { name } = useSelector((state) => ({
+    name: state.name,
+  }));
+
   return (
-    <NameInput />
+    <NameInput name={name} />
   );
 }
