107e2a89fd6033afe49ea43e86084bd1e6faf98d
daadaadaah <60481383+daadaadaah@users.noreply.github.com>
2022-08-08 15:54:06 +0900

    Revert "Create a system for Restaurants"

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
 import React from 'react';
 
-import RestaurantListContainer from './RestaurantListContainer';
-import RestaurantFormContainer from './RestaurantFormContainer';
-
 export default function App() {
   return (
     <div>
-      <h2>Restaurants</h2>
-      <RestaurantListContainer />
-      <RestaurantFormContainer />
+      Hello
     </div>
   );
 }
