9ab9388403b83355d3e83d60747469e03409d01a
gunhee-jeong <kyle4619@naver.com>
2022-08-26 16:29:14 +0900

    기본적인 화면 구성을 확인하기 위한 화면을 구성

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 export default function App() {
   return (
     <div>
-      Hello
+      <h1>Restaurants</h1>
+      <form>
+        <input
+          type="text"
+          placeholder="이름"
+        />
+        <input
+          type="text"
+          placeholder="분류"
+        />
+        <input
+          type="text"
+          placeholder="주소"
+        />
+        <button type="submit">등록</button>
+      </form>
     </div>
   );
 }
