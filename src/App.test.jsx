6db0d5586f0cbda2b1847a4bf090df7f00ac3708
gunhee-jeong <kyle4619@naver.com>
2022-08-26 19:59:14 +0900

    레스토랑 목록을 입력하는 액션을 구현

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,22 @@
 import { render } from '@testing-library/react';
 
+import { useSelector, useDispatch } from 'react-redux';
+
 import App from './App';
 
+jest.mock('react-redux');
+
 test('App', () => {
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
+
+  useSelector.mockImplementation((selector) => selector({
+    name: '마녀주방',
+    sort: '한식',
+    address: '서울시 강남구',
+  }));
+
   const { getByText, getByPlaceholderText } = render((
     <App />
   ));
