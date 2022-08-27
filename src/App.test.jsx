6aaa988ac0604254dcb819d31b33068c09208e02
ChoJinmok <jjmok1127@gmail.com>
2022-08-27 12:09:05 +0900

    restaurants 목록이 있을 때 list를 화면에 렌더링 테스트 작성

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,6 +1,10 @@
 import { render } from '@testing-library/react';
 
+import { useSelector } from 'react-redux';
+
 import App from './App';
+
+jest.mock('react-redux');
 
 describe('App', () => {
   it('renders', () => {
@@ -13,5 +17,31 @@ describe('App', () => {
     expect(getByPlaceholderText('분류')).toHaveAttribute('type', 'text');
     expect(getByPlaceholderText('주소')).toHaveAttribute('type', 'text');
     expect(getByText('등록')).not.toBeNull();
+  });
+
+  context('with restaurants list', () => {
+    const state = {
+      restaurants: [
+        {
+          id: 1,
+          name: '마녀주방',
+          classification: '한식',
+          address: '서울시 강남구',
+        },
+      ],
+    };
+
+    it('renders restaurants list', () => {
+      useSelector.mockImplementation((selector) => selector(state));
+
+      const { getAllByRole } = render((
+        <App />
+      ));
+
+      state.restaurants.forEach((restaurant, index) => {
+        expect(getAllByRole('list')[index])
+          .toBe(`${restaurant.name} | ${restaurant.classification} | ${restaurant.address}`);
+      });
+    });
   });
 });
