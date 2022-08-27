459b47caf983a935ade42b1764b9beb7402898e9
ChoJinmok <jjmok1127@gmail.com>
2022-08-27 11:30:24 +0900

    전체적인 렌더링시에 input의 type값도 test하게끔 리팩터링

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ export default function App() {
   return (
     <>
       <h1>Restaurants</h1>
-      <input placeholder="이름" />
-      <input placeholder="분류" />
-      <input placeholder="주소" />
+      <input type="text" placeholder="이름" />
+      <input type="text" placeholder="분류" />
+      <input type="text" placeholder="주소" />
       <button type="button">등록</button>
     </>
   );
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -9,9 +9,9 @@ describe('App', () => {
     ));
 
     expect(getByText('Restaurants')).not.toBeNull();
-    expect(getByPlaceholderText('이름')).not.toBeNull();
-    expect(getByPlaceholderText('분류')).not.toBeNull();
-    expect(getByPlaceholderText('주소')).not.toBeNull();
+    expect(getByPlaceholderText('이름')).toHaveAttribute('type', 'text');
+    expect(getByPlaceholderText('분류')).toHaveAttribute('type', 'text');
+    expect(getByPlaceholderText('주소')).toHaveAttribute('type', 'text');
     expect(getByText('등록')).not.toBeNull();
   });
 });
