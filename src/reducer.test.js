28cc3d6e5c86fc1c85ba3009c55a7ecd92e50629
ChoJinmok <jjmok1127@gmail.com>
2022-08-27 18:30:23 +0900

    input에 정보 입력 시 정보 저장 테스트

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -6,4 +6,36 @@ describe('reducer', () => {
 
     expect(state.restaurants).toHaveLength(0);
   });
+
+  describe('updateInformation', () => {
+    it('changes name', () => {
+      const { information } = reducer({
+        information: {
+          name: '',
+        },
+      }, updateInformation('name', 'New Name'));
+
+      expect(information.name).toBe('New Name');
+    });
+
+    it('changes classifiaction', () => {
+      const { information } = reducer({
+        information: {
+          classifiaction: '',
+        },
+      }, updateInformation('classifiaction', 'New Classifiaction'));
+
+      expect(information.classifiaction).toBe('New Classifiaction');
+    });
+
+    it('changes address', () => {
+      const { information } = reducer({
+        information: {
+          address: '',
+        },
+      }, updateInformation('address', 'New Address'));
+
+      expect(information.address).toBe('New Address');
+    });
+  });
 });
