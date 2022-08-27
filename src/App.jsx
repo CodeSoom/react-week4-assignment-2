7d0cae9bad83e973b64ffee5a59e73143d93d86b
ChoJinmok <jjmok1127@gmail.com>
2022-08-27 11:02:48 +0900

    전체적인 렌더링 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 export default function App() {
   return (
-    <div>
-      Hello
-    </div>
+    <>
+      <h1>Restaurants</h1>
+      <input placeholder="이름" />
+      <input placeholder="분류" />
+      <input placeholder="주소" />
+      <button type="button">등록</button>
+    </>
   );
 }
