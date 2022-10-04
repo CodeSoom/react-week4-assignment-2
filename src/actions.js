6d16d088a507a5ef513ff89a90a67130ea6fbd21
task11 <task11@naver.com>
2022-10-04 16:23:05 +0900

    feat: Update title actions 구현

--- src/actions.js
+++ src/actions.js
@@ -1,4 +1,31 @@
-export default function updateReservation() {
+export function updateListTitle(listTitle) {
+  return {
+    type: 'updateListTitle',
+    payload: {
+      listTitle,
+    },
+  };
+}
+
+export function updateListMenu(listMenu) {
+  return {
+    type: 'updateListMenu',
+    payload: {
+      listMenu,
+    },
+  };
+}
+
+export function updateListAddress(listAddress) {
+  return {
+    type: 'updateListAddress',
+    payload: {
+      listAddress,
+    },
+  };
+}
+
+export function updateReservation() {
   return {
     type: 'updateReservation',
   };
