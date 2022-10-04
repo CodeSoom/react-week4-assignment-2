e073f137652739f212e43db9375fa20d97a448f7
task11 <task11@naver.com>
2022-10-04 20:33:04 +0900

    refactor: Item 컴포넌트 테스트코드 리팩토링

--- src/Item.test.jsx
+++ src/Item.test.jsx
@@ -1,18 +1,20 @@
-import { render } from "@testing-library/react";
+import { render } from '@testing-library/react';
+
+import Item from './Item';
 
 describe('Item', () => {
-  const item = {
-    id: 1,
-    name: '백암',
-    category: '순댓국',
-    address: '여의도',
-  };
+  it('shows reservation', () => {
+    const item = {
+      id: 1,
+      name: '백암',
+      category: '순댓국',
+      address: '여의도',
+    };
 
-  const { getByText } = render((
-    <Item item={item} />
-  ));
+    const { getByText } = render((
+      <Item item={item} />
+    ));
 
-  expect(getByText('백암')).not.toBeNull();
-  expect(getByText('순댓국')).not.toBeNull();
-  expect(getByText('여의도')).not.toBeNull();
+    expect(getByText('백암|순댓국|여의도')).not.toBeNull();
+  });
 });
