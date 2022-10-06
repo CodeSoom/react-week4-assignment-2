abb58bae09f0308b2f6b6109a9b705421abba8e3
task11 <task11@naver.com>
2022-10-06 16:07:45 +0900

    refactor: Input 컴포넌트 테스트 분리

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -2,18 +2,25 @@ import { fireEvent, render } from '@testing-library/react';
 import Input from './Input';
 
 describe('Input', () => {
+  it('Renders input text', () => {
+    const { getByDisplayValue } = render((
+      <Input
+        label="메뉴1"
+        value="치킨"
+      />
+    ));
+
+    expect(getByDisplayValue('치킨')).not.toBeNull();
+  });
   it('Changes input value', () => {
     const handleChange = jest.fn();
-
-    const { getByDisplayValue, getByLabelText } = render((
+    const { getByLabelText } = render((
       <Input
         label="메뉴1"
         value="치킨"
         handleChange={handleChange}
       />
     ));
-
-    expect(getByDisplayValue('치킨')).not.toBeNull();
 
     fireEvent.change(getByLabelText('메뉴1'), {
       target: { value: '피자' },
