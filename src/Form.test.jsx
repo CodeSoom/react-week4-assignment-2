f23a692a8d24823598c894e3038aab4a7e71bd23
sejineeee <sejinee0918@gmail.com>
2022-11-05 20:17:51 +0900

    test: Form test 파일 추가
    form 렌더링 test 추가
    change 함수에 대한 test 추가
    click 함수에 대한 test 추가

---
+++ src/Form.test.jsx
@@ -0,0 +1,47 @@
+import { render, fireEvent } from '@testing-library/react';
+
+import Form from './Form';
+
+describe('Form', () => {
+  const handleChange = jest.fn();
+  const handleClick = jest.fn();
+
+  function renderForm() {
+    return render((<Form
+      storeName="양키통닭"
+      category="한식"
+      address="서울시 문래동"
+      onChange={handleChange}
+      onClick={handleClick}
+    />));
+  }
+
+  it('Form이 렌더링된다.', () => {
+    const { getByText, getByPlaceholderText } = renderForm();
+
+    expect(getByPlaceholderText('이름').value).toBe('양키통닭');
+    expect(getByPlaceholderText('분류').value).toBe('한식');
+    expect(getByPlaceholderText('주소').value).toBe('서울시 문래동');
+    expect(getByText('등록')).not.toBeNull();
+  });
+
+  it('레스토랑 정보를 입력한다.', () => {
+    const { getByPlaceholderText } = renderForm();
+
+    fireEvent.change(getByPlaceholderText('이름'), {
+      target: {
+        value: '김밥천국',
+      },
+    });
+
+    expect(handleChange).toBeCalled();
+  });
+
+  it('등록 버튼을 클릭하여 레스토랑 정보를 등록한다.', () => {
+    const { getByText } = renderForm();
+
+    fireEvent.click(getByText('등록'));
+
+    expect(handleClick).toBeCalled();
+  });
+});
