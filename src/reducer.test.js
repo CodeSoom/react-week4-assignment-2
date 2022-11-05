cc670e3a364f5c544bd1d8cd17a01a15043c71cd
yxxnhx <tjdbsgk514@hanmail.net>
2022-11-05 18:09:14 +0900

    reducer.test 파일 생성
    reducer의 updateRestaurant, addRestaurant를 테스트할 수 있는 파일을 생성하였습니다.

---
+++ src/reducer.test.js
@@ -0,0 +1,14 @@
+import reducer from './reducer';
+
+import { updateRestaurant, addRestaurant } from './actions';
+
+import restaurantList from '../fixtures/restaurantList';
+
+describe('reducer', () => {
+  describe('updateRestaurant', () => {
+    it('입력한 레스토랑을 업데이트 해준다', () => {});
+  });
+  describe('addRestaurant', () => {
+    it('입력한 레스토랑을 추가해준다', () => {});
+  });
+});
