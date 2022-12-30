3796f3f5732cb74d6ec3b22b021ca253987c4fe5
Jason <jasondal91@gmail.com>
2022-12-30 21:00:51 +0900

    App 컴포넌트 Input 컴포넌트 수정

--- src/Input.jsx
+++ src/Input.jsx
@@ -1,9 +1,31 @@
-export default function Input() {
+export default function Input({
+  name,
+  category,
+  address,
+  onChangeName,
+  onChangeCategory,
+  onChangeAddress,
+}) {
   return (
     <>
-      <input type='text' placeholder='이름' />
-      <input type='text' placeholder='분류' />
-      <input type='text' placeholder='주소' />
+      <input
+        type='text'
+        placeholder='이름'
+        value={name}
+        onChange={onChangeName}
+      />
+      <input
+        type='text'
+        placeholder='분류'
+        value={category}
+        onChange={onChangeCategory}
+      />
+      <input
+        type='text'
+        placeholder='주소'
+        value={address}
+        onChange={onChangeAddress}
+      />
       <button type='button'>등록</button>
     </>
   );
