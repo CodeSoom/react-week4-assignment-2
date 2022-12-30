d1aa48e468d2f39140a294062e78398f82538e5e
Jason <jasondal91@gmail.com>
2022-12-30 09:14:22 +0900

    Input 컴포넌트 테스트 코드 추가

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -3,13 +3,26 @@ import { render } from '@testing-library/react';
 import Input from './Input';
 
 describe('Input', () => {
-  it('renders input boxes', () => {
-    const { getAllByRole } = render(<Input />);
-    expect(getAllByRole('textbox')).toHaveLength(3);
+  describe('inputs', () => {
+    it('renders input boxes', () => {
+      const { getAllByRole } = render(<Input />);
+
+      expect(getAllByRole('textbox')).toHaveLength(3);
+    });
+
+    it('has input boxes with placeHolder', () => {
+      const { getByPlaceholderText } = render(<Input />);
+
+      expect(getByPlaceholderText('이름')).toBeInTheDocument();
+      expect(getByPlaceholderText('분류')).toBeInTheDocument();
+      expect(getByPlaceholderText('주소')).toBeInTheDocument();
+    });
   });
 
-  it('renders button with text', () => {
-    const { getByText } = render(<Input />);
-    expect(getByText('등록')).toBeInTheDocument();
+  describe('button', () => {
+    it('renders button with text', () => {
+      const { getByText } = render(<Input />);
+      expect(getByText('등록')).toBeInTheDocument();
+    });
   });
 });
