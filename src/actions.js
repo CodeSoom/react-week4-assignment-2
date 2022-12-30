3abffc1bd01958870f9cc4d6864531eb71360e54
Jason <jasondal91@gmail.com>
2022-12-30 21:03:25 +0900

    actions 추가

--- src/actions.js
+++ src/actions.js
@@ -3,3 +3,28 @@ export function setRestuarants() {
     type: 'setRestuarants',
   };
 }
+
+export function changeName(name) {
+  return {
+    type: 'changeName',
+    payload: {
+      name,
+    },
+  };
+}
+export function changeCategory(category) {
+  return {
+    type: 'changeCategory',
+    payload: {
+      category,
+    },
+  };
+}
+export function changeAddress(address) {
+  return {
+    type: 'changeAddress',
+    payload: {
+      address,
+    },
+  };
+}
