87422cda1076e5c311b9cad0465523259a1dae27
Jason <jasondal91@gmail.com>
2022-12-30 21:13:26 +0900

    App 테스트  useDispatch useSelector mock 추가

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,27 @@
 import { render } from '@testing-library/react';
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 import App from './App';
 
+jest.mock('react-redux');
+
 describe('App', () => {
+  useSelector.mockImplementation((selector) =>
+    selector({
+      name: '',
+      category: '',
+      address: '',
+      restaurants: [],
+    })
+  );
+
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => {
+    return dispatch;
+  });
+
   it('renders title', () => {
     const { getByText } = render(<App />);
 
