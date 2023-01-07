f5cacf6aa5872248a9138d362549754ee1c2df36
Ubermensch0608 <melancholyee28@gmail.com>
2023-01-07 21:30:18 +0900

    App의 레스토랑 등록 폼 렌더링 테스트
    App은 새로운 레스토랑을 등록할 수 있는 기능이 있어야하므로 해당 폼을 렌더하는 테스트 코드를 작성하였습니다.

--- src/components/App.test.jsx
+++ src/components/App.test.jsx
@@ -8,4 +8,10 @@ describe('App', () => {
 
     expect(screen.getByRole('list')).toBeInTheDocument();
   });
+
+  it('레스토랑 등록 폼을 화면에 렌더한다.', () => {
+    render(<App />);
+
+    expect(screen.getByRole('button', { name: '등록' })).toBeInTheDocument();
+  });
 });
