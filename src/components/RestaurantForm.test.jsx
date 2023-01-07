003e11712af3ce8963eb331e01afc36c608c678d
Ubermensch0608 <melancholyee28@gmail.com>
2023-01-07 22:43:15 +0900

    RestaurantForm - 새로운 레스토랑 등록 액션 테스트
    새로운 레스토랑이 등록되는 액션이 실행되는지 확인할 수 있도록 테스트 코드 작성했습니다.

--- src/components/RestaurantForm.test.jsx
+++ src/components/RestaurantForm.test.jsx
@@ -2,11 +2,16 @@ import { fireEvent, render, screen } from '@testing-library/react';
 
 import RestaurantForm from './RestaurantForm';
 
-const renderRestaurantForm = ({ initialValue = { name: '', category: '', location: '' }, updateRestaurantForm }) => {
+const renderRestaurantForm = ({
+  initialValue = { name: '', category: '', location: '' },
+  updateRestaurantForm,
+  addNewRestaurant,
+}) => {
   render(
     <RestaurantForm
       inputValues={initialValue}
       onUpdateRestaurantForm={updateRestaurantForm}
+      onAddNewRestaurant={addNewRestaurant}
     />,
   );
 };
@@ -28,6 +33,17 @@ describe('RestaurantForm', () => {
       fireEvent.change(screen.getByRole('textbox', { name: '이름' }), { target: { value: '맛있는 밥상' } });
 
       expect(updateRestaurantForm).toBeCalledTimes(1);
+    });
+  });
+
+  describe('addNewRestaurant', () => {
+    it('작성된 폼 기반으로 새로운 레스토랑을 등록하는 액션이 실행되어야 한다.', () => {
+      const addNewRestaurant = jest.fn();
+      renderRestaurantForm({ addNewRestaurant });
+
+      fireEvent.click(screen.getByRole('button', { name: '등록' }));
+
+      expect(addNewRestaurant).toBeCalledTimes(1);
     });
   });
 });
