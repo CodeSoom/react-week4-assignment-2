985c9c52b57ec933eb3f3f845be66bb0aa67dd18
ctaaag <qudxor102@gmail.com>
2023-05-28 19:59:04 +0900

    refactor: reducer.test 리팩토링

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,45 +1,41 @@
 import reducer from './reducer';
 import { updateInput, addRestaurant } from './action';
 
-// 테스트하고자 하는 것의 input과 output을 명확하게 해야하지
-// input? 변경 전 state,action
-// output? 변경 후 state
+describe('reducer', () => {
+  describe('restaurant input이 입력된다', () => {
+    it('restaurant input이 변경된 상태를 반환한다.', () => {
+      const prevState = {
+        id: 100,
+        title: '',
+        sort: '',
+        location: '',
+        restaurants: [],
+      };
+      const inputName = {
+        name: 'title',
+        value: 'test',
+      };
 
-// describe('reducer', () => {
-//   describe('taskTitle을 변경한다', () => {
-//     it('taskTitle이 변경된 상태를 반환한다.', () => {
-//       const prevState = {
-//         id: 100,
-//         title: '',
-//         sort: '',
-//         location: '',
-//         restaurants: [],
-//       };
-//       const inputName = {
-//         name: 'title',
-//         value: 'test',
-//       };
+      const { name, value } = inputName;
+      const state = reducer(prevState, updateInput({ name, value }));
+      expect(state.title).toBe('test');
+    });
+  });
 
-//       const { name, value } = inputName;
-//       const state = reducer(prevState, updateInput({ name, value }));
-//       expect(state.title).toBe('test');
-//     });
-//   });
-
-//   describe('taskTitle을 추가한다', () => {
-//     context('taskTitle이 없는 경우', () => {
-//       const prevState = {
-//         id: 100,
-//         title: 'title test',
-//         sort: 'sort test',
-//         location: 'location test',
-//         restaurants: [],
-//       };
-//       const state = reducer(prevState, addRestaurant());
-//       expect(state.restaurants[0].title).toBe('title test');
-//       expect(state.restaurants[0].id).toBe(100);
-//       expect(state.restaurants).toHaveLength(1);
-//       expect(state.id).toBe(101);
-//     });
-//   });
-// });
+  describe('restaurant를 추가한다', () => {
+    it('restaurants 배열에 추가되어 새로운 상태를 반환한다.', () => {
+      const prevState = {
+        id: 100,
+        title: 'title test',
+        sort: 'sort test',
+        location: 'location test',
+        restaurants: [],
+      };
+      const state = reducer(prevState, addRestaurant());
+      expect(state.restaurants[0].title).toBe('title test');
+      expect(state.restaurants[0].id).toBe(100);
+      expect(state.restaurants).toHaveLength(1);
+      expect(state.id).toBe(101);
+    });
+  });
+});
